eb6cd250bbaca21369e943b0084ef2887955fc4c
mikeaclark <mikec@seas.harvard.edu>
2010-12-03 18:23:35 +0000

    Removed unnecssary block size checking in reductions
    git-svn-id: http://lattice.bu.edu/qcdalg/cuda/branches/quda-0.2@1112 be54200a-260c-0410-bdd7-ce6af2a381ab

--- lib/reduce_core.h
+++ lib/reduce_core.h
@@ -98,9 +98,6 @@ template <typename Float>
 double REDUCE_FUNC_NAME(Cuda) (REDUCE_TYPES, int n, int kernel, QudaPrecision precision) {
   setBlock(kernel, n, precision);
   
-  if (n % blasBlock.x != 0) {
-    errorQuda("reduce_core: length %d must be a multiple of %d", n, blasBlock.x);
-  }
   if (blasGrid.x > REDUCE_MAX_BLOCKS) {
     errorQuda("reduce_core: grid size %d must be smaller than %d", blasGrid.x, REDUCE_MAX_BLOCKS);
   }
