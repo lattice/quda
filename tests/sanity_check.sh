46f9a45c506511449eb9fdca583c0379e834413d
gshi <gshi@ncsa.uiuc.edu>
2011-01-18 19:05:09 +0000

    add sanity check script
    git-svn-id: http://lattice.bu.edu/qcdalg/cuda/quda@1181 be54200a-260c-0410-bdd7-ce6af2a381ab

---
+++ tests/sanity_check.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+OUTFILE=/tmp/quda_test.txt
+echo "" > $OUTFILE
+
+fail_msg="Sanity check for quda failed!"
+success_msg="Sanity check for quda succeeded!"
+
+progs="staggered_dslash_test staggered_invert_test"
+precs="double single"
+recons="18 12 8"
+
+echo "Testing staggered dslash and inverter:"
+for prog in $progs ; do 
+  for prec in $precs ; do 
+    for recon in $recons ; do
+	cmd="$prog --sdim 8 --tdim 16 --recon $recon --prec $prec"
+	echo running $cmd
+	./$cmd >> $OUTFILE 2>&1|| (echo "$prog failed, check $OUTFILE for detail"; echo $fail_msg; exit 1) || exit 1
+    done
+  done
+done
+
+echo $success_msg
