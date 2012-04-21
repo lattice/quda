lattice/quda

commit 76d40476d83a688cdff042270179f2e4e4712844

added an option to disable texture reads for BLAS (via a #define in texture.h)

include/texture.h
@@ -4,6 +4,10 @@
 #include <convert.h>
 
 
+// uncomment to disable texture reads
+//#define DIRECT_ACCESS_BLAS
+
+
 #if (__COMPUTE_CAPABILITY__ >= 130)
 __inline__ __device__ double2 fetch_double2(texture<int4, 1> t, int i)
 {
@@ -65,33 +69,33 @@ class Texture {
   template<> inline void Texture<outtype,intype,id>::unbind() { cudaUnbindTexture(tex_##intype##_##id); }
 
 
-#define DEF_FETCH(outtype, intype, id) \
+#define DEF_FETCH_TEX(outtype, intype, id) \
   template<> __device__ inline outtype Texture<outtype,intype,id>::fetch(unsigned int idx) \
   { return tex1Dfetch(tex_##intype##_##id,idx); }
+
+
+#define DEF_FETCH_DIRECT(outtype, intype, id) \
+  template<> __device__ inline outtype Texture<outtype,intype,id>::fetch(unsigned int idx) \
+  { outtype out; copyFloatN(out, spinor[idx]); return out; }
+
+
+#if defined(DIRECT_ACCESS_BLAS)
+#define DEF_FETCH DEF_FETCH_DIRECT
+#else
+#define DEF_FETCH DEF_FETCH_TEX
+#endif
+
+
+#if defined(DIRECT_ACCESS_BLAS) || defined(FERMI_NO_DBLE_TEX)
+#define DEF_FETCH_DBLE DEF_FETCH_DIRECT
+#else
+#define DEF_FETCH_DBLE DEF_FETCH_TEX
+#endif
 
 
 #define DEF_BIND_UNBIND_FETCH(outtype, intype, id) \
   DEF_BIND_UNBIND(outtype, intype, id)             \
   DEF_FETCH(outtype, intype, id)
-
-
-#ifdef FERMI_NO_DBLE_TEX
-
-#define DEF_FETCH_DOUBLE2(id) \
-  template<> __device__ inline double2 Texture<double2,double2,id>::fetch(unsigned int idx) \
-  { return spinor[idx]; } \
-  template<> __device__ inline float2 Texture<float2,double2,id>::fetch(unsigned int idx) \
-  { return make_float2(spinor[idx].x, spinor[idx].y); }
-
-#else
-
-#define DEF_FETCH_DOUBLE2(id) \
-  template<> __device__ inline double2 Texture<double2,double2,id>::fetch(unsigned int idx) \
-  { return fetch_double2(tex_double2_##id,idx); } \
-  template<> __device__ inline float2 Texture<float2,double2,id>::fetch(unsigned int idx) \
-  { double2 x = fetch_double2(tex_double2_##id,idx); return make_float2(x.x, x.y); }
-
-#endif // FERMI_NO_DBLE_TEX
 
 
 #define DEF_ALL(id)                          \
@@ -103,7 +107,8 @@ class Texture {
   DEF_BIND_UNBIND_FETCH(float4, float4, id)  \
   DEF_BIND_UNBIND(double2, double2, id)      \
   DEF_BIND_UNBIND(float2, double2, id)       \
-  DEF_FETCH_DOUBLE2(id)
+  DEF_FETCH_DBLE(double2, double2, id)       \
+  DEF_FETCH_DBLE(float2, double2, id)
 
 
 // Declare the textures and define the member functions of the corresponding templated classes.
@@ -116,9 +121,11 @@ DEF_ALL(4)
 
 #undef DECL_TEX
 #undef DEF_BIND_UNBIND
+#undef DEF_FETCH_DIRECT
+#undef DEF_FETCH_TEX
 #undef DEF_FETCH
+#undef DEF_FETCH_DBLE
 #undef DEF_BIND_UNBIND_FETCH
-#undef DEF_FETCH_DOUBLE2
 #undef DEF_ALL
 
 

include/convert.h
@@ -1,13 +1,15 @@
-/**
-   @file convert.h
-   @author M Clark
+#ifndef _CONVERT_H
+#define _CONVERT_H
 
-   @section DESCRIPTION 
-   Conversion functions that are used as building blocking for
-   arbitrary field and register ordering.
+/**
+ * @file convert.h
+ *
+ * @section DESCRIPTION 
+ * Conversion functions that are used as building blocks for
+ * arbitrary field and register ordering.
  */
 
-#pragma once
+#include <quda_internal.h> // for MAX_SHORT
 
 template <typename type> int vecLength() { return 0; }
 
@@ -23,11 +25,13 @@ template<> int vecLength<short4>() { return 4; }
 template<> int vecLength<float4>() { return 4; }
 template<> int vecLength<double4>() { return 4; }
 
-__device__ inline void copyFloatN(float2 &a, const float2 &b) { a = b; }
-__device__ inline void copyFloatN(double2 &a, const double2 &b) { a = make_double2(b.x, b.y); }
+static inline __device__ float s2f(const short &a) { return static_cast<float>(a)/MAX_SHORT; }
 
-__device__ inline void copyFloatN(float4 &a, const float4 &b) { a = make_float4(b.x, b.y, b.z, b.w); }
-__device__ inline void copyFloatN(double4 &a, const double4 &b) { a = make_double4(b.x, b.y, b.z, b.w); }
+template <typename FloatN>
+__device__ inline void copyFloatN(FloatN &a, const FloatN &b) { a = b; }
+
+__device__ inline void copyFloatN(float2 &a, const short2 &b) { a = make_float2(s2f(b.x), s2f(b.y)); }
+__device__ inline void copyFloatN(float4 &a, const short4 &b) { a = make_float4(s2f(b.x), s2f(b.y), s2f(b.z), s2f(b.w)); }
 
 __device__ inline void copyFloatN(float2 &a, const double2 &b) { a = make_float2(b.x, b.y); }
 __device__ inline void copyFloatN(double2 &a, const float2 &b) { a = make_double2(b.x, b.y); }
@@ -167,3 +171,5 @@ template<> __device__ inline void convert<float2,double4>(float2 x[], double4 y[
     x[2*j+1] = make_float2(y[j].z, y[j].w);
   }
 }
+
+#endif // _CONVERT_H

lib/Makefile
@@ -11,11 +11,12 @@ QUDA_OBJS = inv_bicgstab_quda.o inv_cg_quda.o inv_multi_cg_quda.o inv_gcr_quda.o
 	dslash_quda.o blas_quda.o ${NUMA_AFFINITY_OBJS} ${FACE_COMMS_OBJS} ${FATLINK_ITF_OBJS}
 
 # header files, found in include/
-QUDA_HDRS = blas_quda.h clover_field.h color_spinor_field.h dirac_quda.h	     \
-	dslash_quda.h enum_quda.h gauge_force_quda.h           \
-	invert_quda.h llfat_quda.h quda.h quda_internal.h util_quda.h        \
-	face_quda.h tune_quda.h comm_quda.h lattice_field.h gauge_field.h \
-	hisq_force_utils.h double_single.h texture.h numa_affinity.h
+QUDA_HDRS = blas_quda.h clover_field.h color_spinor_field.h convert.h	\
+	dirac_quda.h dslash_quda.h enum_quda.h gauge_force_quda.h	\
+	invert_quda.h llfat_quda.h quda.h quda_internal.h util_quda.h	\
+	face_quda.h tune_quda.h comm_quda.h lattice_field.h		\
+	gauge_field.h hisq_force_utils.h double_single.h texture.h	\
+	numa_affinity.h
 
 # These are only inlined into blas_quda.cu
 BLAS_INLN = blas_core.h reduce_core.h

commit fa8fd5424ad22a59d2a30993e687c69302cd1e33

Fixed a regression in commit 76d40476d83a688 that broke double-precision texture reads.  Closes #63.

include/texture.h
@@ -89,7 +89,9 @@ class Texture {
 #if defined(DIRECT_ACCESS_BLAS) || defined(FERMI_NO_DBLE_TEX)
 #define DEF_FETCH_DBLE DEF_FETCH_DIRECT
 #else
-#define DEF_FETCH_DBLE DEF_FETCH_TEX
+#define DEF_FETCH_DBLE(outtype, intype, id) \
+  template<> __device__ inline outtype Texture<outtype,double2,id>::fetch(unsigned int idx) \
+  { outtype out; copyFloatN(out, fetch_double2(tex_double2_##id,idx)); return out; }
 #endif
 
 