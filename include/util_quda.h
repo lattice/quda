lattice/quda

commit 5834a437447d03c14414fd63bf9144773701a479

added additional error checking to initQuda()

lib/interface_quda.cpp
@@ -144,6 +144,7 @@ void initQuda(int dev)
   for(int i=0; i<deviceCount; i++) {
     cudaDeviceProp deviceProp;
     cudaGetDeviceProperties(&deviceProp, i);
+    checkCudaError();
     if (getVerbosity() >= QUDA_SUMMARIZE) {
       printfQuda("Found device %d: %s\n", i, deviceProp.name);
     }
@@ -177,6 +178,7 @@ void initQuda(int dev)
 #endif
   
   cudaGetDeviceProperties(&deviceProp, dev);
+  checkCudaError();
   if (deviceProp.major < 1) {
     errorQuda("Device %d does not support CUDA", dev);
   }
@@ -185,6 +187,7 @@ void initQuda(int dev)
     printfQuda("Using device %d: %s\n", dev, deviceProp.name);
   }
   cudaSetDevice(dev);
+  checkCudaError();
 
 #ifdef NUMA_AFFINITY
   if(numa_affinity_enabled){
@@ -193,6 +196,7 @@ void initQuda(int dev)
 #endif
   // if the device supports host-mapped memory, then enable this
   if(deviceProp.canMapHostMemory) cudaSetDeviceFlags(cudaDeviceMapHost);
+  checkCudaError();
 
   initCache();
   //cudaDeviceSetSharedMemConfig(cudaSharedMemBankSizeEightByte);
@@ -202,6 +206,7 @@ void initQuda(int dev)
   for (int i=0; i<Nstream; i++) {
     cudaStreamCreate(&streams[i]);
   }
+  checkCudaError();
   createDslashEvents();
 
   quda::initBlas();

commit ab7630bb060dd7d56be4cd175d4cb07128a2c721

fixed some problems with HOST_DEBUG introduced by commit 5834a437

include/util_quda.h
@@ -72,22 +72,23 @@ void setOutputFile(FILE *outfile);
 #endif // MULTI_GPU
 
 
-#ifdef HOST_DEBUG
-
-#define checkCudaError() do {                          \
-  cudaDeviceSynchronize();                             \
+#define checkCudaErrorNoSync() do {                    \
   cudaError_t error = cudaGetLastError();              \
   if (error != cudaSuccess)                            \
     errorQuda("(CUDA) %s", cudaGetErrorString(error)); \
 } while (0)
 
+
+#ifdef HOST_DEBUG
+
+#define checkCudaError() do {  \
+  cudaDeviceSynchronize();     \
+  checkCudaErrorNoSync();      \
+} while (0)
+
 #else
 
-#define checkCudaError() do {                           \
-  cudaError_t error = cudaGetLastError();               \
-  if (error != cudaSuccess)                             \
-    errorQuda("(CUDA) %s", cudaGetErrorString(error));  \
-} while (0)
+#define checkCudaError() checkCudaErrorNoSync()
 
 #endif // HOST_DEBUG
 

lib/interface_quda.cpp
@@ -178,7 +178,7 @@ void initQuda(int dev)
 #endif
   
   cudaGetDeviceProperties(&deviceProp, dev);
-  checkCudaError();
+  checkCudaErrorNoSync(); // "NoSync" for correctness in HOST_DEBUG mode
   if (deviceProp.major < 1) {
     errorQuda("Device %d does not support CUDA", dev);
   }
@@ -187,7 +187,7 @@ void initQuda(int dev)
     printfQuda("Using device %d: %s\n", dev, deviceProp.name);
   }
   cudaSetDevice(dev);
-  checkCudaError();
+  checkCudaErrorNoSync(); // "NoSync" for correctness in HOST_DEBUG mode
 
 #ifdef NUMA_AFFINITY
   if(numa_affinity_enabled){
@@ -196,7 +196,7 @@ void initQuda(int dev)
 #endif
   // if the device supports host-mapped memory, then enable this
   if(deviceProp.canMapHostMemory) cudaSetDeviceFlags(cudaDeviceMapHost);
-  checkCudaError();
+  checkCudaErrorNoSync(); // "NoSync" for correctness in HOST_DEBUG mode
 
   initCache();
   //cudaDeviceSetSharedMemConfig(cudaSharedMemBankSizeEightByte);