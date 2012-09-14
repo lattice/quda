lattice/quda

commit 9e5a6343976bc8d80f977f78faf0cce9bc4d9ace

cleaned up management of Dslash constants and events.  Most constants are now set in the Dirac operator constructor.

lib/dslash_constants.h
@@ -1,4 +1,5 @@
-#define MAX(a,b) ((a) > (b)?(a): (b))
+#define MAX(a,b) ((a)>(b) ? (a):(b))
+
 __constant__ int X1h;
 __constant__ int X2h;
 __constant__ int X1;
@@ -101,12 +102,9 @@ __constant__ int E3E2E1;
 __constant__ int last_proc_in_tdim;
 __constant__ int first_proc_in_tdim;
 
-int initDslash = 0;
-int initClover = 0;
-int initDomainWall = 0;
-int initStaggered = 0;
 
-void initCommonConstants(const LatticeField &lat) {
+void initLatticeConstants(const LatticeField &lat)
+{
   int Vh = lat.VolumeCB();
   cudaMemcpyToSymbol("Vh", &Vh, sizeof(int));  
   
@@ -259,23 +257,22 @@ void initCommonConstants(const LatticeField &lat) {
   cudaMemcpyToSymbol("Pt0", &(first_node_in_t), sizeof(bool)); 
   cudaMemcpyToSymbol("PtNm1", &(last_node_in_t), sizeof(bool)); 
 
-  checkCudaError();
-
   // copy a few of the constants needed by tuneLaunch()
   dslashConstants.x[0] = X1;
   dslashConstants.x[1] = X2;
   dslashConstants.x[2] = X3;
   dslashConstants.x[3] = X4;
+
+  checkCudaError();
 }
 
-void initGaugeFieldConstants(const cudaGaugeField &gauge) 
-{
-  initCommonConstants(gauge);
 
+void initGaugeConstants(const cudaGaugeField &gauge) 
+{
   int ga_stride = gauge.Stride();
   cudaMemcpyToSymbol("ga_stride", &ga_stride, sizeof(int));  
 
-  int gf = (gauge.GaugeFixed() == QUDA_GAUGE_FIXED_YES) ? 1 : 0;
+  int gf = (gauge.GaugeFixed() == QUDA_GAUGE_FIXED_YES);
   cudaMemcpyToSymbol("gauge_fixed", &(gf), sizeof(int));
 
   double anisotropy_ = gauge.Anisotropy();
@@ -286,34 +283,6 @@ void initGaugeFieldConstants(const cudaGaugeField &gauge)
 
   double coeff = -24.0*gauge.Tadpole()*gauge.Tadpole();
   cudaMemcpyToSymbol("coeff", &(coeff), sizeof(double));
-
-  return;
-}
-
-
-
-void initDslashConstants(const cudaGaugeField &gauge, const int sp_stride) 
-{
-  initCommonConstants(gauge);
-
-  cudaMemcpyToSymbol("sp_stride", &sp_stride, sizeof(int));  
-  
-  int ga_stride = gauge.Stride();
-  cudaMemcpyToSymbol("ga_stride", &ga_stride, sizeof(int));  
-
-  int gf = (gauge.GaugeFixed() == QUDA_GAUGE_FIXED_YES) ? 1 : 0;
-  cudaMemcpyToSymbol("gauge_fixed", &(gf), sizeof(int));
-
-  double anisotropy_ = gauge.Anisotropy();
-  cudaMemcpyToSymbol("anisotropy", &(anisotropy_), sizeof(double));
-
-  double t_bc = (gauge.TBoundary() == QUDA_PERIODIC_T) ? 1.0 : -1.0;
-  cudaMemcpyToSymbol("t_boundary", &(t_bc), sizeof(double));
-
-  double coeff = -24.0*gauge.Tadpole()*gauge.Tadpole();
-  cudaMemcpyToSymbol("coeff", &(coeff), sizeof(double));
-
-
 
   float anisotropy_f = gauge.Anisotropy();
   cudaMemcpyToSymbol("anisotropy_f", &(anisotropy_f), sizeof(float));
@@ -324,7 +293,7 @@ void initDslashConstants(const cudaGaugeField &gauge, const int sp_stride)
   float coeff_f = -24.0*gauge.Tadpole()*gauge.Tadpole();
   cudaMemcpyToSymbol("coeff_f", &(coeff_f), sizeof(float));
 
-
+  // constants used by the READ_GAUGE() macros in read_gauge.h
   float2 An2 = make_float2(gauge.Anisotropy(), 1.0 / (gauge.Anisotropy()*gauge.Anisotropy()));
   cudaMemcpyToSymbol("An2", &(An2), sizeof(float2));
   float2 TB2 = make_float2(t_bc_f, 1.0 / (t_bc_f * t_bc_f));
@@ -332,87 +301,72 @@ void initDslashConstants(const cudaGaugeField &gauge, const int sp_stride)
   float2 No2 = make_float2(1.0, 1.0);
   cudaMemcpyToSymbol("No2", &(No2), sizeof(float2));
 
-  float h_pi_f = M_PI;
-  cudaMemcpyToSymbol("pi_f", &(h_pi_f), sizeof(float));
+  checkCudaError();
+}
 
-  double TProjScale = (kernelPackT ? 1.0 : 2.0);
+
+/**
+ * This routine gets called often, so be sure not to set any constants unnecessarily
+ * or introduce synchronization (e.g., via checkCudaError()).
+ */
+void initSpinorConstants(const cudaColorSpinorField &spinor)
+{
+  static int last_sp_stride = -1;
+  static int last_Ls = -1;
+
+  int sp_stride = spinor.Stride();
+  if (sp_stride != last_sp_stride) {
+    cudaMemcpyToSymbol("sp_stride", &sp_stride, sizeof(int));
+    checkCudaError();
+  }
+  
+  // for domain wall:
+  if (spinor.Ndim() == 5) {
+    int Ls = spinor.X(4);
+    if (Ls != last_Ls) {
+      cudaMemcpyToSymbol("Ls", &Ls, sizeof(int));  
+      dslashConstants.Ls = Ls; // needed by tuneLaunch()
+      checkCudaError();
+    }
+  }
+}
+
+
+void initDslashConstants()
+{
+  float pi_f = M_PI;
+  cudaMemcpyToSymbol("pi_f", &pi_f, sizeof(float));
+
   // temporary additions (?) for checking Ron's T-packing kernel with old multi-gpu kernel
-  cudaMemcpyToSymbol("tProjScale", &(TProjScale), sizeof(double));
 
-  float TProjScale_f = (float)TProjScale;
-  cudaMemcpyToSymbol("tProjScale_f", &(TProjScale_f), sizeof(float));
+  double tProjScale = (kernelPackT ? 1.0 : 2.0);
+  cudaMemcpyToSymbol("tProjScale", &tProjScale, sizeof(double));
+
+  float tProjScale_f = (float)tProjScale;
+  cudaMemcpyToSymbol("tProjScale_f", &tProjScale_f, sizeof(float));
 
   checkCudaError();
-
-  initDslash = 1;
-
-  // create events
-#ifndef DSLASH_PROFILING
-  // add cudaEventDisableTiming for lower sync overhead
-  for (int i=0; i<Nstream; i++) {
-    cudaEventCreate(&packEnd[i], cudaEventDisableTiming);
-    cudaEventCreate(&gatherStart[i], cudaEventDisableTiming);
-    cudaEventCreate(&gatherEnd[i], cudaEventDisableTiming);
-    cudaEventCreateWithFlags(&scatterStart[i], cudaEventDisableTiming);
-    cudaEventCreateWithFlags(&scatterEnd[i], cudaEventDisableTiming);
-  }
-#else
-  cudaEventCreate(&dslashStart);
-  cudaEventCreate(&dslashEnd);
-  for (int i=0; i<Nstream; i++) {
-    cudaEventCreate(&packStart[i]);
-    cudaEventCreate(&packEnd[i]);
-
-    cudaEventCreate(&gatherStart[i]);
-    cudaEventCreate(&gatherEnd[i]);
-
-    cudaEventCreate(&scatterStart[i]);
-    cudaEventCreate(&scatterEnd[i]);
-
-    cudaEventCreate(&kernelStart[i]);
-    cudaEventCreate(&kernelEnd[i]);
-
-    kernelTime[i][0] = 0.0;
-    kernelTime[i][1] = 0.0;
-
-    gatherTime[i][0] = 0.0;
-    gatherTime[i][1] = 0.0;
-
-    commsTime[i][0] = 0.0;
-    commsTime[i][1] = 0.0;
-
-    scatterTime[i][0] = 0.0;
-    scatterTime[i][1] = 0.0;
-  }
-#endif
 }
 
-void initCloverConstants (const int cl_stride) {
+
+void initCloverConstants (const cudaCloverField &clover)
+{
+  int cl_stride = clover.Stride();
   cudaMemcpyToSymbol("cl_stride", &cl_stride, sizeof(int));  
 
-  initClover = 1;
+  checkCudaError();
 }
 
-void initDomainWallConstants(const int Ls) {
-  cudaMemcpyToSymbol("Ls", &Ls, sizeof(int));  
-  dslashConstants.Ls = Ls; // needed by tuneLaunch()
 
-  initDomainWall = 1;
-}
-
-void
-initStaggeredConstants(const cudaGaugeField &fatgauge, const cudaGaugeField &longgauge)
+void initStaggeredConstants(const cudaGaugeField &fatgauge, const cudaGaugeField &longgauge)
 {
-  
   int fat_ga_stride = fatgauge.Stride();
   int long_ga_stride = longgauge.Stride();
   float fat_link_max = fatgauge.LinkMax();
   
   cudaMemcpyToSymbol("fat_ga_stride", &fat_ga_stride, sizeof(int));  
   cudaMemcpyToSymbol("long_ga_stride", &long_ga_stride, sizeof(int));  
-  
   cudaMemcpyToSymbol("fat_ga_max", &fat_link_max, sizeof(float));
-  initStaggered = 1;
-  return;
+
+  checkCudaError();
 }
-  

include/dirac_quda.h
@@ -60,7 +60,7 @@ class Dirac {
   friend class DiracMdagM;
   friend class DiracMdag;
 
-  protected:
+ protected:
   cudaGaugeField &gauge;
   double kappa;
   double mass;
@@ -313,8 +313,8 @@ class DiracTwistedMassPC : public DiracTwistedMass {
 class DiracStaggered : public Dirac {
 
  protected:
-  cudaGaugeField *fatGauge;
-  cudaGaugeField *longGauge;
+  cudaGaugeField &fatGauge;
+  cudaGaugeField &longGauge;
   FaceBuffer face; // multi-gpu communication buffers
 
  public:

include/dslash_quda.h
@@ -13,13 +13,6 @@ extern "C" {
 #endif
 
   void initCache();
-
-  extern int initDslash;
-  extern int initClover;
-  extern int initDomainWall;
-  extern bool qudaPt0;
-  extern bool qudaPtNm1;
-
   void setDslashTuning(QudaTune tune, QudaVerbosity verbose);
 
 #ifdef DSLASH_PROFILING
@@ -32,13 +25,15 @@ extern "C" {
 
 bool getDslashLaunch();
 
-void initCommonConstants(const LatticeField &lat);
-void initDslashConstants(const cudaGaugeField &gauge, const int sp_stride);
-void initCloverConstants(const int cl_stride);
-void initDomainWallConstants(const int Ls);
-void initStaggeredConstants(const cudaGaugeField &fatgauge, const cudaGaugeField &longgauge);
-void initGaugeFieldConstants(const cudaGaugeField &gauge);
+void createDslashEvents();
+void destroyDslashEvents();
 
+void initLatticeConstants(const LatticeField &lat);
+void initGaugeConstants(const cudaGaugeField &gauge);
+void initSpinorConstants(const cudaColorSpinorField &spinor);
+void initDslashConstants();
+void initCloverConstants (const cudaCloverField &clover);
+void initStaggeredConstants(const cudaGaugeField &fatgauge, const cudaGaugeField &longgauge);
 
 // plain Wilson Dslash  
 void wilsonDslashCuda(cudaColorSpinorField *out, const cudaGaugeField &gauge, const cudaColorSpinorField *in,

lib/dirac.cpp
@@ -4,12 +4,18 @@
 
 #include <iostream>
 
+// FIXME: At the moment, it's unsafe for more than one Dirac operator to be active unless
+// they all have the same volume, etc. (used to initialize the various CUDA constants).
+
 Dirac::Dirac(const DiracParam &param) 
   : gauge(*(param.gauge)), kappa(param.kappa), mass(param.mass), matpcType(param.matpcType), 
     dagger(param.dagger), flops(0), tmp1(param.tmp1), tmp2(param.tmp2), tune(QUDA_TUNE_NO),
     verbose(param.verbose)
 {
   for (int i=0; i<4; i++) commDim[i] = param.commDim[i];
+  initLatticeConstants(gauge);
+  initGaugeConstants(gauge);
+  initDslashConstants();
 }
 
 Dirac::Dirac(const Dirac &dirac) 
@@ -18,11 +24,12 @@ Dirac::Dirac(const Dirac &dirac)
     verbose(dirac.verbose)
 {
   for (int i=0; i<4; i++) commDim[i] = dirac.commDim[i];
+  initLatticeConstants(gauge);
+  initGaugeConstants(gauge);
+  initDslashConstants();
 }
 
-Dirac::~Dirac() {
-
-}
+Dirac::~Dirac() { }
 
 Dirac& Dirac::operator=(const Dirac &dirac)
 {
@@ -40,7 +47,6 @@ Dirac& Dirac::operator=(const Dirac &dirac)
 
     for (int i=0; i<4; i++) commDim[i] = dirac.commDim[i];
   }
-
   return *this;
 }
 

lib/dirac_clover.cpp
@@ -3,15 +3,18 @@
 #include <blas_quda.h>
 
 DiracClover::DiracClover(const DiracParam &param)
-  : DiracWilson(param), clover(*(param.clover)) { }
+  : DiracWilson(param), clover(*(param.clover))
+{
+  initCloverConstants(clover);
+}
 
 DiracClover::DiracClover(const DiracClover &dirac) 
-  : DiracWilson(dirac), clover(dirac.clover) { }
-
-DiracClover::~DiracClover()
+  : DiracWilson(dirac), clover(dirac.clover)
 {
-
+  initCloverConstants(clover);
 }
+
+DiracClover::~DiracClover() { }
 
 DiracClover& DiracClover::operator=(const DiracClover &dirac)
 {
@@ -35,8 +38,7 @@ void DiracClover::checkParitySpinor(const cudaColorSpinorField &out, const cudaC
 // Public method to apply the clover term only
 void DiracClover::Clover(cudaColorSpinorField &out, const cudaColorSpinorField &in, const QudaParity parity) const
 {
-  if (!initDslash) initDslashConstants(gauge, in.Stride());
-  if (!initClover) initCloverConstants(clover.Stride());
+  initSpinorConstants(in);
   checkParitySpinor(in, out);
 
   // regular clover term
@@ -121,8 +123,7 @@ DiracCloverPC& DiracCloverPC::operator=(const DiracCloverPC &dirac)
 void DiracCloverPC::CloverInv(cudaColorSpinorField &out, const cudaColorSpinorField &in, 
 			      const QudaParity parity) const
 {
-  if (!initDslash) initDslashConstants(gauge, in.Stride());
-  if (!initClover) initCloverConstants(clover.Stride());
+  initSpinorConstants(in);
   checkParitySpinor(in, out);
 
   // needs to be cloverinv
@@ -140,8 +141,7 @@ void DiracCloverPC::CloverInv(cudaColorSpinorField &out, const cudaColorSpinorFi
 void DiracCloverPC::Dslash(cudaColorSpinorField &out, const cudaColorSpinorField &in, 
 			   const QudaParity parity) const
 {
-  if (!initDslash) initDslashConstants(gauge, in.Stride());
-  if (!initClover) initCloverConstants(clover.Stride());
+  initSpinorConstants(in);
   checkParitySpinor(in, out);
   checkSpinorAlias(in, out);
 
@@ -160,8 +160,7 @@ void DiracCloverPC::DslashXpay(cudaColorSpinorField &out, const cudaColorSpinorF
 			       const QudaParity parity, const cudaColorSpinorField &x,
 			       const double &k) const
 {
-  if (!initDslash) initDslashConstants(gauge, in.Stride());
-  if (!initClover) initCloverConstants(clover.Stride());
+  initSpinorConstants(in);
   checkParitySpinor(in, out);
   checkSpinorAlias(in, out);
 

lib/dirac_domain_wall.cpp
@@ -24,11 +24,10 @@ void DiracDomainWall::Dslash(cudaColorSpinorField &out, const cudaColorSpinorFie
 			     const QudaParity parity) const
 {
   if ( in.Ndim() != 5 || out.Ndim() != 5) errorQuda("Wrong number of dimensions\n");
-  if (!initDslash) initDslashConstants(gauge, in.Stride());
-  if (!initDomainWall) initDomainWallConstants(in.X(4));
   checkParitySpinor(in, out);
   checkSpinorAlias(in, out);
-  
+
+  initSpinorConstants(in);
   domainWallDslashCuda(&out, gauge, &in, parity, dagger, 0, mass, 0);
 
   long long Ls = in.X(4);
@@ -42,11 +41,10 @@ void DiracDomainWall::DslashXpay(cudaColorSpinorField &out, const cudaColorSpino
 				 const double &k) const
 {
   if ( in.Ndim() != 5 || out.Ndim() != 5) errorQuda("Wrong number of dimensions\n");
-  if (!initDslash) initDslashConstants(gauge, in.Stride());
-  if (!initDomainWall) initDomainWallConstants(in.X(4));
   checkParitySpinor(in, out);
   checkSpinorAlias(in, out);
 
+  initSpinorConstants(in);
   domainWallDslashCuda(&out, gauge, &in, parity, dagger, &x, mass, k);
 
   long long Ls = in.X(4);

lib/dirac_staggered.cpp
@@ -2,20 +2,20 @@
 #include <blas_quda.h>
 
 DiracStaggered::DiracStaggered(const DiracParam &param) : 
-  Dirac(param), fatGauge(param.fatGauge), longGauge(param.longGauge), 
+  Dirac(param), fatGauge(*(param.fatGauge)), longGauge(*(param.longGauge)), 
   face(param.fatGauge->X(), 4, 6, 3, param.fatGauge->Precision()) 
   //FIXME: this may break mixed precision multishift solver since may not have fatGauge initializeed yet
 {
-
+  initStaggeredConstants(fatGauge, longGauge);
 }
 
 DiracStaggered::DiracStaggered(const DiracStaggered &dirac) : Dirac(dirac),
-  fatGauge(dirac.fatGauge), longGauge(dirac.longGauge), face(dirac.face) { }
-
-DiracStaggered::~DiracStaggered()
+  fatGauge(dirac.fatGauge), longGauge(dirac.longGauge), face(dirac.face)
 {
-
+  initStaggeredConstants(fatGauge, longGauge);
 }
+
+DiracStaggered::~DiracStaggered() { }
 
 DiracStaggered& DiracStaggered::operator=(const DiracStaggered &dirac)
 {
@@ -25,7 +25,6 @@ DiracStaggered& DiracStaggered::operator=(const DiracStaggered &dirac)
     longGauge = dirac.longGauge;
     face = dirac.face;
   }
- 
   return *this;
 }
 
@@ -44,9 +43,9 @@ void DiracStaggered::checkParitySpinor(const cudaColorSpinorField &in, const cud
 	      in.SiteSubset(), out.SiteSubset());
   }
 
-  if ((out.Volume() != 2*fatGauge->VolumeCB() && out.SiteSubset() == QUDA_FULL_SITE_SUBSET) ||
-      (out.Volume() != fatGauge->VolumeCB() && out.SiteSubset() == QUDA_PARITY_SITE_SUBSET) ) {
-    errorQuda("Spinor volume %d doesn't match gauge volume %d", out.Volume(), fatGauge->VolumeCB());
+  if ((out.Volume() != 2*fatGauge.VolumeCB() && out.SiteSubset() == QUDA_FULL_SITE_SUBSET) ||
+      (out.Volume() != fatGauge.VolumeCB() && out.SiteSubset() == QUDA_PARITY_SITE_SUBSET) ) {
+    errorQuda("Spinor volume %d doesn't match gauge volume %d", out.Volume(), fatGauge.VolumeCB());
   }
 }
 
@@ -54,14 +53,11 @@ void DiracStaggered::checkParitySpinor(const cudaColorSpinorField &in, const cud
 void DiracStaggered::Dslash(cudaColorSpinorField &out, const cudaColorSpinorField &in, 
 			 const QudaParity parity) const
 {
-  if (!initDslash) {
-    initDslashConstants(*fatGauge, in.Stride());
-    initStaggeredConstants(*fatGauge, *longGauge);
-  }
   checkParitySpinor(in, out);
 
+  initSpinorConstants(in);
   setFace(face); // FIXME: temporary hack maintain C linkage for dslashCuda
-  staggeredDslashCuda(&out, *fatGauge, *longGauge, &in, parity, dagger, 0, 0, commDim);
+  staggeredDslashCuda(&out, fatGauge, longGauge, &in, parity, dagger, 0, 0, commDim);
   
   flops += 1146*in.Volume();
 }
@@ -70,14 +66,11 @@ void DiracStaggered::DslashXpay(cudaColorSpinorField &out, const cudaColorSpinor
 				const QudaParity parity, const cudaColorSpinorField &x,
 				const double &k) const
 {    
-  if (!initDslash){
-    initDslashConstants(*fatGauge, in.Stride());
-    initStaggeredConstants(*fatGauge, *longGauge);
-  }
   checkParitySpinor(in, out);
 
+  initSpinorConstants(in);
   setFace(face); // FIXME: temporary hack maintain C linkage for dslashCuda
-  staggeredDslashCuda(&out, *fatGauge, *longGauge, &in, parity, dagger, &x, k, commDim);
+  staggeredDslashCuda(&out, fatGauge, longGauge, &in, parity, dagger, &x, k, commDim);
   
   flops += (1146+12)*in.Volume();
 }
@@ -85,11 +78,6 @@ void DiracStaggered::DslashXpay(cudaColorSpinorField &out, const cudaColorSpinor
 // Full staggered operator
 void DiracStaggered::M(cudaColorSpinorField &out, const cudaColorSpinorField &in) const
 {
-  if (!initDslash){
-    initDslashConstants(*fatGauge, in.Stride());
-    initStaggeredConstants(*fatGauge, *longGauge);
-  }
-
   bool reset = newTmp(&tmp1, in.Even());
 
   DslashXpay(out.Even(), in.Odd(), QUDA_EVEN_PARITY, *tmp1, 2*mass);  
@@ -100,12 +88,6 @@ void DiracStaggered::M(cudaColorSpinorField &out, const cudaColorSpinorField &in
 
 void DiracStaggered::MdagM(cudaColorSpinorField &out, const cudaColorSpinorField &in) const
 {
-
-  if (!initDslash){
-    initDslashConstants(*fatGauge, in.Stride());
-    initStaggeredConstants(*fatGauge, *longGauge);
-  }
-  
   bool reset = newTmp(&tmp1, in);
   
   cudaColorSpinorField* mytmp = dynamic_cast<cudaColorSpinorField*>(&(tmp1->Even()));
@@ -177,11 +159,6 @@ void DiracStaggeredPC::M(cudaColorSpinorField &out, const cudaColorSpinorField &
 
 void DiracStaggeredPC::MdagM(cudaColorSpinorField &out, const cudaColorSpinorField &in) const
 {
-  if (!initDslash){
-    initDslashConstants(*fatGauge, in.Stride());
-    initStaggeredConstants(*fatGauge, *longGauge);
-  }
-  
   bool reset = newTmp(&tmp1, in);
   
   QudaParity parity = QUDA_INVALID_PARITY;
@@ -214,7 +191,3 @@ void DiracStaggeredPC::reconstruct(cudaColorSpinorField &x, const cudaColorSpino
 {
   // do nothing
 }
-
-
-
-

lib/dirac_twisted_mass.cpp
@@ -22,13 +22,12 @@ void DiracTwistedMass::twistedApply(cudaColorSpinorField &out, const cudaColorSp
 {
   checkParitySpinor(out, in);
   
-  if (!initDslash) initDslashConstants(gauge, in.Stride());
-
   if (in.TwistFlavor() == QUDA_TWIST_NO || in.TwistFlavor() == QUDA_TWIST_INVALID)
     errorQuda("Twist flavor not set %d\n", in.TwistFlavor());
 
   double flavor_mu = in.TwistFlavor() * mu;
   
+  initSpinorConstants(in);
   twistGamma5Cuda(&out, &in, dagger, kappa, flavor_mu, twistType);
 
   flops += 24*in.Volume();
@@ -128,7 +127,6 @@ void DiracTwistedMassPC::TwistInv(cudaColorSpinorField &out, const cudaColorSpin
 void DiracTwistedMassPC::Dslash(cudaColorSpinorField &out, const cudaColorSpinorField &in, 
 				const QudaParity parity) const
 {
-  if (!initDslash) initDslashConstants(gauge, in.Stride());
   checkParitySpinor(in, out);
   checkSpinorAlias(in, out);
 
@@ -140,11 +138,13 @@ void DiracTwistedMassPC::Dslash(cudaColorSpinorField &out, const cudaColorSpinor
   if (!dagger || matpcType == QUDA_MATPC_EVEN_EVEN_ASYMMETRIC || matpcType == QUDA_MATPC_ODD_ODD_ASYMMETRIC) {
     double flavor_mu = in.TwistFlavor() * mu;
     setFace(face); // FIXME: temporary hack maintain C linkage for dslashCuda
+    initSpinorConstants(in);
     twistedMassDslashCuda(&out, gauge, &in, parity, dagger, 0, kappa, flavor_mu, 0.0, commDim);
     flops += (1320+72)*in.Volume();
   } else { // safe to use tmp2 here which may alias in
     bool reset = newTmp(&tmp2, in);
 
+    initSpinorConstants(in);
     TwistInv(*tmp2, in);
     DiracWilson::Dslash(out, *tmp2, parity);
 
@@ -163,7 +163,6 @@ void DiracTwistedMassPC::DslashXpay(cudaColorSpinorField &out, const cudaColorSp
 				    const QudaParity parity, const cudaColorSpinorField &x,
 				    const double &k) const
 {
-  if (!initDslash) initDslashConstants(gauge, in.Stride());
   checkParitySpinor(in, out);
   checkSpinorAlias(in, out);
 
@@ -175,12 +174,13 @@ void DiracTwistedMassPC::DslashXpay(cudaColorSpinorField &out, const cudaColorSp
   if (!dagger) {
     double flavor_mu = in.TwistFlavor() * mu;
     setFace(face); // FIXME: temporary hack maintain C linkage for dslashCuda
+    initSpinorConstants(in);
     twistedMassDslashCuda(&out, gauge, &in, parity, dagger, &x, kappa, 
 			  flavor_mu, k, commDim);
     flops += (1320+96)*in.Volume();
   } else { // tmp1 can alias in, but tmp2 can alias x so must not use this
     bool reset = newTmp(&tmp1, in);
-
+    initSpinorConstants(in);
     TwistInv(*tmp1, in);
     DiracWilson::Dslash(out, *tmp1, parity);
     xpayCuda((cudaColorSpinorField&)x, k, out);

commit d973cc71619b1fdf44ef35a599657518fee2fa02

Fixed performance regression introduced in commit 9e5a6343976bc8d80f977f78faf0cce9bc4d9ace.

lib/dslash_constants.h
@@ -341,6 +341,7 @@ void initSpinorConstants(const cudaColorSpinorField &spinor)
   if (sp_stride_h != last_sp_stride) {
     cudaMemcpyToSymbol(sp_stride, &sp_stride_h, sizeof(int));
     checkCudaError();
+    last_sp_stride = sp_stride_h;
   }
   
   // for domain wall:
@@ -350,6 +351,7 @@ void initSpinorConstants(const cudaColorSpinorField &spinor)
       cudaMemcpyToSymbol(Ls, &Ls_h, sizeof(int));  
       dslashConstants.Ls = Ls_h; // needed by tuneLaunch()
       checkCudaError();
+      last_Ls = Ls_h;
     }
   }
 }