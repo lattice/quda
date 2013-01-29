05e6200d0efd18d347bdf2401637678163e7a561
jpfoley <jfoley@physics.utah.edu>
2013-01-29 11:41:52 -0700

    Updated resize_quda.cu

--- lib/resize_quda.cu
+++ lib/resize_quda.cu
@@ -6,7 +6,6 @@
 
 namespace quda {
 
-/*
   cudaStream_t* getBlasStream();
 
 #include <texture.h>  
@@ -82,117 +81,116 @@ namespace quda {
       return;
     }
 
-  template<int Dir> 
-    __device__ void getCoordinates(int* const x1_p, int* const x2_p,
-        int* const x3_p, int* const x4_p,
-        int cb_index, const DecompParams& params, int parity)
-    {
+  __device__ void getCoordinates(int* const x1_p, int* const x2_p,
+      int* const x3_p, int* const x4_p,
+      int cb_index, const DecompParams& params, int parity, int Dir)
+  {
 
-      int xh, xodd;
-      switch(Dir){
-        case 0:
-          // cb_idx = (x1*X4X3X2 + x4*X3X2 + x3*X2 + x2)/2
-          xh = cb_index % params.X2h;
-          *x3_p = (cb_index/params.X2h) % params.X3;
-          *x1_p = cb_index/params.X4X3X2h;
-          *x4_p = (cb_index/params.X3X2h) % params.X4;
-          xodd = (*x1_p + *x3_p + *x2_p + parity) & 1;
-          *x2_p = 2*xh + xodd;
-          break;
+    int xh, xodd;
+    switch(Dir){
+      case 0:
+        // cb_idx = (x1*X4X3X2 + x4*X3X2 + x3*X2 + x2)/2
+        xh = cb_index % params.X2h;
+        *x3_p = (cb_index/params.X2h) % params.X3;
+        *x1_p = cb_index/params.X4X3X2h;
+        *x4_p = (cb_index/params.X3X2h) % params.X4;
+        xodd = (*x1_p + *x3_p + *x2_p + parity) & 1;
+        *x2_p = 2*xh + xodd;
+        break;
 
-        case 1:
-          // cb_index = (x2*X4X3X1 + x4*X3X1 + x3*X1 + x1)/2
-          xh = cb_index % params.X1h;
-          *x3_p = (cb_index/params.X1h) % params.X3;
-          *x2_p = cb_index/params.X4X3X1h;
-          *x4_p = (cb_index/(params.X3X1h)) % params.X4;
-          xodd = (*x2_p + *x3_p + *x4_p + parity) & 1;
-          *x1_p = 2*xh + xodd;
-          break;
+      case 1:
+        // cb_index = (x2*X4X3X1 + x4*X3X1 + x3*X1 + x1)/2
+        xh = cb_index % params.X1h;
+        *x3_p = (cb_index/params.X1h) % params.X3;
+        *x2_p = cb_index/params.X4X3X1h;
+        *x4_p = (cb_index/(params.X3X1h)) % params.X4;
+        xodd = (*x2_p + *x3_p + *x4_p + parity) & 1;
+        *x1_p = 2*xh + xodd;
+        break;
 
-        case 2:
-          // cb_index = (x3*X4X2X1 + x4*X2X1 + x2*X1 + x1)/2
-          xh = cb_index % params.X1h;
-          *x2_p = (cb_index/params.X1h) % params.X2;
-          *x3_p = cb_index/params.X4X2X1h;
-          *x4_p = (cb_index/params.X2X1h) % params.X4;
-          xodd = (*x2_p + *x3_p + *x4_p + parity) & 1;
-          *x1_p = 2*xh + xodd;
-          break; 
+      case 2:
+        // cb_index = (x3*X4X2X1 + x4*X2X1 + x2*X1 + x1)/2
+        xh = cb_index % params.X1h;
+        *x2_p = (cb_index/params.X1h) % params.X2;
+        *x3_p = cb_index/params.X4X2X1h;
+        *x4_p = (cb_index/params.X2X1h) % params.X4;
+        xodd = (*x2_p + *x3_p + *x4_p + parity) & 1;
+        *x1_p = 2*xh + xodd;
+        break; 
 
-        case 3:
-          // cb_index = (x4*X3X2X1 + x3*X2X1 + x2*X1 + x1)/2
-          // Note that this is the canonical ordering in the interior region.
-          xh = cb_index % params.X1h;
-          *x2_p = (cb_index/params.X1h) % params.X2;
-          *x4_p = (cb_index/params.X3X2X1h);
-          *x3_p = (cb_index/params.X2X1h) % params.X3;
-          xodd = (*x2_p + *x3_p + *x4_p + parity) & 1;
-          *x1_p = 2*xh + xodd;
-          break;
+      case 3:
+        // cb_index = (x4*X3X2X1 + x3*X2X1 + x2*X1 + x1)/2
+        // Note that this is the canonical ordering in the interior region.
+        xh = cb_index % params.X1h;
+        *x2_p = (cb_index/params.X1h) % params.X2;
+        *x4_p = (cb_index/params.X3X2X1h);
+        *x3_p = (cb_index/params.X2X1h) % params.X3;
+        xodd = (*x2_p + *x3_p + *x4_p + parity) & 1;
+        *x1_p = 2*xh + xodd;
+        break;
 
-        default:
-          break;
-      } // switch(Dir)
+      default:
+        break;
+    } // switch(Dir)
 
-      return;
+    return;
+  }
+
+
+
+  __device__ void getDomainCoordsFromGhostCoords(int* const y1_p, 
+      int* const y2_p,
+      int* const y3_p,
+      int* const y4_p,
+      int x1,
+      int x2,
+      int x3, 
+      int x4,
+      const DecompParams& params
+      int Dir)
+  {
+
+    *y1_p = x1;
+    *y2_p = x2;
+    *y3_p = x3;
+    *y4_p = x4; 
+
+    switch(Dir){
+      case 0:
+        if(x1 >= params.B1) *y1_p += params.X1;
+        if(params.B2 > 0) *y2_p += params.B2;
+        if(params.B3 > 0) *y3_p += params.B3;
+        if(params.B4 > 0) *y4_p += params.B4;
+        break;
+
+      case 1:
+        if(x2 >= params.B2) *y2_p += params.X2;
+        if(params.B1 > 0) *y1_p += params.B1; 
+        if(params.B3 > 0) *y3_p += params.B3;
+        if(params.B4 > 0) *y4_p += params.B4;
+        break;
+
+      case 2:
+        if(x3 >= params.B3) *y3_p += params.X3;
+        if(params.B1 > 0) *y1_p += params.B1; 
+        if(params.B2 > 0) *y2_p += params.B2;
+        if(params.B4 > 0) *y4_p += params.B4;
+        break;
+
+      case 3: 
+        if(x4 >= params.B4) *y4_p += params.X4;
+        if(params.B1 > 0) *y1_p += params.B1; 
+        if(params.B2 > 0) *y2_p += params.B2;
+        if(params.B3 > 0) *y3_p += params.B3;
+        break;
     }
-
-
-
-  template<int Dir>
-    __device__ void getDomainCoordsFromGhostCoords(int* const y1_p, 
-        int* const y2_p,
-        int* const y3_p,
-        int* const y4_p,
-        int x1,
-        int x2,
-        int x3, 
-        int x4,
-        const DecompParams& params)
-    {
-
-      *y1_p = x1;
-      *y2_p = x2;
-      *y3_p = x3;
-      *y4_p = x4; 
-
-      switch(Dir){
-        case 0:
-          if(x1 >= params.B1) *y1_p += params.X1;
-          if(params.B2 > 0) *y2_p += params.B2;
-          if(params.B3 > 0) *y3_p += params.B3;
-          if(params.B4 > 0) *y4_p += params.B4;
-          break;
-
-        case 1:
-          if(x2 >= params.B2) *y2_p += params.X2;
-          if(params.B1 > 0) *y1_p += params.B1; 
-          if(params.B3 > 0) *y3_p += params.B3;
-          if(params.B4 > 0) *y4_p += params.B4;
-          break;
-
-        case 2:
-          if(x3 >= params.B3) *y3_p += params.X3;
-          if(params.B1 > 0) *y1_p += params.B1; 
-          if(params.B2 > 0) *y2_p += params.B2;
-          if(params.B4 > 0) *y4_p += params.B4;
-          break;
-
-        case 3: 
-          if(x4 >= params.B4) *y4_p += params.X4;
-          if(params.B1 > 0) *y1_p += params.B1; 
-          if(params.B2 > 0) *y2_p += params.B2;
-          if(params.B3 > 0) *y3_p += params.B3;
-          break;
-      }
-      return;
-    }
+    return;
+  }
 
 
   // Need to generalise SpinorIndex
-  template<int Dir, typename FloatN>
-    __global__ void copyExteriorKernel(int parity, DecompParams params)
+  template<typename FloatN, int N, typename Output, typename Input>
+    __global__ void copyExteriorKernel(int parity, DecompParams params, int Dir)
     {
       int cb_index = blockIdx.x*blockDim.x + threadIdx.x;
       int gridSize = gridDim.x*blockDim.x; 
@@ -200,27 +198,22 @@ namespace quda {
       int x1, x2, x3, x4;
       int y1, y2, y3, y4;
 
-      getCoordinates<Dir>(&x1, &x2, &x3, &x4, cb_index, params, parity);
+      while(cb_index < length){
+        getCoordinates(&x1, &x2, &x3, &x4, cb_index, params, parity, Dir);
 
-      getDomainCoordsFromGhostCoords<Dir>(&y1, &y2, &y3, &y4,
-          x1, x2, x3, x4, params); 
+        getDomainCoordsFromGhostCoords(&y1, &y2, &y3, &y4,
+            x1, x2, x3, x4, params, Dir); 
 
-      int large_cb_index = (y4*params.Y3Y2Y1 + y3*params.Y2Y1 + y2*params.Y1 + y1) >> 1;
+        int large_cb_index = (y4*params.Y3Y2Y1 + y3*params.Y2Y1 + y2*params.Y1 + y1) >> 1;
 
-      // Need to put a copy routine in there
+        FloatN x[N];
+        X.load(x, cb_index);
+        Y.save(x, large_cb_index);
 
+      }
       return;
     }
 
-  /*
-     template<int Dir, typename FloatN, int N, typename Output, typename Input>
-     __global__ void copyExteriorKernel(Output Y, Input X, DecompParams params, int parity)
-     {
-
-     int cb_index = blockIdx.x*blockDim.x + threadIdx.x;
-     int gridSize = gridDim.x*blockDim.x; 
-     }
-   */
 
   template <typename FloatN, int N, typename Output, typename Input>
     class ExtendCuda {
@@ -233,17 +226,8 @@ namespace quda {
         const int parity;
 
         int sharedBytesPerThread() const { return 0; }
-        /*
-           virtual bool advanceSharedBytes(TuneParam &param) const
-           {
-           TuneParam next(param);
-           advanceBlockDim(next); // to get next blockDim
-           int nthreads = next.block.x * next.block.y * next.block.z;
-           param.shared_bytes = sharedBytesPerThread()*nthreads > sharedBytesPerBlock(param) ?
-           sharedBytesPerThread()*nthreads : sharedBytesPerBlock(param);
-           return false;
-           }
-         */
+
+
       public:
         ExtendCuda(Output &Y, Input &X, int length, const DecompParams& params, int parity) : X(X), Y(Y), length(length), params(params), parity(parity) {;}
         virtual ~ExtendCuda();
@@ -269,17 +253,6 @@ namespace quda {
         int parity; // parity of the destination field
 
         int sharedBytesPerThread() const { return 0; }
-        /*
-           virtual bool advanceSharedBytes(TuneParam &param) const
-           {
-           TuneParam next(param);
-           advanceBlockDim(next); // to get next blockDim
-           int nthreads = next.block.x * next.block.y * next.block.z;
-           param.shared_bytes = sharedBytesPerThread()*nthreads > sharedBytesPerBlock(param) ?
-           sharedBytesPerThread()*nthreads : sharedBytesPerBlock(param);
-           return false;
-           }
-         */
       public:
         CropCuda(Output &Y, Input &X, int length, const DecompParams& params, int parity) : X(X), Y(Y), length(length), params(params), parity(parity) {;}
         virtual ~CropCuda();
@@ -310,33 +283,101 @@ namespace quda {
       return;
     }
 
-    const int parity = 1; // Need to change this
+    const int parity = 0; // Need to change this
 
 
-    if(dst.Precision() == src.Precision()) {
-      if(dst.Precision() == QUDA_DOUBLE_PRECISION){
-        SpinorTexture<double2, double2, double2, 3, 0> src_tex(src);
-        Spinor<double2, double2, double2, 3> dst_spinor(dst);
-        ExtendCuda<double2, 3, Spinor<double2, double2, double2, 3>,
-          SpinorTexture<double2, double2, double2, 3, 0> >
-            extend(dst_spinor, src_tex, src.Volume(), params, parity);
-        extend.apply(*getBlasStream());
+    if (dst.Precision() == src.Precision() == QUDA_DOUBLE_PRECISION) {
 
-      }else if(dst.Precision() == QUDA_SINGLE_PRECISION){
-        SpinorTexture<float2, float2, float2, 3, 0> src_tex(src);
-        Spinor<float2, float2, float2, 3> dst_spinor(dst);
-        ExtendCuda<float2, 3, Spinor<float2, float2, float2, 3>,
-          SpinorTexture<float2, float2, float2, 3, 0> >
-            extend(dst_spinor, src_tex, src.Volume(), params, parity);
-        extend.apply(*getBlasStream());
+      Spinor<double2, double2, double2, 3, 0> src_spinor(src);
+      Spinor<double2, double2, double2, 3, 1> dst_spinor(dst);
 
-      }else if(dst.Precision() == QUDA_HALF_PRECISION){ 
+      ExtendCuda<double2, 3, Spinor<double2, double2, double2, 3, 1>,
+        Spinor<double2, double2, double2, 3, 0> >
+          extend(dst_spinor, src_spinor, src.Volume());
+      extend.apply(*getBlasStream());
 
-        errorQuda("half precision not yet supported\n");
-      }
+    } else if (dst.Precision() == src.Precision() == QUDA_SINGLE_PRECISION){
+
+      Spinor<float2, float2, float2, 3, 0> src_spinor(src);
+      Spinor<float2, float2, float2, 3, 1> dst_spinor(dst);
+
+      ExtendCuda<float2, 3, Spinor<float2, float2, float2, 3, 1>,
+        Spinor<float2, float2, float2, 3, 0> >
+          extend(dst_spinor, src_spinor, src.Volume());
+
+      extend.apply(*getBlasStream());:
+
+
+    } else if (dst.Precision() == QUDA_DOUBLE_PRECISION && src.Precision() == QUDA_SINGLE_PRECISION) {
+
+      Spinor<float2, float2, float2, 3, 0> src_spinor(src);
+      Spinor<float2, float2, double2, 3, 1> dst_spinor(dst);
+      ExtendCuda<float2, 3, Spinor<float2, float2, double2, 3, 1>,
+        Spinor<float2, float2, float2, 3, 0> >
+          extend(dst_spinor, src_spinor, src.Volume());
+
+      extend.apply(*getBlasStream());	
+
+    } else if (dst.Precision() == QUDA_SINGLE_PRECISION && src.Precision() == QUDA_DOUBLE_PRECISION) {
+
+      Spinor<float2, float2, double2, 3, 0> src_spinor(src);
+      Spinor<float2, float2, float2, 3, 1> dst_spinor(dst);
+
+      ExtendCuda<float2, 3, Spinor<float2, float2, float2, 3, 1>,
+        Spinor<float2, float2, double2, 3, 0> >
+          extend(dst_spinor, src_spinor, src.Volume());
+
+      extend.apply(*getBlasStream());	
+
+    } else if (dst.Precision() == QUDA_SINGLE_PRECISION && src.Precision() == QUDA_HALF_PRECISION) {
+
+      Spinor<float2, float2, short2, 3, 0> src_spinor(src);
+      Spinor<float2, float2, float2, 3, 1> dst_spinor(dst);
+
+      ExtendCuda<float2, 3, Spinor<float2, float2, float2, 3, 1>,
+        Spinor<float2, float2, short2, 3, 0> >
+          extend(dst_spinor, src_spinor, src.Volume());
+
+      extend.apply(*getBlasStream());	
+
+    } else if (dst.Precision() == QUDA_HALF_PRECISION && src.Precision() == QUDA_SINGLE_PRECISION) {
+      Spinor<float2, float2, float2, 3, 0> src_spinor(src);
+      Spinor<float2, float2, short2, 3, 1> dst_spinor(dst);
+
+      ExtendCuda<float2, 3, Spinor<float2, float2, short2, 3, 1>,
+        Spinor<float2, float2, float2, 3, 0> >
+          extend(dst_spinor, src_spinor, src.Volume());
+
+      extend.apply(*getBlasStream());	
+
+    } else if (dst.Precision() == QUDA_DOUBLE_PRECISION && src.Precision() == QUDA_HALF_PRECISION) {
+
+      Spinor<double2, float2, short2, 3, 0> src_spinor(src);
+      Spinor<double2, double2, double2, 3, 1> dst_spinor(dst);
+
+      ExtendCuda<double2, 3, Spinor<double2, double2, double2, 3, 1>,
+        Spinor<double2, float2, short2, 3, 0> >
+          extend(dst_spinor, src_spinor, src.Volume());
+
+      extend.apply(*getBlasStream());	
+
+    } else if (dst.Precision() == QUDA_HALF_PRECISION && src.Precision() == QUDA_DOUBLE_PRECISION) {
+
+      Spinor<double2, double2, double2, 3, 0> src_spinor(src);
+      Spinor<double2, double2, short2, 3, 1> dst_spinor(dst);
+
+      ExtendCuda<double2, 3, Spinor<double2, double2, short2, 3, 1>,
+        Spinor<double2, double2, double2, 3, 0> >
+          extend(dst_spinor, src_spinor, src.Volume());
+
+      extend.apply(*getBlasStream());	
     }
+
+
+
     return;
   }
+
 
 
   void cropCuda(cudaColorSpinorField &dst, const cudaColorSpinorField &src, const DecompParams& params) {
@@ -344,7 +385,7 @@ namespace quda {
 
     if (src.Nspin() != 1 && src.Nspin() != 4) errorQuda("nSpin(%d) not supported\n");
 
-    if (src.Length() <= dst.Length()) errorQuda("src length should be less greater destination length\n");
+    if (src.Length() >= dst.Length()) errorQuda("src length should be less than destination length\n");
 
     if (dst.SiteSubset() == QUDA_FULL_SITE_SUBSET || src.SiteSubset() == QUDA_FULL_SITE_SUBSET)
     {
@@ -353,29 +394,97 @@ namespace quda {
       return;
     }
 
-    const int parity = 1;
+    const int parity = 0; // Need to change this
 
-    if(dst.Precision() == src.Precision()) {
-      if(dst.Precision() == QUDA_DOUBLE_PRECISION){
-        SpinorTexture<double2, double2, double2, 3, 0> src_tex(src);
-        Spinor<double2, double2, double2, 3> dst_spinor(dst);
-        CropCuda<double2, 3, Spinor<double2, double2, double2, 3>,
-          SpinorTexture<double2, double2, double2, 3, 0> >
-            crop(dst_spinor, src_tex, src.Volume(), params, parity);
-        crop.apply(*getBlasStream());
-      }else if(dst.Precision() == QUDA_SINGLE_PRECISION){
-        SpinorTexture<float2, float2, float2, 3, 0> src_tex(src);
-        Spinor<float2, float2, float2, 3> dst_spinor(dst);
-        CropCuda<float2, 3, Spinor<float2, float2, float2, 3>,
-          SpinorTexture<float2, float2, float2, 3, 0> >
-            crop(dst_spinor, src_tex, dst.Volume(), params, parity);
-        crop.apply(*getBlasStream());
-      }else if(dst.Precision() == QUDA_HALF_PRECISION){ 
-        errorQuda("half precision not yet supported\n");
-      }
+
+    if (dst.Precision() == src.Precision() == QUDA_DOUBLE_PRECISION) {
+
+      Spinor<double2, double2, double2, 3, 0> src_spinor(src);
+      Spinor<double2, double2, double2, 3, 1> dst_spinor(dst);
+
+      CropCuda<double2, 3, Spinor<double2, double2, double2, 3, 1>,
+        Spinor<double2, double2, double2, 3, 0> >
+          crop(dst_spinor, src_spinor, src.Volume());
+
+      crop.apply(*getBlasStream());
+
+    } else if (dst.Precision() == src.Precision() == QUDA_SINGLE_PRECISION){
+
+      Spinor<float2, float2, float2, 3, 0> src_spinor(src);
+      Spinor<float2, float2, float2, 3, 1> dst_spinor(dst);
+
+      CropCuda<float2, 3, Spinor<float2, float2, float2, 3, 1>,
+        Spinor<float2, float2, float2, 3, 0> >
+          crop(dst_spinor, src_spinor, src.Volume());
+
+      crop.apply(*getBlasStream());:
+
+    } else if (dst.Precision() == QUDA_DOUBLE_PRECISION && src.Precision() == QUDA_SINGLE_PRECISION) {
+
+      Spinor<float2, float2, float2, 3, 0> src_spinor(src);
+      Spinor<float2, float2, double2, 3, 1> dst_spinor(dst);
+      CropCuda<float2, 3, Spinor<float2, float2, double2, 3, 1>,
+        Spinor<float2, float2, float2, 3, 0> >
+          crop(dst_spinor, src_spinor, src.Volume());
+
+      crop.apply(*getBlasStream());	
+
+    } else if (dst.Precision() == QUDA_SINGLE_PRECISION && src.Precision() == QUDA_DOUBLE_PRECISION) {
+
+      Spinor<float2, float2, double2, 3, 0> src_spinor(src);
+      Spinor<float2, float2, float2, 3, 1> dst_spinor(dst);
+
+      CropCuda<float2, 3, Spinor<float2, float2, float2, 3, 1>,
+        Spinor<float2, float2, double2, 3, 0> >
+          crop(dst_spinor, src_spinor, src.Volume());
+
+      crop.apply(*getBlasStream());	
+
+    } else if (dst.Precision() == QUDA_SINGLE_PRECISION && src.Precision() == QUDA_HALF_PRECISION) {
+
+      Spinor<float2, float2, short2, 3, 0> src_spinor(src);
+      Spinor<float2, float2, float2, 3, 1> dst_spinor(dst);
+
+      CropCuda<float2, 3, Spinor<float2, float2, float2, 3, 1>,
+        Spinor<float2, float2, short2, 3, 0> >
+          extend(dst_spinor, src_spinor, src.Volume());
+
+      crop.apply(*getBlasStream());	
+
+    } else if (dst.Precision() == QUDA_HALF_PRECISION && src.Precision() == QUDA_SINGLE_PRECISION) {
+      Spinor<float2, float2, float2, 3, 0> src_spinor(src);
+      Spinor<float2, float2, short2, 3, 1> dst_spinor(dst);
+
+      CropCuda<float2, 3, Spinor<float2, float2, short2, 3, 1>,
+        Spinor<float2, float2, float2, 3, 0> >
+          crop(dst_spinor, src_spinor, src.Volume());
+
+      extend.apply(*getBlasStream());	
+
+    } else if (dst.Precision() == QUDA_DOUBLE_PRECISION && src.Precision() == QUDA_HALF_PRECISION) {
+
+      Spinor<double2, float2, short2, 3, 0> src_spinor(src);
+      Spinor<double2, double2, double2, 3, 1> dst_spinor(dst);
+
+      CropCuda<double2, 3, Spinor<double2, double2, double2, 3, 1>,
+        Spinor<double2, float2, short2, 3, 0> >
+          crop(dst_spinor, src_spinor, src.Volume());
+
+      crop.apply(*getBlasStream());	
+
+    } else if (dst.Precision() == QUDA_HALF_PRECISION && src.Precision() == QUDA_DOUBLE_PRECISION) {
+
+      Spinor<double2, double2, double2, 3, 0> src_spinor(src);
+      Spinor<double2, double2, short2, 3, 1> dst_spinor(dst);
+
+      CropCuda<double2, 3, Spinor<double2, double2, short2, 3, 1>,
+        Spinor<double2, double2, double2, 3, 0> >
+          crop(dst_spinor, src_spinor, src.Volume());
+
+      crop.apply(*getBlasStream());	
     }
     return;
-  }
-*/
+  } // cropCuda
+
 
 } // namespace
