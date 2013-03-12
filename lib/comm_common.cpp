lattice/quda

commit 2a72aa813292a7f94308e99962e76f798cb026bc

Major rewrite of comms interface (closes #46)

lib/comm_common.cpp
@@ -1,0 +1,264 @@
+#include <unistd.h> // for gethostname()
+
+#include <quda_internal.h>
+#include <comm_quda.h>
+
+
+struct Topology_s {
+  int ndim;
+  int dims[QUDA_MAX_DIM];
+  int *ranks;
+  int (*coords)[QUDA_MAX_DIM];
+  int my_rank;
+  int my_coords[QUDA_MAX_DIM];
+  // It might be worth adding communicators to allow for efficient reductions:
+  //   #if defined(MPI_COMMS)
+  //     MPI_Comm comm;
+  //   #elif defined(QMP_COMMS)
+  //     QMP_communicator_t comm; // currently only supported by qmp-2.4.0-alpha
+  //   #endif
+};
+
+
+/**
+ * Utility function for indexing into Topology::ranks[]
+ *
+ * @param ndim  Number of grid dimensions in the network topology
+ * @param dims  Array of grid dimensions
+ * @param x     Node coordinates
+ * @return      Linearized index cooresponding to the node coordinates
+ */
+static inline int index(int ndim, const int *dims, const int *x)
+{
+  int idx = x[0];
+  for (int i = 1; i < ndim; i++) {
+    idx = dims[i]*idx + x[i];
+  }
+  return idx;
+}
+
+
+static inline bool advance_coords(int ndim, const int *dims, int *x)
+{
+  bool valid = false;
+  for (int i = ndim-1; i >= 0; i--) {
+    if (x[i] < dims[i]-1) {
+      x[i]++;
+      valid = true;
+      break;
+    } else {
+      x[i] = 0;
+    }
+  }
+  return valid;
+}
+
+
+char *comm_hostname(void)
+{
+  static bool cached = false;
+  static char hostname[128];
+
+  if (!cached) {
+    gethostname(hostname, 128);
+    hostname[127] = '\0';
+    cached = true;
+  }
+
+  return hostname;
+}
+
+
+static unsigned long int rand_seed = 137;
+
+/**
+ * We provide our own random number generator to avoid re-seeding
+ * rand(), which might also be used by the calling application.  This
+ * is a clone of rand48(), provided by stdlib.h on UNIX.
+ *
+ * @return a random double in the interval [0,1)
+ */
+double comm_drand(void)
+{
+  const double twoneg48 = 0.35527136788005009e-14;
+  const unsigned long int m = 25214903917, a = 11, mask = 281474976710655;
+  rand_seed = (m * rand_seed + a) & mask;
+  return (twoneg48 * rand_seed);
+}
+
+
+// QudaCommsMap is declared in quda.h:
+//   typedef int (*QudaCommsMap)(const int *coords, void *fdata);
+
+Topology *comm_create_topology(int ndim, const int *dims, QudaCommsMap rank_from_coords, void *map_data)
+{
+  if (ndim > QUDA_MAX_DIM) {
+    errorQuda("ndim exceeds QUDA_MAX_DIM");
+  }
+
+  Topology *topo = (Topology *) safe_malloc(sizeof(Topology));
+
+  topo->ndim = ndim;
+
+  int nodes = 1;
+  for (int i=0; i<ndim; i++) {
+    topo->dims[i] = dims[i];
+    nodes *= dims[i];
+  }
+
+  topo->ranks = (int *) safe_malloc(nodes*sizeof(int));
+  topo->coords = (int (*)[QUDA_MAX_DIM]) safe_malloc(nodes*sizeof(int[QUDA_MAX_DIM]));
+
+  int x[QUDA_MAX_DIM];
+  for (int i = 0; i < ndim; i++) {
+    x[i] = 0;
+  }
+
+  do {
+    int rank = rank_from_coords(x, map_data);
+    topo->ranks[index(ndim, dims, x)] = rank;
+    for (int i=0; i<ndim; i++) {
+      topo->coords[rank][i] = x[i];
+    }
+  } while (advance_coords(ndim, dims, x));
+
+  int my_rank = comm_rank();
+  topo->my_rank = my_rank;
+  for (int i = 0; i < ndim; i++) {
+    topo->my_coords[i] = topo->coords[my_rank][i];
+  }
+
+  // initialize the random number generator with a rank-dependent seed
+  rand_seed = 17*my_rank + 137;
+
+  return topo;
+}
+
+
+void comm_destroy_topology(Topology *topo)
+{
+  host_free(topo->ranks);
+  host_free(topo->coords);
+  host_free(topo);
+}
+
+
+int comm_ndim(const Topology *topo)
+{
+  return topo->ndim;
+}
+
+
+const int *comm_dims(const Topology *topo)
+{
+  return topo->dims;
+}
+
+
+const int *comm_coords(const Topology *topo)
+{
+  return topo->my_coords;
+}
+
+
+const int *comm_coords_from_rank(const Topology *topo, int rank)
+{
+  return topo->coords[rank];
+}
+
+
+int comm_rank_from_coords(const Topology *topo, const int *coords)
+{
+  return topo->ranks[index(topo->ndim, topo->dims, coords)];
+}
+
+
+int comm_rank_displaced(const Topology *topo, const int displacement[])
+{
+  int coords[QUDA_MAX_DIM];
+
+  for (int i = 0; i < topo->ndim; i++) {
+    coords[i] = (comm_coords(topo)[i] + displacement[i]) % comm_dims(topo)[i];
+  }
+  return comm_rank_from_coords(topo, coords);
+}
+
+
+// FIXME: The following routines rely on a "default" topology.
+// They should probably be reworked or eliminated eventually.
+
+Topology *default_topo = NULL;
+
+void comm_set_default_topology(Topology *topo)
+{
+  default_topo = topo;
+}
+
+
+Topology *comm_default_topology(void)
+{
+  if (!default_topo) {
+    errorQuda("Default topology has not been declared");
+  }
+  return default_topo;
+}
+
+
+int comm_dim(int dim)
+{
+  Topology *topo = comm_default_topology();
+  return comm_dims(topo)[dim];
+}
+
+
+int comm_coord(int dim)
+{
+  Topology *topo = comm_default_topology();
+  return comm_coords(topo)[dim];
+}
+
+
+/**
+ * Send to the "dir" direction in the "dim" dimension
+ */
+MsgHandle *comm_declare_send_relative(void *buffer, int dim, int dir, size_t nbytes)
+{
+  int disp[QUDA_MAX_DIM] = {0};
+  disp[dim] = dir;
+
+  return comm_declare_send_displaced(buffer, disp, nbytes);
+}
+
+
+/**
+ * Receive from the "dir" direction in the "dim" dimension
+ */
+MsgHandle *comm_declare_receive_relative(void *buffer, int dim, int dir, size_t nbytes)
+{
+  int disp[QUDA_MAX_DIM] = {0};
+  disp[dim] = dir;
+
+  return comm_declare_receive_displaced(buffer, disp, nbytes);
+}
+
+
+void comm_finalize(void)
+{
+  Topology *topo = comm_default_topology();
+  comm_destroy_topology(topo);
+  comm_set_default_topology(NULL);
+}
+
+
+static int manual_set_partition[QUDA_MAX_DIM] = {0};
+
+void comm_dim_partitioned_set(int dim)
+{ 
+  manual_set_partition[dim] = 1;
+}
+
+
+int comm_dim_partitioned(int dim)
+{
+  return (manual_set_partition[dim] || (comm_dim(dim) > 1));
+}

README
@@ -1,4 +1,4 @@
-Release Notes for QUDA v0.5.0                        3 December 2012
+Release Notes for QUDA v0.5.0 (PRE-RELEASE)              XX March 2013
 -----------------------------
 
 Overview:
@@ -135,10 +135,6 @@ Known Issues:
   Dslash kernels (visible if the verbosity is set to at least
   QUDA_SUMMARIZE), rather than the correct values.  This does not
   affect the tuning process or actual performance.
-
-* At present, using MPI directly for communications (as opposed to
-  QMP) requires calling some initialization routines that are not
-  exposed in quda.h.  This will be corrected in the next release.
 
 * Attempts to compile with GCC 4.7.x will fail due to bugs in the
   compiler.

include/comm_quda.h
@@ -1,58 +1,59 @@
 #ifndef _COMM_QUDA_H
 #define _COMM_QUDA_H
 
-#define BACK_NBR 1
-#define FWD_NBR 2
-
-#define X_BACK_NBR 1
-#define Y_BACK_NBR 2
-#define Z_BACK_NBR 3
-#define T_BACK_NBR 4
-#define X_FWD_NBR  5
-#define Y_FWD_NBR  6
-#define Z_FWD_NBR  7
-#define T_FWD_NBR  8
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct MsgHandle_s MsgHandle;
+  typedef struct MsgHandle_s MsgHandle;
+  typedef struct Topology_s Topology;
 
-/* The following routines are implemented over MPI only. */
+  /* defined in quda.h; redefining here to avoid circular references */ 
+  typedef int (*QudaCommsMap)(const int *coords, void *fdata);
 
-MsgHandle *	comm_send_to_rank(void*, int, int);
-MsgHandle *	comm_recv_from_rank(void*, int, int);
-int		comm_get_neighbor_rank(int dx, int dy, int dz, int dt);
+  /* implemented in comm_common.cpp */
 
-/* implemented over MPI, QMP, and single */
+  char *comm_hostname(void);
+  double comm_drand(void);
+  Topology *comm_create_topology(int ndim, const int *dims, QudaCommsMap rank_from_coords, void *map_data);
+  void comm_destroy_topology(Topology *topo);
+  int comm_ndim(const Topology *topo);
+  const int *comm_dims(const Topology *topo);
+  const int *comm_coords(const Topology *topo);
+  const int *comm_coords_from_rank(const Topology *topo, int rank);
+  int comm_rank_from_coords(const Topology *topo, const int *coords);
+  int comm_rank_displaced(const Topology *topo, const int displacement[]);
+  void comm_set_default_topology(Topology *topo);
+  Topology *comm_default_topology(void);
+  int comm_dim(int dim);
+  int comm_coord(int dim);
+  MsgHandle *comm_declare_send_relative(void *buffer, int dim, int dir, size_t nbytes);
+  MsgHandle *comm_declare_receive_relative(void *buffer, int dim, int dir, size_t nbytes);
+  void comm_finalize(void);
+  void comm_dim_partitioned_set(int dim);
+  int comm_dim_partitioned(int dim);
 
-int		comm_gpuid();
-void            comm_create(int argc, char **argv);
-void		comm_init(void);
-void		comm_cleanup(void);
-void		comm_exit(int);
-char *          comm_hostname(void);
-void            comm_set_gridsize(const int *X, int nDim);
-void		comm_barrier(void);
-void            comm_broadcast(void *data, size_t nbytes);
-int		comm_rank(void);
-int		comm_size(void);
-MsgHandle *     comm_declare_send_relative(void *buffer, int i, int dir, size_t bytes);
-MsgHandle *     comm_declare_receive_relative(void *buffer, int i, int dir, size_t bytes);
-void            comm_free(MsgHandle *handle);
-void            comm_start(MsgHandle *handle);
-void		comm_wait(MsgHandle *handle);
-int             comm_query(MsgHandle *handle);
-int             comm_dim_partitioned(int dir);
-void            comm_dim_partitioned_set(int dir);
-int             comm_dim(int);
-int             comm_coords(int);
-void		comm_allreduce(double* data);
-void            comm_allreduce_int(int* data);
-void		comm_allreduce_array(double* data, size_t size);
-void		comm_allreduce_max(double* data);
-  
+
+  /* implemented in comm_single.cpp, comm_qmp.cpp, and comm_mpi.cpp */
+
+  void comm_init(int ndim, const int *dims, QudaCommsMap rank_from_coords, void *map_data);
+  int comm_rank(void);
+  int comm_size(void);
+  int comm_gpuid(void);
+  MsgHandle *comm_declare_send_displaced(void *buffer, const int displacement[], size_t nbytes);
+  MsgHandle *comm_declare_receive_displaced(void *buffer, const int displacement[], size_t nbytes);
+  void comm_free(MsgHandle *mh);
+  void comm_start(MsgHandle *mh);
+  void comm_wait(MsgHandle *mh);
+  int comm_query(MsgHandle *mh);
+  void comm_allreduce(double* data);
+  void comm_allreduce_max(double* data);
+  void comm_allreduce_array(double* data, size_t size);
+  void comm_allreduce_int(int* data);
+  void comm_broadcast(void *data, size_t nbytes);
+  void comm_barrier(void);
+  void comm_abort(int status);
+
 #ifdef __cplusplus
 }
 #endif

include/quda.h
@@ -242,10 +242,50 @@ extern "C" {
 			FILE *outfile);
 
   /**
+   * initCommsGridQuda() takes an optional "rank_from_coords" argument that
+   * should be a pointer to a user-defined function with this prototype.  
+   *
+   * @param coords  Node coordinates
+   * @param fdata   Any auxiliary data needed by the function
+   * @return        MPI rank or QMP node ID cooresponding to the node coordinates
+   *
+   * @see initCommsGridQuda
+   */
+  typedef int (*QudaCommsMap)(const int *coords, void *fdata);
+
+  /**
+   * Declare the grid mapping ("logical topology" in QMP parlance)
+   * used for communications in a multi-GPU grid.  This function
+   * should be called prior to initQuda().  The only case in which
+   * it's optional is when QMP is used for communication and the
+   * logical topology has already been declared by the application.
+   *
+   * @param nDim   Number of grid dimensions.  "4" is the only supported
+   *               value currently.
+   *
+   * @param dims   Array of grid dimensions.  dims[0]*dims[1]*dims[2]*dims[3]
+   *               must equal the total number of MPI ranks or QMP nodes.
+   *
+   * @param func   Pointer to a user-supplied function that maps coordinates
+   *               in the communication grid to MPI ranks (or QMP node IDs).
+   *               If the pointer is NULL, the default mapping depends on
+   *               whether QMP or MPI is being used for communication.  With
+   *               QMP, the existing logical topology is used if it's been
+   *               declared.  With MPI or as a fallback with QMP, the default
+   *               ordering is lexicographical with the fourth ("t") index
+   *               varying fastest.
+   *
+   * @param fdata  Pointer to any data required by "func" (may be NULL)               
+   *
+   * @see QudaCommsMap
+   */
+  void initCommsGridQuda(int nDim, const int *dims, QudaCommsMap func, void *fdata);
+
+  /**
    * Initialize the library.
    *
    * @param device  CUDA device number to use.  In a multi-GPU build,
-   *                this parameter may be either set explicitly on a
+   *                this parameter may either be set explicitly on a
    *                per-process basis or set to -1 to enable a default
    *                allocation of devices to processes.
    */
@@ -401,13 +441,6 @@ extern "C" {
   int computeGaugeForceQuda(void* mom, void* sitelink,  int*** input_path_buf, int* path_length,
 			    void* loop_coeff, int num_paths, int max_length, double eb3,
 			    QudaGaugeParam* qudaGaugeParam, double* timeinfo);
-  
-  /*
-   * The following routines are only used by the examples in tests/ .
-   * They should not be called in a typical application.
-   */  
-  void initCommsQuda(int argc, char **argv, const int *X, int nDim);
-  void endCommsQuda();
 
 #ifdef __cplusplus
 }

include/util_quda.h
@@ -39,7 +39,7 @@ char *getPrintBuffer();
   fprintf(getOutputFile(), " (rank %d, host %s, " __FILE__ ":%d in %s())\n", \
           comm_rank(), comm_hostname(), __LINE__, __func__);                 \
   fflush(getOutputFile());                                                   \
-  comm_exit(1);                                                              \
+  comm_abort(1);                                                             \
 } while (0)
 
 #define warningQuda(...) do {                                   \

lib/Makefile
@@ -10,8 +10,8 @@ QUDA_OBJS = timer.o malloc.o solver.o inv_bicgstab_quda.o		\
 	cuda_gauge_field.o dirac_clover.o dirac_wilson.o		\
 	dirac_staggered.o dirac_domain_wall.o dirac_twisted_mass.o	\
 	tune.o fat_force_quda.o llfat_quda_itf.o clover_quda.o		\
-	dslash_quda.o blas_quda.o copy_quda.o reduce_quda.o face_buffer.o ${FACE_COMMS_OBJS}	\
-	${NUMA_AFFINITY_OBJS}
+	dslash_quda.o blas_quda.o copy_quda.o reduce_quda.o face_buffer.o \
+	comm_common.o ${FACE_COMMS_OBJS} ${NUMA_AFFINITY_OBJS}
 
 # header files, found in include/
 QUDA_HDRS = blas_quda.h clover_field.h color_spinor_field.h convert.h	\

commit dfc5ae2e96f10bec847f8d0a29889ff057392135

Fixed comms bug introduced with commit 2a72aa81

lib/comm_common.cpp
@@ -173,12 +173,17 @@ int comm_rank_from_coords(const Topology *topo, const int *coords)
 }
 
 
+static inline int mod(int a, int b)
+{
+  return ((a % b) + b) % b;
+}
+
 int comm_rank_displaced(const Topology *topo, const int displacement[])
 {
   int coords[QUDA_MAX_DIM];
 
   for (int i = 0; i < topo->ndim; i++) {
-    coords[i] = (comm_coords(topo)[i] + displacement[i]) % comm_dims(topo)[i];
+    coords[i] = mod(comm_coords(topo)[i] + displacement[i], comm_dims(topo)[i]);
   }
   return comm_rank_from_coords(topo, coords);
 }