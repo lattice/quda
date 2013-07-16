5a4087825ce1ff2ef6b694eb09dc870879f6dec3
Frank Winter <frank.winter@ed.ac.uk>
2013-07-16 10:41:38 -0400

    Bug fix, save parity in QDPJITDiracOrder constructor

--- include/color_spinor_field_order.h
+++ include/color_spinor_field_order.h
@@ -381,8 +381,8 @@ struct QDPJITDiracOrder {
   int volumeCB;
   int parity;
   int stride;
-  QDPJITDiracOrder(const ColorSpinorField &a, Float *field_=0, int parity=1) 
-  : field(field_ ? field_ : (Float*)a.V()), volumeCB(a.VolumeCB()), stride(stride)
+  QDPJITDiracOrder(const ColorSpinorField &a, Float *field_=0, int parity_=1) 
+  : field(field_ ? field_ : (Float*)a.V()), volumeCB(a.VolumeCB()), stride(stride), parity(parity_)
   { if (volumeCB != a.Stride()) errorQuda("Stride must equal volume for this field order"); }
   virtual ~QDPJITDiracOrder() { ; }
 
