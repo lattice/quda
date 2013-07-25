0b48ad7b4f878241a93d6a5b362100821cb13813
Michael Cheng <micheng@bu.edu>
2013-07-25 15:01:32 -0400

    Updating multigrid_test to test correctness of coarse operator.

--- tests/multigrid_test.cpp
+++ tests/multigrid_test.cpp
@@ -15,6 +15,7 @@
 #include <transfer.h>
 
 #include <cstring>
+#include <wilson_dslash_reference.h>
 
 #if defined(QMP_COMMS)
 #include <qmp.h>
@@ -23,11 +24,13 @@
 #endif
 
 
+#define MAX(a,b) ((a)>(b)?(a):(b))
+
 using namespace quda;
 
 ColorSpinorParam csParam, coarsecsParam;
 
-float kappa = 1.0;
+float kappa = 1.0/(2.0*(4-.4125));
 
 extern int device;
 extern int xdim;
@@ -43,14 +46,27 @@ extern int nvec;
 extern char vecfile[];
 
 std::vector<ColorSpinorField*> W; // array of bad guys
+cpuColorSpinorField *V1, *V2;
 int Nvec; // number of bad guys for the transfer operator
 QudaPrecision prec_cpu = QUDA_SINGLE_PRECISION;
 
 // where is the packing / unpacking taking place
 //most orders are CPU only currently
 const QudaFieldLocation location = QUDA_CPU_FIELD_LOCATION;
+QudaGaugeParam gauge_param;
+QudaInvertParam inv_param;
+void *hostGauge[4];
 
-void init() {
+DiracWilson *dirac;
+extern int test_type;
+
+// Dirac operator type
+extern QudaDslashType dslash_type;
+cudaGaugeField *cudagauge;
+cpuGaugeField *cpugauge;
+
+
+void init(int argc, char **argv) {
 
   csParam.nColor = 3;
   csParam.nSpin = 4;
@@ -78,6 +94,115 @@ void init() {
       printf("Could not allocate W[%d]\n", i);      
     }
   }
+  V1 = new cpuColorSpinorField(csParam);
+  V2 = new cpuColorSpinorField(csParam);
+
+
+  gauge_param = newQudaGaugeParam();
+  inv_param = newQudaInvertParam();
+
+  gauge_param.X[0] = xdim;
+  gauge_param.X[1] = ydim;
+  gauge_param.X[2] = zdim;
+  gauge_param.X[3] = tdim;
+
+  //setDims(gauge_param.X);
+  setKernelPackT(false);
+  Ls = 1;
+  setSpinorSiteSize(24);
+
+  gauge_param.anisotropy = 1.0;
+  gauge_param.type = QUDA_WILSON_LINKS;
+  gauge_param.gauge_order = QUDA_QDP_GAUGE_ORDER;
+  gauge_param.t_boundary = QUDA_ANTI_PERIODIC_T;
+  gauge_param.cpu_prec = prec_cpu;
+  gauge_param.cuda_prec = prec_cpu;
+  gauge_param.reconstruct = link_recon;
+  gauge_param.reconstruct_sloppy = link_recon;
+  gauge_param.cuda_prec_sloppy = prec_cpu;
+  gauge_param.gauge_fix = QUDA_GAUGE_FIXED_NO;
+
+
+#ifndef MULTI_GPU // free parameter for single GPU
+  gauge_param.ga_pad = 0;
+#else // must be this one c/b face for multi gpu
+  int x_face_size = gauge_param.X[1]*gauge_param.X[2]*gauge_param.X[3]/2;
+  int y_face_size = gauge_param.X[0]*gauge_param.X[2]*gauge_param.X[3]/2;
+  int z_face_size = gauge_param.X[0]*gauge_param.X[1]*gauge_param.X[3]/2;
+  int t_face_size = gauge_param.X[0]*gauge_param.X[1]*gauge_param.X[2]/2;
+  int pad_size =MAX(x_face_size, y_face_size);
+  pad_size = MAX(pad_size, z_face_size);
+  pad_size = MAX(pad_size, t_face_size);
+  gauge_param.ga_pad = pad_size;    
+#endif
+
+  inv_param.kappa = kappa;
+
+  inv_param.Ls = (inv_param.twist_flavor != QUDA_TWIST_NONDEG_DOUBLET) ? Ls : 1;
+  
+  inv_param.matpc_type = QUDA_MATPC_EVEN_EVEN_ASYMMETRIC;
+  inv_param.dagger = QUDA_DAG_NO;
+
+  inv_param.cpu_prec = prec_cpu;
+  if (inv_param.cpu_prec != gauge_param.cpu_prec) {
+    errorQuda("Gauge and spinor CPU precisions must match");
+  }
+  inv_param.cuda_prec = prec_cpu;
+  inv_param.input_location = QUDA_CPU_FIELD_LOCATION;
+  inv_param.output_location = QUDA_CPU_FIELD_LOCATION;
+  inv_param.sp_pad = 0;
+  inv_param.cl_pad = 0;
+
+
+  inv_param.gamma_basis = QUDA_DEGRAND_ROSSI_GAMMA_BASIS; // test code only supports DeGrand-Rossi Basis
+  inv_param.dirac_order = QUDA_DIRAC_ORDER;
+
+  switch(test_type) {
+  case 0:
+  case 1:
+    inv_param.solution_type = QUDA_MATPC_SOLUTION;
+    break;
+  case 2:
+    inv_param.solution_type = QUDA_MAT_SOLUTION;
+    break;
+  case 3:
+    inv_param.solution_type = QUDA_MATPCDAG_MATPC_SOLUTION;
+    break;
+  case 4:
+    inv_param.solution_type = QUDA_MATDAG_MAT_SOLUTION;
+    break;
+  default:
+    errorQuda("Test type %d not defined\n", test_type);
+  }
+
+  inv_param.dslash_type = dslash_type;
+
+  // construct input fields
+  for (int dir = 0; dir < 4; dir++) hostGauge[dir] = malloc(V*gaugeSiteSize*gauge_param.cpu_prec);
+
+
+ if (strcmp(latfile,"")) {  // load in the command line supplied gauge field
+   read_gauge_field(latfile, hostGauge, gauge_param.cpu_prec, gauge_param.X, argc, argv);
+   construct_gauge_field(hostGauge, 2, gauge_param.cpu_prec, &gauge_param);
+  } else { // else generate a random SU(3) field
+   construct_gauge_field(hostGauge, 1, gauge_param.cpu_prec, &gauge_param);
+  }
+ GaugeFieldParam gParam(hostGauge, gauge_param);  
+  cpugauge = new cpuGaugeField(gParam);
+
+  GaugeFieldParam gParam2(0, gauge_param);  
+  gParam2.create = QUDA_NULL_FIELD_CREATE;
+  cudagauge = new cudaGaugeField(gParam2);
+  cudagauge->loadCPUField(*cpugauge, QUDA_CPU_FIELD_LOCATION);
+
+
+    DiracParam diracParam;
+    setDiracParam(diracParam, &inv_param, 0);
+    diracParam.verbose = QUDA_VERBOSE;
+    //diracParam.tmp1 = tmp1;
+    //diracParam.tmp2 = tmp2;
+    
+    dirac = new DiracWilson(diracParam);
 
   initQuda(device);
 }
@@ -87,11 +212,18 @@ void end() {
   
   for (int i=0; i<Nvec; i++) delete W[i];
   //delete []W;
+  for (int dir = 0; dir < 4; dir++) free(hostGauge[dir]);
+  delete cpugauge;
+  delete cudagauge;
+  delete V1;
+  delete V2;
+  delete dirac;
 
   endQuda();
 }
 
 void loadTest() {
+
 
   void **V = new void*[Nvec];
   for (int i=0; i<Nvec; i++) { 
@@ -139,7 +271,24 @@ void loadTest() {
     //Wfine.Compare(Wfine,*W[i]); // strong check
   }
 
+  //  V1->Source(QUDA_RANDOM_SOURCE);
+  //    printfQuda("Absolute Norm^2 of V1 = %e\n", blas::norm2(*V1));
+  //wil_mat(V2->V(), hostGauge, V1->V(), kappa, QUDA_DAG_NO, prec_cpu, gauge_param);
+  //    printfQuda("Absolute Norm^2 of V2 = %e\n", blas::norm2(*V2));
+
+      int ndim = 4;
+      std::complex<float> *Y[2*ndim+1];
+      int Nc = Wcoarse->Ncolor();
+      int Ns = Wcoarse->Nspin();
+      printfQuda("Vol = %d, Nc_c = %d, Ns_c = %d\n", Wcoarse->Volume(), Nc, Ns);
+      for(int d = 0; d < 2*ndim+1; d++) {
+	Y[d] = (std::complex<float> *) malloc(Wcoarse->Volume()*Ns*Ns*Nc*Nc*sizeof(float));
+      }
+
   delete Wcoarse;
+      for(int d = 0; d < 2*ndim+1; d++) {
+	free(Y[d]);
+      }
 
 }
 
@@ -159,7 +308,7 @@ int main(int argc, char **argv) {
 
   initComms(argc, argv, gridsize_from_cmdline);
 
-  init();
+  init(argc, argv);
   loadTest();
   end();
 
