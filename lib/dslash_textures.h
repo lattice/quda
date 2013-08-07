lattice/quda

commit cdc5907f90066258322613197bc7230b1213adc1

Reconstruct 9 and 13 support for HISQ longlinks

lib/dslash_textures.h
@@ -8,6 +8,14 @@
 #endif
 
 #if (__COMPUTE_CAPABILITY__ >= 130)
+
+template<typename Tex>
+static __inline__ __device__ double fetch_double(Tex t, int i)
+{
+  int2 v = TEX1DFETCH(int2, t, i);
+  return __hiloint2double(v.y, v.x);
+}
+
 template <typename Tex>
 static __inline__ __device__ double2 fetch_double2(Tex t, int i)
 {
@@ -41,15 +49,24 @@ texture<short2, 1, cudaReadModeNormalizedFloat> gauge1TexHalf2;
 
 texture<int4, 1> longGauge0TexDouble;
 texture<int4, 1> longGauge1TexDouble;
+texture<int2, 1> longPhase0TexDouble;
+texture<int2, 1> longPhase1TexDouble;
+
 texture<float4, 1, cudaReadModeElementType> longGauge0TexSingle;
 texture<float4, 1, cudaReadModeElementType> longGauge1TexSingle;
 texture<float2, 1, cudaReadModeElementType> longGauge0TexSingle_norecon;
 texture<float2, 1, cudaReadModeElementType> longGauge1TexSingle_norecon;
+texture<float, 1, cudaReadModeElementType> longPhase0TexSingle;
+texture<float, 1, cudaReadModeElementType> longPhase1TexSingle;
+
 
 texture<short4, 1, cudaReadModeNormalizedFloat> longGauge0TexHalf;
 texture<short4, 1, cudaReadModeNormalizedFloat> longGauge1TexHalf;
 texture<short2, 1, cudaReadModeNormalizedFloat> longGauge0TexHalf_norecon;
 texture<short2, 1, cudaReadModeNormalizedFloat> longGauge1TexHalf_norecon;
+texture<short, 1, cudaReadModeNormalizedFloat> longPhase0TexHalf;
+texture<short, 1, cudaReadModeNormalizedFloat> longPhase1TexHalf;
+
 
 // Double precision input spinor field
 texture<int4, 1> spinorTexDouble;
@@ -249,10 +266,19 @@ void bindLongGaugeTex(const cudaGaugeField &gauge, const int oddBit, void **gaug
 #ifdef USE_TEXTURE_OBJECTS
   dslashParam.longGauge0Tex = oddBit ? gauge.OddTex() : gauge.EvenTex();
   dslashParam.longGauge1Tex = oddBit ? gauge.EvenTex() : gauge.OddTex();
+
+  if(gauge.Reconstruct() == QUDA_RECONSTRUCT_13 || gauge.Reconstruct() == QUDA_RECONSTRUCT_9){ 
+    dslashParam.longPhase0Tex = oddBit ? gauge.OddPhaseTex() : gauge.EvenPhaseTex();
+    dslashParam.longPhase1Tex = oddBit ? gauge.EvenPhaseTex() : gauge.OddPhaseTex();
+  }
 #else
   if (gauge.precision == QUDA_DOUBLE_PRECISION) {
     cudaBindTexture(0, longGauge0TexDouble, *gauge0, gauge.bytes/2); 
     cudaBindTexture(0, longGauge1TexDouble, *gauge1, gauge.bytes/2);
+    if(gauge.Reconstruct() == QUDA_RECONSTRUCT_13 || gauge.Reconstruct() == QUDA_RECONSTRUCT_9){
+      cudaBindTexture(0, longPhase0TexDouble, (char*)(*gauge0 + gauge.phase_offset), gauge.phase_bytes/2);
+      cudaBindTexture(0, longPhase1TexDouble, (char*)(*gauge1 + gauge.phase_offset), gauge.phase_bytes/2);
+    }
   } else if (gauge.precision == QUDA_SINGLE_PRECISION) {
     if (gauge.reconstruct == QUDA_RECONSTRUCT_NO) { //18 reconstruct
       cudaBindTexture(0, longGauge0TexSingle_norecon, *gauge0, gauge.bytes/2); 
@@ -260,6 +286,10 @@ void bindLongGaugeTex(const cudaGaugeField &gauge, const int oddBit, void **gaug
     } else {
       cudaBindTexture(0, longGauge0TexSingle, *gauge0, gauge.bytes/2); 
       cudaBindTexture(0, longGauge1TexSingle, *gauge1, gauge.bytes/2);
+      if(gauge.Reconstruct() == QUDA_RECONSTRUCT_13 || gauge.Reconstruct() == QUDA_RECONSTRUCT_9){
+        cudaBindTexture(0, longPhase0TexSingle, (char*)(*gauge0 + gauge.phase_offset), gauge.phase_bytes/2);
+        cudaBindTexture(0, longPhase1TexSingle, (char*)(*gauge1 + gauge.phase_offset), gauge.phase_bytes/2);
+      }
     }
   } else {
     if (gauge.reconstruct == QUDA_RECONSTRUCT_NO) { //18 reconstruct
@@ -268,6 +298,10 @@ void bindLongGaugeTex(const cudaGaugeField &gauge, const int oddBit, void **gaug
     } else {
       cudaBindTexture(0, longGauge0TexHalf, *gauge0, gauge.bytes/2); 
       cudaBindTexture(0, longGauge1TexHalf, *gauge1, gauge.bytes/2);
+      if(gauge.Reconstruct() == QUDA_RECONSTRUCT_13 || gauge.Reconstruct() == QUDA_RECONSTRUCT_9){
+        cudaBindTexture(0, longPhase0TexHalf, (char*)(*gauge0 + gauge.phase_offset), gauge.phase_bytes/2);
+        cudaBindTexture(0, longPhase1TexHalf, (char*)(*gauge1 + gauge.phase_offset), gauge.phase_bytes/2);
+      }
     }
   }
 #endif // USE_TEXTURE_OBJECTS
@@ -279,6 +313,10 @@ void unbindLongGaugeTex(const cudaGaugeField &gauge)
   if (gauge.precision == QUDA_DOUBLE_PRECISION) {
     cudaUnbindTexture(longGauge0TexDouble);
     cudaUnbindTexture(longGauge1TexDouble);
+    if(gauge.Reconstruct() == QUDA_RECONSTRUCT_13 || gauge.Reconstruct() == QUDA_RECONSTRUCT_9){
+      cudaUnbindTexture(longPhase0TexDouble);
+      cudaUnbindTexture(longPhase1TexDouble);
+    }
   } else if (gauge.precision == QUDA_SINGLE_PRECISION) {
     if (gauge.reconstruct == QUDA_RECONSTRUCT_NO) { //18 reconstruct
       cudaUnbindTexture(longGauge0TexSingle_norecon);
@@ -286,7 +324,10 @@ void unbindLongGaugeTex(const cudaGaugeField &gauge)
     } else {
       cudaUnbindTexture(longGauge0TexSingle);
       cudaUnbindTexture(longGauge1TexSingle);
-    }
+      if(gauge.Reconstruct() == QUDA_RECONSTRUCT_13 || gauge.Reconstruct() == QUDA_RECONSTRUCT_9){
+        cudaUnbindTexture(longPhase0TexSingle);
+        cudaUnbindTexture(longPhase1TexSingle);
+      }
   } else {
     if (gauge.reconstruct == QUDA_RECONSTRUCT_NO) { //18 reconstruct
       cudaUnbindTexture(longGauge0TexHalf_norecon);
@@ -294,6 +335,10 @@ void unbindLongGaugeTex(const cudaGaugeField &gauge)
     } else {
       cudaUnbindTexture(longGauge0TexHalf);
       cudaUnbindTexture(longGauge1TexHalf);
+      if(gauge.Reconstruct() == QUDA_RECONSTRUCT_13 || gauge.Reconstruct() ==  QUDA_RECONSTRUCT_9){
+        cudaUnbindTexture(longPhase0TexHalf);
+        cudaUnbindTexture(longPhase1TexHalf);
+      }
     }
   }
 #endif

include/enum_quda.h
@@ -54,6 +54,8 @@ extern "C" {
     QUDA_RECONSTRUCT_NO = 18, // store all 18 real numbers explicitly
     QUDA_RECONSTRUCT_12 = 12, // reconstruct from 12 real numbers
     QUDA_RECONSTRUCT_8 = 8,  // reconstruct from 8 real numbers
+    QUDA_RECONSTRUCT_9 = 9,   // used for storing HISQ long-link variables
+    QUDA_RECONSTRUCT_13 = 13, // used for storing HISQ long-link variables
     QUDA_RECONSTRUCT_10 = 10, // 10-number parameterization used for storing the momentum field
     QUDA_RECONSTRUCT_INVALID = QUDA_INVALID_ENUM
   } QudaReconstructType;

include/enum_quda_fortran.h
@@ -53,6 +53,8 @@
 #define QUDA_RECONSTRUCT_NO 18 // store all 18 real numbers explicitly
 #define QUDA_RECONSTRUCT_12 12 // reconstruct from 12 real numbers
 #define QUDA_RECONSTRUCT_8 8  // reconstruct from 8 real numbers
+#define QUDA_RECONSTRUCT_13 13 // used for storing HISQ long-link variables
+#define QUDA_RECONSTRUCT_9 9 // used for storing HISQ long-link variables
 #define QUDA_RECONSTRUCT_10 10 // 10-number parameterization used for storing the momentum field
 #define QUDA_RECONSTRUCT_INVALID QUDA_INVALID_ENUM
 

include/gauge_field.h
@@ -19,6 +19,7 @@ namespace quda {
 
     double anisotropy;
     double tadpole;
+    double scale;
 
     void *gauge; // used when we use a reference to an external field
 
@@ -38,6 +39,7 @@ namespace quda {
       t_boundary(QUDA_INVALID_T_BOUNDARY),
       anisotropy(1.0),
       tadpole(1.0),
+      scale(1.0),
       gauge(h_gauge),
       create(QUDA_REFERENCE_FIELD_CREATE), 
       geometry(QUDA_VECTOR_GEOMETRY),
@@ -55,7 +57,7 @@ namespace quda {
 		  const int pad, const QudaFieldGeometry geometry) : LatticeFieldParam(), nColor(3), nFace(0), 
       reconstruct(reconstruct), order(QUDA_INVALID_GAUGE_ORDER), fixed(QUDA_GAUGE_FIXED_NO), 
       link_type(QUDA_WILSON_LINKS), t_boundary(QUDA_INVALID_T_BOUNDARY), anisotropy(1.0), 
-      tadpole(1.0), gauge(0), create(QUDA_NULL_FIELD_CREATE), geometry(geometry), pinned(0)
+      tadpole(1.0), scale(1.0), gauge(0), create(QUDA_NULL_FIELD_CREATE), geometry(geometry), pinned(0)
       {
 	// variables declared in LatticeFieldParam
 	this->precision = precision;
@@ -68,7 +70,7 @@ namespace quda {
   GaugeFieldParam(void *h_gauge, const QudaGaugeParam &param) : LatticeFieldParam(param),
       nColor(3), nFace(0), reconstruct(QUDA_RECONSTRUCT_NO), order(param.gauge_order), 
       fixed(param.gauge_fix), link_type(param.type), t_boundary(param.t_boundary), 
-      anisotropy(param.anisotropy), tadpole(param.tadpole_coeff), gauge(h_gauge), 
+      anisotropy(param.anisotropy), tadpole(param.tadpole_coeff), scale(param.scale), gauge(h_gauge), 
       create(QUDA_REFERENCE_FIELD_CREATE), geometry(QUDA_VECTOR_GEOMETRY), pinned(0) {
 
       if (link_type == QUDA_WILSON_LINKS || link_type == QUDA_ASQTAD_FAT_LINKS) nFace = 1;
@@ -83,6 +85,8 @@ namespace quda {
 
   protected:
     size_t bytes; // bytes allocated per full field 
+    int phase_offset; // offset in bytes to gauge phases - useful to keep track of texture alignment
+    int phase_bytes;  // bytes needed to store the phases
     int length;
     int real_length;
     int nColor;
@@ -98,6 +102,7 @@ namespace quda {
     double anisotropy;
     double tadpole;
     double fat_link_max;
+    double scale;
     
     QudaFieldCreate create; // used to determine the type of field created
 
@@ -120,6 +125,7 @@ namespace quda {
     QudaGaugeFieldOrder Order() const { return order; }
     double Anisotropy() const { return anisotropy; }
     double Tadpole() const { return tadpole; }
+    double Scale() const { return scale; }  
     QudaTboundary TBoundary() const { return t_boundary; }
     QudaLinkType LinkType() const { return link_type; }
     QudaGaugeFixed GaugeFixed() const { return fixed; }
@@ -132,6 +138,7 @@ namespace quda {
     void checkField(const GaugeField &);
 
     const size_t& Bytes() const { return bytes; }
+    size_t PhaseOffset() const { return phase_offset; }
 
     virtual void* Gauge_p() { errorQuda("Not implemented"); return (void*)0;}
     virtual void* Even_p() { errorQuda("Not implemented"); return (void*)0;}
@@ -167,7 +174,9 @@ namespace quda {
 #ifdef USE_TEXTURE_OBJECTS
     cudaTextureObject_t evenTex;
     cudaTextureObject_t oddTex;
-    void createTexObject(cudaTextureObject_t &tex, void *gauge);
+    cudaTextureObject_t evenPhaseTex;
+    cudaTextureObject_t oddPhaseTex;
+    void createTexObject(cudaTextureObject_t &tex, void *gauge, int isPhase=0);
     void destroyTexObject();
 #endif
 
@@ -193,6 +202,8 @@ namespace quda {
 #ifdef USE_TEXTURE_OBJECTS
     const cudaTextureObject_t& EvenTex() const { return evenTex; }
     const cudaTextureObject_t& OddTex() const { return oddTex; }
+    const cudaTextureObject_t& EvenPhaseTex() const { return evenPhaseTex; }
+    const cudaTextureObject_t& OddPhaseTex() const { return oddPhaseTex; }
 #endif
 
     mutable char *backup_h;

include/quda.h
@@ -55,6 +55,7 @@ extern "C" {
 
     double anisotropy;    /**< Used for Wilson and Wilson-clover */
     double tadpole_coeff; /**< Used for staggered only */
+    double scale; /**< Used by staggered long links */
 
     QudaLinkType type; /**< The link type of the gauge field (e.g., Wilson, fat, long, etc.) */
     QudaGaugeFieldOrder gauge_order; /**< The ordering on the input gauge field */

include/register_traits.h
@@ -1,3 +1,6 @@
+#ifndef REGISTER_TRAITS_H
+#define REGISTER_TRAITS_H
+
 #include <quda_internal.h>
 
 namespace quda {
@@ -34,6 +37,9 @@ namespace quda {
       __device__ __host__ static T Sin( const T &a ) { return sin(a); }
       template<typename T> 
       __device__ __host__ static T Cos( const T &a ) { return cos(a); }
+
+      template<typename T>
+      __device__ __host__ static void SinCos(const T& a, T *s, T *c) { *s = sin(a); *c = cos(a); }
     };
   
   /**
@@ -49,4 +55,9 @@ namespace quda {
       __device__ __host__ static T Cos( const T &a ) { return cos(a*M_PI); }
   };
 
+  
+
+
 } // namespace quda
+
+#endif

include/texture.h
@@ -46,6 +46,9 @@ public:
 };
 
 #ifndef DIRECT_ACCESS_BLAS
+__device__ inline double fetch_double(int2 v)
+{ return __hiloint2double(v.y, v.x); }
+
 __device__ inline double2 fetch_double2(int4 v)
 { return make_double2(__hiloint2double(v.y, v.x), __hiloint2double(v.w, v.z)); }
 
@@ -61,12 +64,21 @@ template<> __device__ inline float2 Texture<float2,double2>::fetch(unsigned int 
 // legacy Texture references
 
 #if (__COMPUTE_CAPABILITY__ >= 130)
+
+  __inline__ __device__ double fetch_double(texture<int2, 1> t, int i)
+  {
+    int2 v = tex1Dfetch(t,i);
+    return __hiloint2double(v.y, v.x);
+  }
+
   __inline__ __device__ double2 fetch_double2(texture<int4, 1> t, int i)
   {
     int4 v = tex1Dfetch(t,i);
     return make_double2(__hiloint2double(v.y, v.x), __hiloint2double(v.w, v.z));
   }
 #else
+  __inline__ __device__ double fetch_double(texture<int2, 1> t, int i){ return 0.0; }
+
   __inline__ __device__ double2 fetch_double2(texture<int4, 1> t, int i)
   {
     // do nothing

commit 1caad6ed7e20b4081c00cd56b8700bd7f99e450e

Fixed compilation errors and warning on pre-Kepler architectures, introduced in commit cdc5907f90066258322613197bc7230b1213adc1.

lib/dslash_textures.h
@@ -276,8 +276,8 @@ void bindLongGaugeTex(const cudaGaugeField &gauge, const int oddBit, void **gaug
     cudaBindTexture(0, longGauge0TexDouble, *gauge0, gauge.bytes/2); 
     cudaBindTexture(0, longGauge1TexDouble, *gauge1, gauge.bytes/2);
     if(gauge.Reconstruct() == QUDA_RECONSTRUCT_13 || gauge.Reconstruct() == QUDA_RECONSTRUCT_9){
-      cudaBindTexture(0, longPhase0TexDouble, (char*)(*gauge0 + gauge.phase_offset), gauge.phase_bytes/2);
-      cudaBindTexture(0, longPhase1TexDouble, (char*)(*gauge1 + gauge.phase_offset), gauge.phase_bytes/2);
+      cudaBindTexture(0, longPhase0TexDouble, (char*)(*gauge0) + gauge.phase_offset, gauge.phase_bytes/2);
+      cudaBindTexture(0, longPhase1TexDouble, (char*)(*gauge1) + gauge.phase_offset, gauge.phase_bytes/2);
     }
   } else if (gauge.precision == QUDA_SINGLE_PRECISION) {
     if (gauge.reconstruct == QUDA_RECONSTRUCT_NO) { //18 reconstruct
@@ -287,8 +287,8 @@ void bindLongGaugeTex(const cudaGaugeField &gauge, const int oddBit, void **gaug
       cudaBindTexture(0, longGauge0TexSingle, *gauge0, gauge.bytes/2); 
       cudaBindTexture(0, longGauge1TexSingle, *gauge1, gauge.bytes/2);
       if(gauge.Reconstruct() == QUDA_RECONSTRUCT_13 || gauge.Reconstruct() == QUDA_RECONSTRUCT_9){
-        cudaBindTexture(0, longPhase0TexSingle, (char*)(*gauge0 + gauge.phase_offset), gauge.phase_bytes/2);
-        cudaBindTexture(0, longPhase1TexSingle, (char*)(*gauge1 + gauge.phase_offset), gauge.phase_bytes/2);
+        cudaBindTexture(0, longPhase0TexSingle, (char*)(*gauge0) + gauge.phase_offset, gauge.phase_bytes/2);
+        cudaBindTexture(0, longPhase1TexSingle, (char*)(*gauge1) + gauge.phase_offset, gauge.phase_bytes/2);
       }
     }
   } else {
@@ -299,8 +299,8 @@ void bindLongGaugeTex(const cudaGaugeField &gauge, const int oddBit, void **gaug
       cudaBindTexture(0, longGauge0TexHalf, *gauge0, gauge.bytes/2); 
       cudaBindTexture(0, longGauge1TexHalf, *gauge1, gauge.bytes/2);
       if(gauge.Reconstruct() == QUDA_RECONSTRUCT_13 || gauge.Reconstruct() == QUDA_RECONSTRUCT_9){
-        cudaBindTexture(0, longPhase0TexHalf, (char*)(*gauge0 + gauge.phase_offset), gauge.phase_bytes/2);
-        cudaBindTexture(0, longPhase1TexHalf, (char*)(*gauge1 + gauge.phase_offset), gauge.phase_bytes/2);
+        cudaBindTexture(0, longPhase0TexHalf, (char*)(*gauge0) + gauge.phase_offset, gauge.phase_bytes/2);
+	cudaBindTexture(0, longPhase1TexHalf, (char*)(*gauge1) + gauge.phase_offset, gauge.phase_bytes/2);
       }
     }
   }
@@ -328,7 +328,8 @@ void unbindLongGaugeTex(const cudaGaugeField &gauge)
         cudaUnbindTexture(longPhase0TexSingle);
         cudaUnbindTexture(longPhase1TexSingle);
       }
-  } else {
+    }
+  } else { // half precision
     if (gauge.reconstruct == QUDA_RECONSTRUCT_NO) { //18 reconstruct
       cudaUnbindTexture(longGauge0TexHalf_norecon);
       cudaUnbindTexture(longGauge1TexHalf_norecon);