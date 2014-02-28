6704e9bdddfd846cd595c0752184db4a08b3a2dd
M Clark <mclark@nvidia.com>
2014-02-27 21:02:03 -0800

    Removed trailing ,

--- include/enum_quda.h
+++ include/enum_quda.h
@@ -152,7 +152,7 @@ extern "C" {
 
   typedef enum QudaSolverNormalization_s {
     QUDA_DEFAULT_NORMALIZATION, // leave source and solution untouched
-    QUDA_SOURCE_NORMALIZATION, // normalize such that || src || = 1
+    QUDA_SOURCE_NORMALIZATION // normalize such that || src || = 1
   } QudaSolverNormalization;
 
   typedef enum QudaPreserveSource_s {
