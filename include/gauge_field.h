ad5f466463c7fcc42c90971c76da1ffa4fe3609e
Michael Cheng <micheng@bu.edu>
2014-03-19 01:06:22 -0400

    Fixed initialization of GaugeField.siteDim

--- include/gauge_field.h
+++ include/gauge_field.h
@@ -94,8 +94,10 @@ namespace quda {
       create(QUDA_REFERENCE_FIELD_CREATE), geometry(QUDA_VECTOR_GEOMETRY), pinned(0), 
       compute_fat_link_max(false), ghostExchange(QUDA_GHOST_EXCHANGE_PAD),
       staggeredPhaseType(param.staggered_phase_type), 
-      staggeredPhaseApplied(param.staggered_phase_applied), siteDim(param.nDim) 
+      staggeredPhaseApplied(param.staggered_phase_applied) 
       {
+	//Following is irrelevant for non-coarse gauge fields.
+	this->siteDim = this->nDim;
 	if (link_type == QUDA_WILSON_LINKS || link_type == QUDA_ASQTAD_FAT_LINKS) nFace = 1;
 	else if (link_type == QUDA_ASQTAD_LONG_LINKS) nFace = 3;
 	else errorQuda("Error: invalid link type(%d)\n", link_type);
