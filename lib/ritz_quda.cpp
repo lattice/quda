7e8ee93cc29e517b7094afc32438d38a415f1b2f
Hyung-Jin Kim <windy510@gmail.com>
2014-03-26 02:01:19 -0400

    data return bug fixed

---
+++ lib/ritz_quda.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <quda_internal.h>
+#include <color_spinor_field.h>
+#include <blas_quda.h>
+#include <dslash_quda.h>
+#include <invert_quda.h>
+#include <util_quda.h>
+#include <sys/time.h>
+#include <lanczos_quda.h>
+#include <ritz_quda.h>
+#include <face_quda.h>
+#include <iostream>
+
+namespace quda {
+
+  void RitzMat::operator()(cudaColorSpinorField &out, const cudaColorSpinorField &in) const
+  {
+    const double alpha = pow(cheby_param[0], 2);
+    const double beta  = pow(cheby_param[1]+fabs(shift), 2);
+
+    const double c1 = 2.0*(alpha+beta)/(alpha-beta); 
+    const double c0 = 2.0/(alpha+beta); 
+
+    bool reset1 = newTmp( &tmp1, in);
+    bool reset2 = newTmp( &tmp2, in);
+
+    *(tmp2) = in;
+    dirac_mat( *(tmp1), in);
+
+    axpbyCuda(-0.5*c1, const_cast<cudaColorSpinorField&>(in), 0.5*c0*c1, *(tmp1));
+    for(int i=2; i < N_Poly+1; ++i)
+    {
+      dirac_mat(out,*(tmp1));
+      axpbyCuda(-c1,*(tmp1),c0*c1,out);
+      axpyCuda(-1.0,*(tmp2),out);
+      //printfQuda("ritzMat: Ritz mat loop %d\n",i);
+
+      if(i != N_Poly)
+      {
+        // tmp2 = tmp
+        // tmp = out
+        cudaColorSpinorField *swap_Tmp = tmp2;
+        tmp2 = tmp1;
+        tmp1 = swap_Tmp;
+        *(tmp1) = out;
+      }
+    }
+    deleteTmp(&(tmp1), reset1);
+    deleteTmp(&(tmp2), reset2);
+
+  }
+  RitzMat::~RitzMat() {;}
+  bool RitzMat::newTmp(cudaColorSpinorField **tmp, const cudaColorSpinorField &a) const{
+    if (*tmp) return false;
+    ColorSpinorParam param(a);
+    param.create = QUDA_ZERO_FIELD_CREATE;
+    *tmp = new cudaColorSpinorField(a, param);
+    return true;
+  }
+
+  void RitzMat::deleteTmp(cudaColorSpinorField **a, const bool &reset) const{
+    if (reset) {
+      delete *a;
+      *a = NULL;
+    }
+  }
+}
