ebda7449f52b0b4b4d1593333c85ef7d21656664
M Clark <mclark@nvidia.com>
2014-06-24 11:31:08 -0400

    Updated quda version number to reflect the fact this will be the 0.7 branch.

--- include/quda.h
+++ include/quda.h
@@ -14,8 +14,8 @@
 #include <stdio.h> /* for FILE */
 
 #define QUDA_VERSION_MAJOR     0
-#define QUDA_VERSION_MINOR     6
-#define QUDA_VERSION_SUBMINOR  1
+#define QUDA_VERSION_MINOR     7
+#define QUDA_VERSION_SUBMINOR  0
 
 /**
  * @def   QUDA_VERSION
