a42fc615bceaedc1614de015201de68c434c30c1
nmrcardoso <nmrcardoso@gmail.com>
2015-06-01 14:07:00 -0500

    Update gauge_tools.h
    Added doxygen comments

--- include/gauge_tools.h
+++ include/gauge_tools.h
@@ -1,14 +1,36 @@
 namespace quda {
-  double	plaquette	(const GaugeField& data, QudaFieldLocation location);
-  void		APEStep		(GaugeField &dataDs, const GaugeField& dataOr, double alpha, QudaFieldLocation location);
+  double        plaquette       (const GaugeField& data, QudaFieldLocation location);
+  void          APEStep         (GaugeField &dataDs, const GaugeField& dataOr, double alpha, QudaFieldLocation location);
 
-  
+
+  /*
+  \brief Gauge fixing with overrelaxation with support for single and multi GPU.
+  \param[in,out] data, quda gauge field
+  \param[in] gauge_dir, 3 for Coulomb gauge fixing, other for Landau gauge fixing
+  \param[in] Nsteps, maximum number of steps to perform gauge fixing
+  \param[in] verbose_interval, print gauge fixing info when iteration count is a multiple of this
+  \param[in] relax_boost, gauge fixing parameter of the overrelaxation method, most common value is 1.5 or 1.7.
+  \param[in] tolerance, torelance value to stop the method, if this value is zero then the method stops when iteration reachs the maximum number of steps defined by Nsteps
+  \param[in] reunit_interval, reunitarize gauge field when iteration count is a multiple of this
+  \param[in] stopWtheta, 0 for MILC criterium and 1 to use the theta value
+  */
   void gaugefixingOVR( cudaGaugeField& data, const unsigned int gauge_dir, \
-    const unsigned int Nsteps, const unsigned int verbose_interval, const double relax_boost, \
-    const double tolerance, const unsigned int reunit_interval, const unsigned int stopWtheta);
+                       const unsigned int Nsteps, const unsigned int verbose_interval, const double relax_boost, \
+                       const double tolerance, const unsigned int reunit_interval, const unsigned int stopWtheta);
 
 
+  /*
+  \brief Gauge fixing with overrelaxation with support for single and multi GPU.
+  \param[in,out] data, quda gauge field
+  \param[in] gauge_dir, 3 for Coulomb gauge fixing, other for Landau gauge fixing
+  \param[in] Nsteps, maximum number of steps to perform gauge fixing
+  \param[in] verbose_interval, print gauge fixing info when iteration count is a multiple of this
+  \param[in] alpha, gauge fixing parameter of the method, most common value is 0.08
+  \param[in] autotune, 1 to autotune the method, i.e., if the Fg inverts its tendency we decrease the alpha value 
+  \param[in] tolerance, torelance value to stop the method, if this value is zero then the method stops when iteration reachs the maximum number of steps defined by Nsteps
+  \param[in] stopWtheta, 0 for MILC criterium and 1 to use the theta value
+  */
   void gaugefixingFFT( cudaGaugeField& data, const unsigned int gauge_dir, \
-    const unsigned int Nsteps, const unsigned int verbose_interval, const double alpha, const unsigned int autotune, \
-    const double tolerance, const unsigned int stopWtheta) ;
+                       const unsigned int Nsteps, const unsigned int verbose_interval, const double alpha, const unsigned int autotune, \
+                       const double tolerance, const unsigned int stopWtheta);
 }
