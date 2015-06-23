6d1e37bd5352ee46ba8929d1753f4f9373f83702
Mathias Wagner <mathwagn@indiana.edu>
2015-06-23 09:20:36 -0400

    adding first NVML support back into cmake build

--- lib/interface_quda.cpp
+++ lib/interface_quda.cpp
@@ -425,7 +425,7 @@ void initQudaDevice(int dev) {
 
 #ifdef NUMA_AFFINITY
   if(numa_affinity_enabled){
-#if (CUDA_VERSION >= 6000)
+#if ((CUDA_VERSION >= 6000) && defined NVML)
     setNumaAffinityNVML(dev);
 #else
     setNumaAffinity(dev);
