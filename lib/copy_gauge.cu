d6b389e56a4c9125f1c80c104bf477224e72bc84
M Clark <mclark@nvidia.com>
2015-09-25 11:06:56 -0700

    New MG specialization of copyGauge for copying gauge fields with arbitrary number of colors but only supports a small number of field orderings.

--- lib/copy_gauge.cu
+++ lib/copy_gauge.cu
@@ -11,6 +11,11 @@ namespace quda {
   void copyGenericGaugeSingleOut(GaugeField &out, const GaugeField &in, QudaFieldLocation location,
       void *Out, void *In, void **ghostOut, void **ghostIn, int type);
 
+  // specialized variation where we restrict different field orders supported but instantiate different colors
+  // this, as with all of the above are hacks until JIT is supported
+  void copyGenericGaugeMG(GaugeField &out, const GaugeField &in, QudaFieldLocation location,
+			  void *Out, void *In, void **ghostOut, void **ghostIn, int type);
+
   void checkMomOrder(const GaugeField &u) {
     if (u.Order() == QUDA_FLOAT2_GAUGE_ORDER) {
       if (u.Reconstruct() != QUDA_RECONSTRUCT_10)
@@ -28,12 +33,20 @@ namespace quda {
 
     // this is the function that is actually called, from here on down we instantiate all required templates
   void copyGenericGauge(GaugeField &out, const GaugeField &in, QudaFieldLocation location,
-      void *Out, void *In, void **ghostOut, void **ghostIn, int type) {
+			void *Out, void *In, void **ghostOut, void **ghostIn, int type) {
     // do not copy the ghost zone if it does not exist
     if (type == 0 && (in.GhostExchange() != QUDA_GHOST_EXCHANGE_PAD || 
           out.GhostExchange() != QUDA_GHOST_EXCHANGE_PAD)) type = 2;
 
-    if (out.Precision() == QUDA_DOUBLE_PRECISION) {
+    if (in.Ncolor() != out.Ncolor())
+      errorQuda("Colors (%d,%d) do not match", out.Ncolor(), in.Ncolor());
+
+    if (out.Geometry() != in.Geometry())
+      errorQuda("Field geometries %d %d do not match", out.Geometry(), in.Geometry());
+
+    if (in.Ncolor() != 3) {
+      copyGenericGaugeMG(out, in, location, Out, In, ghostOut, ghostIn, type);
+    } else if (out.Precision() == QUDA_DOUBLE_PRECISION) {
       copyGenericGaugeDoubleOut(out, in, location, Out, In, ghostOut, ghostIn, type);
     } else if (out.Precision() == QUDA_SINGLE_PRECISION) {
       copyGenericGaugeSingleOut(out, in, location, Out, In, ghostOut, ghostIn, type);
---
+++ lib/copy_gauge_mg.cu
@@ -0,0 +1,99 @@
+#include <gauge_field_order.h>
+#include <copy_gauge_helper.cuh>
+
+namespace quda {
+  
+  template <typename FloatOut, typename FloatIn, int length, typename InOrder>
+  void copyGauge(const InOrder &inOrder, GaugeField &out, QudaFieldLocation location, 
+		 FloatOut *Out, FloatOut **outGhost, int type) {
+    if (out.Reconstruct() != QUDA_RECONSTRUCT_NO) 
+      errorQuda("Reconstruct type %d not supported", out.Reconstruct());
+
+    int faceVolumeCB[QUDA_MAX_DIM];
+    for (int i=0; i<4; i++) faceVolumeCB[i] = out.SurfaceCB(i) * out.Nface(); 
+    if (out.isNative()) {
+      typedef typename gauge_mapper<FloatOut,QUDA_RECONSTRUCT_NO,length>::type G;
+      copyGauge<FloatOut,FloatIn,length>
+	(G(out,Out,outGhost), inOrder, out.Volume(), faceVolumeCB,
+	 out.Ndim(), out.Geometry(), out, location, type);
+    } else if (out.Order() == QUDA_QDP_GAUGE_ORDER) {
+
+#ifdef BUILD_QDP_INTERFACE
+      copyGauge<FloatOut,FloatIn,length>
+	(QDPOrder<FloatOut,length>(out, Out, outGhost), inOrder, out.Volume(), 
+	 faceVolumeCB, out.Ndim(), out.Geometry(), out, location, type);
+#else
+      errorQuda("QDP interface has not been built\n");
+#endif
+
+    } else {
+      errorQuda("Gauge field %d order not supported", out.Order());
+    }
+
+  }
+
+  template <typename FloatOut, typename FloatIn, int length>
+    void copyGauge(GaugeField &out, const GaugeField &in, QudaFieldLocation location, 
+		   FloatOut *Out, FloatIn *In, FloatOut **outGhost, FloatIn **inGhost, int type) {
+    if (in.Reconstruct() != QUDA_RECONSTRUCT_NO) 
+      errorQuda("Reconstruct type %d not supported", in.Reconstruct());
+
+    // reconstruction only supported on FloatN fields currently
+    if (in.isNative()) {      
+      typedef typename gauge_mapper<FloatIn,QUDA_RECONSTRUCT_NO>::type G;
+      copyGauge<FloatOut,FloatIn,length> (G(in,In,inGhost), out, location, Out, outGhost, type);
+    } else if (in.Order() == QUDA_QDP_GAUGE_ORDER) {
+
+#ifdef BUILD_QDP_INTERFACE
+      copyGauge<FloatOut,FloatIn,length>(QDPOrder<FloatIn,length>(in, In, inGhost), 
+					 out, location, Out, outGhost, type);
+#else
+      errorQuda("QDP interface has not been built\n");
+#endif
+
+    } else {
+      errorQuda("Gauge field %d order not supported", in.Order());
+    }
+
+  }
+
+  template <typename FloatOut, typename FloatIn>
+  void copyGaugeMG(GaugeField &out, const GaugeField &in, QudaFieldLocation location, FloatOut *Out, 
+		   FloatIn *In, FloatOut **outGhost, FloatIn **inGhost, int type) {
+
+    if (in.Ncolor() == 4) {
+      // we are doing gauge field packing
+      const int Nc = 4;
+      copyGauge<FloatOut,FloatIn,2*Nc*Nc>(out, in, location, Out, In, outGhost, inGhost, type);
+    } else {
+      errorQuda("Unsupported number of colors; out.Nc=%d, in.Nc=%d", out.Ncolor(), in.Ncolor());
+    }
+  }
+
+  // this is the function that is actually called, from here on down we instantiate all required templates
+  void copyGenericGaugeMG(GaugeField &out, const GaugeField &in, QudaFieldLocation location,
+			  void *Out, void *In, void **ghostOut, void **ghostIn, int type) {
+    if (out.Precision() == QUDA_DOUBLE_PRECISION) {
+      if (in.Precision() == QUDA_DOUBLE_PRECISION) {
+	copyGaugeMG(out, in, location, (double*)Out, (double*)In, (double**)ghostOut, (double**)ghostIn, type);
+      } else if (in.Precision() == QUDA_SINGLE_PRECISION) {
+	copyGaugeMG(out, in, location, (double*)Out, (float*)In, (double**)ghostOut, (float**)ghostIn, type);
+      } else {
+	errorQuda("Precision %d not supported", in.Precision());
+      }
+    } else if (out.Precision() == QUDA_SINGLE_PRECISION) {
+      if (in.Precision() == QUDA_DOUBLE_PRECISION) {
+	copyGaugeMG(out, in, location, (float*)Out, (double*)In, (float**)ghostOut, (double**)ghostIn, type);
+      } else if (in.Precision() == QUDA_SINGLE_PRECISION) {
+	copyGaugeMG(out, in, location, (float*)Out, (float*)In, (float**)ghostOut, (float**)ghostIn, type);
+      } else {
+	errorQuda("Precision %d not supported", in.Precision());
+      }
+    } else {
+      errorQuda("Precision %d not supported", out.Precision());
+    } 
+  } 
+
+
+
+} // namespace quda
