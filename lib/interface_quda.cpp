f9e2d5ea181ae879aaddd5917481b21ef521802f
Alejandro Vaquero <serianam@gmail.com>
2015-11-25 17:17:25 +0100

    Fixed plaqQuda returning wrong value with multiGPU
    Ghosts were not being exchanged.

--- lib/interface_quda.cpp
+++ lib/interface_quda.cpp
@@ -5457,10 +5457,10 @@ void plaqQuda (double plq[3])
   } else {
     int y[4];
     int R[4] = {2,2,2,2}; // radius of the extended region in each dimension / direction
-    for(int dir=0; dir<4; ++dir) y[dir] = gaugePrecise->X()[dir] + 4;
+    for(int dir=0; dir<4; ++dir) y[dir] = gaugePrecise->X()[dir] + 2*R[dir];
     int pad = 0;
     GaugeFieldParam gParamEx(y, gaugePrecise->Precision(), gaugePrecise->Reconstruct(),
-			     pad, QUDA_VECTOR_GEOMETRY, QUDA_GHOST_EXCHANGE_NO);
+			     pad, QUDA_VECTOR_GEOMETRY, QUDA_GHOST_EXCHANGE_EXTENDED);
     gParamEx.create = QUDA_ZERO_FIELD_CREATE;
     gParamEx.order = gaugePrecise->Order();
     gParamEx.siteSubset = QUDA_FULL_SITE_SUBSET;
