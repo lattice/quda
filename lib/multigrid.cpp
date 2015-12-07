lattice/quda

commit d005ca8c9cf0fa2660e7f5ba7efdb97910ef8028

Moved nullspace generation into the MG class (seems to be working but more testing required).  The multigridQuda interface function now essentially mirrors invertQuda so these can merged now.

lib/multigrid.cpp
@@ -17,6 +17,14 @@ namespace quda {
 
     printfQuda("Creating level %d of %d levels\n", param.level+1, param.Nlevel);
 
+    if (param.level == 0) { // null space generation only on level 1 currently
+      if (param.mg_global.compute_null_vector == QUDA_COMPUTE_NULL_VECTOR_YES) {
+	generateNullVectors(param.B);
+      } else {
+	loadVectors(param.B);
+      }
+    }
+
     if (param.level >= QUDA_MAX_MG_LEVEL)
       errorQuda("Level=%d is greater than limit of multigrid recursion depth", param.level+1);
 
@@ -341,7 +349,7 @@ namespace quda {
   }
 
   //supports seperate reading or single file read
-  void loadVectors(std::vector<ColorSpinorField*> &B) {
+  void MG::loadVectors(std::vector<ColorSpinorField*> &B) {
     const int Nvec = B.size();
     printfQuda("Start loading %d vectors from %s\n", Nvec, vec_infile);
 
@@ -399,7 +407,7 @@ namespace quda {
     printfQuda("Done loading vectors\n");
   }
 
-  void saveVectors(std::vector<ColorSpinorField*> &B) {
+  void MG::saveVectors(std::vector<ColorSpinorField*> &B) {
     if (strcmp(vec_outfile,"")!=0) {
       const int Nvec = B.size();
       printfQuda("Start saving %d vectors from %s\n", Nvec, vec_outfile);
@@ -429,6 +437,76 @@ namespace quda {
       printfQuda("Done saving vectors\n");
     }
 
+  }
+
+  void MG::generateNullVectors(std::vector<ColorSpinorField*> B) {
+    printfQuda("\nGenerate null vectors\n");
+    //Create spinor field parameters:
+
+    SolverParam solverParam(param);
+
+    // set null-space generation options - need to expose these
+    solverParam.maxiter = 500;
+    solverParam.tol = 5e-4;
+    solverParam.use_init_guess = QUDA_USE_INIT_GUESS_YES;
+    solverParam.delta = 1e-7;
+    solverParam.inv_type = QUDA_BICGSTAB_INVERTER;
+    // end setting null-space generation options
+
+    solverParam.residual_type = static_cast<QudaResidualType>(QUDA_L2_RELATIVE_RESIDUAL);
+    solverParam.compute_null_vector = QUDA_COMPUTE_NULL_VECTOR_YES;
+
+    ColorSpinorParam csParam(*B[0]);
+    csParam.fieldOrder = QUDA_FLOAT2_FIELD_ORDER;
+    csParam.setPrecision(csParam.precision); // set to native order
+    csParam.location = QUDA_CUDA_FIELD_LOCATION; // hard code to GPU location for null-space generation for now
+    csParam.gammaBasis = QUDA_UKQCD_GAMMA_BASIS;
+
+    // Generate sources and launch solver for each source:
+    for(std::vector<ColorSpinorField*>::iterator nullvec = B.begin() ; nullvec != B.end(); ++nullvec) {
+	cpuColorSpinorField *curr_nullvec = static_cast<cpuColorSpinorField*> (*nullvec);
+	curr_nullvec->Source(QUDA_RANDOM_SOURCE);//random initial guess
+
+	csParam.create = QUDA_ZERO_FIELD_CREATE;
+	ColorSpinorField *b = static_cast<ColorSpinorField*>(new cudaColorSpinorField(csParam));
+	//copy fields:
+	csParam.create = QUDA_COPY_FIELD_CREATE;
+	ColorSpinorField *x = static_cast<ColorSpinorField*>(new cudaColorSpinorField(*curr_nullvec, csParam));
+
+	if (getVerbosity() >= QUDA_VERBOSE) printfQuda("Prepared source = %g\n", blas::norm2(*x));
+
+	Solver *solve = Solver::create(solverParam, param.matResidual, param.matSmooth, param.matSmooth, profile);
+	(*solve)(*b, *x);
+	delete solve;
+
+	if (getVerbosity() >= QUDA_VERBOSE) printfQuda("Solution = %g\n", blas::norm2(*x));
+
+	*curr_nullvec = *x;
+
+	// global orthoonormalization of the generated null-space vectors
+	Complex alpha = Complex(0.0, 0.0);
+
+	for (std::vector<ColorSpinorField*>::iterator prevvec = B.begin(); prevvec != nullvec; ++prevvec)//row id
+	  {
+	    cpuColorSpinorField *prev_nullvec = static_cast<cpuColorSpinorField*> (*prevvec);
+
+	    alpha = blas::cDotProduct(*prev_nullvec, *curr_nullvec);//<j,i>
+	    Complex scale = Complex(-alpha.real(), -alpha.imag());
+	    blas::caxpy(scale, *prev_nullvec, *curr_nullvec); //i-<j,i>j
+	  }
+
+	alpha = blas::norm2(*curr_nullvec);
+	if (alpha.real() > 1e-16)  blas::ax(1.0 /sqrt(alpha.real()), *curr_nullvec);
+	else errorQuda("\nCannot orthogonalize %d vector\n", nullvec-B.begin());
+
+	delete b;
+	delete x;
+
+      }//stop for-loop:
+
+    saveVectors(B);
+
+    return;
   }
 
 }

include/multigrid.h
@@ -11,10 +11,6 @@ extern char vec_outfile[];
   
 namespace quda {
 
-  // FIXME - these definitions are strictly temporary
-  void loadVectors(std::vector<ColorSpinorField*> &B);
-  void saveVectors(std::vector<ColorSpinorField*> &B);
-
   // forward declarations
   class MG;
   class DiracCoarse;
@@ -31,7 +27,7 @@ namespace quda {
        This is top level instantiation done when we start creating the multigrid operator.
      */
     MGParam(const QudaMultigridParam &param, 
-	    std::vector<ColorSpinorField*> &B, 
+	    std::vector<ColorSpinorField*> &B,
 	    DiracMatrix &matResidual, 
 	    DiracMatrix &matSmooth,
 	    int level=0) :
@@ -54,7 +50,7 @@ namespace quda {
       }
 
     MGParam(const MGParam &param, 
-	    const std::vector<ColorSpinorField*> &B, 
+	    std::vector<ColorSpinorField*> &B,
 	    DiracMatrix &matResidual, 
 	    DiracMatrix &matSmooth,
 	    int level=0) :
@@ -104,7 +100,7 @@ namespace quda {
     MG *fine;
 
     /** The null space vectors */
-    const std::vector<ColorSpinorField*> &B;
+    std::vector<ColorSpinorField*> &B;
 
     /** Number of pre-smoothing applications to perform */
     int nu_pre;
@@ -207,6 +203,25 @@ namespace quda {
        @param in The residual vector (or equivalently the right hand side vector)
      */
     void operator()(ColorSpinorField &out, ColorSpinorField &in);
+
+    /**
+       Load the null space vectors in from file
+       @param B Loaded null-space vectors (pre-allocated)
+    */
+    void loadVectors(std::vector<ColorSpinorField*> &B);
+
+    /**
+       Save the null space vectors in from file
+       @param B Save null-space vectors from here
+    */
+    void saveVectors(std::vector<ColorSpinorField*> &B);
+
+    /**
+       Generate the null-space vectors
+       @param B Generated null-space vectors
+     */
+    void generateNullVectors(std::vector<ColorSpinorField*> B);
+
   };
 
   void CoarseOp(const Transfer &T, GaugeField &Y, GaugeField &X, QudaPrecision precision, const cudaGaugeField &gauge);

lib/interface_quda.cpp
@@ -2397,209 +2397,6 @@ void invertQuda(void *hp_x, void *hp_b, QudaInvertParam *param)
   profileInvert.TPSTOP(QUDA_PROFILE_TOTAL);
 }
 
-void generateNullVectors(std::vector<ColorSpinorField*> B, QudaInvertParam *mg_inv_param)//input/output => cpu fields!
-{
-   printfQuda("\nGenerate null vectors\n");
-   //Create spinor field parameters:
-
-   ColorSpinorParam csParam(*B[0]);//
-   csParam.create = QUDA_NULL_FIELD_CREATE;
-
-   csParam.setPrecision(csParam.precision);
-   csParam.location = QUDA_CUDA_FIELD_LOCATION;
-   csParam.gammaBasis = QUDA_UKQCD_GAMMA_BASIS;//UKQCD
-
-   //New invert parameter for the null vector generations:
-   QudaInvertParam inv_param = newQudaInvertParam();
-
-   inv_param.dslash_type = mg_inv_param->dslash_type;
-
-   inv_param.matpc_type  = QUDA_MATPC_EVEN_EVEN;
-   inv_param.verbosity   = QUDA_VERBOSE;
-
-   inv_param.gcrNkrylov  = 10;
-   inv_param.maxiter  = 500;
-   inv_param.tol      = 5e-4;
-
-   inv_param.use_init_guess      = QUDA_USE_INIT_GUESS_YES;
-   // removed this from QudaInvertParam so set this by hand below for now
-   //inv_param.compute_null_vector = QUDA_COMPUTE_NULL_VECTOR_YES;
-
-   inv_param.preserve_source = QUDA_PRESERVE_SOURCE_NO;
-   inv_param.reliable_delta    = 1e-7;
-
-   inv_param.inv_type      = QUDA_BICGSTAB_INVERTER;//use bicgstab solver for null vector generation
-   inv_param.solve_type    = QUDA_DIRECT_SOLVE;
-   inv_param.solution_type = QUDA_MAT_SOLUTION;
-
-   inv_param.dagger = QUDA_DAG_NO;
-   inv_param.mass_normalization   = QUDA_KAPPA_NORMALIZATION;
-   inv_param.solver_normalization = QUDA_DEFAULT_NORMALIZATION;
-   //
-   inv_param.residual_type = static_cast<QudaResidualType>(QUDA_L2_RELATIVE_RESIDUAL);
-   inv_param.tol_hq = 1e-3; // specify a tolerance for the residual for heavy quark residual
-
-   inv_param.inv_type_precondition = QUDA_INVALID_INVERTER;
-
-   inv_param.schwarz_type = QUDA_ADDITIVE_SCHWARZ;
-   inv_param.precondition_cycle = 1;
-   inv_param.tol_precondition = 1e-1;
-   inv_param.maxiter_precondition = 10;
-   inv_param.verbosity_precondition = QUDA_SILENT;
-   inv_param.cuda_prec_precondition = QUDA_INVALID_PRECISION;
-   inv_param.omega = 1.0;
-
-   inv_param.cpu_prec = csParam.precision;
-   inv_param.cuda_prec = csParam.precision;
-   inv_param.cuda_prec_sloppy = csParam.precision;
-   inv_param.gamma_basis = QUDA_UKQCD_GAMMA_BASIS;
-   inv_param.dirac_order = QUDA_DIRAC_ORDER;
-
-   inv_param.input_location = QUDA_CPU_FIELD_LOCATION;
-   inv_param.output_location = QUDA_CPU_FIELD_LOCATION;
-
-   inv_param.tune = getTuning();
-
-   inv_param.sp_pad = 0; // 24*24*24/2;
-   inv_param.cl_pad = 0; // 24*24*24/2;
-
-   inv_param.kappa   = mg_inv_param->kappa;//1.0 / (2.0 * (1 + 3/anisotropy + mass));
-
-   pushVerbosity(inv_param.verbosity);
-
-   // check the gauge fields have been created
-   //cudaGaugeField *cudaGauge = checkGauge(&inv_param);
-   //WARNING!!! :  new checkGauge routine cannot be used here. 
-   //We assume that gauge field check was done in multigridQuda, and just assign pointers. 
-   //This must be fix in the future. 
-   cudaGaugeField *cudaGauge = (mg_inv_param->dslash_type != QUDA_STAGGERED_DSLASH) ? gaugePrecise : gaugeFatPrecise;
-  
-   checkInvertParam(&inv_param);
-
-   // It was probably a bad design decision to encode whether the system is even/odd preconditioned (PC) in
-   // solve_type and solution_type, rather than in separate members of QudaInvertParam.  We're stuck with it
-   // for now, though, so here we factorize everything for convenience.
-
-//   bool pc_solution = false;
-   bool pc_solve    = false;
-//   bool mat_solution = true;
-//   bool direct_solve = true;
-
-   inv_param.spinorGiB = cudaGauge->VolumeCB() * spinorSiteSize;
-   if (!pc_solve) inv_param.spinorGiB *= 2;
-   inv_param.spinorGiB *= (inv_param.cuda_prec == QUDA_DOUBLE_PRECISION ? sizeof(double) : sizeof(float));
-   if (inv_param.preserve_source == QUDA_PRESERVE_SOURCE_NO) {
-     inv_param.spinorGiB *= 7.0 /(double)(1<<30);
-   } else {
-     inv_param.spinorGiB *= 9.0 /(double)(1<<30);
-   }
-
-   inv_param.secs   = 0;
-   inv_param.gflops = 0;
-   inv_param.iter   = 0;
-
-   Dirac *d        = NULL;
-   Dirac *dSloppy  = NULL;
-   Dirac *dPre     = NULL;
-
-   // create the dirac operator: in fact, solo-precision defined by b[0]->Precision().
-   createDirac(d, dSloppy, dPre, inv_param, pc_solve);
-
-   Dirac &dirac       = *d;
-   Dirac &diracSloppy = *dSloppy;
-   Dirac &diracPre    = *dPre;
-
-   ColorSpinorField *b = NULL;
-   ColorSpinorField *x = NULL;
-   ColorSpinorField *in = NULL;
-   ColorSpinorField *out = NULL;
-
-   ColorSpinorParam bParam(csParam, inv_param);
-   bParam.create = QUDA_ZERO_FIELD_CREATE;
-
-   ColorSpinorParam xParam(csParam, inv_param);
-   xParam.create     = QUDA_COPY_FIELD_CREATE;
-
-   //profileInvert.TPSTART(QUDA_PROFILE_TOTAL);
-
-   // Generate sources and launch bicgstab for each source:
-   for(std::vector<ColorSpinorField*>::iterator nullvec = B.begin() ; nullvec != B.end(); ++nullvec)
-   {
-     cpuColorSpinorField *curr_nullvec = static_cast<cpuColorSpinorField*> (*nullvec); 
-     curr_nullvec->Source(QUDA_RANDOM_SOURCE);//random initial guess
-
-     b = static_cast<ColorSpinorField*>(new cudaColorSpinorField(bParam)); 
-     //copy fields:
-     x = static_cast<ColorSpinorField*>(new cudaColorSpinorField(*curr_nullvec, xParam)); 
-
-     setTuning(inv_param.tune);
-
-     dirac.prepare(in, out, *x, *b, inv_param.solution_type);
-
-     if (getVerbosity() >= QUDA_VERBOSE) 
-     {
-       double nin = blas::norm2(*out);
-       printfQuda("Prepared source = %g\n", nin);      
-     }
-
-
-     DiracM m(dirac), mSloppy(diracSloppy), mPre(diracPre);
-     SolverParam solverParam(inv_param);
-     solverParam.compute_null_vector = QUDA_COMPUTE_NULL_VECTOR_YES;
-     Solver *solve = Solver::create(solverParam, mSloppy, mSloppy, mPre, profileInvert);
-     (*solve)(*out, *in);
-     solverParam.updateInvertParam(inv_param);
-     delete solve;
-
-     if (getVerbosity() >= QUDA_VERBOSE)
-     {
-       double nx = blas::norm2(*x);
-       printfQuda("Solution = %g\n",nx);
-     }
-     dirac.reconstruct(*x, *b, inv_param.solution_type);
-
-     *curr_nullvec = *x;
-
-     // global orthoonormalization of the generated null-space vectors
-     Complex alpha = Complex(0.0, 0.0);
-
-     for (std::vector<ColorSpinorField*>::iterator prevvec = B.begin(); prevvec != nullvec; ++prevvec)//row id
-     {
-        cpuColorSpinorField *prev_nullvec = static_cast<cpuColorSpinorField*> (*prevvec);
-
-        alpha = blas::cDotProduct(*prev_nullvec, *curr_nullvec);//<j,i>
-        Complex scale = Complex(-alpha.real(), -alpha.imag());
-        blas::caxpy(scale, *prev_nullvec, *curr_nullvec); //i-<j,i>j
-     }
-
-     alpha = blas::norm2(*curr_nullvec);
-     if(alpha.real() > 1e-16)
-        blas::ax(1.0 /sqrt(alpha.real()), *curr_nullvec);  
-     else
-        errorQuda("\nCannot orthogonalize ??th vector\n");
-
-     delete b;
-     delete x;
-
-  }//stop for-loop:
-
-  delete d;
-  delete dSloppy;
-  delete dPre;
-
-  popVerbosity();
-
-  saveVectors(B);
-
-  // FIXME: added temporarily so that the cache is written out even if a long benchmarking job gets interrupted
-  saveTuneCache(getVerbosity());
-
-  //profileInvert.TPSTOP(QUDA_PROFILE_TOTAL);
-
-  return;
-}
-
-
 void multigridQuda(void *hp_x, void *hp_b, QudaMultigridParam *mg_param)
 {
   QudaInvertParam *param = mg_param->invert_param;
@@ -2778,20 +2575,11 @@ void multigridQuda(void *hp_x, void *hp_b, QudaMultigridParam *mg_param)
     B[i] = new cpuColorSpinorField(cpuParam);
   }
 
-  if(mg_param->compute_null_vector == QUDA_COMPUTE_NULL_VECTOR_YES)
-  {
-     generateNullVectors(B, param);
-  }
-  else
-  {
-     loadVectors(B);
-  }
-
   // fill out the MG parameters for the fine level
-  MGParam mgParam(*mg_param, B, mSloppy, mSloppy);
+  MGParam *mgParam = new MGParam(*mg_param, B, mSloppy, mSloppy);
 
   // create the MG preconditioner
-  Solver *K = new MG(mgParam, profileInvert);
+  Solver *K = new MG(*mgParam, profileInvert);
 
   profileInvert.TPSTOP(QUDA_PROFILE_INIT);
 
@@ -2803,6 +2591,7 @@ void multigridQuda(void *hp_x, void *hp_b, QudaMultigridParam *mg_param)
   solverParam.updateInvertParam(*param);
 
   delete K;
+  delete mgParam;
   for (int i=0; i<mg_param->n_vec[0]; i++) delete B[i];
 
 

commit 4a953e22ae7fbf73b470ffb9cdbdfe8ffaf002a3

Fixed bug in dynamic null-space generation introduced in d005ca8c9cf0fa2660e7f5ba7efdb97910ef8028

lib/multigrid.cpp
@@ -488,7 +488,7 @@ namespace quda {
 	if (getVerbosity() >= QUDA_VERBOSE) printfQuda("Initial guess = %g\n", blas::norm2(*x));
 
 	Solver *solve = Solver::create(solverParam, param.matResidual, param.matSmooth, param.matSmooth, profile);
-	(*solve)(*b, *x);
+	(*solve)(*x, *b);
 	delete solve;
 
 	if (getVerbosity() >= QUDA_VERBOSE) printfQuda("Solution = %g\n", blas::norm2(*x));