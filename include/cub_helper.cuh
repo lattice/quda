97e3ad379f4df5050b410aefc4b44933c8e9c730
maddyscientist <mclark@nvidia.com>
2016-02-08 16:50:51 -0800

    reduce2d now can perform multiple indepdent reductions in a single kernel, where the number of reductions corresponds to gridDim.y.  Added reduceRow, which gives the the ability to do multiple indepdent reductions per thread block, with blockDim.y being the number of reductions.

--- include/cub_helper.cuh
+++ include/cub_helper.cuh
@@ -68,7 +68,7 @@ namespace quda {
   __device__ __host__ inline void zero(doubledouble3 &x) { zero(x.x); zero(x.y); zero(x.z); }
 #endif
 
-  __device__ unsigned int count = 0;
+  __device__ unsigned int count[QUDA_MAX_MULTI_REDUCE] = { };
   __shared__ bool isLastBlockDone;
 
   template <int block_size_x, int block_size_y, typename T>
@@ -77,15 +77,14 @@ namespace quda {
     typedef cub::BlockReduce<T, block_size_x, cub::BLOCK_REDUCE_WARP_REDUCTIONS, block_size_y> BlockReduce;
     __shared__ typename BlockReduce::TempStorage cub_tmp;
 
-    //T aggregate = BlockReduce(cub_tmp).Reduce(in, Summ<T>());
     T aggregate = BlockReduce(cub_tmp).Sum(in);
 
     if (threadIdx.x == 0 && threadIdx.y == 0) {
-      arg.partial[blockIdx.x] = aggregate;
+      arg.partial[idx*gridDim.x + blockIdx.x] = aggregate;
       __threadfence(); // flush result
 
       // increment global block counter
-      unsigned int value = atomicInc(&count, gridDim.x);
+      unsigned int value = atomicInc(&count[idx], gridDim.x);
 
       // determine if last block
       isLastBlockDone = (value == (gridDim.x-1));
@@ -99,24 +98,116 @@ namespace quda {
       T sum;
       zero(sum);
       while (i<gridDim.x) {
-	sum += arg.partial[i];
+	sum += arg.partial[idx*gridDim.x + i];
 	i += block_size_x*block_size_y;
       }
 
-      //sum = BlockReduce(cub_tmp).Reduce(sum, Summ<T>());
       sum = BlockReduce(cub_tmp).Sum(sum);
 
       // write out the final reduced value
       if (threadIdx.y*block_size_x + threadIdx.x == 0) {
 	arg.result_d[idx] = sum;
-	count = 0; // set to zero for next time
+	count[idx] = 0; // set to zero for next time
       }
     }
   }
 
   template <int block_size, typename T>
-  __device__ inline void reduce(ReduceArg<T> arg, const T &in, const int idx=0) {
-    reduce2d<block_size, 1, T>(arg, in, idx);
+  __device__ inline void reduce(ReduceArg<T> arg, const T &in, const int idx=0) { reduce2d<block_size, 1, T>(arg, in, idx); }
+
+
+  /**
+     struct which acts as a wrapper to a vector of data.
+   */
+  template <typename scalar, int n>
+  struct vector_type {
+    scalar data[n];
+    __device__ __host__ inline scalar& operator[](int i) { return data[i]; }
+    __device__ __host__ inline const scalar& operator[](int i) const { return data[i]; }
+    __device__ __host__ inline static constexpr int size() { return n; }
+    __device__ __host__ inline void operator+=(const vector_type &a) { for (int i=0; i<n; i++) data[i] += a[i]; }
+    __device__ __host__ vector_type() { for (int i=0; i<n; i++) zero(data[i]); }
+  };
+
+  /**
+     functor that defines how to do a row-wise vector reduction
+   */
+  template <typename T>
+  struct reduce_vector {
+    __device__ __host__ inline T operator()(const T &a, const T &b) {
+      T sum;
+      for (int i=0; i<sum.size(); i++) sum[i] = a[i] + b[i];
+      return sum;
+    }
+  };
+
+  template <int block_size_x, int block_size_y, typename T>
+  __device__ inline void reduceRow(ReduceArg<T> arg, const T &in) {
+
+    typedef vector_type<T,block_size_y> vector;
+    typedef cub::BlockReduce<vector, block_size_x, cub::BLOCK_REDUCE_WARP_REDUCTIONS, block_size_y> BlockReduce;
+    constexpr int n_word = sizeof(T) / sizeof(int);
+
+    __shared__ union {
+      typename BlockReduce::TempStorage cub;
+      int exchange[n_word*block_size_x*block_size_y];
+    } shared;
+
+    // first move all data at y>0 to y=0 slice and pack in a vector of length block_size_y
+    if (threadIdx.y > 0) {
+      for (int i=0; i<n_word; i++)
+	shared.exchange[(i * block_size_y + threadIdx.y)*block_size_x + threadIdx.x] = reinterpret_cast<const int*>(&in)[i];
+    }
+
+    __syncthreads();
+
+    vector data;
+
+    if (threadIdx.y == 0) {
+      data[0] = in;
+      for (int y=1; y<block_size_y; y++)
+	for (int i=0; i<n_word; i++)
+	  reinterpret_cast<int*>(&data[y])[i] = shared.exchange[(i * block_size_y + y)*block_size_x + threadIdx.x];
+    }
+
+    __syncthreads();
+
+    reduce_vector<vector> reducer;
+
+    vector aggregate = BlockReduce(shared.cub).Reduce(data, reducer, block_size_x);
+
+    if (threadIdx.x == 0 && threadIdx.y == 0) {
+      reinterpret_cast<vector*>(arg.partial)[blockIdx.x] = aggregate;
+      __threadfence(); // flush result
+
+      // increment global block counter
+      unsigned int value = atomicInc(&count[0], gridDim.x);
+
+      // determine if last block
+      isLastBlockDone = (value == (gridDim.x-1));
+    }
+
+    __syncthreads();
+
+    // finish the reduction if last block
+    if (isLastBlockDone) {
+      vector sum;
+      if (threadIdx.y == 0) { // only use x-row to do final reduction since we've only allocated space for this
+	unsigned int i = threadIdx.x;
+	while (i < gridDim.x) {
+	  sum += reinterpret_cast<vector*>(arg.partial)[i];
+	  i += block_size_x;
+	}
+      }
+
+      sum = BlockReduce(shared.cub).Reduce(sum, reducer, block_size_x);
+
+      // write out the final reduced value
+      if (threadIdx.y*block_size_x + threadIdx.x == 0) {
+	reinterpret_cast<vector*>(arg.result_d)[0] = sum;
+	count[0] = 0; // set to zero for next time
+      }
+    }
   }
 
 } // namespace quda
