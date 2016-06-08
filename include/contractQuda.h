43761c88cb9f150251e55b7c82a22b5406d5ed58
Alejandro Vaquero <serianam@gmail.com>
2016-06-08 11:03:31 +0200

    Contraction code and covariant derivative: headers
    Changed cudaColorSpinorField references to ColorSpinorField.

--- include/contractQuda.h
+++ include/contractQuda.h
@@ -5,9 +5,9 @@
 #include <quda.h>
 
 namespace quda {
-  void contractCuda(const cudaColorSpinorField &x, const cudaColorSpinorField &y, void *result, const QudaContractType contract_type, const QudaParity parity, TimeProfile &profile);
-  void contractCuda(const cudaColorSpinorField &x, const cudaColorSpinorField &y, void *result, const QudaContractType contract_type, const int tSlice, const QudaParity parity, TimeProfile &profile);
-  void covDev(cudaColorSpinorField *out, cudaGaugeField &gauge, const cudaColorSpinorField *in, const int parity, const int mu, TimeProfile &profile);
+  void contractCuda(const ColorSpinorField &x, const ColorSpinorField &y, void *result, const QudaContractType contract_type, const QudaParity parity, TimeProfile &profile);
+  void contractCuda(const ColorSpinorField &x, const ColorSpinorField &y, void *result, const QudaContractType contract_type, const int tSlice, const QudaParity parity, TimeProfile &profile);
+  void covDev(ColorSpinorField *out, cudaGaugeField &gauge, const ColorSpinorField *in, const int parity, const int mu, TimeProfile &profile);
 
   class CovD {
     protected:
@@ -25,12 +25,12 @@ namespace quda {
       ~CovD();
       CovD& operator=(const CovD &cov);
   
-      void checkFullSpinor(const cudaColorSpinorField &, const cudaColorSpinorField &) const;
-      void checkSpinorAlias(const cudaColorSpinorField &, const cudaColorSpinorField &) const;
-      void checkParitySpinor(const cudaColorSpinorField &out, const cudaColorSpinorField &in) const;
+      void checkFullSpinor(const ColorSpinorField &, const ColorSpinorField &) const;
+      void checkSpinorAlias(const ColorSpinorField &, const ColorSpinorField &) const;
+      void checkParitySpinor(const ColorSpinorField &out, const ColorSpinorField &in) const;
   
-      void Apply(cudaColorSpinorField &out, const cudaColorSpinorField &in, const QudaParity parity, const int mu);
-      void M(cudaColorSpinorField &out, const cudaColorSpinorField &in, const int mu);
+      void Apply(ColorSpinorField &out, const ColorSpinorField &in, const QudaParity parity, const int mu);
+      void M(ColorSpinorField &out, const ColorSpinorField &in, const int mu);
   
       unsigned long long Flops();
   };
