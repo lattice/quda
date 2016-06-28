lattice/quda

commit 38fda505cc1aec25c9c351ad8a093a5c2fcf8f0b

Added chiral-block-level accessor for clover::FloatNOrder to try to give the compiler an easier time with register allocation.  Applied this to clover creation and inversion.

lib/clover_invert.cu
@@ -32,13 +32,12 @@ namespace quda {
   template <int blockSize, typename Float, typename Clover, bool computeTrLog, bool twist>
   __device__ __host__ inline double cloverInvertCompute(CloverInvertArg<Clover> arg, int x, int parity) {
 
-    Float A[72];
-    double trlogA = 0.0; 
-
-    // load the clover term into memory
-    arg.clover.load(A, x, parity);
+    double trlogA = 0.0;
 
     for (int ch=0; ch<2; ch++) {
+      Float A[36];
+      // load the clover term into memory
+      arg.clover.load(A, x, parity, ch);
 
       Float diag[6];
       Float tmp[6]; // temporary storage
@@ -51,6 +50,7 @@ namespace quda {
       for (int i=0; i<6; i++) diag[i] = two*A[ch*36+i];
 
       const int idtab[15]={0,1,3,6,10,2,4,7,11,5,8,12,9,13,14};
+#pragma unroll
       for (int i=0; i<15; i++) tri[idtab[i]] = complex<Float>(two*A[ch*36+6+2*i], two*A[ch*36+6+2*i+1]);
 
       //Compute (T^2 + mu2) first, then invert (not optimized!):
@@ -60,37 +60,21 @@ namespace quda {
          //the global buffer):
          //complex<Float>* aux = (complex<Float>*)&A[ch*36];
          //compute off-diagonal terms:
-//
          aux[ 0] = tri[0]*diag[0]+diag[1]*tri[0]+conj(tri[2])*tri[1]+conj(tri[4])*tri[3]+conj(tri[7])*tri[6]+conj(tri[11])*tri[10];
-//
          aux[ 1] = tri[1]*diag[0]+diag[2]*tri[1]+tri[2]*tri[0]+conj(tri[5])*tri[3]+conj(tri[8])*tri[6]+conj(tri[12])*tri[10];
-
          aux[ 2] = tri[2]*diag[1]+diag[2]*tri[2]+tri[1]*conj(tri[0])+conj(tri[5])*tri[4]+conj(tri[8])*tri[7]+conj(tri[12])*tri[11];
-//
          aux[ 3] = tri[3]*diag[0]+diag[3]*tri[3]+tri[4]*tri[0]+tri[5]*tri[1]+conj(tri[9])*tri[6]+conj(tri[13])*tri[10];
-
          aux[ 4] = tri[4]*diag[1]+diag[3]*tri[4]+tri[3]*conj(tri[0])+tri[5]*tri[2]+conj(tri[9])*tri[7]+conj(tri[13])*tri[11];
-
          aux[ 5] = tri[5]*diag[2]+diag[3]*tri[5]+tri[3]*conj(tri[1])+tri[4]*conj(tri[2])+conj(tri[9])*tri[8]+conj(tri[13])*tri[12];
-//
          aux[ 6] = tri[6]*diag[0]+diag[4]*tri[6]+tri[7]*tri[0]+tri[8]*tri[1]+tri[9]*tri[3]+conj(tri[14])*tri[10];
-
          aux[ 7] = tri[7]*diag[1]+diag[4]*tri[7]+tri[6]*conj(tri[0])+tri[8]*tri[2]+tri[9]*tri[4]+conj(tri[14])*tri[11];
-
          aux[ 8] = tri[8]*diag[2]+diag[4]*tri[8]+tri[6]*conj(tri[1])+tri[7]*conj(tri[2])+tri[9]*tri[5]+conj(tri[14])*tri[12];
-
          aux[ 9] = tri[9]*diag[3]+diag[4]*tri[9]+tri[6]*conj(tri[3])+tri[7]*conj(tri[4])+tri[8]*conj(tri[5])+conj(tri[14])*tri[13];
-//
          aux[10] = tri[10]*diag[0]+diag[5]*tri[10]+tri[11]*tri[0]+tri[12]*tri[1]+tri[13]*tri[3]+tri[14]*tri[6];
-
          aux[11] = tri[11]*diag[1]+diag[5]*tri[11]+tri[10]*conj(tri[0])+tri[12]*tri[2]+tri[13]*tri[4]+tri[14]*tri[7];
-
          aux[12] = tri[12]*diag[2]+diag[5]*tri[12]+tri[10]*conj(tri[1])+tri[11]*conj(tri[2])+tri[13]*tri[5]+tri[14]*tri[8];
-
          aux[13] = tri[13]*diag[3]+diag[5]*tri[13]+tri[10]*conj(tri[3])+tri[11]*conj(tri[4])+tri[12]*conj(tri[5])+tri[14]*tri[9];
-
          aux[14] = tri[14]*diag[4]+diag[5]*tri[14]+tri[10]*conj(tri[6])+tri[11]*conj(tri[7])+tri[12]*conj(tri[8])+tri[13]*conj(tri[9]);
-
 
          //update diagonal elements:
          diag[0] = (Float)arg.mu2+diag[0]*diag[0]+norm(tri[ 0])+norm(tri[ 1])+norm(tri[ 3])+norm(tri[ 6])+norm(tri[10]);
@@ -164,13 +148,12 @@ namespace quda {
 
       constexpr Float half = static_cast<Float>(0.5);
       for (int i=0; i<6; i++) A[ch*36+i] = half * diag[i];
-      for (int i=0; i<15; i++) {
-	A[ch*36+6+2*i] = half*tri[idtab[i]].real(); A[ch*36+6+2*i+1] = half*tri[idtab[i]].imag();
-      }
-    }	     
+#pragma unroll
+      for (int i=0; i<15; i++) { A[ch*36+6+2*i] = half*tri[idtab[i]].real(); A[ch*36+6+2*i+1] = half*tri[idtab[i]].imag(); }
 
-    // save the inverted matrix
-    arg.inverse.save(A, x, parity);
+      // save the inverted matrix
+      arg.inverse.save(A, x, parity, ch);
+    }
 
     return trlogA;
   }

include/clover_field_order.h
@@ -253,6 +253,8 @@ namespace quda {
       struct FloatNOrder {
 	typedef typename mapper<Float>::type RegType;
 	typedef typename VectorType<Float,N>::type Vector;
+	static const int M=length/(N*2); // number of short vectors per chiral block
+	static const int block=length/2; // chiral block size
 
 	Float *clover;
 	float *norm;
@@ -282,57 +284,82 @@ namespace quda {
 	bool  Twisted()	const	{return twisted;}
 	Float Mu2()	const	{return mu2;}
 	
-	__device__ __host__ inline void load(RegType v[length], int x, int parity) const {
-	  const int M = length/(N*2);
+	/**
+	   @brief Load accessor for a single chiral block
+	   @param[out] v Vector of loaded elements
+	   @param[in] x Checkerboarded site index
+	   @param[in] parity Field parity
+	   @param[in] chirality Chiral block index
+	 */
+	__device__ __host__ inline void load(RegType v[block], int x, int parity, int chirality) const {
 #pragma unroll
-	  for (int chirality=0; chirality<2; chirality++) {
+	  for (int i=0; i<M; i++) {
+	    // first do vectorized copy from memory
+	    Vector vecTmp = vector_load<Vector>(clover + parity*offset, x + stride*(chirality*M+i));
+	    // second do scalar copy converting into register type
 #pragma unroll
-	    for (int i=0; i<M; i++) {
-	      // first do vectorized copy from memory
-	      Vector vecTmp = vector_load<Vector>(clover + parity*offset, x + stride*(chirality*M+i));
-	      // second do scalar copy converting into register type
-	      for (int j=0; j<N; j++) copy(v[(chirality*M+i)*N+j], reinterpret_cast<Float*>(&vecTmp)[j]);
-	    }
-
-	    if (sizeof(Float)==sizeof(short))
-#pragma unroll
-	      for (int i=0; i<length/2; i++) v[chirality*(length/2)+i] *= norm[parity*norm_offset + chirality*stride + x];
+	    for (int j=0; j<N; j++) copy(v[i*N+j], reinterpret_cast<Float*>(&vecTmp)[j]);
 	  }
+
+	  if (sizeof(Float)==sizeof(short))
+#pragma unroll
+	    for (int i=0; i<block; i++) v[i] *= norm[parity*norm_offset + chirality*stride + x];
 	}
   
-	__device__ __host__ inline void save(const RegType v[length], int x, int parity) {
+	/**
+	   @brief Store accessor for a single chiral block
+	   @param[out] v Vector of elements to be stored
+	   @param[in] x Checkerboarded site index
+	   @param[in] parity Field parity
+	   @param[in] chirality Chiral block index
+	 */
+	__device__ __host__ inline void save(const RegType v[block], int x, int parity, int chirality) {
 	  // find the norm of each chiral block
-	  RegType scale[2];
+	  RegType scale = 0.0;
 	  if (sizeof(Float)==sizeof(short)) {
-	    const int M = length/2;
 #pragma unroll
-	    for (int chi=0; chi<2; chi++) { // chirality
-	      scale[chi] = 0.0;
-#pragma unroll
-	      for (int i=0; i<M; i++) 
-		scale[chi] = fabs(v[chi*M+i]) > scale[chi] ? fabs(v[chi*M+i]) : scale[chi];
-	      norm[parity*norm_offset + chi*stride + x] = scale[chi];
-	    }
+	    for (int i=0; i<block; i++) scale = fabs(v[i]) > scale ? fabs(v[i]) : scale;
+	    norm[parity*norm_offset + chirality*stride + x] = scale;
 	  }
 
-	  const int M=length/(N*2);
-	  for (int chirality=0; chirality<2; chirality++) {
+#pragma unroll
+	  for (int i=0; i<M; i++) {
+	    Vector vecTmp;
+	    // first do scalar copy converting into storage type and rescaling if necessary
+	    if (sizeof(Float)==sizeof(short))
+#pragma unroll
+	      for (int j=0; j<N; j++) copy(reinterpret_cast<Float*>(&vecTmp)[j], v[i*N+j] / scale);
+	    else
+#pragma unroll
+	      for (int j=0; j<N; j++) copy(reinterpret_cast<Float*>(&vecTmp)[j], v[i*N+j]);
 
-#pragma unroll
-	    for (int i=0; i<M; i++) {
-	      Vector vecTmp;
-	      // first do scalar copy converting into storage type and rescaling if necessary
-	      if (sizeof(Float)==sizeof(short))
-#pragma unroll
-		for (int j=0; j<N; j++) copy(reinterpret_cast<Float*>(&vecTmp)[j], v[(chirality*M+i)*N+j] / scale[chirality]);
-	      else
-#pragma unroll
-		for (int j=0; j<N; j++) copy(reinterpret_cast<Float*>(&vecTmp)[j], v[(chirality*M+i)*N+j]);
-
-	      // second do vectorized copy into memory
-	      reinterpret_cast<Vector*>(clover + parity*offset)[x + stride*(chirality*M+i)] = vecTmp;
-	    }
+	    // second do vectorized copy into memory
+	    reinterpret_cast<Vector*>(clover + parity*offset)[x + stride*(chirality*M+i)] = vecTmp;
 	  }
+	}
+
+	/**
+	   @brief Load accessor for the clover matrix
+	   @param[out] v Vector of loaded elements
+	   @param[in] x Checkerboarded site index
+	   @param[in] parity Field parity
+	   @param[in] chirality Chiral block index
+	 */
+	__device__ __host__ inline void load(RegType v[length], int x, int parity) const {
+#pragma unroll
+	  for (int chirality=0; chirality<2; chirality++) load(&v[chirality*36], x, parity, chirality);
+	}
+
+	/**
+	   @brief Store accessor for the clover matrix
+	   @param[out] v Vector of elements to be stored
+	   @param[in] x Checkerboarded site index
+	   @param[in] parity Field parity
+	   @param[in] chirality Chiral block index
+	 */
+	__device__ __host__ inline void save(const RegType v[length], int x, int parity) {
+#pragma unroll
+	  for (int chirality=0; chirality<2; chirality++) save(&v[chirality*36], x, parity, chirality);
 	}
 
 	/**

lib/clover_quda.cu
@@ -95,7 +95,6 @@ namespace quda {
       const int idtab[15]={0,1,3,6,10,2,4,7,11,5,8,12,9,13,14};
       Float diag[6];
       Complex triangle[15];
-      Float A[72];
 
       // This uses lots of unnecessary memory
       for(int ch=0; ch<2; ++ch){ 
@@ -129,18 +128,17 @@ namespace quda {
         triangle[14] =   block1[ch](2,1);
 
 
-        for(int i=0; i<6; ++i){
-          A[ch*36 + i] = 0.5*diag[i];
-        } 
+	Float A[36];
+        for(int i=0; i<6; ++i) A[i] = static_cast<Float>(0.5)*diag[i];
+
         for(int i=0; i<15; ++i){
-          A[ch*36+6+2*i]     = 0.5*triangle[idtab[i]].x;
-          A[ch*36+6+2*i + 1] = 0.5*triangle[idtab[i]].y;
+          A[6+2*i]     = 0.5*triangle[idtab[i]].x;
+          A[6+2*i + 1] = 0.5*triangle[idtab[i]].y;
         } 
+	arg.clover.save(A, idx, parity, ch);
       } // ch
       // 84 floating-point ops
 
-
-      arg.clover.save(A, idx, parity);
       return;
     }
 

tests/test_util.cpp
@@ -1590,7 +1590,7 @@ bool verify_results = true;
 double mass = 0.1;
 double mu = 0.1;
 double anisotropy = 1.0;
-double clover_coeff = 1.0;
+double clover_coeff = 0.1;
 bool compute_clover = false;
 double tol = 1e-7;
 double tol_hq = 0.;

commit 5c20ecf8b5cba8e223ced5d55a9ed989f432a5f1

Fixed bug in clover inversion introduced with commit 38fda505cc1aec25c9c351ad8a093a5c2fcf8f0b.

lib/clover_invert.cu
@@ -47,11 +47,11 @@ namespace quda {
       // FIXME use native ordering in the Cholseky 
       // factor of two is inherent to QUDA clover storage
       constexpr Float two = static_cast<Float>(2.0);
-      for (int i=0; i<6; i++) diag[i] = two*A[ch*36+i];
+      for (int i=0; i<6; i++) diag[i] = two*A[i];
 
       const int idtab[15]={0,1,3,6,10,2,4,7,11,5,8,12,9,13,14};
 #pragma unroll
-      for (int i=0; i<15; i++) tri[idtab[i]] = complex<Float>(two*A[ch*36+6+2*i], two*A[ch*36+6+2*i+1]);
+      for (int i=0; i<15; i++) tri[idtab[i]] = complex<Float>(two*A[6+2*i], two*A[6+2*i+1]);
 
       //Compute (T^2 + mu2) first, then invert (not optimized!):
       if (twist) {
@@ -147,9 +147,9 @@ namespace quda {
       }
 
       constexpr Float half = static_cast<Float>(0.5);
-      for (int i=0; i<6; i++) A[ch*36+i] = half * diag[i];
+      for (int i=0; i<6; i++) A[i] = half * diag[i];
 #pragma unroll
-      for (int i=0; i<15; i++) { A[ch*36+6+2*i] = half*tri[idtab[i]].real(); A[ch*36+6+2*i+1] = half*tri[idtab[i]].imag(); }
+      for (int i=0; i<15; i++) { A[6+2*i] = half*tri[idtab[i]].real(); A[6+2*i+1] = half*tri[idtab[i]].imag(); }
 
       // save the inverted matrix
       arg.inverse.save(A, x, parity, ch);