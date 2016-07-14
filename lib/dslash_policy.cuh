064194a007f35c7354681608a5e32bec50e06573
maddyscientist <mclark@nvidia.com>
2016-07-14 13:04:15 -0700

    Check whether tuning when doing pre tuning in dslash policy tuning

--- lib/dslash_policy.cuh
+++ lib/dslash_policy.cuh
@@ -1077,7 +1077,7 @@ struct DslashFactory {
    {
      // before we do policy tuning we must ensure the kernel
      // constituents have been tuned since we can't do nested tuning
-     if (getTuneCache().find(tuneKey()) == getTuneCache().end()) {
+     if (getTuning() && getTuneCache().find(tuneKey()) == getTuneCache().end()) {
        disableProfileCount();
 
        for (unsigned int i=0; i<n_policy; i++) {
