10d6e44d05b6c6acba16698299ffc7de34d626eb
maddyscientist <mclark@nvidia.com>
2016-12-10 23:16:02 -0800

    Add QUDA_ENABLE_DEVICE_MEMORY_POOL and QUDA_ENABLE_PINNED_MEMORY_POOL environment variables to provide runtime enable/disable of the pool managers

--- lib/lattice_field.cpp
+++ lib/lattice_field.cpp
@@ -30,11 +30,41 @@ namespace quda {
   // sizes of active allocations
   std::map<void *, size_t> LatticeField::deviceSize;
 
+  static bool pool_init = false;
+
+  // whether to use a memory pool allocator for device memory
+  static bool device_memory_pool = true;
+
+  // whether to use a memory pool allocator for pinned memory
+  static bool pinned_memory_pool = true;
 
   LatticeField::LatticeField(const LatticeFieldParam &param)
     : volume(1), pad(param.pad), total_bytes(0), nDim(param.nDim), precision(param.precision),
       siteSubset(param.siteSubset)
   {
+    if (!pool_init) {
+      // device memory pool
+      char *enable_device_pool = getenv("QUDA_ENABLE_DEVICE_MEMORY_POOL");
+      if (!enable_device_pool || strcmp(enable_device_pool,"0")!=0) {
+	warningQuda("Using device memory pool allocator");
+	device_memory_pool = true;
+      } else {
+	warningQuda("Not using device memory pool allocator");
+	device_memory_pool = false;
+      }
+
+      // pinned memory pool
+      char *enable_pinned_pool = getenv("QUDA_ENABLE_PINNED_MEMORY_POOL");
+      if (!enable_pinned_pool || strcmp(enable_pinned_pool,"0")!=0) {
+	warningQuda("Using pinned memory pool allocator");
+	pinned_memory_pool = true;
+      } else {
+	warningQuda("Not using pinned memory pool allocator");
+	pinned_memory_pool = false;
+      }
+      pool_init = true;
+    }
+
     for (int i=0; i<nDim; i++) {
       x[i] = param.x[i];
       volume *= param.x[i];
@@ -166,90 +196,110 @@ namespace quda {
 
   void *LatticeField::allocatePinned(size_t nbytes)
   {
-    std::multimap<size_t, void *>::iterator it;
-    void *ptr = 0;
+    void *ptr = nullptr;
+    if (pinned_memory_pool) {
+      std::multimap<size_t, void *>::iterator it;
 
-    if (pinnedCache.empty()) {
-      ptr = pinned_malloc(nbytes);
-    } else {
-      it = pinnedCache.lower_bound(nbytes);
-      if (it != pinnedCache.end()) { // sufficiently large allocation found
-	nbytes = it->first;
-	ptr = it->second;
-	pinnedCache.erase(it);
-      } else { // sacrifice the smallest cached allocation
-	it = pinnedCache.begin();
-	ptr = it->second;
-	pinnedCache.erase(it);
-	host_free(ptr);
+      if (pinnedCache.empty()) {
 	ptr = pinned_malloc(nbytes);
+      } else {
+	it = pinnedCache.lower_bound(nbytes);
+	if (it != pinnedCache.end()) { // sufficiently large allocation found
+	  nbytes = it->first;
+	  ptr = it->second;
+	  pinnedCache.erase(it);
+	} else { // sacrifice the smallest cached allocation
+	  it = pinnedCache.begin();
+	  ptr = it->second;
+	  pinnedCache.erase(it);
+	  host_free(ptr);
+	  ptr = pinned_malloc(nbytes);
+	}
       }
+      pinnedSize[ptr] = nbytes;
+    } else {
+      ptr = pinned_malloc(nbytes);
     }
-    pinnedSize[ptr] = nbytes;
     return ptr;
   }
 
   void LatticeField::freePinned(void *ptr)
   {
-    if (!pinnedSize.count(ptr)) {
-      errorQuda("Attempt to free invalid pointer");
+    if (pinned_memory_pool) {
+      if (!pinnedSize.count(ptr)) {
+	errorQuda("Attempt to free invalid pointer");
+      }
+      pinnedCache.insert(std::make_pair(pinnedSize[ptr], ptr));
+      pinnedSize.erase(ptr);
+    } else {
+      host_free(ptr);
     }
-    pinnedCache.insert(std::make_pair(pinnedSize[ptr], ptr));
-    pinnedSize.erase(ptr);
   }
 
   void LatticeField::flushPinnedCache()
   {
-    std::multimap<size_t, void *>::iterator it;
-    for (it = pinnedCache.begin(); it != pinnedCache.end(); it++) {
-      void *ptr = it->second;
-      host_free(ptr);
+    if (pinned_memory_pool) {
+      std::multimap<size_t, void *>::iterator it;
+      for (it = pinnedCache.begin(); it != pinnedCache.end(); it++) {
+	void *ptr = it->second;
+	host_free(ptr);
+      }
+      pinnedCache.clear();
     }
-    pinnedCache.clear();
   }
 
   void *LatticeField::allocateDevice(size_t nbytes) const
   {
-    std::multimap<size_t, void *>::iterator it;
-    void *ptr = 0;
+    void *ptr = nullptr;
+    if (device_memory_pool) {
+      std::multimap<size_t, void *>::iterator it;
 
-    if (deviceCache.empty()) {
-      ptr = device_malloc(nbytes);
-    } else {
-      it = deviceCache.lower_bound(nbytes);
-      if (it != deviceCache.end()) { // sufficiently large allocation found
-	nbytes = it->first;
-	ptr = it->second;
-	deviceCache.erase(it);
-      } else { // sacrifice the smallest cached allocation
-	it = deviceCache.begin();
-	ptr = it->second;
-	deviceCache.erase(it);
-	device_free(ptr);
+      if (deviceCache.empty()) {
 	ptr = device_malloc(nbytes);
+      } else {
+	it = deviceCache.lower_bound(nbytes);
+	if (it != deviceCache.end()) { // sufficiently large allocation found
+	  nbytes = it->first;
+	  ptr = it->second;
+	  deviceCache.erase(it);
+	} else { // sacrifice the smallest cached allocation
+	  it = deviceCache.begin();
+	  ptr = it->second;
+	  deviceCache.erase(it);
+	  device_free(ptr);
+	  ptr = device_malloc(nbytes);
+	}
       }
+      deviceSize[ptr] = nbytes;
+    } else {
+      ptr = device_malloc(nbytes);
     }
-    deviceSize[ptr] = nbytes;
     return ptr;
   }
 
   void LatticeField::freeDevice(void *ptr) const
   {
-    if (!deviceSize.count(ptr)) {
-      errorQuda("Attempt to free invalid pointer");
+    if (device_memory_pool) {
+      if (!deviceSize.count(ptr)) {
+	errorQuda("Attempt to free invalid pointer");
+      }
+      deviceCache.insert(std::make_pair(deviceSize[ptr], ptr));
+      deviceSize.erase(ptr);
+    } else {
+      device_free(ptr);
     }
-    deviceCache.insert(std::make_pair(deviceSize[ptr], ptr));
-    deviceSize.erase(ptr);
   }
 
   void LatticeField::flushDeviceCache()
   {
-    std::multimap<size_t, void *>::iterator it;
-    for (it = deviceCache.begin(); it != deviceCache.end(); it++) {
-      void *ptr = it->second;
-      device_free(ptr);
+    if (device_memory_pool) {
+      std::multimap<size_t, void *>::iterator it;
+      for (it = deviceCache.begin(); it != deviceCache.end(); it++) {
+	void *ptr = it->second;
+	device_free(ptr);
+      }
+      deviceCache.clear();
     }
-    deviceCache.clear();
   }
 
 
