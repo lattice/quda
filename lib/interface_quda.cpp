00df450da478595c5f152e6101285702f7c1ded1
Alejandro Vaquero <serianam@gmail.com>
2017-10-17 15:05:24 -0700

    Added test for the covariant derivative, works for single GPU

--- lib/interface_quda.cpp
+++ lib/interface_quda.cpp
@@ -1396,6 +1396,9 @@ namespace quda {
     case QUDA_LAPLACE_DSLASH:
       diracParam.type = pc ? QUDA_GAUGE_LAPLACEPC_DIRAC : QUDA_GAUGE_LAPLACE_DIRAC;
       break;
+    case QUDA_COVDEV_DSLASH:
+      diracParam.type = QUDA_GAUGE_COVDEV_DIRAC;
+      break;
     default:
       errorQuda("Unsupported dslash_type %d", inv_param->dslash_type);
     }
