31e4f218927e9f102b23fd26466e82b08e11c93a
Evan Weinberg <eweinberg@nvidia.com>
2018-08-08 14:02:07 -0700

    Avoided some printfs for alternative reliable updates.

--- lib/inv_cg_quda.cpp
+++ lib/inv_cg_quda.cpp
@@ -214,7 +214,8 @@ namespace quda {
 #ifdef ALTRELIABLE
     // hack to select alternative reliable updates
     constexpr bool alternative_reliable = true;
-    warningQuda("Using alternative reliable updates. This feature is mostly ok but needs a little more testing in the real world.\n");
+    // ESW: avoiding printing
+    //warningQuda("Using alternative reliable updates. This feature is mostly ok but needs a little more testing in the real world.\n");
 #else
     constexpr bool alternative_reliable = false;
 #endif
@@ -525,8 +526,8 @@ namespace quda {
 	  pnorm = pnorm + alpha[j] * alpha[j]* (ppnorm);
 	  xnorm = sqrt(pnorm);
 	  d_new = d + u*rNorm + uhigh*Anorm * xnorm;
-	  if(steps_since_reliable==0)
-	    printfQuda("New dnew: %e (r %e , y %e)\n",d_new,u*rNorm,uhigh*Anorm * sqrt(blas::norm2(y)) );
+	  //if(steps_since_reliable==0)
+	  //  printfQuda("New dnew: %e (r %e , y %e)\n",d_new,u*rNorm,uhigh*Anorm * sqrt(blas::norm2(y)) );
 	}
 	steps_since_reliable++;
 
@@ -558,7 +559,8 @@ namespace quda {
           d = d_new;
           xnorm = 0;//sqrt(norm2(x));
           pnorm = 0;//pnorm + alpha * sqrt(norm2(p));
-          printfQuda("New dinit: %e (r %e , y %e)\n",dinit,uhigh*sqrt(r2),uhigh*Anorm*sqrt(blas::norm2(y)));
+          // ESW: save time
+          //printfQuda("New dinit: %e (r %e , y %e)\n",dinit,uhigh*sqrt(r2),uhigh*Anorm*sqrt(blas::norm2(y)));
           d_new = dinit;
         }
         else{
