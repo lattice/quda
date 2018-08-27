c5effae62440c783de278d9e1ea3f368ba5b9e70
Christos Kallidonis <kallidonis@gmail.com>
2018-08-27 16:30:46 -0400

    Set the third propagator to the forward propagator in the TMD case

--- lib/interface_qlua.cpp
+++ lib/interface_qlua.cpp
@@ -1157,9 +1157,9 @@ QuarkContract_momProj_Quda(XTRN_CPLX *momproj_buf, XTRN_CPLX *corrQuda, const qu
       checkCudaError();
     }//-ivec
   }
-  else if(paramAPI.mpParam.cntrType == what_tmd_g_F_B){ //-- Use the third propagator as output in the tmd case, initialize it to zero
+  else if(paramAPI.mpParam.cntrType == what_tmd_g_F_B){ //-- Use the third propagator as output in the tmd case, initialize it to the first (forward) propagator
     for(int ivec=0;ivec<nVec;ivec++){
-      cudaProp3[ivec] = new_cudaColorSpinorField(gp, ip, Nc, Ns, NULL );
+      cudaProp3[ivec] = new_cudaColorSpinorField(gp, ip, Nc, Ns, &(hprop1[ivec * fieldLgh]) );
       if(cudaProp3[ivec] == NULL)
 	errorQuda("%s: Cannot allocate propagator-3 for cntrType = %s. Exiting.\n", func_name, qc_contractTypeStr[paramAPI.mpParam.cntrType]);
 
