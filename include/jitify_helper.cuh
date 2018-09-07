0a8b7a4c367742953c0b58f5ae8d71600148f48c
maddyscientist <mclark@nvidia.com>
2018-09-07 14:48:17 -0700

    Pass __COMPUTE_CAPABILITY__ macro to jitify and expose option to pass additional parameters to create_jitify_program

--- include/jitify_helper.cuh
+++ include/jitify_helper.cuh
@@ -34,14 +34,20 @@ namespace quda {
   static jitify::Program *program;
   static bool jitify_init = false;
 
-  static void create_jitify_program(const char *file) {
+  static void create_jitify_program(const char *file, const std::vector<std::string> extra_options = {}) {
     if (!jitify_init) {
       kernel_cache = new jitify::JitCache;
+
 #ifdef DEVICE_DEBUG
       std::vector<std::string> options = {"-std=c++11", "-ftz=true", "-prec-div=false", "-prec-sqrt=false", "-G"};
 #else
       std::vector<std::string> options = {"-std=c++11", "-ftz=true", "-prec-div=false", "-prec-sqrt=false"};
 #endif
+      options.push_back( std::string("-D__COMPUTE_CAPABILITY__=") + std::to_string(__COMPUTE_CAPABILITY__) );
+
+      // add an extra compilation options specific to this instance
+      for (auto option : extra_options) options.push_back(option);
+
       program = new jitify::Program(kernel_cache->program(file, 0, options));
       jitify_init = true;
     }
