35ad50c7d44775a8323001083b5578e5445a1028
maddyscientist <mclark@nvidia.com>
2018-10-20 15:48:59 -0700

    Fixed bugs in SU(3) projection routine, and optimized both that and HISQ link unitarization

--- include/su3_project.cuh
+++ include/su3_project.cuh
@@ -20,79 +20,93 @@ namespace quda {
    * @param in The input matrix to which we're reporting its unitarity
    * @param tol Tolerance to which this check is applied
    */
-  template <typename Float2, typename Float>
-  __host__ __device__ int checkUnitary(Matrix<Float2,3> &inv,  Matrix<Float2,3> in, const Float tol)
+  template <typename Matrix, typename Float>
+  __host__ __device__ inline bool checkUnitary(const Matrix &inv, const Matrix &in, const Float tol)
   {
-    computeMatrixInverse(in, &inv);
 
-    for (int i=0;i<3;i++)
-      for (int j=0;j<3;j++)
-      {
-        if (fabs(in(i,j).x - inv(j,i).x) > tol)
-          return 1;
-        if (fabs(in(i,j).y + inv(j,i).y) > tol)
-          return 1;
+    // first check U - U^{-1} = 0
+#pragma unroll
+    for (int i=0; i<in.size(); i++) {
+#pragma unroll
+      for (int j=0; j<in.size(); j++) {
+        if (fabs(in(i,j).real() - inv(j,i).real()) > tol ||
+            fabs(in(i,j).imag() + inv(j,i).imag()) > tol) return false;
       }
-    return 0;
+    }
+
+    // now check 1 - U^dag * U = 0
+    // this check is more expensive so delay until we have passed first check
+    const Matrix identity = conj(in)*in;
+#pragma unroll
+    for (int i=0; i<in.size(); i++) {
+      if (fabs(identity(i,i).real() - static_cast<Float>(1.0)) > tol ||
+          fabs(identity(i,i).imag()) > tol)
+        return false;
+#pragma unroll
+      for (int j=0; j<in.size(); j++) {
+        if (i>j) { // off-diagonal identity check
+        if (fabs(identity(i,j).real()) > tol || fabs(identity(i,j).imag()) > tol ||
+            fabs(identity(j,i).real()) > tol || fabs(identity(j,i).imag()) > tol )
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 
   /**
-   * @brief Check the unitarity of the input matrix to a given
-   * tolerance (1e-14) and print out deviation for each component (used for
+   * @brief Print out deviation for each component (used for
    * debugging only).
    *
    * @param inv The inverse of the input matrix
    * @param in The input matrix to which we're reporting its unitarity
-   */  template <typename Float2>
-  __host__ __device__ int checkUnitaryPrint(Matrix<Float2,3> &inv, Matrix<Float2,3> in)
+   */
+  template <typename Matrix>
+  __host__ __device__ void checkUnitaryPrint(const Matrix &inv, const Matrix &in)
   {
-    computeMatrixInverse(in, &inv);
-    for (int i=0;i<3;i++)
-      for (int j=0;j<3;j++)
-      {
-        printf("TESTR: %+.3le %+.3le %+.3le\n", in(i,j).x, (*inv)(j,i).x, fabs(in(i,j).x - (*inv)(j,i).x));
-	printf("TESTI: %+.3le %+.3le %+.3le\n", in(i,j).y, (*inv)(j,i).y, fabs(in(i,j).y + (*inv)(j,i).y));
-        cudaDeviceSynchronize();
-        if (fabs(in(i,j).x - inv(j,i).x) > 1e-14)
-          return 1;
-        if (fabs(in(i,j).y + inv(j,i).y) > 1e-14)
-          return 1;
+    for (int i=0; i<in.size(); i++) {
+      for (int j=0; j<in.size(); j++) {
+        printf("TESTR: %+.13le %+.13le %+.13le\n",
+               in(i,j).real(), inv(j,i).real(), fabs(in(i,j).real() - inv(j,i).real()));
+	printf("TESTI: %+.13le %+.13le %+.13le\n",
+               in(i,j).imag(), inv(j,i).imag(), fabs(in(i,j).imag() + inv(j,i).imag()));
       }
-    return 0;  
+    }
   }
 
   /**
-   * @brief Project the input matrix on the SU(3) group.  First unitarize the matrix and then project onto the special unitary group.
+   * @brief Project the input matrix on the SU(3) group.  First
+   * unitarize the matrix and then project onto the special unitary
+   * group.
    *
    * @param in The input matrix to which we're projecting
    * @param tol Tolerance to which this check is applied
    */
   template <typename Float>
-  __host__ __device__ void polarSu3(Matrix<complex<Float>,3> &in, Float tol)
+  __host__ __device__ inline void polarSu3(Matrix<complex<Float>,3> &in, Float tol)
   {
-    Matrix<complex<Float>,3> inv, out;
+    constexpr Float negative_third = -1.0/3.0;
+    constexpr Float negative_sixth = -1.0/6.0;
+    Matrix<complex<Float>,3> out = in;
+    Matrix<complex<Float>,3> inv = inverse(in);
 
-    out = in;
-    computeMatrixInverse(out, &inv);
-
-    // iterate until matrix is unitary
-    do {
+    constexpr int max_iter = 100;
+    int i = 0;
+    do { // iterate until matrix is unitary
       out = 0.5*(out + conj(inv));
-    } while(checkUnitary(inv, out, tol));
+      inv = inverse(out);
+    } while (!checkUnitary(inv, out, tol) && ++i < max_iter);
 
     // now project onto special unitary group
     complex<Float> det = getDeterminant(out);
-    double mod = norm(det);
-    mod = pow(mod, (1./6.));
-    double angle = atan2(det.y, det.x);
-    angle /= -3.;
-    
+    Float mod = pow(norm(det), negative_sixth);
+    Float angle = arg(det);
+
     complex<Float> cTemp;
+    sincos(negative_third * angle, &cTemp.y, &cTemp.x);
 
-    sincos(angle, &cTemp.y, &cTemp.x);
-    cTemp /= mod;
-
-    in = out*cTemp;
+    in = (mod*cTemp)*out;
   }
 
   
