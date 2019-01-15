1a75f6987fba228214e05131ea62282e33d7e33a
Evan Weinberg <evansweinberg@gmail.com>
2019-01-15 00:30:03 -0500

    Fixed recursion relation hack.

--- lib/inv_ca_cg.cpp
+++ lib/inv_ca_cg.cpp
@@ -542,20 +542,20 @@ namespace quda {
         matSloppy(*AS[0], *S[0], tmpSloppy, tmpSloppy2);
 
         if (nKrylov > 1) {
-          Complex facs1[3] = { m_map, b_map, 0. };
+          // S_1 = m AS_0 + b S_0
+          Complex facs1[3] = { m_map, b_map, -1. };
           std::vector<ColorSpinorField*> recur1{AS[0],S[0],S[1]};
           std::vector<ColorSpinorField*> S1{S[1]};
-          //blas::zero(*S[1]);
           blas::caxpy(facs1,recur1,S1);
           matSloppy(*AS[1], *S[1], tmpSloppy, tmpSloppy2);
 
           // Enter recursion relation
           if (nKrylov > 2) {
-            Complex factors[4] = { 2.*m_map, 2.*b_map, -1., 0. };
+            // S_k = 2 m AS_{k-1} + 2 b S_{k-1} - S_{k-2}
+            Complex factors[4] = { 2.*m_map, 2.*b_map, -1., -1. }; // last -1 is to cancel itself out. really need a axy
             for (int k = 2; k < nKrylov; k++) {
               std::vector<ColorSpinorField*> recur2{AS[k-1],S[k-1],S[k-2],S[k]};
               std::vector<ColorSpinorField*> Sk{S[k]};
-              //blas::zero(*S[k]);
               blas::caxpy(factors, recur2, Sk);
               matSloppy(*AS[k], *S[k], tmpSloppy, tmpSloppy2);
             }
