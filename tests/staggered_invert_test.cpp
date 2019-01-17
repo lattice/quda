deea45605663fd8add72ecf14e4cef4ef20d015a
Evan Weinberg <evansweinberg@gmail.com>
2019-01-17 01:54:39 -0500

    Revert "Fixed factor of 3 bug in padding."
    This reverts commit 438faeba75d2b5cf4b2d6bd14e63d1dd5e97e956.

--- tests/staggered_invert_test.cpp
+++ tests/staggered_invert_test.cpp
@@ -186,7 +186,7 @@ set_params(QudaGaugeParam* gaugeParam, QudaInvertParam* inv_param,
   int pad_size =MAX(x_face_size, y_face_size);
   pad_size = MAX(pad_size, z_face_size);
   pad_size = MAX(pad_size, t_face_size);
-  gaugeParam->ga_pad = 3*pad_size;    
+  gaugeParam->ga_pad = pad_size;    
 #endif
 
 
