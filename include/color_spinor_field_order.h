lattice/quda

commit 3d0db60f4199fcb8d452a01aa2c95ed28225ab0c

Remove dangerous use of reinterpret_cast on stack arrays which is dangerous if arrays are spilled to local memory

include/color_spinor_field_order.h
@@ -865,7 +865,8 @@ namespace quda {
         // first do texture load from memory
         TexVector vecTmp = tex1Dfetch<TexVector>(tex, parity*tex_offset + stride*i + x);
         // now insert into output array
-        copy(reinterpret_cast<RegVector*>(v)[i], vecTmp);
+#pragma unroll
+        for (int j=0; j<N; j++) copy(v[i*N+j], reinterpret_cast<Float*>(&vecTmp)[j]);
         if ( isFixed<Float>::value ) {
 #pragma unroll
           for (int j=0; j<N; j++) v[i*N+j] *= nrm;
@@ -876,7 +877,8 @@ namespace quda {
         // first load from memory
         Vector vecTmp = vector_load<Vector>(field + parity*offset, x + stride*i);
         // now copy into output and scale
-        copy_and_scale(reinterpret_cast<RegVector*>(v)[i], vecTmp, nrm);
+#pragma unroll
+        for (int j=0; j<N; j++) copy_and_scale(v[i*N+j], reinterpret_cast<Float*>(&vecTmp)[j], nrm);
       }
     }
 
@@ -910,8 +912,9 @@ namespace quda {
 #pragma unroll
     for (int i=0; i<M; i++) {
       Vector vecTmp;
-      // first do vectorized copy converting into storage type
-      copy_scaled(vecTmp, reinterpret_cast<RegVector*>(tmp)[i]);
+      // first do scalar copy converting into storage type
+#pragma unroll
+      for (int j=0; j<N; j++) copy_scaled(reinterpret_cast<Float*>(&vecTmp)[j], tmp[i*N+j]);
       // second do vectorized copy into memory
       vector_store(field + parity*offset, x + stride*i, vecTmp);
     }
@@ -960,7 +963,8 @@ namespace quda {
 #if defined(USE_TEXTURE_OBJECTS) && defined(__CUDA_ARCH__) && 0 // buggy - need to account for dim/dir offset
       if (!huge_alloc) { // use textures unless we have a huge alloc
         TexVector vecTmp = tex1Dfetch<TexVector>(ghostTex, parity*tex_offset + stride*i + x);
-        copy(reinterpret_cast<RegVector*>(v)[i], vecTmp);
+#pragma unroll
+        for (int j=0; j<N; j++) copy(v[i*N+j], reinterpret_cast<Float*>(&vecTmp)[j]);
         if (isFixed<Float>::value ) {
 #pragma unroll
           for (int i=0; i<N; i++) v[i*N+j] *= nrm;
@@ -970,7 +974,8 @@ namespace quda {
       {
         Vector vecTmp = vector_load<Vector>(ghost[2*dim+dir]+parity*faceVolumeCB[dim]*M_ghost*N,
                                             i*faceVolumeCB[dim]+x);
-        copy_and_scale(reinterpret_cast<RegVector*>(v)[i], vecTmp, nrm);
+#pragma unroll
+        for (int j=0; j<N; j++) copy_and_scale(v[i*N+j], reinterpret_cast<Float*>(&vecTmp)[j], nrm);
       }
     }
 
@@ -1004,8 +1009,9 @@ namespace quda {
 #pragma unroll
     for (int i=0; i<M_ghost; i++) {
       Vector vecTmp;
-      // first do vectorized copy converting into storage type
-      copy_scaled(vecTmp, reinterpret_cast< RegVector* >(tmp)[i]);
+      // first do scalar copy converting into storage type
+#pragma unroll
+      for (int j=0; j<N; j++) copy_scaled(reinterpret_cast<Float*>(&vecTmp)[j], tmp[i*N+j]);
       // second do vectorized copy into memory
       vector_store(ghost[2*dim+dir]+parity*faceVolumeCB[dim]*M_ghost*N, i*faceVolumeCB[dim]+x, vecTmp);
     }

include/gauge_field_order.h
@@ -1256,16 +1256,14 @@ namespace quda {
 	template<typename I>
 	__device__ __host__ inline void Unpack(RegType out[18], const RegType in[12], int idx, int dir,
 					       const RegType phase, const I *X, const int *R) const {
-	  const Complex *In = reinterpret_cast<const Complex*>(in);
-	  Complex *Out = reinterpret_cast<Complex*>(out);
+	  Complex Out[9];
+#pragma unroll
+	  for (int i=0; i<6; i++) Out[i] = Complex(in[2*i+0], in[2*i+1]);
 
 	  const RegType u0 = dir < 3 ? anisotropy :
 	    timeBoundary<ghostExchange_>(idx, X, R, tBoundary, static_cast<RegType>(1.0),
                                          firstTimeSliceBound, lastTimeSliceBound,
                                          isFirstTimeSlice, isLastTimeSlice, ghostExchange);
-
-#pragma unroll
-	  for(int i=0; i<6; ++i) Out[i] = In[i];
 
 	  //Out[6] = u0*conj(Out[1]*Out[5] - Out[2]*Out[4]);
           Out[6] = cmul(Out[2], Out[4]);
@@ -1281,7 +1279,13 @@ namespace quda {
           Out[8] = cmul(Out[1], Out[3]);
           Out[8] = cmac(Out[0], Out[4], -Out[8]);
           Out[8] = u0*conj(Out[8]);
-	}
+
+#pragma unroll
+          for (int i=0; i<9; i++) {
+            out[2*i+0] = Out[i].real();
+            out[2*i+1] = Out[i].imag();
+          }
+        }
 
 	__device__ __host__ inline RegType getPhase(const RegType in[18]) { return 0; }
       };
@@ -1365,12 +1369,11 @@ namespace quda {
       template<typename I>
       __device__ __host__ inline void Unpack(RegType out[18], const RegType in[12], int idx, int dir,
 					     const RegType phase, const I *X, const int *R) const {
-	const Complex *In = reinterpret_cast<const Complex*>(in);
-	Complex *Out = reinterpret_cast<Complex*>(out);
 	const RegType coeff = static_cast<RegType>(1.0)/scale;
 
+        Complex Out[9];
 #pragma unroll
-	for(int i=0; i<6; ++i) Out[i] = In[i];
+        for (int i=0; i<6; i++) Out[i] = Complex(in[2*i+0], in[2*i+1]);
 
         Out[6] = cmul(Out[2], Out[4]);
         Out[6] = cmac(Out[1], Out[5], -Out[6]);
@@ -1392,11 +1395,19 @@ namespace quda {
 	Out[6] *= A;
 	Out[7] *= A;
 	Out[8] *= A;
+
+#pragma unroll
+        for (int i=0; i<9; i++) {
+          out[2*i+0] = Out[i].real();
+          out[2*i+1] = Out[i].imag();
+        }
       }
 
       __device__ __host__ inline RegType getPhase(const RegType in[18]) const {
 #if 1 // phase from cross product
-	const Complex *In = reinterpret_cast<const Complex*>(in);
+        Complex In[9];
+#pragma unroll
+        for (int i=0; i<9; i++) In[i] = Complex(in[2*i+0], in[2*i+1]);
 	// denominator = (U[0][0]*U[1][1] - U[0][1]*U[1][0])*
 	Complex denom = conj(In[0]*In[4] - In[1]*In[3]) / scale;
 	Complex expI3Phase = In[8] / denom; // numerator = U[2][2]
@@ -1404,9 +1415,9 @@ namespace quda {
 #else // phase from determinant
 	Matrix<Complex,3> a;
 #pragma unroll
-	for (int i=0; i<9; i++) a(i) = Complex(in[2*i]/scale, in[2*i+1]/scale);
-	const Complex det = getDeterminant( a );
-	RegType phase = arg(det)/3;
+        for (int i=0; i<9; i++) a(i) = Complex(in[2*i]/scale, in[2*i+1]/scale);
+        const Complex det = getDeterminant( a );
+        RegType phase = arg(det)/3;
 #endif
 	return phase;
       }
@@ -1461,11 +1472,9 @@ namespace quda {
                                            const RegType phase, const I *X, const int *R,
                                            const Complex scale=Complex(static_cast<RegType>(1.0),
                                                                        static_cast<RegType>(1.0))) const {
-      const Complex *In = reinterpret_cast<const Complex*>(in);
-      Complex *Out = reinterpret_cast<Complex*>(out);
-
+      Complex Out[9];
 #pragma unroll
-      for (int i=1; i<=3; i++) Out[i] = In[i]; // these elements are copied directly
+      for (int i=1; i<=3; i++) Out[i] = Complex(in[2*i+0], in[2*i+1]); // these elements are copied directly
 
       Trig<isFixed<Float>::value,RegType>::SinCos(in[0], &Out[0].y, &Out[0].x);
       Trig<isFixed<Float>::value,RegType>::SinCos(in[1], &Out[6].y, &Out[6].x);
@@ -1528,6 +1537,12 @@ namespace quda {
         Out[8] = cmac(u0*A, Out[2], Out[8]);
         Out[8] = -r_inv2*Out[8];
       }
+
+#pragma unroll
+      for (int i=0; i<9; i++) {
+        out[2*i+0] = Out[i].real();
+        out[2*i+1] = Out[i].imag();
+      }
     }
 
     __device__ __host__ inline RegType getPhase(const RegType in[18]){ return 0; }
@@ -1556,8 +1571,10 @@ namespace quda {
 
       __device__ __host__ inline RegType getPhase(const RegType in[18]) const {
 #if 1 // phase from cross product
-	const Complex *In = reinterpret_cast<const Complex*>(in);
-	// denominator = (U[0][0]*U[1][1] - U[0][1]*U[1][0])*
+        Complex In[9];
+#pragma unroll
+        for (int i=0; i<9; i++) In[i] = Complex(in[2*i+0], in[2*i+1]);
+        // denominator = (U[0][0]*U[1][1] - U[0][1]*U[1][0])*
 	Complex denom = conj(In[0]*In[4] - In[1]*In[3]) * scale.imag();
 	Complex expI3Phase = In[8] / denom; // numerator = U[2][2]
 	RegType phase = arg(expI3Phase)/static_cast<RegType>(3.0);
@@ -1577,10 +1594,14 @@ namespace quda {
 	RegType cos_sin[2];
 	Trig<isFixed<RegType>::value,RegType>::SinCos(static_cast<RegType>(-phase), &cos_sin[1], &cos_sin[0]);
 	Complex z(cos_sin[0], cos_sin[1]);
-	Complex su3[9];
+	RegType su3[18];
 #pragma unroll
-	for (int i=0; i<9; i++) su3[i] = z * reinterpret_cast<const Complex*>(in)[i];
-	reconstruct_8.Pack(out, reinterpret_cast<RegType*>(su3), idx);
+	for (int i=0; i<9; i++) {
+          Complex su3_ = z*Complex(in[2*i+0], in[2*i+1]);
+          su3[2*i+0] = su3_.real();
+          su3[2*i+1] = su3_.imag();
+        }
+	reconstruct_8.Pack(out, su3, idx);
       }
 
       template<typename I>
@@ -1591,7 +1612,11 @@ namespace quda {
 	Trig<isFixed<RegType>::value,RegType>::SinCos(static_cast<RegType>(phase), &cos_sin[1], &cos_sin[0]);
 	Complex z(cos_sin[0], cos_sin[1]);
 #pragma unroll
-	for (int i=0; i<9; i++) reinterpret_cast<Complex*>(out)[i] *= z;
+	for (int i=0; i<9; i++) {
+          Complex z_out = z*Complex(out[2*i+0], out[2*i+1]);
+          out[2*i+0] = z_out.real();
+          out[2*i+1] = z_out.imag();
+        }
       }
 
     };

commit 02832f89be34f5d591331b8c1fe028e0898128f4

Fix bug in colorspinor::FloatNOrder load and loadGhost accessors introduced in 3d0db60f4199fcb8d452a01aa2c95ed28225ab0c when using textures

include/color_spinor_field_order.h
@@ -866,7 +866,7 @@ namespace quda {
         TexVector vecTmp = tex1Dfetch<TexVector>(tex, parity*tex_offset + stride*i + x);
         // now insert into output array
 #pragma unroll
-        for (int j=0; j<N; j++) copy(v[i*N+j], reinterpret_cast<Float*>(&vecTmp)[j]);
+        for (int j=0; j<N; j++) copy(v[i*N+j], reinterpret_cast<RegType*>(&vecTmp)[j]);
         if ( isFixed<Float>::value ) {
 #pragma unroll
           for (int j=0; j<N; j++) v[i*N+j] *= nrm;
@@ -964,7 +964,7 @@ namespace quda {
       if (!huge_alloc) { // use textures unless we have a huge alloc
         TexVector vecTmp = tex1Dfetch<TexVector>(ghostTex, parity*tex_offset + stride*i + x);
 #pragma unroll
-        for (int j=0; j<N; j++) copy(v[i*N+j], reinterpret_cast<Float*>(&vecTmp)[j]);
+        for (int j=0; j<N; j++) copy(v[i*N+j], reinterpret_cast<RegType*>(&vecTmp)[j]);
         if (isFixed<Float>::value ) {
 #pragma unroll
           for (int i=0; i<N; i++) v[i*N+j] *= nrm;