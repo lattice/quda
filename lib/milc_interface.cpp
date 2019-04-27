818f46ed039286089586779c361d619d5816f9b5
maddyscientist <mclark@nvidia.com>
2019-04-27 13:55:54 -0700

    Markup invalidateGaugeQuda in milc interface tracking

--- lib/milc_interface.cpp
+++ lib/milc_interface.cpp
@@ -223,8 +223,10 @@ static QudaGaugeParam newMILCGaugeParam(const int* dim, QudaPrecision prec, Quda
 }
 
 static  void invalidateGaugeQuda() {
+  qudamilc_called<true>(__func__);
   freeGaugeQuda();
   invalidate_quda_gauge = true;
+  qudamilc_called<false>(__func__);
 }
 
 void qudaLoadKSLink(int prec, QudaFatLinkArgs_t fatlink_args,
