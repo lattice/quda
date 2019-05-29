e9180200e9e6eab5985ef99f4b33aa93b7bd095f
maddyscientist <mclark@nvidia.com>
2019-05-29 00:12:22 -0700

    Clean up of deflation.cpp

--- lib/deflation.cpp
+++ lib/deflation.cpp
@@ -4,8 +4,6 @@
 
 #include <memory>
 
-
-
 #ifdef MAGMA_LIB
 #include <blas_magma.h>
 #endif
@@ -16,26 +14,20 @@ namespace quda
 {
 
   using namespace blas;
-
   using namespace Eigen;
-
   using DynamicStride = Stride<Dynamic, Dynamic>;
 
   static auto pinned_allocator = [] (size_t bytes ) { return static_cast<Complex*>(pool_pinned_malloc(bytes)); };
   static auto pinned_deleter   = [] (Complex *hptr) { pool_pinned_free(hptr); };
 
-
-  //static bool debug = false;
-
   Deflation::Deflation(DeflationParam &param, TimeProfile &profile)
     : param(param),   profile(profile),
-      r(nullptr), Av(nullptr), r_sloppy(nullptr), Av_sloppy(nullptr) {
-
-
+      r(nullptr), Av(nullptr), r_sloppy(nullptr), Av_sloppy(nullptr)
+  {
     // for reporting level 1 is the fine level but internally use level 0 for indexing
     printfQuda("Creating deflation space of %d vectors.\n", param.tot_dim);
 
-    if( param.eig_global.import_vectors ) loadVectors(param.RV);//whether to load eigenvectors
+    if ( param.eig_global.import_vectors ) loadVectors(param.RV);//whether to load eigenvectors
     // create aux fields
     ColorSpinorParam csParam(param.RV->Component(0));
     csParam.create = QUDA_ZERO_FIELD_CREATE;
@@ -52,19 +44,16 @@ namespace quda
     r  = ColorSpinorField::Create(csParam);
     Av = ColorSpinorField::Create(csParam);
 
-    if(param.eig_global.cuda_prec_ritz != QUDA_DOUBLE_PRECISION ) { //allocate sloppy fields
-
+    if (param.eig_global.cuda_prec_ritz != QUDA_DOUBLE_PRECISION ) { //allocate sloppy fields
       csParam.setPrecision(param.eig_global.cuda_prec_ritz);//accum fields always full precision
       if (csParam.location==QUDA_CUDA_FIELD_LOCATION) csParam.fieldOrder = QUDA_FLOAT4_FIELD_ORDER;
 
       r_sloppy  = ColorSpinorField::Create(csParam);
       Av_sloppy = ColorSpinorField::Create(csParam);
-
     } else {
       r_sloppy  = r;
       Av_sloppy = Av;
     }
-    
 
     printfQuda("Deflation space setup completed\n");
     // now we can run through the verification if requested
@@ -73,8 +62,8 @@ namespace quda
     if (getVerbosity() >= QUDA_SUMMARIZE) profile.Print();
   }
 
-  Deflation::~Deflation() {
-
+  Deflation::~Deflation()
+  {
     if( param.eig_global.cuda_prec_ritz != QUDA_DOUBLE_PRECISION ) {
       if (r_sloppy) delete r_sloppy;
       if (Av_sloppy) delete Av_sloppy;
@@ -86,9 +75,9 @@ namespace quda
     if (getVerbosity() >= QUDA_SUMMARIZE) profile.Print();
   }
 
-  double Deflation::flops() const {
+  double Deflation::flops() const
+  {
     double flops = 0;//Do we need to report this?
-    
     //compute total flops for deflation application. Not sure we really need this.
     return flops;
   }
@@ -96,10 +85,10 @@ namespace quda
   /**
      Verification that the computed approximate eigenvectors are (not) valid
   */
-
-  void Deflation::verify() {
+  void Deflation::verify()
+  {
     const int nevs_to_print = param.cur_dim;
-    if(nevs_to_print == 0) errorQuda("\nIncorrect size of current deflation space. \n"); 
+    if (nevs_to_print == 0) errorQuda("Incorrect size of current deflation space");
 
     std::unique_ptr<Complex, decltype(pinned_deleter) > projm( pinned_allocator(param.ld*param.cur_dim * sizeof(Complex)), pinned_deleter);
 
@@ -109,7 +98,7 @@ namespace quda
       std::unique_ptr<double[] > evals(new double[param.ld]);
       magma_Xheev(projm.get(), param.cur_dim, param.ld, evals.get(), sizeof(Complex));
 #else
-      errorQuda("MAGMA library was not built.\n");
+      errorQuda("MAGMA library was not built");
 #endif
     } else if( param.eig_global.extlib_type == QUDA_EIGEN_EXTLIB ) {
       Map<MatrixXcd, Unaligned, DynamicStride> projm_(param.matProj, param.cur_dim, param.cur_dim, DynamicStride(param.ld, 1));
@@ -118,7 +107,7 @@ namespace quda
       SelfAdjointEigenSolver<MatrixXcd> es_projm( projm_ );
       evecs_.block(0, 0, param.cur_dim, param.cur_dim) = es_projm.eigenvectors();
     } else {
-      errorQuda("Library type %d is currently not supported.\n", param.eig_global.extlib_type);
+      errorQuda("Library type %d is currently not supported", param.eig_global.extlib_type);
     }
 
     std::vector<ColorSpinorField*> rv(param.RV->Components().begin(), param.RV->Components().begin() + param.cur_dim);
@@ -127,32 +116,22 @@ namespace quda
 
     for (int i = 0; i < nevs_to_print; i++) {
       zero(*r);
-
       blas::caxpy(&projm.get()[i * param.ld], rv, res); // multiblas
-
       *r_sloppy = *r;
-
       param.matDeflation(*Av_sloppy, *r_sloppy);
-
       double3 dotnorm = cDotProductNormA(*r_sloppy, *Av_sloppy);
-
       double eval = dotnorm.x / dotnorm.z;
-
       blas::xpay(*Av_sloppy, -eval, *r_sloppy);
-
       double relerr = sqrt(norm2(*r_sloppy) / dotnorm.z);
-
       printfQuda("Eigenvalue %d: %1.12e Residual: %1.12e\n", i + 1, eval, relerr);
     }
-
-    return;
   }
 
-  void Deflation::operator()(ColorSpinorField &x, ColorSpinorField &b) {
-
+  void Deflation::operator()(ColorSpinorField &x, ColorSpinorField &b)
+  {
     if (param.eig_global.invert_param->inv_type != QUDA_EIGCG_INVERTER
         && param.eig_global.invert_param->inv_type != QUDA_INC_EIGCG_INVERTER)
-      errorQuda("\nMethod is not implemented for %d inverter type.\n", param.eig_global.invert_param->inv_type);
+      errorQuda("Method is not implemented for %d inverter type", param.eig_global.invert_param->inv_type);
 
     if(param.cur_dim == 0) return;//nothing to do
 
@@ -176,7 +155,7 @@ namespace quda
 #ifdef MAGMA_LIB
         magma_Xgesv(vec.get(), param.ld, param.cur_dim, param.matProj, param.ld, sizeof(Complex));
 #else
-        errorQuda("MAGMA library was not built.\n");
+        errorQuda("MAGMA library was not built");
 #endif
       } else if (param.eig_global.extlib_type == QUDA_EIGEN_EXTLIB) {
         Map<MatrixXcd, Unaligned, DynamicStride> projm_(param.matProj, param.cur_dim, param.cur_dim,
@@ -185,10 +164,9 @@ namespace quda
 
         VectorXcd vec2_(param.cur_dim);
         vec2_ = projm_.fullPivHouseholderQr().solve(vec_);
-
         vec_ = vec2_;
       } else {
-        errorQuda("Library type %d is currently not supported.\n", param.eig_global.extlib_type);
+        errorQuda("Library type %d is currently not supported", param.eig_global.extlib_type);
       }
     } else {
       for(int i = 0; i < param.cur_dim; i++) vec[i] *= param.invRitzVals[i];
@@ -201,20 +179,19 @@ namespace quda
 
     check_nrm2 = norm2(x);
     printfQuda("\nDeflated guess spinor norm (gpu): %1.15e\n", sqrt(check_nrm2));
-
-    return;
   }
 
-  void Deflation::increment(ColorSpinorField &Vm, int nev) {
+  void Deflation::increment(ColorSpinorField &Vm, int nev)
+  {
     if (param.eig_global.invert_param->inv_type != QUDA_EIGCG_INVERTER
         && param.eig_global.invert_param->inv_type != QUDA_INC_EIGCG_INVERTER)
-      errorQuda("\nMethod is not implemented for %d inverter type.\n", param.eig_global.invert_param->inv_type);
+      errorQuda("Method is not implemented for %d inverter type", param.eig_global.invert_param->inv_type);
 
     if( nev == 0 ) return; //nothing to do
 
     const int first_idx = param.cur_dim;
 
-    if(param.RV->CompositeDim() < (first_idx+nev) || param.tot_dim < (first_idx+nev)) { 
+    if (param.RV->CompositeDim() < (first_idx+nev) || param.tot_dim < (first_idx+nev)) {
       warningQuda("\nNot enough space to add %d vectors. Keep deflation space unchanged.\n", nev);
       return;
     }
@@ -235,7 +212,6 @@ namespace quda
 
       int offset = 0;
       while (offset < i) {
-
         const int local_length = (i - offset) > cdot_pipeline_length ? cdot_pipeline_length : (i - offset);
 
         std::vector<ColorSpinorField *> vj_(param.RV->Components().begin() + offset,
@@ -245,7 +221,6 @@ namespace quda
 
         blas::cDotProduct(alpha.get(), vj_, vi_);
         for (int j = 0; j < local_length; j++) alpha[j] = -alpha[j];
-
         blas::caxpy(alpha.get(), vj_, vi_); // i-<j,i>j
 
         offset += cdot_pipeline_length;
@@ -256,7 +231,7 @@ namespace quda
       if (alpha[0].real() > 1e-16)
         blas::ax(1.0 / sqrt(alpha[0].real()), *accum);
       else
-        errorQuda("\nCannot orthogonalize %dth vector\n", i);
+        errorQuda("Cannot orthogonalize %dth vector", i);
 
       param.RV->Component(i) = *accum;
 
@@ -282,11 +257,11 @@ namespace quda
     param.cur_dim += nev;
 
     printfQuda("\nNew curr deflation space dim = %d\n", param.cur_dim);
-    return;
   }
 
 
-  void Deflation::reduce(double tol, int max_nev) {
+  void Deflation::reduce(double tol, int max_nev)
+  {
     if (param.cur_dim < max_nev) {
       printf("\nToo big number of eigenvectors was requested, switched to maximum available number %d\n", param.cur_dim);
       max_nev = param.cur_dim;
@@ -302,19 +277,17 @@ namespace quda
 #ifdef MAGMA_LIB
       magma_Xheev(projm.get(), param.cur_dim, param.ld, evals.get(), sizeof(Complex));
 #else
-      errorQuda("MAGMA library was not built.\n");
+      errorQuda("MAGMA library was not built");
 #endif
     } else if (param.eig_global.extlib_type == QUDA_EIGEN_EXTLIB) {
       Map<MatrixXcd, Unaligned, DynamicStride> projm_(projm.get(), param.cur_dim, param.cur_dim,
                                                       DynamicStride(param.ld, 1));
       Map<VectorXd, Unaligned> evals_(evals.get(), param.cur_dim);
-
       SelfAdjointEigenSolver<MatrixXcd> es(projm_);
-
       projm_ = es.eigenvectors();
       evals_ = es.eigenvalues();
     } else {
-      errorQuda("Library type %d is currently not supported.\n", param.eig_global.extlib_type);
+      errorQuda("Library type %d is currently not supported", param.eig_global.extlib_type);
     }
 
     // reset projection matrix, now we will use inverse ritz values when deflate an initial guess:
@@ -323,7 +296,7 @@ namespace quda
       if (fabs(evals[i]) > 1e-16) {
         param.invRitzVals[i] = 1.0 / evals[i];
       } else {
-        errorQuda("\nCannot invert Ritz value.\n");
+        errorQuda("Cannot invert Ritz value");
       }
     }
 
@@ -350,23 +323,17 @@ namespace quda
       blas::caxpy(&projm.get()[idx * param.ld], rv, res); // multiblas
       blas::copy(buff->Component(idx), *r);
 
-      if (do_residual_check) // if tol=0.0 then disable relative residual norm check
-      {
+      if (do_residual_check) {// if tol=0.0 then disable relative residual norm check
         *r_sloppy = *r;
         param.matDeflation(*Av_sloppy, *r_sloppy);
-
         double3 dotnorm = cDotProductNormA(*r_sloppy, *Av_sloppy);
-
         double eval = dotnorm.x / dotnorm.z;
-
         blas::xpay(*Av_sloppy, -eval, *r_sloppy);
-
         relerr = sqrt(norm2(*r_sloppy) / dotnorm.z);
-
         if (getVerbosity() >= QUDA_VERBOSE) printfQuda("Eigenvalue: %1.12e Residual: %1.12e\n", eval, relerr);
       }
 
-      idx += 1;
+      idx++;
     }
 
     printfQuda("\nReserved eigenvectors: %d\n", idx);
@@ -376,27 +343,24 @@ namespace quda
     // reset current dimension:
     param.cur_dim = idx; // idx never exceeds cur_dim.
     param.tot_dim = idx;
-
-    return;
   }
 
   //supports seperate reading or single file read
-  void Deflation::loadVectors(ColorSpinorField *RV) {
-
-    if(RV->IsComposite()) errorQuda("\nNot a composite field.\n");
+  void Deflation::loadVectors(ColorSpinorField *RV)
+  {
+    if (RV->IsComposite()) errorQuda("Not a composite field");
 
     profile.TPSTOP(QUDA_PROFILE_INIT);
     profile.TPSTART(QUDA_PROFILE_IO);
 
     std::string vec_infile(param.eig_global.vec_infile);
-
-    std::vector<ColorSpinorField*> &B = RV->Components(); 
+    std::vector<ColorSpinorField*> &B = RV->Components();
 
     const int Nvec = B.size();
     printfQuda("Start loading %d vectors from %s\n", Nvec, vec_infile.c_str());
 
     void **V = new void*[Nvec];
-    for (int i=0; i<Nvec; i++) { 
+    for (int i=0; i<Nvec; i++) {
       V[i] = B[i]->V();
       if (V[i] == NULL) {
 	printfQuda("Could not allocate V[%d]\n", i);
@@ -407,25 +371,22 @@ namespace quda
       read_spinor_field(vec_infile.c_str(), &V[0], B[0]->Precision(), B[0]->X(),
 			B[0]->Ncolor(), B[0]->Nspin(), Nvec, 0,  (char**)0);
     } else {
-      errorQuda("No eigenspace file defined.");
+      errorQuda("No eigenspace file defined");
     }
 
     printfQuda("Done loading vectors\n");
     profile.TPSTOP(QUDA_PROFILE_IO);
     profile.TPSTART(QUDA_PROFILE_INIT);
-
-    return;
   }
 
-  void Deflation::saveVectors(ColorSpinorField *RV) {
-    if(RV->IsComposite()) errorQuda("\nNot a composite field.\n");
+  void Deflation::saveVectors(ColorSpinorField *RV)
+  {
+    if (RV->IsComposite()) errorQuda("Not a composite field");
 
     profile.TPSTOP(QUDA_PROFILE_INIT);
     profile.TPSTART(QUDA_PROFILE_IO);
 
     std::string vec_outfile(param.eig_global.vec_outfile);
-
-
     std::vector<ColorSpinorField*> &B = RV->Components();
 
     if (strcmp(param.eig_global.vec_outfile,"")!=0) {
@@ -449,8 +410,6 @@ namespace quda
 
     profile.TPSTOP(QUDA_PROFILE_IO);
     profile.TPSTART(QUDA_PROFILE_INIT);
-
-    return;
   }
 
 } // namespace quda
