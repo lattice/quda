55eb6b871a13d043e43696350da3514103cd9aa6
maddyscientist <mclark@nvidia.com>
2020-01-03 13:44:59 -0800

    Remove explicit unroll parameters from dslash kernels

--- include/kernels/dslash_staggered.cuh
+++ include/kernels/dslash_staggered.cuh
@@ -83,7 +83,7 @@ namespace quda
     typedef Matrix<complex<real>, Arg::nColor> Link;
     const int their_spinor_parity = (arg.nParity == 2) ? 1 - parity : 0;
 
-#pragma unroll 4
+#pragma unroll
     for (int d = 0; d < 4; d++) { // loop over dimension
 
       // standard - forward direction
