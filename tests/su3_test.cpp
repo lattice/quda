lattice/quda

commit fe735502681d9d3e16fb22e7378ab4a22c12f9f0

Remove C++ from QUDA C interface (bool is strictly C++ not C) - replace with QudaBoolean.  Optimization of wilson flow to remove an unnecessary halo exchange

include/gauge_tools.h
@@ -76,14 +76,18 @@ namespace quda
   void OvrImpSTOUTStep(GaugeField &dataDs, GaugeField &dataOr, double rho, double epsilon);
 
   /**
-     @brief Apply Wilson Flow steps W1, W2, Vt to the gauge field
+     @brief Apply Wilson Flow steps W1, W2, Vt to the gauge field.
+     This routine assumes that the input and output fields are
+     extended, with the input field being exchanged prior to calling
+     this function.  On exit from this routine, the output field will
+     have been exchanged.
      @param[out] dataDs Output smeared field
      @param[in] dataTemp Temp space
      @param[in] dataOr Input gauge field
      @param[in] epsilon Step size
      @param[in] wflow_type Wilson (1x1) or Symanzik improved (2x1) staples
   */
-  void WFlowStep(GaugeField &dataDs, GaugeField &dataTemp, GaugeField &dataOr, double epsilon, QudaWFlowType wflow_type);
+  void WFlowStep(GaugeField &out, GaugeField &temp, GaugeField &in, double epsilon, QudaWFlowType wflow_type);
 
   /**
    * @brief Gauge fixing with overrelaxation with support for single and multi GPU.

include/quda.h
@@ -1266,28 +1266,32 @@ extern "C" {
   void performWFlownStep(unsigned int n_steps, double step_size, int meas_interval, QudaWFlowType wflow_type);
 
   /**
-   * Calculates the topological charge from gaugeSmeared, if it exist, or from gaugePrecise if no smeared fields are present.
-   */
-  //double qChargeQuda();
-
-  /**
-   * Calculates the field energy from gaugeSmeared, if it exists, or from gaugePrecise
-   * if no smeared fields are present.
-   * @param energy Array for total, spatial, temporal energies
-   */
-  //void energyQuda(double energy[3]);
-
-  /**
-     @brief Calculates the topological charge from gaugeSmeared, if it exist,
-     or from gaugePrecise if no smeared fields are present.
-     @param[out] qDensity array holding Q charge density
+   * @brief Calculates the topological charge and field energy from
+   * gaugeSmeared, if it exists, or from gaugePrecise if no smeared
+   * fields are present.
+   * @param[out] obs host array that will stores the result.  The
+   * first three elements are the total, spatial and temporal
+   * energies, repsectively, and the final element is the topological
+   * charge.
+   * @param[in] Whether to compute the field energy
+   * @param[in] Whether to compute the topological charge
   */
-  //double qChargeDensityQuda(void *qDensity);
-
-  void tensorObservablesQuda(double obs[4], bool energy = true, bool charge = true);
+  void tensorObservablesQuda(double obs[4], QudaBoolean energy, QudaBoolean charge);
   
-  void tensorDensityObservablesQuda(double obs[4], void *qDensity, bool energy = true, bool charge = true);
-  
+  /**
+   * @brief Calculates the topological density field, as well as the
+   * topological charge and and field energy from gaugeSmeared, if it
+   * exists, or from gaugePrecise if no smeared fields are present.
+   * @param[out] obs host array that will stores the result.  The
+   * first three elements are the total, spatial and temporal
+   * energies, repsectively, and the final element is the topological
+   * charge.
+   * @param[in,out] Pointer to host array of length volume where the
+   * topological charge density will be copied
+   * @param[in] Whether to compute the field energy
+   * @param[in] Whether to compute the topological charge
+  */
+  void tensorDensityObservablesQuda(double obs[4], void *qDensity, QudaBoolean energy, QudaBoolean charge);
   
   /**
    * Public function to perform color contractions of the host spinors x and y.

commit ad027ecd89917899e275755c8efde0df02c5e85e

Reworked gauge-observables interface into struct param driven approach.  Fixed bug in prior commit with wilson flow introduced in fe735502681d9d3e16fb22e7378ab4a22c12f9f0

include/gauge_tools.h
@@ -4,6 +4,15 @@ namespace quda
 {
 
   /**
+   * @brief Calculates a variety of gauge-field observables.
+   * @param[in] Gauge field upon which we are measuring.
+   * @param[in,out] param Parameter struct that defines which
+   * observables we are making and the resulting observables.
+   * @param[in] profile TimeProfile instance used for profiling.
+  */
+  void gaugeObservables(GaugeField &u, QudaGaugeObservableParam &param, TimeProfile &profile);
+
+  /**
      @brief Compute the plaquette of the gauge field
 
      @param[in] U The gauge field upon which to compute the plaquette

include/quda.h
@@ -1265,33 +1265,26 @@ extern "C" {
    */
   void performWFlownStep(unsigned int n_steps, double step_size, int meas_interval, QudaWFlowType wflow_type);
 
+  typedef struct QudaGaugeObservableParam_s {
+    QudaBoolean compute_plaquette;        /**< Whether to compute the plaquette */
+    double plaquette[3];                  /**< Total, spatial and temporal field energies, respectively */
+    QudaBoolean compute_qcharge;          /**< Whether to compute the topological charge */
+    double qcharge;                       /**< Computed topological charge */
+    QudaBoolean compute_qcharge_density;  /**< Whether to compute the topological charge density */
+    void *qcharge_density;                /**< Pointer to host array of length volume where the q-charge density will be copied */
+    QudaBoolean compute_energy;           /**< Whether to compute the field energy */
+    double energy[3];                     /**< Total, spatial and temporal field energies, respectively */
+  } QudaGaugeObservableParam;
+
   /**
-   * @brief Calculates the topological charge and field energy from
-   * gaugeSmeared, if it exists, or from gaugePrecise if no smeared
-   * fields are present.
-   * @param[out] obs host array that will stores the result.  The
-   * first three elements are the total, spatial and temporal
-   * energies, repsectively, and the final element is the topological
-   * charge.
-   * @param[in] Whether to compute the field energy
-   * @param[in] Whether to compute the topological charge
+   * @brief Calculates a variety of gauge-field observables.  If a
+   * smeared gauge field is presently loaded (in gaugeSmeared) the
+   * observables are computed on this, else the resident gauge field
+   * will be used.
+   * @param[in,out] param Parameter struct that defines which
+   * observables we are making and the resulting observables.
   */
-  void tensorObservablesQuda(double obs[4], QudaBoolean energy, QudaBoolean charge);
-  
-  /**
-   * @brief Calculates the topological density field, as well as the
-   * topological charge and and field energy from gaugeSmeared, if it
-   * exists, or from gaugePrecise if no smeared fields are present.
-   * @param[out] obs host array that will stores the result.  The
-   * first three elements are the total, spatial and temporal
-   * energies, repsectively, and the final element is the topological
-   * charge.
-   * @param[in,out] Pointer to host array of length volume where the
-   * topological charge density will be copied
-   * @param[in] Whether to compute the field energy
-   * @param[in] Whether to compute the topological charge
-  */
-  void tensorDensityObservablesQuda(double obs[4], void *qDensity, QudaBoolean energy, QudaBoolean charge);
+  void gaugeObservablesQuda(QudaGaugeObservableParam *param);
   
   /**
    * Public function to perform color contractions of the host spinors x and y.
@@ -1302,7 +1295,6 @@ extern "C" {
    * @param[in] param meta data for construction of ColorSpinorFields.
    * @param[in] X spacetime data for construction of ColorSpinorFields.
    */
-
   void contractQuda(const void *x, const void *y, void *result, const QudaContractType cType, QudaInvertParam *param,
                     const int *X);
 

lib/CMakeLists.txt
@@ -12,7 +12,7 @@ set (QUDA_OBJS
   solver.cpp inv_bicgstab_quda.cpp inv_cg_quda.cpp inv_bicgstabl_quda.cpp
   inv_multi_cg_quda.cpp inv_eigcg_quda.cpp gauge_ape.cu
   gauge_stout.cu gauge_wilson_flow.cu gauge_plaq.cu gauge_energy.cu
-  laplace.cu gauge_laplace.cpp
+  laplace.cu gauge_laplace.cpp gauge_observable.cpp
   inv_cg3_quda.cpp inv_cg3ne_quda.cpp inv_ca_gcr.cpp inv_ca_cg.cpp
   inv_gcr_quda.cpp inv_mr_quda.cpp inv_sd_quda.cpp inv_xsd_quda.cpp
   inv_pcg_quda.cpp inv_mre.cpp interface_quda.cpp util_quda.cpp

lib/interface_quda.cpp
@@ -209,11 +209,8 @@ static TimeProfile profileWuppertal("wuppertalQuda");
 //!<Profiler for gaussQuda
 static TimeProfile profileGauss("gaussQuda");
 
-//!< Profiler for qChargeQuda
-static TimeProfile profileQCharge("qChargeQuda");
-
 //!< Profiler for tensorQuda
-static TimeProfile profileTensor("tensorQuda");
+static TimeProfile profileGaugeObs("gaugeObservablesQuda");
 
 //!< Profiler for APEQuda
 static TimeProfile profileAPE("APEQuda");
@@ -1545,7 +1542,7 @@ void endQuda(void)
     profileContract.Print();
     profileCovDev.Print();
     profilePlaq.Print();
-    profileQCharge.Print();
+    profileGaugeObs.Print();
     profileAPE.Print();
     profileSTOUT.Print();
     profileOvrImpSTOUT.Print();
@@ -5430,10 +5427,15 @@ void performAPEnStep(unsigned int n_steps, double alpha, int meas_interval)
   GaugeFieldParam gParam(*gaugeSmeared);
   auto *cudaGaugeTemp = new cudaGaugeField(gParam);
   
+  QudaGaugeObservableParam param;
+  param.compute_plaquette = QUDA_BOOLEAN_FALSE;
+  param.compute_qcharge = QUDA_BOOLEAN_TRUE;
+  param.compute_qcharge_density = QUDA_BOOLEAN_FALSE;
+  param.compute_energy = QUDA_BOOLEAN_FALSE;
+
   if (getVerbosity() >= QUDA_SUMMARIZE) {
-    double obs[4];
-    tensorObservablesQuda(obs, QUDA_BOOLEAN_FALSE, QUDA_BOOLEAN_TRUE);
-    printfQuda("Q charge at step %03d = %+.16e\n", 0, obs[3]);
+    gaugeObservablesQuda(&param);
+    printfQuda("Q charge at step %03d = %+.16e\n", 0, param.qcharge);
   }
 
   for (unsigned int i = 0; i < n_steps; i++) {
@@ -5441,9 +5443,8 @@ void performAPEnStep(unsigned int n_steps, double alpha, int meas_interval)
     APEStep(*gaugeSmeared, *cudaGaugeTemp, alpha);
     profileAPE.TPSTOP(QUDA_PROFILE_COMPUTE);
     if ((i + 1) % meas_interval == 0 && getVerbosity() >= QUDA_VERBOSE) {
-      double obs[4];
-      tensorObservablesQuda(obs, QUDA_BOOLEAN_FALSE, QUDA_BOOLEAN_TRUE);
-      printfQuda("Q charge at step %03d = %+.16e\n", i + 1, obs[3]);
+      gaugeObservablesQuda(&param);
+      printfQuda("Q charge at step %03d = %+.16e\n", i + 1, param.qcharge);
     }
   }
   
@@ -5463,10 +5464,15 @@ void performSTOUTnStep(unsigned int n_steps, double rho, int meas_interval)
   GaugeFieldParam gParam(*gaugeSmeared);
   auto *cudaGaugeTemp = new cudaGaugeField(gParam);
 
+  QudaGaugeObservableParam param;
+  param.compute_plaquette = QUDA_BOOLEAN_FALSE;
+  param.compute_qcharge = QUDA_BOOLEAN_TRUE;
+  param.compute_qcharge_density = QUDA_BOOLEAN_FALSE;
+  param.compute_energy = QUDA_BOOLEAN_FALSE;
+
   if (getVerbosity() >= QUDA_SUMMARIZE) {
-    double obs[4];
-    tensorObservablesQuda(obs, QUDA_BOOLEAN_FALSE, QUDA_BOOLEAN_TRUE);
-    printfQuda("Q charge at step %03d = %+.16e\n", 0, obs[3]);
+    gaugeObservablesQuda(&param);
+    printfQuda("Q charge at step %03d = %+.16e\n", 0, param.qcharge);
   }
 
   for (unsigned int i = 0; i < n_steps; i++) {
@@ -5474,9 +5480,8 @@ void performSTOUTnStep(unsigned int n_steps, double rho, int meas_interval)
     STOUTStep(*gaugeSmeared, *cudaGaugeTemp, rho);
     profileSTOUT.TPSTOP(QUDA_PROFILE_COMPUTE);
     if ((i + 1) % meas_interval == 0 && getVerbosity() >= QUDA_VERBOSE) {
-      double obs[4];
-      tensorObservablesQuda(obs, QUDA_BOOLEAN_FALSE, QUDA_BOOLEAN_TRUE);
-      printfQuda("Q charge at step %03d = %+.16e\n", i + 1, obs[3]);
+      gaugeObservablesQuda(&param);
+      printfQuda("Q charge at step %03d = %+.16e\n", i + 1, param.qcharge);
     }
   }
   
@@ -5496,10 +5501,15 @@ void performOvrImpSTOUTnStep(unsigned int n_steps, double rho, double epsilon, i
   GaugeFieldParam gParam(*gaugeSmeared);
   auto *cudaGaugeTemp = new cudaGaugeField(gParam);
 
+  QudaGaugeObservableParam param;
+  param.compute_plaquette = QUDA_BOOLEAN_FALSE;
+  param.compute_qcharge = QUDA_BOOLEAN_TRUE;
+  param.compute_qcharge_density = QUDA_BOOLEAN_FALSE;
+  param.compute_energy = QUDA_BOOLEAN_FALSE;
+
   if (getVerbosity() >= QUDA_SUMMARIZE) {
-    double obs[4];
-    tensorObservablesQuda(obs, QUDA_BOOLEAN_FALSE, QUDA_BOOLEAN_TRUE);
-    printfQuda("Q charge at step %03d = %+.16e\n", 0, obs[3]);
+    gaugeObservablesQuda(&param);
+    printfQuda("Q charge at step %03d = %+.16e\n", 0, param.qcharge);
   }
 
   for (unsigned int i = 0; i < n_steps; i++) {
@@ -5507,9 +5517,8 @@ void performOvrImpSTOUTnStep(unsigned int n_steps, double rho, double epsilon, i
     OvrImpSTOUTStep(*gaugeSmeared, *cudaGaugeTemp, rho, epsilon);
     profileOvrImpSTOUT.TPSTOP(QUDA_PROFILE_COMPUTE);
     if ((i + 1) % meas_interval == 0 && getVerbosity() >= QUDA_VERBOSE) {
-      double obs[4];
-      tensorObservablesQuda(obs, QUDA_BOOLEAN_FALSE, QUDA_BOOLEAN_TRUE);
-      printfQuda("Q charge at step %03d = %+.16e\n", i + 1, obs[3]);
+      gaugeObservablesQuda(&param);
+      printfQuda("Q charge at step %03d = %+.16e\n", i + 1, param.qcharge);
     }
   }
   
@@ -5535,28 +5544,32 @@ void performWFlownStep(unsigned int n_steps, double step_size, int meas_interval
   GaugeField *in = gaugeSmeared;
   GaugeField *out = gaugeAux;
 
-  double3 plaq;
-  double obs[4];
+  QudaGaugeObservableParam param;
+  param.compute_plaquette = QUDA_BOOLEAN_TRUE;
+  param.compute_qcharge = QUDA_BOOLEAN_TRUE;
+  param.compute_qcharge_density = QUDA_BOOLEAN_FALSE;
+  param.compute_energy = QUDA_BOOLEAN_TRUE;
+
   if (getVerbosity() >= QUDA_SUMMARIZE) {
-    tensorObservablesQuda(obs, QUDA_BOOLEAN_TRUE, QUDA_BOOLEAN_TRUE);
-    plaq = plaquette(*in);
+    gaugeObservables(*in, param, profileWFlow);
     printfQuda("flow t, plaquette, E_tot, E_spatial, E_temporal, Q charge\n");
-    printfQuda("%le %.16e %+.16e %+.16e %+.16e %+.16e\n", 0.0, plaq.x, obs[0], obs[1], obs[2], obs[3]);
+    printfQuda("%le %.16e %+.16e %+.16e %+.16e %+.16e\n",
+               0.0, param.plaquette[0], param.energy[0], param.energy[1], param.energy[2], param.qcharge);
   }  
 
   for (unsigned int i = 0; i < n_steps; i++) {    
     // Perform W1, W2, and Vt Wilson Flow steps as defined in
     // https://arxiv.org/abs/1006.4518v3
     profileWFlow.TPSTART(QUDA_PROFILE_COMPUTE);
-    if (i > 1) std::swap(in, out); // output from prior step becomes input for next step
+    if (i > 0) std::swap(in, out); // output from prior step becomes input for next step
 
     WFlowStep(*out, *gaugeTemp, *in, step_size, wflow_type);
     profileWFlow.TPSTOP(QUDA_PROFILE_COMPUTE);
     
     if ((i + 1) % meas_interval == 0 && getVerbosity() >= QUDA_SUMMARIZE) {
-      tensorObservablesQuda(obs, QUDA_BOOLEAN_TRUE, QUDA_BOOLEAN_TRUE);
-      plaq = plaquette(*out);
-      printfQuda("%le %.16e %+.16e %+.16e %+.16e %+.16e\n", step_size*(i+1), plaq.x, obs[0], obs[1], obs[2], obs[3]);
+      gaugeObservables(*out, param, profileWFlow);
+      printfQuda("%le %.16e %+.16e %+.16e %+.16e %+.16e\n",
+                 step_size*(i+1), param.plaquette[0], param.energy[0], param.energy[1], param.energy[2], param.qcharge);
     }
   }
   
@@ -5570,7 +5583,6 @@ int computeGaugeFixingOVRQuda(void *gauge, const unsigned int gauge_dir, const u
                               const unsigned int reunit_interval, const unsigned int stopWtheta, QudaGaugeParam *param,
                               double *timeinfo)
 {
-
   GaugeFixOVRQuda.TPSTART(QUDA_PROFILE_TOTAL);
 
   checkGaugeParam(param);
@@ -5589,9 +5601,7 @@ int computeGaugeFixingOVRQuda(void *gauge, const unsigned int gauge_dir, const u
   auto *cudaInGauge = new cudaGaugeField(gParam);
 
   GaugeFixOVRQuda.TPSTOP(QUDA_PROFILE_INIT);
-
   GaugeFixOVRQuda.TPSTART(QUDA_PROFILE_H2D);
-
 
   ///if (!param->use_resident_gauge) {   // load fields onto the device
   cudaInGauge->loadCPUField(*cpuGauge);
@@ -5779,73 +5789,20 @@ void contractQuda(const void *hp_x, const void *hp_y, void *h_result, const Quda
   profileContract.TPSTOP(QUDA_PROFILE_TOTAL);
 }
 
-void tensorObservablesQuda(double obs[4], QudaBoolean compute_energy, QudaBoolean compute_qcharge) {
-  void *h_qDensity = nullptr;
-  tensorDensityObservablesQuda(obs, h_qDensity, compute_energy, compute_qcharge);
-}
-
-void tensorDensityObservablesQuda(double obs[4], void *h_qDensity, QudaBoolean compute_energy, QudaBoolean compute_qcharge)
+void gaugeObservablesQuda(QudaGaugeObservableParam *param)
 {
-  profileTensor.TPSTART(QUDA_PROFILE_TOTAL);
+  profileGaugeObs.TPSTART(QUDA_PROFILE_TOTAL);
+
+  profileGaugeObs.TPSTART(QUDA_PROFILE_INIT);
   cudaGaugeField *gauge = nullptr;
   if (!gaugeSmeared) {
-    if (!extendedGaugeResident) extendedGaugeResident = createExtendedGauge(*gaugePrecise, R, profileTensor);
+    if (!extendedGaugeResident) extendedGaugeResident = createExtendedGauge(*gaugePrecise, R, profileGaugeObs);
     gauge = extendedGaugeResident;
   } else {
     gauge = gaugeSmeared;
   }
+  profileGaugeObs.TPSTOP(QUDA_PROFILE_INIT);
 
-  profileTensor.TPSTART(QUDA_PROFILE_INIT);
-  // create the Fmunu field
-  GaugeFieldParam tensorParam(gaugePrecise->X(), gauge->Precision(), QUDA_RECONSTRUCT_NO, 0, QUDA_TENSOR_GEOMETRY);
-  tensorParam.siteSubset = QUDA_FULL_SITE_SUBSET;
-  tensorParam.order = QUDA_FLOAT2_GAUGE_ORDER;
-  tensorParam.ghostExchange = QUDA_GHOST_EXCHANGE_NO;
-  cudaGaugeField gaugeFmunu(tensorParam);
-  profileTensor.TPSTOP(QUDA_PROFILE_INIT);
-
-  profileTensor.TPSTART(QUDA_PROFILE_COMPUTE);
-  computeFmunu(gaugeFmunu, *gauge);
-  profileTensor.TPSTOP(QUDA_PROFILE_COMPUTE);
-  profileTensor.TPSTOP(QUDA_PROFILE_TOTAL);
-
-  if(compute_qcharge) {
-    profileQCharge.TPSTART(QUDA_PROFILE_TOTAL);
-    void *d_qDensity = nullptr;
-    size_t size = 0;
-    profileQCharge.TPSTART(QUDA_PROFILE_INIT);
-    if(h_qDensity) {
-      size = gaugeFmunu.Volume() * gaugeFmunu.Precision();
-      d_qDensity = device_malloc(size);
-    }
-    profileQCharge.TPSTOP(QUDA_PROFILE_INIT);
-
-    profileQCharge.TPSTART(QUDA_PROFILE_COMPUTE);
-    if(h_qDensity) obs[3] = quda::computeQChargeDensity(gaugeFmunu, d_qDensity);
-    else obs[3] = quda::computeQCharge(gaugeFmunu);
-    profileQCharge.TPSTOP(QUDA_PROFILE_COMPUTE);
-
-    if(h_qDensity) {
-      profileQCharge.TPSTART(QUDA_PROFILE_D2H);
-      qudaMemcpy(h_qDensity, d_qDensity, size, cudaMemcpyDeviceToHost);
-      profileQCharge.TPSTOP(QUDA_PROFILE_D2H);
-      
-      profileQCharge.TPSTART(QUDA_PROFILE_FREE);
-      device_free(d_qDensity);
-      profileQCharge.TPSTOP(QUDA_PROFILE_FREE);
-    }
-    profileQCharge.TPSTOP(QUDA_PROFILE_TOTAL);
-  }
-    
-  if(compute_energy) {
-    profileQCharge.TPSTART(QUDA_PROFILE_TOTAL);
-    profileQCharge.TPSTART(QUDA_PROFILE_COMPUTE);
-    double3 energy3 = quda::computeEnergy(gaugeFmunu);
-    // Volume normalised in kernel reduction
-    obs[0] = energy3.x;
-    obs[1] = energy3.y;
-    obs[2] = energy3.z;
-    profileQCharge.TPSTOP(QUDA_PROFILE_COMPUTE);
-    profileQCharge.TPSTOP(QUDA_PROFILE_TOTAL);
-  }
+  gaugeObservables(*gauge, *param, profileGaugeObs);
+  profileGaugeObs.TPSTOP(QUDA_PROFILE_TOTAL);
 }

lib/tune.cpp
@@ -291,7 +291,6 @@ namespace quda {
   static void broadcastTuneCache()
   {
 #ifdef MULTI_GPU
-
     std::stringstream serialized;
     size_t size;
 

tests/heatbath_test.cpp
@@ -38,22 +38,22 @@ void
 display_test_info()
 {
   printfQuda("running the following test:\n");
-    
+
   printfQuda("prec    sloppy_prec    link_recon  sloppy_link_recon S_dimension T_dimension Ls_dimension\n");
   printfQuda("%s   %s             %s            %s            %d/%d/%d          %d         %d\n",
 	     get_prec_str(prec),get_prec_str(prec_sloppy),
-	     get_recon_str(link_recon), 
-	     get_recon_str(link_recon_sloppy),  xdim, ydim, zdim, tdim, Lsdim);     
+	     get_recon_str(link_recon),
+	     get_recon_str(link_recon_sloppy),  xdim, ydim, zdim, tdim, Lsdim);
 
-  printfQuda("Grid partition info:     X  Y  Z  T\n"); 
-  printfQuda("                         %d  %d  %d  %d\n", 
+  printfQuda("Grid partition info:     X  Y  Z  T\n");
+  printfQuda("                         %d  %d  %d  %d\n",
 	     dimPartitioned(0),
 	     dimPartitioned(1),
 	     dimPartitioned(2),
-	     dimPartitioned(3)); 
-  
+	     dimPartitioned(3));
+
   return ;
-  
+
 }
 
 QudaPrecision &cpu_prec = prec;
@@ -71,7 +71,7 @@ void setGaugeParam(QudaGaugeParam &gauge_param) {
   gauge_param.type = QUDA_WILSON_LINKS;
   gauge_param.gauge_order = QUDA_QDP_GAUGE_ORDER;
   gauge_param.t_boundary = QUDA_PERIODIC_T;
-  
+
   gauge_param.cpu_prec = cpu_prec;
 
   gauge_param.cuda_prec = cuda_prec;
@@ -92,7 +92,7 @@ void setGaugeParam(QudaGaugeParam &gauge_param) {
   int pad_size =MAX(x_face_size, y_face_size);
   pad_size = MAX(pad_size, z_face_size);
   pad_size = MAX(pad_size, t_face_size);
-  gauge_param.ga_pad = pad_size;    
+  gauge_param.ga_pad = pad_size;
 #endif
 }
 
@@ -221,7 +221,7 @@ int main(int argc, char **argv)
     printfQuda("  %d Warmup steps\n", nwarm);
     printfQuda("  %d Measurement steps\n", nsteps);
 
-    if (strcmp(latfile,"")) { // Copy in loaded gauge field 
+    if (strcmp(latfile,"")) { // Copy in loaded gauge field
 
       printfQuda("Loading the gauge field in %s\n", latfile);
 
@@ -248,23 +248,26 @@ int main(int argc, char **argv)
     gauge_param.location = QUDA_CUDA_FIELD_LOCATION;
 
     loadGaugeQuda(gauge->Gauge_p(), &gauge_param);
-    double3 plaq = plaquette(*gaugeEx);
-    double obs[4];
-    tensorObservablesQuda(obs, QUDA_BOOLEAN_FALSE, QUDA_BOOLEAN_TRUE);
-    printfQuda("Initial gauge field plaquette = %e topological charge = %e\n", plaq.x, obs[3]);
-    
+
+    QudaGaugeObservableParam param;
+    param.compute_plaquette = QUDA_BOOLEAN_TRUE;
+    param.compute_qcharge = QUDA_BOOLEAN_TRUE;
+    param.compute_qcharge_density = QUDA_BOOLEAN_FALSE;
+    param.compute_energy = QUDA_BOOLEAN_FALSE;
+
+    gaugeObservablesQuda(&param);
+    printfQuda("Initial gauge field plaquette = %e topological charge = %e\n", param.plaquette[0], param.qcharge);
+
     // Reunitarization setup
     setReunitarizationConsts();
-    plaquette(*gaugeEx);
 
     // Do a warmup if requested
     if (nwarm > 0) {
       for (int step = 1; step <= nwarm; ++step) {
-        Monte( *gaugeEx, *randstates, beta_value, nhbsteps, novrsteps);  
+        Monte( *gaugeEx, *randstates, beta_value, nhbsteps, novrsteps);
         CallUnitarizeLinks(gaugeEx);
       }
     }
-      
 
     // copy into regular field
     copyExtendedGauge(*gauge, *gaugeEx, QUDA_CUDA_FIELD_LOCATION);
@@ -275,9 +278,8 @@ int main(int argc, char **argv)
     gauge_param.location = QUDA_CUDA_FIELD_LOCATION;
 
     loadGaugeQuda(gauge->Gauge_p(), &gauge_param);
-    plaq = plaquette(*gaugeEx);
-    tensorObservablesQuda(obs, QUDA_BOOLEAN_FALSE, QUDA_BOOLEAN_TRUE);
-    printfQuda("step=0 plaquette = %e topological charge = %e\n", plaq.x, obs[3]);    
+    gaugeObservablesQuda(&param);
+    printfQuda("step=0 plaquette = %e topological charge = %e\n", param.plaquette[0], param.qcharge);
 
     freeGaugeQuda();
 
@@ -292,9 +294,8 @@ int main(int argc, char **argv)
       copyExtendedGauge(*gauge, *gaugeEx, QUDA_CUDA_FIELD_LOCATION);
 
       loadGaugeQuda(gauge->Gauge_p(), &gauge_param);
-      plaq = plaquette(*gaugeEx);
-      tensorObservablesQuda(obs, QUDA_BOOLEAN_FALSE, QUDA_BOOLEAN_TRUE);
-      printfQuda("step=%d plaquette = %e topological charge = %e\n", step, plaq.x, obs[3]);
+      gaugeObservablesQuda(&param);
+      printfQuda("step=%d plaquette = %e topological charge = %e\n", step, param.plaquette[0], param.qcharge);
 
       freeGaugeQuda();
     }
@@ -330,7 +331,7 @@ int main(int argc, char **argv)
     delete gaugeEx;
     //Release all temporary memory used for data exchange between GPUs in multi-GPU mode
     PGaugeExchangeFree();
- 
+
     randstates->Release();
     delete randstates;
   }
@@ -338,14 +339,14 @@ int main(int argc, char **argv)
   // stop the timer
   time0 += clock();
   time0 /= CLOCKS_PER_SEC;
-    
-  //printfQuda("\nDone: %i iter / %g secs = %g Gflops, total time = %g secs\n", 
+
+  //printfQuda("\nDone: %i iter / %g secs = %g Gflops, total time = %g secs\n",
   //inv_param.iter, inv_param.secs, inv_param.gflops/inv_param.secs, time0);
-  printfQuda("\nDone, total time = %g secs\n", 
+  printfQuda("\nDone, total time = %g secs\n",
 	 time0);
 
   freeGaugeQuda();
-  
+
   // finalize the QUDA library
   endQuda();
 

tests/su3_test.cpp
@@ -178,19 +178,25 @@ int main(int argc, char **argv)
   display_test_info();
 
   // Topological charge and gauge energy
-  double obs[4], q_charge_check = 0.0;
+  double q_charge_check = 0.0;
   // Size of floating point data
   size_t sSize = prec == QUDA_DOUBLE_PRECISION ? sizeof(double) : sizeof(float);
   size_t array_size = V * sSize;
   void *qDensity = malloc(array_size);
   // start the timer
   double time0 = -((double)clock());
-  tensorDensityObservablesQuda(obs, qDensity, QUDA_BOOLEAN_TRUE, QUDA_BOOLEAN_TRUE);
+  QudaGaugeObservableParam param;
+  param.compute_qcharge = QUDA_BOOLEAN_TRUE;
+  param.compute_qcharge_density = QUDA_BOOLEAN_TRUE;
+  param.qcharge_density = qDensity;
+  param.compute_energy = QUDA_BOOLEAN_TRUE;
+
+  gaugeObservablesQuda(&param);
 
   // stop the timer
   time0 += clock();
   time0 /= CLOCKS_PER_SEC;
-  printfQuda("Computed Etot, Es, Et, Q is\n%.16e %.16e, %.16e %.16e\nDone in %g secs\n", obs[0], obs[1], obs[2], obs[3], time0);
+  printfQuda("Computed Etot, Es, Et, Q is\n%.16e %.16e, %.16e %.16e\nDone in %g secs\n", param.energy[0], param.energy[1], param.energy[2], param.qcharge, time0);
   
   // Ensure host array sums to return value
   if (prec == QUDA_DOUBLE_PRECISION) {
@@ -202,7 +208,7 @@ int main(int argc, char **argv)
   // Q charge Reduction and normalisation
   comm_allreduce(&q_charge_check);
 
-  printfQuda("GPU value %e and host density sum %e. Q charge deviation: %e\n", obs[3], q_charge_check, obs[3] - q_charge_check);
+  printfQuda("GPU value %e and host density sum %e. Q charge deviation: %e\n", param.qcharge, q_charge_check, param.qcharge - q_charge_check);
   
   // Gauge Smearing Routines
   //---------------------------------------------------------------------------