532219aa9b2afd3c8b8ddcb976e0abd1eb9fb007
cpviolator <dmhowarth26@gmail.com>
2020-05-10 12:01:26 -0700

    added files

---
+++ programs/example_utils.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <quda.h>
+#include <quda_internal.h>
+#include <dirac_quda.h>
+#include <dslash_quda.h>
+#include <invert_quda.h>
+#include <util_quda.h>
+#include <blas_quda.h>
+
+#include <command_line_params.h>
+#include <host_utils.h>
+#include <misc.h>
+
+#include <stoch_laph_quark_smear.h>
+
+using namespace quda;
+
+//!< Profiler for laphInvertSourcesQuda
+static TimeProfile profileLaphInvert("laphInvertSourcesQuda");
+
+void display_driver_info() {
+  printfQuda("Running the example driver:\n");
+    
+  printfQuda("prec    prec_sloppy   multishift  matpc_type  recon  recon_sloppy solve_type S_dimension T_dimension Ls_dimension   dslash_type  normalization\n");
+  printfQuda("%6s   %6s          %d     %12s     %2s     %2s         %10s %3d/%3d/%3d     %3d         %2d       %14s  %8s\n",
+             get_prec_str(prec), get_prec_str(prec_sloppy), multishift, get_matpc_str(matpc_type),
+             get_recon_str(link_recon), get_recon_str(link_recon_sloppy),
+             get_solve_str(solve_type), xdim, ydim, zdim, tdim, Lsdim,
+             get_dslash_str(dslash_type), get_mass_normalization_str(normalization));
+
+  if(inv_multigrid) {
+    printfQuda("MG parameters\n");
+    printfQuda(" - number of levels %d\n", mg_levels);
+    for (int i=0; i<mg_levels-1; i++) {
+      printfQuda(" - level %d number of null-space vectors %d\n", i+1, nvec[i]);
+      printfQuda(" - level %d number of pre-smoother applications %d\n", i+1, nu_pre[i]);
+      printfQuda(" - level %d number of post-smoother applications %d\n", i+1, nu_post[i]);
+    }
+
+    printfQuda("MG Eigensolver parameters\n");
+    for (int i = 0; i < mg_levels; i++) {
+      if (low_mode_check || mg_eig[i]) {
+	printfQuda(" - level %d solver mode %s\n", i + 1, get_eig_type_str(mg_eig_type[i]));
+	printfQuda(" - level %d spectrum requested %s\n", i + 1, get_eig_spectrum_str(mg_eig_spectrum[i]));
+	printfQuda(" - level %d number of eigenvectors requested nConv %d\n", i + 1, nvec[i]);
+	printfQuda(" - level %d size of eigenvector search space %d\n", i + 1, mg_eig_nEv[i]);
+	printfQuda(" - level %d size of Krylov space %d\n", i + 1, mg_eig_nKr[i]);
+	printfQuda(" - level %d solver tolerance %e\n", i + 1, mg_eig_tol[i]);
+	printfQuda(" - level %d convergence required (%s)\n", i + 1, mg_eig_require_convergence[i] ? "true" : "false");
+	printfQuda(" - level %d Operator: daggered (%s) , norm-op (%s)\n", i + 1, mg_eig_use_dagger[i] ? "true" : "false",
+		   mg_eig_use_normop[i] ? "true" : "false");
+	if (mg_eig_use_poly_acc[i]) {
+	  printfQuda(" - level %d Chebyshev polynomial degree %d\n", i + 1, mg_eig_poly_deg[i]);
+	  printfQuda(" - level %d Chebyshev polynomial minumum %e\n", i + 1, mg_eig_amin[i]);
+	  if (mg_eig_amax[i] <= 0)
+	    printfQuda(" - level %d Chebyshev polynomial maximum will be computed\n", i + 1);
+	  else
+	    printfQuda(" - level %d Chebyshev polynomial maximum %e\n", i + 1, mg_eig_amax[i]);
+	}
+	printfQuda("\n");
+      }
+    }
+  }
+  
+  if (inv_deflate) {
+    printfQuda("\n   Eigensolver parameters\n");
+    printfQuda(" - solver mode %s\n", get_eig_type_str(eig_type));
+    printfQuda(" - spectrum requested %s\n", get_eig_spectrum_str(eig_spectrum));
+    printfQuda(" - number of eigenvectors requested %d\n", eig_nConv);
+    printfQuda(" - size of eigenvector search space %d\n", eig_nEv);
+    printfQuda(" - size of Krylov space %d\n", eig_nKr);
+    printfQuda(" - solver tolerance %e\n", eig_tol);
+    printfQuda(" - convergence required (%s)\n", eig_require_convergence ? "true" : "false");
+    if (eig_compute_svd) {
+      printfQuda(" - Operator: MdagM. Will compute SVD of M\n");
+      printfQuda(" - ***********************************************************\n");
+      printfQuda(" - **** Overriding any previous choices of operator type. ****\n");
+      printfQuda(" - ****    SVD demands normal operator, will use MdagM    ****\n");
+      printfQuda(" - ***********************************************************\n");
+    } else {
+      printfQuda(" - Operator: daggered (%s) , norm-op (%s)\n", eig_use_dagger ? "true" : "false",
+                 eig_use_normop ? "true" : "false");
+    }
+    if (eig_use_poly_acc) {
+      printfQuda(" - Chebyshev polynomial degree %d\n", eig_poly_deg);
+      printfQuda(" - Chebyshev polynomial minumum %e\n", eig_amin);
+      if (eig_amax <= 0)
+        printfQuda(" - Chebyshev polynomial maximum will be computed\n");
+      else
+        printfQuda(" - Chebyshev polynomial maximum %e\n\n", eig_amax);
+    }
+  }
+  
+  printfQuda("Grid partition info:     X  Y  Z  T\n");
+  printfQuda("                         %d  %d  %d  %d\n", dimPartitioned(0), dimPartitioned(1), dimPartitioned(2),
+             dimPartitioned(3));
+}
+
+void exampleInvert(std::vector<ColorSpinorField*> &quarks, QudaInvertParam *inv_param)
+{  
+  bool pc_solve = (inv_param->solve_type == QUDA_DIRECT_PC_SOLVE) ||
+    (inv_param->solve_type == QUDA_NORMOP_PC_SOLVE) || (inv_param->solve_type == QUDA_NORMERR_PC_SOLVE);
+
+  Dirac *d = nullptr;
+  Dirac *dSloppy = nullptr;
+  Dirac *dPre = nullptr;
+
+  // create the dirac operator
+  createDirac(d, dSloppy, dPre, *inv_param, pc_solve);
+
+  Dirac &dirac = *d;
+  Dirac &diracSloppy = *dSloppy;
+  Dirac &diracPre = *dPre;
+
+  // `in` will point to the relevant quark[i] source
+  // `out` will be copy back to quark[i]
+  ColorSpinorField *in = nullptr;
+  ColorSpinorField *out = nullptr;
+  ColorSpinorField *x = nullptr;
+  ColorSpinorField *b = nullptr;
+   
+  ColorSpinorParam cuda_param(*quarks[0]);
+  b = ColorSpinorField::Create(cuda_param);  
+  x = ColorSpinorField::Create(cuda_param);
+  
+  // Zero solver stats
+  inv_param->secs = 0;
+  inv_param->gflops = 0;
+  inv_param->iter = 0;
+  double secs = 0.0;
+  double gflops = 0.0;
+  int iter = 0;
+  
+  for(int i=0; i<(int)quarks.size(); i++) {
+    *b = *quarks[i];
+    dirac.prepare(in, out, *x, *b, inv_param->solution_type);  
+    DiracM m(dirac), mSloppy(diracSloppy), mPre(diracPre);
+    SolverParam solverParam(*inv_param);    
+    Solver *solve = Solver::create(solverParam, m, mSloppy, mPre, profileLaphInvert);
+    
+    (*solve)(*out, *in);
+
+    *quarks[i] = *x;
+    solverParam.updateInvertParam(*inv_param);
+
+    // Accumulate Solver stats
+    secs += inv_param->secs;
+    gflops += inv_param->gflops;
+    iter += inv_param->iter;
+    // Zero solver stats
+    inv_param->secs = 0;
+    inv_param->gflops = 0;
+    inv_param->iter = 0;
+    delete solve;
+  }
+  
+  delete x;
+  delete b;
+  delete d;
+  delete dSloppy;
+  delete dPre;
+}
+
+void exampleContract(std::vector<ColorSpinorField*> &quarks, void *host_corr)
+{
+  int n_quarks = (int)quarks.size();
+  for(int i=0; i<n_quarks; i++) {
+    exampleContractQuda(*quarks[i], (double*)host_corr);
+  }
+}
+
+void programExample(void **host_quarks, QudaInvertParam inv_param, const int X[4])
+{
+  // Parameter object describing the sources/quarks
+  ColorSpinorParam cpu_quark_param(host_quarks[0], inv_param, X, false, QUDA_CPU_FIELD_LOCATION);
+  cpu_quark_param.gammaBasis = QUDA_UKQCD_GAMMA_BASIS;
+  // QUDA style wrappers around the host data
+  std::vector<ColorSpinorField*> quarks;
+  quarks.reserve(n_sources);
+  for (int i = 0; i < n_sources; i++) {
+    cpu_quark_param.v = host_quarks[i];
+    quarks.push_back(ColorSpinorField::Create(cpu_quark_param));
+  }  
+  
+  // Create device vectors for sources/quarks
+  ColorSpinorParam cuda_quark_param(cpu_quark_param);
+  cuda_quark_param.location = QUDA_CUDA_FIELD_LOCATION;
+  cuda_quark_param.create = QUDA_ZERO_FIELD_CREATE;
+  cuda_quark_param.setPrecision(inv_param.cpu_prec, inv_param.cpu_prec, true);
+  std::vector<ColorSpinorField *> quda_quarks;
+  quda_quarks.reserve(n_sources);
+  for (int i = 0; i < n_sources; i++) {
+    quda_quarks.push_back(ColorSpinorField::Create(cuda_quark_param));
+    // Copy data from host to device
+    *quda_quarks[i] = *quarks[i];
+  }
+
+  // The sources are located in quda_quarks. We invert using those
+  // sources and place the propagator from that solve back into quda_quarks.
+  // The code in exampleInvert is very similar to the code found in the
+  // invertQuda() function in interface_quda.cpp.
+  double time_inv = -clock();
+  exampleInvert(quda_quarks, &inv_param);
+  time_inv += clock();
+  
+  // We now perfrom a contraction on the propagators, written using the
+  // QUDA framework, but external to the QUDA library.
+  // The code is an augmented version of QUDA's contraction code found
+  // in lib/contrcat.cu and include/kernels/contraction.cuh.
+  // Use it as demonstration to show that one can write GPU kernels,
+  // autotuned by QUDA tuning.
+  // It is a simple gamma5 insertion <phi| gamma_5 |phi> typical
+  // for a pion correlation function. The correlation function is placed
+  // in host_corr.
+  
+  double time_contract = -clock();
+  exampleContract(quda_quarks, host_corr);
+  time_lsp += clock();
+
+  printfQuda("Invert time = %e\n", time_lsc/CLOCKS_PER_SEC);
+  printfQuda("Contraction time = %e\n", time_lsi/CLOCKS_PER_SEC);
+
+  // Clean up memory allocations
+  for (int i = 0; i < n_sources; i++) {
+    delete quarks[i];
+    delete quda_quarks[i];
+  }
+}
