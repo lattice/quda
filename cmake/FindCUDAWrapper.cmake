be315faa9095a6934ece2c5edd122546ca265319
Mathias Wagner <mathiasw@nvidia.com>
2020-05-16 21:41:32 +0200

    first iteration of cmake cleanup and fixes for 2.17

--- cmake/FindCUDAWrapper.cmake
+++ cmake/FindCUDAWrapper.cmake
@@ -8,10 +8,10 @@
 # FindCUDAWrapper.cmake
 
 #Very important the first step is to enable the CUDA language.
-enable_language(CUDA)
+
 
 # Find the CUDA_INCLUDE_DIRS and CUDA_TOOLKIT_INCLUDE like FindCUDA does
-find_path(CUDA_TOOLKIT_INCLUDE
+find_path(CUDAToolkit_INCLUDE_DIR
   device_functions.h # Header included in toolkit
   PATHS ${CMAKE_CUDA_IMPLICIT_LINK_DIRECTORIES}
   PATH_SUFFIXES include ../include
@@ -23,36 +23,36 @@ set(CUDA_TOOLKIT_TARGET_DIR_INTERNAL "${CUDA_TOOLKIT_TARGET_DIR}" CACHE INTERNAL
 set(CUDA_INCLUDE_DIRS ${CUDA_TOOLKIT_INCLUDE})
 
 
-# Setup CUDA_LIBRARIES
-set(CUDA_LIBRARIES ${CMAKE_CUDA_IMPLICIT_LINK_LIBRARIES})
-if(APPLE)
-  # We need to add the default path to the driver (libcuda.dylib) as an rpath, so that
-  # the static cuda runtime can find it at runtime.
-  list(APPEND CUDA_LIBRARIES -Wl,-rpath,/usr/local/cuda/lib)
-endif()
+# # Setup CUDA_LIBRARIES
+# set(CUDA_LIBRARIES ${CMAKE_CUDA_IMPLICIT_LINK_LIBRARIES})
+# if(APPLE)
+#   # We need to add the default path to the driver (libcuda.dylib) as an rpath, so that
+#   # the static cuda runtime can find it at runtime.
+#   list(APPEND CUDA_LIBRARIES -Wl,-rpath,/usr/local/cuda/lib)
+# endif()
 
-# wrapper for cuda_add_library
-# Issues:
-#
-function(cuda_add_library)
-  add_library(${ARGV})
-  target_include_directories(${ARGV0} PUBLIC
-                             ${CUDA_INCLUDE_DIRS})
-  target_link_libraries(${ARGV0} PUBLIC ${CUDA_LIBRARIES})
-  set_target_properties(${ARGV0} PROPERTIES LINKER_LANGUAGE CUDA)
-endfunction()
+# # wrapper for cuda_add_library
+# # Issues:
+# #
+# function(cuda_add_library)
+#   add_library(${ARGV})
+#   target_include_directories(${ARGV0} PUBLIC
+#                              ${CUDA_INCLUDE_DIRS})
+#   target_link_libraries(${ARGV0} PUBLIC ${CUDA_LIBRARIES})
+#   set_target_properties(${ARGV0} PROPERTIES LINKER_LANGUAGE CUDA)
+# endfunction()
 
 
-# wrapper for cuda_add_library
-# Issues:
-#
-function(cuda_add_executable)
-  add_executable(${ARGV})
-  target_include_directories(${ARGV0} PUBLIC
-                             ${CUDA_INCLUDE_DIRS})
-  target_link_libraries(${ARGV0}  ${CUDA_LIBRARIES})
-  set_target_properties(${ARGV0} PROPERTIES LINKER_LANGUAGE CUDA)
-endfunction()
+# # wrapper for cuda_add_library
+# # Issues:
+# #
+# function(cuda_add_executable)
+#   add_executable(${ARGV})
+#   target_include_directories(${ARGV0} PUBLIC
+#                              ${CUDA_INCLUDE_DIRS})
+#   target_link_libraries(${ARGV0}  ${CUDA_LIBRARIES})
+#   set_target_properties(${ARGV0} PROPERTIES LINKER_LANGUAGE CUDA)
+# endfunction()
 
 
 find_package(CUDALibs)
