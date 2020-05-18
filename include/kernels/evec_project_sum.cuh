a888241922f2415798d7183b44c18b07a5a78931
maddyscientist <mclark@nvidia.com>
2020-05-17 19:37:56 -0700

    Simplified the evec projection sum kernel

--- include/kernels/evec_project_sum.cuh
+++ include/kernels/evec_project_sum.cuh
@@ -10,17 +10,17 @@
 namespace quda
 {
 
-  template <typename Float_, int nColor_, int t_> struct EvecProjectSumArg :
+  template <typename Float_, int nColor_> struct EvecProjectSumArg :
     public ReduceArg<double2>
   {
     int threads; // number of active threads required
     int X[4]; // true grid dimensions
-    
+    int vol_3d;
+
     using Float = Float_;
     static constexpr int nColor = nColor_;
     static_assert(nColor == 3, "Only nColor=3 enabled at this time");
     
-    static constexpr int t = t_;
     static constexpr int nSpinX = 4;
     static constexpr int nSpinY = 1;
     static constexpr bool spin_project = true;
@@ -35,64 +35,60 @@ namespace quda
     
     EvecProjectSumArg(const ColorSpinorField &x_vec, const ColorSpinorField &y_vec) :
       ReduceArg<double2>(),
-      threads(x_vec.VolumeCB()),
+      threads(x_vec.VolumeCB() / x_vec.X(3)),
+      vol_3d(1),
       x_vec(x_vec),
       y_vec(y_vec)
     {
-      for (int dir=0; dir<4; ++dir) X[dir] = x_vec.X()[dir];
+      for (int dir=0; dir<4; ++dir) {
+        X[dir] = x_vec.X()[dir];
+        if (dir < 3) vol_3d *= X[dir];
+      }
     }
   };
 
   template <int blockSize, typename Arg> __global__ void computeEvecProjectSum(Arg arg)
   {
-    int idx_cb = threadIdx.x + blockIdx.x * blockDim.x;
+    int t = blockIdx.z; // map t to z block index
+    int xyz = threadIdx.x + blockIdx.x * blockDim.x;
     int parity = threadIdx.y;
-    
+
     using real = typename Arg::Float;
     constexpr int nSpinX = Arg::nSpinX;
     constexpr int nSpinY = Arg::nSpinY;
     constexpr int nColor = Arg::nColor;
-    constexpr int t = Arg::t;
     typedef ColorSpinor<real, nColor, nSpinX> Vector4;
     typedef ColorSpinor<real, nColor, nSpinY> Vector1;
 
-    double2 res_local[t*nSpinX];
-    for(int i=0; i<t*nSpinX; i++) {
-      res_local[i].x = 0.0;
-      res_local[i].y = 0.0;
-    }
-    
-    int x[4];
-    complex<real> res[nSpinX];
+    double2 res[nSpinX];
+    for (int i=0; i<nSpinX; i++) res[i] = make_double2(0.0, 0.0);
+
     Vector4 x_vec_local;
     Vector1 y_vec_local;
     
-    while (idx_cb < arg.threads) {
+    // the while loop is restricted to the same time slice
+    while (xyz < arg.threads) {
 
-      // Compute spacetime and local coords to query t coord
-      getCoords(x, idx_cb, arg.X, parity);
-      
+      // we now have a coordinate in 3-d space, and a t coordinate
+      // need to find the corresponding 4-d coordinate for this
+      // I suspect this isn't quite correct
+      int idx_cb = t * arg.vol_3d + xyz;
+
       // Get vector data for this spacetime point
       x_vec_local = arg.x_vec(idx_cb, parity);
       y_vec_local = arg.y_vec(idx_cb, parity);
-      
+     
       // Compute the inner product over color
       for (int mu = 0; mu < nSpinX; mu++) {
-	res[mu] = innerProduct(y_vec_local, x_vec_local, 0, mu);
+	auto res_ = innerProduct(y_vec_local, x_vec_local, 0, mu);
+        res[mu].x += res_.real();
+        res[mu].y += res_.imag();
       }
 
-      // Place color contracted data in local array for reduction
-      for (int mu = 0; mu < nSpinX; mu++) {
-	
-	res_local[x[3]*nSpinX + mu].x += res[mu].x; 
-	res_local[x[3]*nSpinX + mu].y += res[mu].y;
-	
-	// Fake data to test veracity 
-	//res_local[x[3]*nSpinX + mu].x = 1.0*(x[3]+1)*(mu+1); 
-	//res_local[x[3]*nSpinX + mu].y = 2.0*(x[3]+1)*(mu+1);	    
-      }
-      idx_cb += blockDim.x * gridDim.x;
+      xyz += blockDim.x * gridDim.x;
     }
-    for(int i=0; i<nSpinX*t; i++) reduce2d<blockSize, 2>(arg, res_local[i], i);
+
+    for (int i=0; i<nSpinX; i++) reduce2d<blockSize, 2>(arg, res[i], t * nSpinX + i);
   }
+
 } // namespace quda
--- lib/evec_project_sum.cu
+++ lib/evec_project_sum.cu
@@ -11,7 +11,7 @@
 namespace quda
 {
 
-  template <typename Float, typename Arg> class EvecProjectSumCompute : TunableLocalParity
+  template <typename Arg> class EvecProjectSumCompute : TunableLocalParity
   {
     Arg &arg;
     const ColorSpinorField &x;
@@ -21,8 +21,8 @@ namespace quda
     bool tuneSharedBytes() const { return false; }
     bool tuneGridDim() const { return false; } // Don't tune the grid dimensions.
     unsigned int minThreads() const { return arg.threads; }
-    unsigned int tuneBlockDimMultiple() const { return x.X()[0]*x.X()[1]*x.X()[2]/2; } // Only tune multiples 32 that are factors of this number
-    
+    //unsigned int tuneBlockDimMultiple() const { return x.X()[0]*x.X()[1]*x.X()[2]/2; } // Only tune multiples 32 that are factors of this number
+
   public:
     EvecProjectSumCompute(Arg &arg, const ColorSpinorField &x, const ColorSpinorField &y) :
       TunableLocalParity(),
@@ -40,9 +40,10 @@ namespace quda
     void apply(const cudaStream_t &stream)
     {
       if (x.Location() == QUDA_CUDA_FIELD_LOCATION) {
-	for (int i=0; i<2*x.Nspin()*Arg::t; i++) ((double*)arg.result_h)[i] = 0.0; 
+	for (int i=0; i<2*x.Nspin()*x.X(3); i++) ((double*)arg.result_h)[i] = 0.0; 
         TuneParam tp = tuneLaunch(*this, getTuning(), getVerbosity());
-	
+        tp.grid.z = x.X(3);
+
 #ifdef JITIFY
         std::string function_name = "quda::spin";
 	
@@ -72,19 +73,19 @@ namespace quda
 
     long long bytes() const
     {
-      return x.Bytes() + y.Bytes() + x.Nspin() * x.Nspin() * x.Volume() * sizeof(complex<Float>);
+      return x.Bytes() + y.Bytes();
     }
   };
   
-  template <typename Float, int nColor, int t>
+  template <typename Float, int nColor>
   void evecProjectSum(const ColorSpinorField &x, const ColorSpinorField &y, double *result)
   {
-    EvecProjectSumArg<Float, nColor, t> arg(x, y);
-    EvecProjectSumCompute<Float, EvecProjectSumArg<Float, nColor, t>> evec_project_sum(arg, x, y);
+    EvecProjectSumArg<Float, nColor> arg(x, y);
+    EvecProjectSumCompute<decltype(arg)> evec_project_sum(arg, x, y);
     evec_project_sum.apply(0);
     qudaDeviceSynchronize();
 
-    for(int i=0; i<2*x.Nspin()*t; i++) result[i] = ((Float*)arg.result_h)[i];
+    for (int i=0; i<2*x.Nspin()*x.X(4); i++) result[i] = ((Float*)arg.result_h)[i];
   }
   
   void evecProjectSumQuda(const ColorSpinorField &x, const ColorSpinorField &y, double *result)
@@ -94,106 +95,13 @@ namespace quda
     if (x.Ncolor() != 3 || y.Ncolor() != 3) errorQuda("Unexpected number of colors x=%d y=%d", x.Ncolor(), y.Ncolor());
     if (x.Nspin() != 4 || y.Nspin() != 1) errorQuda("Unexpected number of spins x=%d y=%d", x.Nspin(), y.Nspin());
     
-    if(x.Ncolor() == 3) {
-
-      int t_dim_size = x.X()[3];
-
-      switch(t_dim_size) {
-      case 4 :
-	if (x.Precision() == QUDA_SINGLE_PRECISION) {
-	  evecProjectSum<float, 3, 4>(x, y, result);
-	} else if (x.Precision() == QUDA_DOUBLE_PRECISION) {
-	  evecProjectSum<double, 3, 4>(x, y, result);
-	} else {
-	  errorQuda("Precision %d not supported", x.Precision());
-	}
-	break;
-
-      case 8 :
-	if (x.Precision() == QUDA_SINGLE_PRECISION) {
-	  evecProjectSum<float, 3, 8>(x, y, result);
-	} else if (x.Precision() == QUDA_DOUBLE_PRECISION) {
-	  evecProjectSum<double, 3, 8>(x, y, result);
-	} else {
-	  errorQuda("Precision %d not supported", x.Precision());
-	}
-	break;
-
-      case 12 :
-	if (x.Precision() == QUDA_SINGLE_PRECISION) {
-	  evecProjectSum<float, 3, 12>(x, y, result);
-	} else if (x.Precision() == QUDA_DOUBLE_PRECISION) {
-	  evecProjectSum<double, 3, 12>(x, y, result);
-	} else {
-	  errorQuda("Precision %d not supported", x.Precision());
-	}
-	break;
-
-      case 16 :
-	if (x.Precision() == QUDA_SINGLE_PRECISION) {
-	  evecProjectSum<float, 3, 16>(x, y, result);
-	} else if (x.Precision() == QUDA_DOUBLE_PRECISION) {
-	  evecProjectSum<double, 3, 16>(x, y, result);
-	} else {
-	  errorQuda("Precision %d not supported", x.Precision());
-	}
-	break;
-
-      case 24 :
-	if (x.Precision() == QUDA_SINGLE_PRECISION) {
-	  evecProjectSum<float, 3, 24>(x, y, result);
-	} else if (x.Precision() == QUDA_DOUBLE_PRECISION) {
-	  evecProjectSum<double, 3, 24>(x, y, result);
-	  printfQuda("Double prec!\n");
-	} else {
-	  errorQuda("Precision %d not supported", x.Precision());
-	}
-	break;
-
-      case 32 :
-	if (x.Precision() == QUDA_SINGLE_PRECISION) {
-	  evecProjectSum<float, 3, 32>(x, y, result);
-	} else if (x.Precision() == QUDA_DOUBLE_PRECISION) {
-	  evecProjectSum<double, 3, 32>(x, y, result);
-	} else {
-	  errorQuda("Precision %d not supported", x.Precision());
-	}
-	break;
-
-      case 48 :
-	if (x.Precision() == QUDA_SINGLE_PRECISION) {
-	  evecProjectSum<float, 3, 48>(x, y, result);
-	} else if (x.Precision() == QUDA_DOUBLE_PRECISION) {
-	  evecProjectSum<double, 3, 48>(x, y, result);
-	} else {
-	  errorQuda("Precision %d not supported", x.Precision());
-	}
-	break;
-
-
-      case 64 :
-	if (x.Precision() == QUDA_SINGLE_PRECISION) {
-	  evecProjectSum<float, 3, 64>(x, y, result);
-	} else if (x.Precision() == QUDA_DOUBLE_PRECISION) {
-	  evecProjectSum<double, 3, 64>(x, y, result);
-	} else {
-	  errorQuda("Precision %d not supported", x.Precision());
-	}
-	break;
-
-      case 96 :
-	if (x.Precision() == QUDA_SINGLE_PRECISION) {
-	  evecProjectSum<float, 3, 96>(x, y, result);
-	} else if (x.Precision() == QUDA_DOUBLE_PRECISION) {
-	  evecProjectSum<double, 3, 96>(x, y, result);
-	} else {
-	  errorQuda("Precision %d not supported", x.Precision());
-	}
-	break;
-	
-      default :
-	errorQuda("Uninstantiated T dim %d", t_dim_size);
-	
+    if (x.Ncolor() == 3) {
+      if (x.Precision() == QUDA_SINGLE_PRECISION) {
+        evecProjectSum<float, 3>(x, y, result);
+      } else if (x.Precision() == QUDA_DOUBLE_PRECISION) {
+        evecProjectSum<double, 3>(x, y, result);
+      } else {
+        errorQuda("Precision %d not supported", x.Precision());
       }
     } else {
       errorQuda("nColors = %d is not supported", x.Ncolor());
