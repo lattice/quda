e3f4a9bbf1f0a38ceb6984d2eba3ef0dde5e3e10
maddyscientist <mclark@nvidia.com>
2020-06-05 06:36:33 -0700

    Add temporary allocations to PCG to avoid continual allocation / freeing.  This restores performance to regular CG level

--- lib/inv_pcg_quda.cpp
+++ lib/inv_pcg_quda.cpp
@@ -133,11 +133,31 @@ namespace quda
     csParam.create = QUDA_ZERO_FIELD_CREATE;
     cudaColorSpinorField y(b, csParam);
 
+    csParam.setPrecision(param.precision_sloppy);
+
+    // temporary fields
+    ColorSpinorField *tmpp = ColorSpinorField::Create(csParam);
+    ColorSpinorField *tmp2p = nullptr;
+    ColorSpinorField *tmp3p = nullptr;
+    if (!mat.isStaggered()) {
+      // tmp2 only needed for multi-gpu Wilson-like kernels
+      tmp2p = ColorSpinorField::Create(csParam);
+      // additional high-precision temporary if Wilson and mixed-precision
+      csParam.setPrecision(param.precision);
+      tmp3p = (param.precision != param.precision_sloppy) ? ColorSpinorField::Create(csParam) : tmpp;
+      csParam.setPrecision(param.precision_sloppy);
+    } else {
+      tmp3p = tmp2p = tmpp;
+    }
+    ColorSpinorField &tmp = *tmpp;
+    ColorSpinorField &tmp2 = *tmp2p;
+    ColorSpinorField &tmp3 = *tmp3p;
+
     // compute initial residual
     double r2 = 0.0;
     if (param.use_init_guess == QUDA_USE_INIT_GUESS_YES) {
       // Compute r = b - A * x
-      mat(r, x);
+      mat(r, x, y, tmp3);
       r2 = blas::xmyNorm(b, r);
       if (b2 == 0) b2 = r2;
       // y contains the original guess.
@@ -151,12 +171,10 @@ namespace quda
     if (param.deflate && param.maxiter > 1) {
       // Deflate and accumulate to solution vector
       eig_solve->deflate(y, r, evecs, evals, true);
-      mat(r, y);
+      mat(r, y, x, tmp3);
       r2 = blas::xmyNorm(b, r);
     }
 
-    csParam.setPrecision(param.precision_sloppy);
-    cudaColorSpinorField tmpSloppy(x, csParam);
     cudaColorSpinorField Ap(x, csParam);
 
     cudaColorSpinorField *r_sloppy;
@@ -178,8 +196,8 @@ namespace quda
       x_sloppy = new cudaColorSpinorField(x, csParam);
     }
 
-    cudaColorSpinorField &xSloppy = *x_sloppy;
-    cudaColorSpinorField &rSloppy = *r_sloppy;
+    ColorSpinorField &xSloppy = *x_sloppy;
+    ColorSpinorField &rSloppy = *r_sloppy;
 
     blas::zero(x);
     if (&x != &xSloppy) blas::zero(xSloppy);
@@ -243,7 +261,7 @@ namespace quda
 
     while (!convergence(r2, heavy_quark_res, stop, param.tol_hq) && k < param.maxiter) {
 
-      matSloppy(Ap, *p, tmpSloppy);
+      matSloppy(Ap, *p, tmp, tmp2);
 
       double sigma;
       pAp = reDotProduct(*p, Ap);
@@ -292,7 +310,7 @@ namespace quda
         axpy(alpha, *p, xSloppy); // xSloppy += alpha*p
         xpy(xSloppy, y);          // y += x
         // Now compute r
-        mat(r, y, x); // x is just a temporary here
+        mat(r, y, x, tmp3); // x is just a temporary here
         r2 = xmyNorm(b, r);
 
         if (param.deflate && sqrt(r2) < maxr_deflate * param.tol_restart) {
@@ -300,7 +318,7 @@ namespace quda
           eig_solve->deflate(y, r, evecs, evals, true);
 
           // Compute r_defl = RHS - A * LHS
-          mat(r, y, x);
+          mat(r, y, x, tmp3);
           r2 = blas::xmyNorm(b, r);
 
           maxr_deflate = sqrt(r2);
@@ -367,10 +385,10 @@ namespace quda
 
     if (k == param.maxiter) warningQuda("Exceeded maximum iterations %d", param.maxiter);
 
-    if (getVerbosity() >= QUDA_VERBOSE) printfQuda("CG: Reliable updates = %d\n", rUpdate);
+    if (getVerbosity() >= QUDA_VERBOSE) printfQuda("PCG: Reliable updates = %d\n", rUpdate);
 
     // compute the true residual
-    mat(r, x, y);
+    mat(r, x, y, tmp3);
     double true_res = xmyNorm(b, r);
     param.true_res = sqrt(true_res / b2);
 
@@ -382,6 +400,12 @@ namespace quda
 
     profile.TPSTOP(QUDA_PROFILE_EPILOGUE);
     profile.TPSTART(QUDA_PROFILE_FREE);
+
+    if (tmpp) delete tmpp;
+    if (!mat.isStaggered()) {
+      if (tmp2p && tmpp != tmp2p) delete tmp2p;
+      if (tmp3p && tmpp != tmp3p && param.precision != param.precision_sloppy) delete tmp3p;
+    }
 
     if (K) { // These are only needed if preconditioning is used
       delete minvrPre;
