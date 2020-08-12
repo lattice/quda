e01451e9bf5108f18fe62704319b4c7609e225d0
cpviolator <dmhowarth26@gmail.com>
2020-08-12 13:36:34 -0700

    Revert to previous behaviour where the entire deflation space is kept in memory

--- lib/inv_pcg_quda.cpp
+++ lib/inv_pcg_quda.cpp
@@ -66,7 +66,10 @@ namespace quda
     Kparam(param)
   {
     fillInnerSolverParam(Kparam, param);
-
+    // Preconditioners do not need a deflation space,
+    // so we explicily set this here.
+    Kparam.deflate = false;
+    
     if (param.inv_type_precondition == QUDA_CG_INVERTER) {
       K = new CG(matPrecon, matPrecon, matPrecon, matEig, Kparam, profile);
     } else if (param.inv_type_precondition == QUDA_MR_INVERTER) {
