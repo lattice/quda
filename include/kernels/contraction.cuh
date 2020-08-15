1596fe3ab31098769a56e4f16200a83b51347b0f
cpviolator <dmhowarth26@gmail.com>
2020-08-14 22:18:36 -0700

    Remove duplicated tutorial comments

--- include/kernels/contraction.cuh
+++ include/kernels/contraction.cuh
@@ -8,6 +8,9 @@
 
 namespace quda
 {
+
+  using spinor_array = vector_type<double2, 16>;
+  
   template <typename real> class DRGammaMatrix
   {
 
@@ -192,7 +195,121 @@ namespace quda
     //inline complex<real> get_gm_z(const int G_idx, const int col_idx) const {return gm_z[G_idx][col_idx];};
   };
 
-  using spinor_array = vector_type<double2, 16>;
+  template <typename Float_, int reduction_dim_ = 3> struct ContractionSpatialSumArg :
+    public ReduceArg<spinor_array>  
+  {
+    static constexpr int reduction_dim = reduction_dim_; // This the direction we are performing reduction on. default to 3.
+
+    //- Welcome back! We first define the variables in the argument structure:
+    //- Naturally, this is the number of threads in the block
+    int threads; // number of active threads required
+    //- This is the number of lattice sites on the MPI node
+    int_fastdiv X[4];    // grid dimensions - using int_fastdiv to reduce division overhead on device
+
+    using Float = Float_;
+    static constexpr int nColor = 3;
+    static constexpr int nSpin = 4;
+    static constexpr bool spin_project = true;
+    static constexpr bool spinor_direct_load = false; // false means texture load
+
+    int s1, b1;
+
+    typedef typename colorspinor_mapper<Float, nSpin, nColor, spin_project, spinor_direct_load>::type F;
+    F x;
+    F y;
+
+    DRGammaMatrix<Float_> Gamma;
+    int t_offset;
+    ContractionSpatialSumArg(const ColorSpinorField &x, const ColorSpinorField &y,
+        const int s1, const int b1) :
+      ReduceArg<spinor_array>(comm_dim(reduction_dim) * x.X(reduction_dim)), // n_reduce = global_dim_t
+      threads(x.VolumeCB() / x.X(reduction_dim)),
+      x(x),
+      y(y),
+      s1(s1),
+      b1(b1),
+      Gamma(),
+      t_offset(comm_coord(reduction_dim) * x.X(reduction_dim)) // offset of the slice we are doing reduction on
+    {
+      for (int dir = 0; dir < 4; dir++) { X[dir] = x.X()[dir]; }
+    }
+  };
+
+  template <int t_d, class T> __device__ int x_cb_from_t_xyz_d(int t, int xyz_cb, T X[4], int parity)
+  {
+
+    int x[4];
+    int xyz = xyz_cb * 2;
+
+#pragma unroll
+    for (int d = 0; d < 4; d++) {
+      if (d != t_d) {
+        x[d] = xyz % X[d];
+        xyz /= X[d];
+      }
+    }
+
+    x[t_d] = t;
+
+    if (t_d > 0) {
+      x[0] += (x[0] + x[1] + x[2] + x[3] + parity) & 1;
+    } else {
+      x[1] += (x[0] + x[1] + x[2] + x[3] + parity) & 1;
+    }
+
+    return (((x[3] * X[2] + x[2]) * X[1] + x[1]) * X[0] + x[0]) / 2;
+  }
+
+  template <int blockSize, typename Arg> __global__ void computeDegrandRossiContractionSpatialSum(Arg arg)
+  {
+    int t = blockIdx.z; // map t to z block index
+    int xyz = threadIdx.x + blockIdx.x * blockDim.x;
+    int parity = threadIdx.y;
+
+    int s1 = arg.s1;
+    int b1 = arg.b1;
+
+    using real = typename Arg::Float;
+    constexpr int nSpin = Arg::nSpin;
+    constexpr int nColor = Arg::nColor;
+
+    complex<real> propagator_product;
+
+    // result array needs to be a spinor_array type object because of the reduce function at the end
+    vector_type<double2, 16> result_all_channels;
+
+    while (xyz < arg.threads) { // loop over all space-coordinates of one time slice
+      // extract current ColorSpinor at xyzt from ColorSpinorField
+
+      // This function calculates the index_cb assuming t is the coordinate in
+      // direction reduction_dim, and xyz is the linearized index_cb excluding reduction_dim.
+      // So this will work for reduction_dim < 3 as well.
+      int idx_cb = x_cb_from_t_xyz_d<Arg::reduction_dim>(t, xyz, arg.X, parity);
+      
+      ColorSpinor<real, nColor, nSpin> x = arg.x(idx_cb, parity);
+      ColorSpinor<real, nColor, nSpin> y = arg.y(idx_cb, parity);
+
+      // loop over channels
+      for (int G_idx = 0; G_idx < 16; G_idx++) {
+        for (int s2 = 0; s2 < nSpin; s2++) {
+          int b2 = arg.Gamma.gm_i[G_idx][s2];
+          // get non-zero column index for current s1
+          int b1_tmp = arg.Gamma.gm_i[G_idx][s1];
+          // only contributes if we're at the correct b1 from the outer loop
+          if (b1_tmp == b1) {
+            propagator_product = arg.Gamma.gm_z[G_idx][b2] * innerProduct(x, y, b2, s2) * arg.Gamma.gm_z[G_idx][b1];
+            result_all_channels[G_idx].x += propagator_product.real();
+            result_all_channels[G_idx].y += propagator_product.imag();
+          }
+        }
+      }
+      xyz += blockDim.x * gridDim.x;
+    }
+
+    // This function reduces the data in result_all_channels in all threads -
+    // different threads reduce result to different index t + arg.t_offset
+    reduce2d<blockSize, 2>(arg, result_all_channels, t + arg.t_offset);
+  }
 
   template <typename real> struct ContractionArg {
     int threads; // number of active threads required
@@ -276,46 +393,6 @@ namespace quda
   };
 
   
-  template <typename Float_, int reduction_dim_ = 3> struct ContractionSpatialSumArg :
-    public ReduceArg<spinor_array>  
-  {
-    static constexpr int reduction_dim = reduction_dim_; // This the direction we are performing reduction on. default to 3.
-
-    //- Welcome back! We first define the variables in the argument structure:
-    //- Naturally, this is the number of threads in the block
-    int threads; // number of active threads required
-    //- This is the number of lattice sites on the MPI node
-    int_fastdiv X[4];    // grid dimensions - using int_fastdiv to reduce division overhead on device
-
-    using Float = Float_;
-    static constexpr int nColor = 3;
-    static constexpr int nSpin = 4;
-    static constexpr bool spin_project = true;
-    static constexpr bool spinor_direct_load = false; // false means texture load
-
-    int s1, b1;
-
-    typedef typename colorspinor_mapper<Float, nSpin, nColor, spin_project, spinor_direct_load>::type F;
-    F x;
-    F y;
-
-    DRGammaMatrix<Float_> Gamma;
-    int t_offset;
-    ContractionSpatialSumArg(const ColorSpinorField &x, const ColorSpinorField &y,
-        const int s1, const int b1) :
-      ReduceArg<spinor_array>(comm_dim(reduction_dim) * x.X(reduction_dim)), // n_reduce = global_dim_t
-      threads(x.VolumeCB() / x.X(reduction_dim)),
-      x(x),
-      y(y),
-      s1(s1),
-      b1(b1),
-      Gamma(),
-      t_offset(comm_coord(reduction_dim) * x.X(reduction_dim)) // offset of the slice we are doing reduction on
-    {
-      for (int dir = 0; dir < 4; dir++) { X[dir] = x.X()[dir]; }
-    }
-  };
-
   template <typename real, typename Arg> __global__ void computeColorContraction(Arg arg)
   {
     int x_cb = threadIdx.x + blockIdx.x * blockDim.x;
@@ -669,80 +746,5 @@ namespace quda
     //- quda/lib/contract.cu, and the line:
     //- LAUNCH_KERNEL_LOCAL_PARITY(computeDegrandRossiContractionSum, (*this), tp, stream, arg, Arg);
   }
-  
-  template <int t_d, class T> __device__ int x_cb_from_t_xyz_d(int t, int xyz_cb, T X[4], int parity)
-  {
-
-    int x[4];
-    int xyz = xyz_cb * 2;
-
-#pragma unroll
-    for (int d = 0; d < 4; d++) {
-      if (d != t_d) {
-        x[d] = xyz % X[d];
-        xyz /= X[d];
-      }
-    }
-
-    x[t_d] = t;
-
-    if (t_d > 0) {
-      x[0] += (x[0] + x[1] + x[2] + x[3] + parity) & 1;
-    } else {
-      x[1] += (x[0] + x[1] + x[2] + x[3] + parity) & 1;
-    }
-
-    return (((x[3] * X[2] + x[2]) * X[1] + x[1]) * X[0] + x[0]) / 2;
-  }
-
-  template <int blockSize, typename Arg> __global__ void computeDegrandRossiContractionSpatialSum(Arg arg)
-  {
-    int t = blockIdx.z; // map t to z block index
-    int xyz = threadIdx.x + blockIdx.x * blockDim.x;
-    int parity = threadIdx.y;
-
-    int s1 = arg.s1;
-    int b1 = arg.b1;
-
-    using real = typename Arg::Float;
-    constexpr int nSpin = Arg::nSpin;
-    constexpr int nColor = Arg::nColor;
-
-    complex<real> propagator_product;
-
-    // result array needs to be a spinor_array type object because of the reduce function at the end
-    vector_type<double2, 16> result_all_channels;
-
-    while (xyz < arg.threads) { // loop over all space-coordinates of one time slice
-      // extract current ColorSpinor at xyzt from ColorSpinorField
-
-      // This function calculates the index_cb assuming t is the coordinate in direction reduction_dim,
-      // and xyz is the linearized index_cb excluding reduction_dim. So this will work for reduction_dim < 3 as well.
-      int idx_cb = x_cb_from_t_xyz_d<Arg::reduction_dim>(t, xyz, arg.X, parity);
-
-      ColorSpinor<real, nColor, nSpin> x = arg.x(idx_cb, parity);
-      ColorSpinor<real, nColor, nSpin> y = arg.y(idx_cb, parity);
-
-      // loop over channels
-      for (int G_idx = 0; G_idx < 16; G_idx++) {
-        for (int s2 = 0; s2 < nSpin; s2++) {
-          int b2 = arg.Gamma.gm_i[G_idx][s2];
-          //get non-zero column index for current s1
-          int b1_tmp = arg.Gamma.gm_i[G_idx][s1];
-          //only contributes if were at the correct b1 from the outer loop
-          if (b1_tmp == b1) {
-            propagator_product = arg.Gamma.gm_z[G_idx][b2] * innerProduct(x, y, b2, s2) * arg.Gamma.gm_z[G_idx][b1];
-            result_all_channels[G_idx].x += propagator_product.real();
-            result_all_channels[G_idx].y += propagator_product.imag();
-          }
-        }
-      }
-      xyz += blockDim.x * gridDim.x;
-    }
-
-    // This function reduces the data in result_all_channels in all threads - different threads reduce result to
-    // different index t + arg.t_offset
-    reduce2d<blockSize, 2>(arg, result_all_channels, t + arg.t_offset);
-  }
-
+    
 } // namespace quda
--- lib/contract.cu
+++ lib/contract.cu
@@ -90,63 +90,31 @@ public:
       return x.Bytes() + y.Bytes() + x.Nspin() * x.Nspin() * x.Volume() * sizeof(complex<real>);
     }
   };
-
+  
   template <typename Arg> class ContractionSpatialSumCompute : TunableLocalParityReduction
   {
-    //- The protected members of the class are the argument structure we have already seen, 
-    //- the memory addresses of the two fermion fields to be contracted, and the type of 
-    //- contraction to compute.
   protected:
     Arg &arg;
     const ColorSpinorField &x;
     const ColorSpinorField &y;
     const QudaContractType cType;
 
-    //- The private members are explicit definitions of helper functions defined virtually 
-    //- in the Tunable class. We must define them explicitly whenever we create a class 
-    //- that inherits from Tunable. 
   private:
-    //- In this particular case, we instruct the autotuner to not tune the grid dimensions because
-    //- we are going to use the grid dimension to denote which timeslice we are on.
     bool staticGridDim() const { return true; } // Maintain grid dims set in this class.
-
-    //- We also request that there be a minimum number of threads, given by arg.threads. 
-    //- This value was set when we created the ContractionArg structure. It was inferred 
-    //- from the checkerboard volume of one of the lattice fermions. Make sure you can 
-    //- identify in the body of the ContractionSpatialSumArg code where this happened. 
     unsigned int minThreads() const { return arg.threads; }
-
-    //- We no not tune shared bytes in the kernel as there is no locality to exploit
     bool tuneSharedBytes() const { return false; }
-
-    //- We initialise the tuning parameter object by explitly declaring that there will be
-    //- two parities, denoted by the block index in the .y dim. NB, this is not referencing
-    //- the `y` vector of our data, rather this is a CUDA specific way of splitting up the 
-    //- the block, threads, and grids in a 3D (x,y,z) fashion. We use the .z dim of the grids
-    //- to denote which timeslice we are working on. This is accessed by a handy member
-    //- function of the ColorSpinorField object x, namely `.X(3)`.
     void initTuneParam(TuneParam &param) const {
       TunableLocalParityReduction::initTuneParam(param);
       param.block.y = 2;
       param.grid.z = x.X(Arg::reduction_dim); // T dimension is mapped to different blocks in the Z dimension
     }
 
-    //- This is just making sure the Tunable class defaults to the same params.
     void defaultTuneParam(TuneParam &param) const {
       TunableLocalParityReduction::defaultTuneParam(param);
       param.block.y = 2;
       param.grid.z = x.X(Arg::reduction_dim); // T dimension is mapped to different blocks in the Z dimension
     }
 
-    //- Next is the all important public interface of the class. The first method is the 
-    //- constructor. This is what was called when we created the `ContractionSumCompute` 
-    //- object named `contract_with_sum`.
-    //- It is fairly standard for a constructor. We initialise the class from which we 
-    //- inherit `TunableLocalParityReduction()` which just means that all the data the thread uses 
-    //- is local (no collecting data from other lattice points) and that we may also split the
-    //- threads by parity. That particular class of `Tunable` will then tune for that (and only
-    //- that) type of calculation. We then initialise the arg, teh x and y vectors, and 
-    //- contraction type. 
   public:
     ContractionSpatialSumCompute(Arg &arg, const ColorSpinorField &x, const ColorSpinorField &y, const QudaContractType cType) :
       TunableLocalParityReduction(),
@@ -155,11 +123,6 @@ public:
       y(y),
       cType(cType)
     {
-      //- The object aux is a member of the Tunable class. It is simply a string that defines 
-      //- the kernel name and attributes for the autotuner. Once the kernel has tuned, 
-      //- the autotuner will save the optimal set up to a file and, if the same kernel 
-      //- (as defined by the aux string) is called again, the autotuner will use the 
-      //- optimal setup immediately. 
       switch (cType) {
       case QUDA_CONTRACT_TYPE_OPEN_SUM: strcat(aux, "open-summed,"); break;
       case QUDA_CONTRACT_TYPE_DR_SUM: strcat(aux, "degrand-rossi-summed,"); break;
@@ -167,40 +130,16 @@ public:
       default: errorQuda("Unexpected contraction type %d", cType);
       }
       strcat(aux, x.AuxString());
-      //- The part that is encapsulated by the JITIFY define is an instruction to the compiler 
-      //- that the kernel is to be compiled at run time, or Just In Time.
 #ifdef JITIFY
       create_jitify_program("kernels/contraction.cuh");
 #endif
     }
-    //- The destructor is about as boiler plate as it gets.
     virtual ~ContractionSpatialSumCompute() {}
-    //- Please go back to the creation of the `contract_with_sum` oject below to follow the rest
-    //- of the workflow.
-    
-    //- Ok, here we go, nearly there. We will now apply the kernel computation we have 
-    //- meticulously and assiduously constrcuted. You'll remember that the argument we passed
-    //- to `apply` was `0`. That just means `please use the 0 stream in this function. This 
-    //- is the stream QUDA reserves for main program workflow. Other streams are availible,
-    //- but that is an advanced topic for later exploration.
-    //- Before we go any further we must make note of something very important. `apply` is NOT
-    //- a member function of this class. It is a pure virtual member function of the Tunable class.
-    //- By calling this member function, we have passed control of workflow to the parent class
-    //- namely TunableLocalParityReduction.
+
     void apply(const qudaStream_t &stream)
     {
-      //- QUDA is evolving so that the C++ in the kernels can be compiled by both NVCC and 
-      //- possibly a host compiler such as gcc with parallel stl compatibility. Therefore, 
-      //- we place the CUDA specific code under a boolean x.Location() == QUDA_CUDA_FIELD_LOCATION 
-      //- so we know we are using a GPU.
       if (x.Location() == QUDA_CUDA_FIELD_LOCATION) {
-	//- Ensure the reduce array is zeroed out. This is the array that comes baked in to the
-	//- `ReduceArg` class from which the `arg` structure inherited.
-	//- We next define a TuneParam object that is used by the Tunable class. It takes 
-	//- `this` class (see now what *this means?) and some other data
 	TuneParam tp = tuneLaunch(*this, getTuning(), getVerbosity());
-	//- This, again, is for when Just In Time compilation is used. You can skip over this for 
-	//- now.
 #ifdef JITIFY
         std::string function_name;
         switch (cType) {
@@ -215,31 +154,9 @@ public:
 	  .configure(tp.grid, tp.block, tp.shared_bytes, stream)
 	  .launch(arg);
 #else
-	//- Here are are at the calling of the compute kernels, albeit wrapped in Macros!
-	//- computeColorContractionSum will contract only the colour indices, leaving the 16
-	//- complex numbers per open spin index per lattice site
-	//- (one for each \mu, and \nu combination) and then sum each one of those 16 elements
-	//- with its counterparts on the same timeslice.
-	//- computeDegrandRossiContractionSum will insert all 16 Gamma matrices into the
-	//- contraction, also giving 16 complex numbers, one for each unique gamma matrix 
-	//- combination. It will then do the same timeslice sum. Here we also see how the 
-	//- autotuner works. The standard CUDA kernel arguments of blocks, threads, and 
-	//- shared memory size are controlled by the TuneParam tp object. The macro 
-	//- `LAUNCH_KERNEL_LOCAL_PARITY` will use the tp object to define a set of possible 
-	//- configurations, then save the best one.
         switch (cType) {
-        case QUDA_CONTRACT_TYPE_OPEN_SUM: 
-	  LAUNCH_KERNEL_LOCAL_PARITY(computeColorContractionSum, (*this), tp, stream, arg, Arg); 
-	  break;
-        case QUDA_CONTRACT_TYPE_DR_SUM:
         case QUDA_CONTRACT_TYPE_DR_SUM_SPATIAL:
-	  //- This is the kernel we are going to follow. We will NOT go into the Caves of Moria
-	  //- hunting the veritable Balrog that is QUDA's tuning class! Rather we will cheat
-	  //- slighly and look only at the compute code. Please navigate to 
-	  //- quda/include/kernels/contraction.cuh and the function
-	  //- `computeDegrandRossiContractionSum` then return here.
 	  LAUNCH_KERNEL_LOCAL_PARITY(computeDegrandRossiContractionSpatialSum, (*this), tp, stream, arg, Arg);
-	  //- Welcome back! We're almost done! Back you go to `contraction_with_sum.apply(0);`
           break;
         default: errorQuda("Unexpected contraction type %d", cType);
         }
@@ -272,35 +189,21 @@ public:
   void contract_spatial_quda(const ColorSpinorField &x, const ColorSpinorField &y, const size_t s1,
 			     const size_t b1, complex<real> *result, const QudaContractType cType)
   {
-    if(cType == QUDA_CONTRACT_TYPE_OPEN_SUM || cType == QUDA_CONTRACT_TYPE_DR_SUM) {
-      ContractionSpatialSumArg<real> arg(x, y, s1, b1);
-      ContractionSpatialSumCompute<decltype(arg)> contraction_with_sum(arg, x, y, cType);
-      contraction_with_sum.apply(0);
-      arg.complete(result);
-    } else if (cType == QUDA_CONTRACT_TYPE_DR_SUM_SPATIAL){
+    if (cType == QUDA_CONTRACT_TYPE_DR_SUM_SPATIAL){
       ContractionSpatialSumArg<real, 2> arg(x, y, s1, b1); // reduce in the z direction
       ContractionSpatialSumCompute<decltype(arg)> contraction_with_sum_spatial(arg, x, y, cType);
       contraction_with_sum_spatial.apply(0);
       arg.complete(result);
     } else {
-      ContractionArg<real> arg(x, y, result);
-      Contraction<real, ContractionArg<real>> contraction(arg, x, y, cType);
-      contraction.apply(0);
-      qudaDeviceSynchronize();
+      errorQuda("Unexpected contraction type %d given.", cType);
     }
   }
 
 #endif
 
-  //- The interface function has called the GPU function contractQuda. The first thing to
-  //- notice is that the body of the code is encapsulated in a preprocessor definition,
-  //- which one can adjust at CMake configure time. If the QUDA_CONTRACT CMake option
-  //- is not set, none of the following code will be compiled. This is an important
-  //- feature to include as it keeps compile time to a minimum.
   void contractSpatialQuda(const ColorSpinorField &x, const ColorSpinorField &y, const size_t s1, const size_t b1, void *result, const QudaContractType cType)
   {
 #ifdef GPU_CONTRACT
-    //- After some checks, 
     checkPrecision(x, y);
 
     if (x.GammaBasis() != QUDA_DEGRAND_ROSSI_GAMMA_BASIS || y.GammaBasis() != QUDA_DEGRAND_ROSSI_GAMMA_BASIS)
@@ -308,8 +211,6 @@ public:
     if (x.Ncolor() != 3 || y.Ncolor() != 3) errorQuda("Unexpected number of colors x=%d y=%d", x.Ncolor(), y.Ncolor());
     if (x.Nspin() != 4 || y.Nspin() != 4) errorQuda("Unexpected number of spins x=%d y=%d", x.Nspin(), y.Nspin());
 
-    //- we see that the contract_quda function is instantiated on the desired precision. Go to
-    //- the `contract_quda` and then return here.
     if (x.Precision() == QUDA_SINGLE_PRECISION) {
       contract_spatial_quda<float>(x, y, s1, b1, (complex<float> *)result, cType);
     } else if (x.Precision() == QUDA_DOUBLE_PRECISION) {
@@ -317,24 +218,10 @@ public:
     } else {
       errorQuda("Precision %d not supported", x.Precision());
     }
-    //- All done, head on back to quda/lib/interface_quda.cpp to finish up.
 #else
     errorQuda("Contraction code has not been built");
 #endif
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   //- Welcome! The first thing to notice is that this class inherits from TunableVectorY. 
   //- In fact, all of QUDA's kernel classes inherit from some form of Tunable class. This 
@@ -514,8 +401,6 @@ public:
       return x.Bytes() + y.Bytes();
     }
   };
-
-
   
   //- This deceptively simple function executes the contraction kernel.
   //- We must study it line by line. You are STRONGLY advised to follow the 
@@ -604,6 +489,5 @@ public:
 #else
     errorQuda("Contraction code has not been built");
 #endif
-  }
-  
+  }  
 } // namespace quda
