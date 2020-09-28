1c577a957f1f75db7fc98a1825adce9d2f453c78
cpviolator <dmhowarth26@gmail.com>
2020-09-27 23:51:18 -0400

    Move convergence checks to reduce restarts by one, add OMP parallelism to Eigen

--- lib/eig_iram.cpp
+++ lib/eig_iram.cpp
@@ -241,6 +241,10 @@ namespace quda
       for(int i=0; i<n_kr; i++) upperHess[i][i] -= evals[shift];
       
       for(int i=0; i<n_kr-1; i++) {
+
+	// If the sub-diagonal element is numerically
+	// small enough, there is no need to perfrom a Givens
+	// rotation
 	if (abs(upperHess[i+1][i]) < epsilon) {
 	  upperHess[i+1][i] = 0.0;
 	  continue;
@@ -383,6 +387,9 @@ namespace quda
 
     // Eigen object for computing Ritz values from the upper Hessenberg matrix
     Eigen::ComplexEigenSolver<MatrixXcd> eigenSolverUH;
+
+    omp_set_num_threads(atoi(getenv("OMP_NUM_THREADS")));
+    Eigen::setNbThreads(atoi(getenv("OMP_NUM_THREADS")));
     
     // Print Eigensolver params
     printEigensolverSetup();
@@ -430,32 +437,32 @@ namespace quda
 	  break;
 	}
       }
-      
+
+      int num_keep0 = num_keep;
+      iter_keep = std::min(iter_converged + (n_kr - num_converged) / 2, n_kr - 12);
+
+      num_converged = iter_converged;
+      num_keep = iter_keep;
+      num_shifts = n_kr - num_keep;
+
       if (getVerbosity() >= QUDA_VERBOSE) printfQuda("%04d converged eigenvalues at iter %d\n", num_converged, restart_iter);
 
       if (num_converged >= n_conv) {
-	profile.TPSTOP(QUDA_PROFILE_COMPUTE);
-	eigensolveFromUpperHess(evals, beta);
-	rotateBasis(kSpace, n_kr);
-	reorder(kSpace, evals, eig_param->spectrum);
-	converged = true;
-	profile.TPSTART(QUDA_PROFILE_COMPUTE);
+        profile.TPSTOP(QUDA_PROFILE_COMPUTE);
+        eigensolveFromUpperHess(evals, beta);
+        rotateBasis(kSpace, n_kr);
+        reorder(kSpace, evals, eig_param->spectrum);
+        profile.TPSTART(QUDA_PROFILE_COMPUTE);
 	computeEvals(mat, kSpace, evals);
+        converged = true;
 	
       } else {
-	
-	int num_keep0 = num_keep;
-	iter_keep = std::min(iter_converged + (n_kr - num_converged) / 2, n_kr - 12);
-	
-	num_converged = iter_converged;
-	num_keep = iter_keep;      
-	num_shifts = n_kr - num_keep;
 
-	// If num_keep changed, we resort the Ritz values and residua
-	if(num_keep0 < num_keep) {
-	  sortArrays(eig_param->spectrum, n_kr, evals, residua);
-	  sortArrays(QUDA_SPECTRUM_LM_EIG, num_shifts, residua, evals);    
-	}
+        // If num_keep changed, we resort the Ritz values and residua
+        if(num_keep0 < num_keep) {
+          sortArrays(eig_param->spectrum, n_kr, evals, residua);
+          sortArrays(QUDA_SPECTRUM_LM_EIG, num_shifts, residua, evals);
+        }
 	
 	profile.TPSTOP(QUDA_PROFILE_COMPUTE);
 	// Apply the shifts of the unwated Ritz values via QR
