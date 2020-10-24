e94302ddd07fbaed188a08fb0ab65baff01bb0b8
Balint Joo <bjoo@jlab.org>
2020-10-24 11:40:26 -0400

    Cherry Picked FFT Refactor, changed to use TARGET_CUDA

--- lib/FFT_Plans.h
+++ lib/FFT_Plans.h
@@ -1,7 +1,5 @@
 #include "quda_define.h"
 
-#if defined(QUDA_BUILD_TARGET_CUDA)
+#if defined(TARGET_CUDA)
 #include "targets/cuda/CUFFT_Plans.h"
-#elif defined(QUDA_BUILD_TARGET_HIP)
-#include "targets/hip/HIPFFT_Plans.h"
 #endif
