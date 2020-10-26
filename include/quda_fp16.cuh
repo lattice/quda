4b32b3a6beb3c498fc9712f9492180aadd32ab93
Balint Joo <bjoo@jlab.org>
2020-10-26 15:54:28 -0400

    Removed switchbox headers and renamed the target files as per request from  @maddyscientist

--- include/quda_fp16.cuh
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <quda_define.h>
-
-#if defined(QUDA_TARGET_CUDA)
-#include "targets/cuda/quda_fp16_cuda.cuh"
-#elif defined(QUDA_TARGET_HIP)
-#include "targets/hip/quda_fp16_hip.cuh"
-#endif
--- include/random_quda.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "quda_define.h"
-
-#if defined(QUDA_TARGET_CUDA)
-#include "targets/cuda/random_quda_cuda.h"
-#elif defined(QUDA_TARGET_HIP)
-#include "targets/hip/random_quda_hip.h"
-#endif
