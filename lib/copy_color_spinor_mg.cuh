261f626b328b61290882a14ffd837d32ea276cf6
maddyscientist <mclark@nvidia.com>
2020-10-27 17:49:38 -0700

    Move copy_color_spinor_mg to generic framework

--- lib/copy_color_spinor_mg.cuh
+++ lib/copy_color_spinor_mg.cuh
@@ -8,126 +8,80 @@
   - field ordering
 */
 
-#include <color_spinor_field.h>
-#include <color_spinor_field_order.h>
-#include <tune_quda.h>
 #include <utility> // for std::swap
+#include <color_spinor_field.h>
+#include <tunable_nd.h>
+#include <kernels/copy_color_spinor_mg.cuh>
 
 namespace quda {
 
-  using namespace colorspinor;
-
-  /** CPU function to reorder spinor fields.  */
   template <typename FloatOut, typename FloatIn, int Ns, int Nc, typename OutOrder, typename InOrder>
-    void packSpinor(OutOrder &outOrder, const InOrder &inOrder, int volume) {
-    for (int x=0; x<volume; x++) {
-      for (int s=0; s<Ns; s++) {
-	for (int c=0; c<Nc; c++) {
-	  outOrder(0, x, s, c) = inOrder(0, x, s, c);
-	}
-      }
-    }
-  }
+  class CopySpinor : TunableKernel1D {
+    ColorSpinorField &out;
+    const ColorSpinorField &in;
+    FloatOut *Out;
+    FloatIn *In;
 
-  /** CUDA kernel to reorder spinor fields.  Adopts a similar form as the CPU version, using the same inlined functions. */
-  template <typename FloatOut, typename FloatIn, int Ns, int Nc, typename OutOrder, typename InOrder>
-    __global__ void packSpinorKernel(OutOrder outOrder, const InOrder inOrder, int volume) {
-    int x = blockIdx.x * blockDim.x + threadIdx.x;
-    if (x >= volume) return;
-
-    for (int s=0; s<Ns; s++) {
-      for (int c=0; c<Nc; c++) {
-	outOrder(0, x, s, c) = inOrder(0, x, s, c);
-      }
-    }
-  }
-
-  template <typename FloatOut, typename FloatIn, int Ns, int Nc, typename OutOrder, typename InOrder>
-    class CopySpinor : Tunable {
-    const InOrder &in;
-    OutOrder &out;
-    const ColorSpinorField &meta; // this reference is for meta data only
-    QudaFieldLocation location;
-
-  private:
-    unsigned int sharedBytesPerThread() const { return 0; }
-
-    unsigned int sharedBytesPerBlock(const TuneParam &param) const { return 0; }
     bool advanceSharedBytes(TuneParam &param) const { return false; } // Don't tune shared mem
-    bool tuneGridDim() const { return false; } // Don't tune the grid dimensions.
-    unsigned int minThreads() const { return meta.VolumeCB(); }
+    unsigned int minThreads() const { return in.VolumeCB(); }
 
   public:
-    CopySpinor(OutOrder &out, const InOrder &in, const ColorSpinorField &meta, QudaFieldLocation location)
-      : out(out), in(in), meta(meta), location(location) { }
-    virtual ~CopySpinor() { ; }
-
-    void apply(const qudaStream_t &stream) {
-      if (location == QUDA_CPU_FIELD_LOCATION) {
-	packSpinor<FloatOut, FloatIn, Ns, Nc>(out, in, meta.VolumeCB());
-      } else {
-	TuneParam tp = tuneLaunch(*this, getTuning(), getVerbosity());
-	qudaLaunchKernel(packSpinorKernel<FloatOut, FloatIn, Ns, Nc, OutOrder, InOrder>, tp, stream, out, in, meta.VolumeCB());
-      }
+    CopySpinor(ColorSpinorField &out, const ColorSpinorField &in, QudaFieldLocation location, FloatOut* Out, FloatIn* In) :
+      TunableKernel1D(in, location),
+      out(out),
+      in(in),
+      Out(Out),
+      In(In)
+    {
+      apply(0);
     }
 
-    TuneKey tuneKey() const { return TuneKey(meta.VolString(), typeid(*this).name(), meta.AuxString()); }
+    void apply(const qudaStream_t &stream)
+    {
+      TuneParam tp = tuneLaunch(*this, getTuning(), getVerbosity());
+      launch<CopySpinor_>(tp, stream, CopyArg<Ns, Nc, OutOrder, InOrder>(out, in, Out, In));
+    }
 
     long long flops() const { return 0; }
     long long bytes() const { return in.Bytes() + out.Bytes(); }
   };
 
-
-  template <typename FloatOut, typename FloatIn, int Ns, int Nc, typename OutOrder, typename InOrder>
-    void genericCopyColorSpinor(OutOrder &outOrder, const InOrder &inOrder,
-				const ColorSpinorField &out, QudaFieldLocation location) {
-    CopySpinor<FloatOut, FloatIn, Ns, Nc, OutOrder, InOrder> copy(outOrder, inOrder, out, location);
-    copy.apply(0);
-  }
-
   /** Decide on the output order*/
-  template <typename FloatOut, typename FloatIn, int Ns, int Nc, typename InOrder>
-    void genericCopyColorSpinor(InOrder &inOrder, ColorSpinorField &out,
-				QudaFieldLocation location, FloatOut *Out) {
-
+  template <typename FloatOut, typename FloatIn, int Ns, int Nc, typename I>
+  void genericCopyColorSpinor(ColorSpinorField &out, const ColorSpinorField &in, QudaFieldLocation location,
+                              FloatOut *Out, FloatIn *In)
+  {
     if (out.FieldOrder() == QUDA_FLOAT2_FIELD_ORDER) {
-      typedef typename colorspinor::FieldOrderCB<typename mapper<FloatOut>::type, Ns, Nc, 1, QUDA_FLOAT2_FIELD_ORDER,FloatOut> ColorSpinor;
-      ColorSpinor outOrder(out, 1, Out);
-      genericCopyColorSpinor<FloatOut,FloatIn,Ns,Nc>(outOrder, inOrder, out, location);
+      using O = FieldOrderCB<typename mapper<FloatOut>::type, Ns, Nc, 1, QUDA_FLOAT2_FIELD_ORDER,FloatOut>;
+      CopySpinor<FloatOut, FloatIn, Ns, Nc, O, I>(out, in, location, Out, In);
     } else if (out.FieldOrder() == QUDA_SPACE_SPIN_COLOR_FIELD_ORDER) {
-      typedef typename colorspinor::FieldOrderCB<typename mapper<FloatOut>::type, Ns, Nc, 1, QUDA_SPACE_SPIN_COLOR_FIELD_ORDER,FloatOut> ColorSpinor;
-      ColorSpinor outOrder(out, 1, Out);
-      genericCopyColorSpinor<FloatOut,FloatIn,Ns,Nc>(outOrder, inOrder, out, location);
+      using O = FieldOrderCB<typename mapper<FloatOut>::type, Ns, Nc, 1, QUDA_SPACE_SPIN_COLOR_FIELD_ORDER,FloatOut>;
+      CopySpinor<FloatOut, FloatIn, Ns, Nc, O, I>(out, in, location, Out, In);
     } else {
       errorQuda("Order %d not defined (Ns=%d, Nc=%d)", out.FieldOrder(), Ns, Nc);
     }
-
   }
 
   /** Decide on the input order*/
   template <typename FloatOut, typename FloatIn, int Ns, int Nc>
-    void genericCopyColorSpinor(ColorSpinorField &out, const ColorSpinorField &in,
-				QudaFieldLocation location, FloatOut *Out, FloatIn *In) {
-
+  void genericCopyColorSpinor(ColorSpinorField &out, const ColorSpinorField &in, QudaFieldLocation location,
+                              FloatOut *Out, FloatIn *In)
+  {
     if (in.FieldOrder() == QUDA_FLOAT2_FIELD_ORDER) {
-      typedef typename colorspinor::FieldOrderCB<typename mapper<FloatIn>::type, Ns, Nc, 1, QUDA_FLOAT2_FIELD_ORDER,FloatIn> ColorSpinor;
-      ColorSpinor inOrder(in, 1, In);
-      genericCopyColorSpinor<FloatOut,FloatIn,Ns,Nc>(inOrder, out, location, Out);
+      using I = FieldOrderCB<typename mapper<FloatIn>::type, Ns, Nc, 1, QUDA_FLOAT2_FIELD_ORDER,FloatIn>;
+      genericCopyColorSpinor<FloatOut, FloatIn, Ns, Nc, I>(out, in, location, Out, In);
     } else if (in.FieldOrder() == QUDA_SPACE_SPIN_COLOR_FIELD_ORDER) {
-      typedef typename colorspinor::FieldOrderCB<typename mapper<FloatIn>::type, Ns, Nc, 1, QUDA_SPACE_SPIN_COLOR_FIELD_ORDER,FloatIn> ColorSpinor;
-      ColorSpinor inOrder(in, 1, In);
-      genericCopyColorSpinor<FloatOut,FloatIn,Ns,Nc>(inOrder, out, location, Out);
+      using I = FieldOrderCB<typename mapper<FloatIn>::type, Ns, Nc, 1, QUDA_SPACE_SPIN_COLOR_FIELD_ORDER,FloatIn>;
+      genericCopyColorSpinor<FloatOut, FloatIn, Ns, Nc, I>(out, in, location, Out, In);
     } else {
       errorQuda("Order %d not defined (Ns=%d, Nc=%d)", in.FieldOrder(), Ns, Nc);
     }
-
   }
 
-
   template <int Ns, int Nc, typename dstFloat, typename srcFloat>
-    void copyGenericColorSpinor(ColorSpinorField &dst, const ColorSpinorField &src,
-				QudaFieldLocation location, dstFloat *Dst, srcFloat *Src) {
-
+  void copyGenericColorSpinor(ColorSpinorField &dst, const ColorSpinorField &src,
+                              QudaFieldLocation location, dstFloat *Dst, srcFloat *Src)
+  {
     if (dst.Ndim() != src.Ndim())
       errorQuda("Number of dimensions %d %d don't match", dst.Ndim(), src.Ndim());
 
@@ -174,7 +128,6 @@ namespace quda {
     } else { // parity field
       genericCopyColorSpinor<dstFloat, srcFloat, Ns, Nc>(dst, src, location, Dst, Src);
     }
-
   }
 
   template <int Nc, typename dstFloat, typename srcFloat>
