ea475f6ee8ad3c90bb7852989127a3bb05fa3aad
Jiqun Tu <jtu@nvidia.com>
2020-11-06 11:23:26 -0500

    Use instantiate for simply code for gauge and color spinor copies with offset.

--- lib/copy_color_spinor_offset.cu
+++ lib/copy_color_spinor_offset.cu
@@ -3,62 +3,48 @@
 
 #include <kernels/copy_field_offset.cuh>
 
+#include <instantiate.h>
+
 namespace quda
 {
 
-  template <typename Float, int nColor>
-  void copy_color_spinor_offset(ColorSpinorField &out, const ColorSpinorField &in, const int offset[4])
-  {
-    using Field = ColorSpinorField;
-    using real = typename mapper<Float>::type;
-    using Element = ColorSpinor<real, nColor, 4>;
+  template <class Float, int nColor> struct CopyColorSpinorOffset {
+    CopyColorSpinorOffset(ColorSpinorField &out, const ColorSpinorField &in, const int offset[4])
+    {
+      using Field = ColorSpinorField;
+      using real = typename mapper<Float>::type;
+      using Element = ColorSpinor<real, nColor, 4>;
 
-    if (in.Location() == QUDA_CPU_FIELD_LOCATION) {
-      if (in.FieldOrder() == QUDA_SPACE_COLOR_SPIN_FIELD_ORDER) {
-        using Accessor = typename colorspinor_order_mapper<Float, QUDA_SPACE_COLOR_SPIN_FIELD_ORDER, 4, nColor>::type;
-        using Arg = CopyFieldOffsetArg<Field, Element, Accessor>;
-        Arg arg(out, in, offset);
-        CopyFieldOffset<Arg> dummy(arg, in);
-      } else if (in.FieldOrder() == QUDA_SPACE_SPIN_COLOR_FIELD_ORDER) {
-        using Accessor = typename colorspinor_order_mapper<Float, QUDA_SPACE_SPIN_COLOR_FIELD_ORDER, 4, nColor>::type;
+      if (in.Location() == QUDA_CPU_FIELD_LOCATION) {
+        if (in.FieldOrder() == QUDA_SPACE_COLOR_SPIN_FIELD_ORDER) {
+          using Accessor = typename colorspinor_order_mapper<Float, QUDA_SPACE_COLOR_SPIN_FIELD_ORDER, 4, nColor>::type;
+          using Arg = CopyFieldOffsetArg<Field, Element, Accessor>;
+          Arg arg(out, in, offset);
+          CopyFieldOffset<Arg> dummy(arg, in);
+        } else if (in.FieldOrder() == QUDA_SPACE_SPIN_COLOR_FIELD_ORDER) {
+          using Accessor = typename colorspinor_order_mapper<Float, QUDA_SPACE_SPIN_COLOR_FIELD_ORDER, 4, nColor>::type;
+          using ArgType = CopyFieldOffsetArg<Field, Element, Accessor>;
+          ArgType arg(out, in, offset);
+          CopyFieldOffset<ArgType> dummy(arg, in);
+        } else {
+          errorQuda("Unsupported field order = %d.", in.FieldOrder());
+        }
+      } else {
+
+        if (!in.isNative() || !out.isNative()) { errorQuda("CUDA field has be in native order."); }
+
+        using Accessor = typename colorspinor_mapper<Float, 4, nColor>::type;
         using ArgType = CopyFieldOffsetArg<Field, Element, Accessor>;
         ArgType arg(out, in, offset);
         CopyFieldOffset<ArgType> dummy(arg, in);
-      } else {
-        errorQuda("Unsupported field order = %d.", in.FieldOrder());
       }
-    } else {
-
-      if (!in.isNative() || !out.isNative()) { errorQuda("CUDA field has be in native order."); }
-
-      using Accessor = typename colorspinor_mapper<Float, 4, nColor>::type;
-      using ArgType = CopyFieldOffsetArg<Field, Element, Accessor>;
-      ArgType arg(out, in, offset);
-      CopyFieldOffset<ArgType> dummy(arg, in);
     }
-  }
-
-  // template on the number of colors
-  template <typename Float>
-  void copy_color_spinor_offset(ColorSpinorField &out, const ColorSpinorField &in, const int offset[4])
-  {
-    switch (in.Ncolor()) {
-    case 3: copy_color_spinor_offset<Float, 3>(out, in, offset); break;
-    default: errorQuda("Unsupported number of colors %d\n", in.Ncolor());
-    }
-  }
+  };
 
   void copyFieldOffset(ColorSpinorField &out, const ColorSpinorField &in, const int offset[4])
   {
     checkLocation(out, in); // check all locations match
-
-    switch (checkPrecision(out, in)) {
-    case QUDA_DOUBLE_PRECISION: copy_color_spinor_offset<double>(out, in, offset); break;
-    case QUDA_SINGLE_PRECISION: copy_color_spinor_offset<float>(out, in, offset); break;
-    case QUDA_HALF_PRECISION: copy_color_spinor_offset<short>(out, in, offset); break;
-    case QUDA_QUARTER_PRECISION: copy_color_spinor_offset<int8_t>(out, in, offset); break;
-    default: errorQuda("Unsupported precision %d\n", in.Precision());
-    }
+    instantiate<CopyColorSpinorOffset>(out, in, offset);
   }
 
 } // namespace quda
--- lib/copy_gauge_offset.cu
+++ lib/copy_gauge_offset.cu
@@ -2,148 +2,122 @@
 #include <gauge_field_order.h>
 
 #include <kernels/copy_field_offset.cuh>
+#include <instantiate.h>
 
 namespace quda
 {
 
-  template <typename Float, int nColor> void copy_gauge_offset(GaugeField &out, const GaugeField &in, const int offset[4])
+  template <class Field, class Element, class G>
+  void copy_gauge_offset(Field &out, const Field &in, const int offset[4])
   {
-    using Field = GaugeField;
-    using real = typename mapper<Float>::type;
-    using Element = Matrix<complex<real>, nColor>;
+    using Arg = CopyFieldOffsetArg<Field, Element, G>;
+    Arg arg(out, in, offset);
+    CopyFieldOffset<Arg> copier(arg, in);
+  }
 
-    if (in.isNative()) {
-      if (in.Reconstruct() == QUDA_RECONSTRUCT_NO) {
-        using G = typename gauge_mapper<Float, QUDA_RECONSTRUCT_NO>::type;
-        using Arg = CopyFieldOffsetArg<Field, Element, G>;
-        Arg arg(out, in, offset);
-        CopyFieldOffset<Arg> copier(arg, in);
-      } else if (in.Reconstruct() == QUDA_RECONSTRUCT_12) {
+  template <typename Float, int nColor> struct CopyGaugeOffset {
+    CopyGaugeOffset(GaugeField &out, const GaugeField &in, const int offset[4])
+    {
+      using Field = GaugeField;
+      using real = typename mapper<Float>::type;
+      using Element = Matrix<complex<real>, nColor>;
+
+      if (in.isNative()) {
+        if (in.Reconstruct() == QUDA_RECONSTRUCT_NO) {
+          using G = typename gauge_mapper<Float, QUDA_RECONSTRUCT_NO>::type;
+          copy_gauge_offset<Field, Element, G>(out, in, offset);
+        } else if (in.Reconstruct() == QUDA_RECONSTRUCT_12) {
 #if QUDA_RECONSTRUCT & 2
-        using G = typename gauge_mapper<Float, QUDA_RECONSTRUCT_12>::type;
-        using Arg = CopyFieldOffsetArg<Field, Element, G>;
-        Arg arg(out, in, offset);
-        CopyFieldOffset<Arg> copier(arg, in);
+          using G = typename gauge_mapper<Float, QUDA_RECONSTRUCT_12>::type;
+          copy_gauge_offset<Field, Element, G>(out, in, offset);
 #else
-        errorQuda("QUDA_RECONSTRUCT=%d does not enable reconstruct-12", QUDA_RECONSTRUCT);
+          errorQuda("QUDA_RECONSTRUCT=%d does not enable reconstruct-12", QUDA_RECONSTRUCT);
 #endif
-      } else if (in.Reconstruct() == QUDA_RECONSTRUCT_8) {
+        } else if (in.Reconstruct() == QUDA_RECONSTRUCT_8) {
 #if QUDA_RECONSTRUCT & 1
-        using G = typename gauge_mapper<Float, QUDA_RECONSTRUCT_8>::type;
-        using Arg = CopyFieldOffsetArg<Field, Element, G>;
-        Arg arg(out, in, offset);
-        CopyFieldOffset<Arg> copier(arg, in);
+          using G = typename gauge_mapper<Float, QUDA_RECONSTRUCT_8>::type;
+          copy_gauge_offset<Field, Element, G>(out, in, offset);
 #else
-        errorQuda("QUDA_RECONSTRUCT=%d does not enable reconstruct-8", QUDA_RECONSTRUCT);
+          errorQuda("QUDA_RECONSTRUCT=%d does not enable reconstruct-8", QUDA_RECONSTRUCT);
 #endif
 #ifdef GPU_STAGGERED_DIRAC
-      } else if (in.Reconstruct() == QUDA_RECONSTRUCT_13) {
+        } else if (in.Reconstruct() == QUDA_RECONSTRUCT_13) {
 #if QUDA_RECONSTRUCT & 2
-        using G = typename gauge_mapper<Float, QUDA_RECONSTRUCT_13>::type;
-        using Arg = CopyFieldOffsetArg<Field, Element, G>;
-        Arg arg(out, in, offset);
-        CopyFieldOffset<Arg> copier(arg, in);
+          using G = typename gauge_mapper<Float, QUDA_RECONSTRUCT_13>::type;
+          copy_gauge_offset<Field, Element, G>(out, in, offset);
 #else
-        errorQuda("QUDA_RECONSTRUCT=%d does not enable reconstruct-13", QUDA_RECONSTRUCT);
+          errorQuda("QUDA_RECONSTRUCT=%d does not enable reconstruct-13", QUDA_RECONSTRUCT);
 #endif
-      } else if (in.Reconstruct() == QUDA_RECONSTRUCT_9) {
+        } else if (in.Reconstruct() == QUDA_RECONSTRUCT_9) {
 #if QUDA_RECONSTRUCT & 1
-        using G = typename gauge_mapper<Float, QUDA_RECONSTRUCT_9>::type;
-        using Arg = CopyFieldOffsetArg<Field, Element, G>;
-        Arg arg(out, in, offset);
-        CopyFieldOffset<Arg> copier(arg, in);
+          using G = typename gauge_mapper<Float, QUDA_RECONSTRUCT_9>::type;
+          copy_gauge_offset<Field, Element, G>(out, in, offset);
 #else
-        errorQuda("QUDA_RECONSTRUCT=%d does not enable reconstruct-9", QUDA_RECONSTRUCT);
+          errorQuda("QUDA_RECONSTRUCT=%d does not enable reconstruct-9", QUDA_RECONSTRUCT);
 #endif
 #endif // GPU_STAGGERED_DIRAC
-      } else {
-        errorQuda("Reconstruction %d and order %d not supported", in.Reconstruct(), in.Order());
-      }
-    } else if (in.Order() == QUDA_QDP_GAUGE_ORDER) {
+        } else {
+          errorQuda("Reconstruction %d and order %d not supported", in.Reconstruct(), in.Order());
+        }
+      } else if (in.Order() == QUDA_QDP_GAUGE_ORDER) {
 #ifdef BUILD_QDP_INTERFACE
-      using G = typename gauge_order_mapper<Float, QUDA_QDP_GAUGE_ORDER, nColor>::type;
-      using Arg = CopyFieldOffsetArg<Field, Element, G>;
-      Arg arg(out, in, offset);
-      CopyFieldOffset<Arg> copier(arg, in);
+        using G = typename gauge_order_mapper<Float, QUDA_QDP_GAUGE_ORDER, nColor>::type;
+        copy_gauge_offset<Field, Element, G>(out, in, offset);
 #else
-      errorQuda("QDP interface has not been built\n");
+        errorQuda("QDP interface has not been built\n");
 #endif
-    } else if (in.Order() == QUDA_QDPJIT_GAUGE_ORDER) {
+      } else if (in.Order() == QUDA_QDPJIT_GAUGE_ORDER) {
 #ifdef BUILD_QDPJIT_INTERFACE
-      using G = typename gauge_order_mapper<Float, QUDA_QDPJIT_GAUGE_ORDER, nColor>::type;
-      using Arg = CopyFieldOffsetArg<Field, Element, G>;
-      Arg arg(out, in, offset);
-      CopyFieldOffset<Arg> copier(arg, in);
+        using G = typename gauge_order_mapper<Float, QUDA_QDPJIT_GAUGE_ORDER, nColor>::type;
+        copy_gauge_offset<Field, Element, G>(out, in, offset);
 #else
-      errorQuda("QDP interface has not been built\n");
+        errorQuda("QDP interface has not been built\n");
 #endif
 
-    } else if (in.Order() == QUDA_MILC_GAUGE_ORDER) {
+      } else if (in.Order() == QUDA_MILC_GAUGE_ORDER) {
 #ifdef BUILD_MILC_INTERFACE
-      using G = typename gauge_order_mapper<Float, QUDA_MILC_GAUGE_ORDER, nColor>::type;
-      using Arg = CopyFieldOffsetArg<Field, Element, G>;
-      Arg arg(out, in, offset);
-      CopyFieldOffset<Arg> copier(arg, in);
+        using G = typename gauge_order_mapper<Float, QUDA_MILC_GAUGE_ORDER, nColor>::type;
+        copy_gauge_offset<Field, Element, G>(out, in, offset);
 #else
-      errorQuda("MILC interface has not been built\n");
+        errorQuda("MILC interface has not been built\n");
 #endif
-    } else if (in.Order() == QUDA_CPS_WILSON_GAUGE_ORDER) {
+      } else if (in.Order() == QUDA_CPS_WILSON_GAUGE_ORDER) {
 #ifdef BUILD_CPS_INTERFACE
-      using G = typename gauge_order_mapper<Float, QUDA_CPS_WILSON_GAUGE_ORDER, nColor>::type;
-      using Arg = CopyFieldOffsetArg<Field, Element, G>;
-      Arg arg(out, in, offset);
-      CopyFieldOffset<Arg> copier(arg, in);
+        using G = typename gauge_order_mapper<Float, QUDA_CPS_WILSON_GAUGE_ORDER, nColor>::type;
+        copy_gauge_offset<Field, Element, G>(out, in, offset);
 #else
-      errorQuda("CPS interface has not been built\n");
+        errorQuda("CPS interface has not been built\n");
 #endif
-    } else if (in.Order() == QUDA_BQCD_GAUGE_ORDER) {
+      } else if (in.Order() == QUDA_BQCD_GAUGE_ORDER) {
 #ifdef BUILD_BQCD_INTERFACE
-      using G = typename gauge_order_mapper<Float, QUDA_BQCD_GAUGE_ORDER, nColor>::type;
-      using Arg = CopyFieldOffsetArg<Field, Element, G>;
-      Arg arg(out, in, offset);
-      CopyFieldOffset<Arg> copier(arg, in);
+        using G = typename gauge_order_mapper<Float, QUDA_BQCD_GAUGE_ORDER, nColor>::type;
+        copy_gauge_offset<Field, Element, G>(out, in, offset);
 #else
-      errorQuda("BQCD interface has not been built\n");
+        errorQuda("BQCD interface has not been built\n");
 #endif
-    } else if (in.Order() == QUDA_TIFR_GAUGE_ORDER) {
+      } else if (in.Order() == QUDA_TIFR_GAUGE_ORDER) {
 #ifdef BUILD_TIFR_INTERFACE
-      using G = typename gauge_order_mapper<Float, QUDA_TIFR_GAUGE_ORDER, nColor>::type;
-      using Arg = CopyFieldOffsetArg<Field, Element, G>;
-      Arg arg(out, in, offset);
-      CopyFieldOffset<Arg> copier(arg, in);
+        using G = typename gauge_order_mapper<Float, QUDA_TIFR_GAUGE_ORDER, nColor>::type;
+        copy_gauge_offset<Field, Element, G>(out, in, offset);
 #else
-      errorQuda("TIFR interface has not been built\n");
+        errorQuda("TIFR interface has not been built\n");
 #endif
-    } else {
-      errorQuda("Gauge field %d order not supported", in.Order());
+      } else {
+        errorQuda("Gauge field %d order not supported", in.Order());
+      }
     }
-  }
-
-  template <typename Float> void copy_gauge_offset(GaugeField &out, const GaugeField &in, const int offset[4])
-  {
-    if (in.Ncolor() != 3 && out.Ncolor() != 3) {
-      errorQuda("Unsupported number of colors; out.Nc = %d, in.Nc = %d", out.Ncolor(), in.Ncolor());
-    }
-
-    if (out.Geometry() != in.Geometry()) {
-      errorQuda("Field geometries %d %d do not match", out.Geometry(), in.Geometry());
-    }
-
-    copy_gauge_offset<Float, 3>(out, in, offset);
-  }
+  };
 
   void copyFieldOffset(GaugeField &out, const GaugeField &in, const int offset[4])
   {
     checkPrecision(out, in);
     checkReconstruct(out, in);
 
-    switch (in.Precision()) {
-    case QUDA_DOUBLE_PRECISION: copy_gauge_offset<double>(out, in, offset); break;
-    case QUDA_SINGLE_PRECISION: copy_gauge_offset<float>(out, in, offset); break;
-    case QUDA_HALF_PRECISION: copy_gauge_offset<short>(out, in, offset); break;
-    case QUDA_QUARTER_PRECISION: copy_gauge_offset<int8_t>(out, in, offset); break;
-    default: errorQuda("unknown precision.");
+    if (out.Geometry() != in.Geometry()) {
+      errorQuda("Field geometries %d %d do not match", out.Geometry(), in.Geometry());
     }
+
+    instantiate<CopyGaugeOffset>(out, in, offset);
   }
 
 } // namespace quda
