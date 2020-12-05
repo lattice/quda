67dbca7bd0d64c81cdabe75f8bd45c7f9477650f
Balint Joo <bjoo@jlab.org>
2020-12-05 12:51:14 -0600

    Removed old FFT plans in favor of Kate's approach in generic Kernels (CUDA only just now)

--- lib/FFT_Plans.h
+++
@@ -1,9 +0,0 @@
-#include "quda_define.h"
-
-#if defined(QUDA_TARGET_CUDA)
-#include "targets/cuda/CUFFT_Plans.h"
-#elif defined(QUDA_TARGET_HIP)
-#include "targets/hip/HIPFFT_Plans.h"
-#else
-#error "Neither QUDA_TARGET_CUDA nor QUDA_TARGET_HIP are defined"
-#endif
