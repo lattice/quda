fc2581993f0f0fdeb74f82f1ef4044b2397c0e64
Jiqun Tu <jtu@nvidia.com>
2020-12-11 12:55:47 -0500

    Add more comments and make variable names clearer in split_grid.h

--- include/split_grid.h
+++ include/split_grid.h
@@ -17,14 +17,27 @@ namespace quda
   void inline split_field(Field &collect_field, std::vector<Field *> &v_base_field, const CommKey &comm_key,
                           QudaPCType pc_type = QUDA_4D_PC)
   {
-    CommKey full_dim = {comm_dim(0), comm_dim(1), comm_dim(2), comm_dim(3)};
-    CommKey full_idx = {comm_coord(0), comm_coord(1), comm_coord(2), comm_coord(3)};
+    CommKey comm_grid_dim = {comm_dim(0), comm_dim(1), comm_dim(2), comm_dim(3)};
+    CommKey comm_grid_idx = {comm_coord(0), comm_coord(1), comm_coord(2), comm_coord(3)};
 
     int rank = comm_rank();
-    int total_rank = product(full_dim);
+    int total_rank = product(comm_grid_dim);
 
-    auto grid_dim = full_dim / comm_key;  // Communicator grid.
-    auto block_dim = full_dim / grid_dim; // The full field needs to be partitioned according to the communicator grid.
+    /**
+      The term partition in the variable names and comments can mean two things:
+      - The processor grid (with dimension comm_grid_dim) is divided into (sub)partitions.
+      - For the collecting field, on each processor it contains several partitions, each partition is a copy of
+        the base field.
+      The term partition_dim means the number of partitions in each direction, and (unsurprisingly) partition_dim
+      is the same for the above two meanings, i.e. if I divide the overall processor grid by 3 in one direction,
+      the collect field will be 3 times fatter compared to the base field, in that direction.
+
+      In this file the term *_dim and *_idx are all arrays of 4 int's - one can simplify them as 1d-int to understand
+      things and the extension to 4d is trivial.
+    */
+
+    auto processor_dim = comm_grid_dim / comm_key;      // How many processors are there in a parititon?
+    auto partition_dim = comm_grid_dim / processor_dim; // How many partitions are there? partition_dim == comm_key
 
     int n_replicates = product(comm_key);
     std::vector<void *> v_send_buffer_h(n_replicates, nullptr);
@@ -37,10 +50,10 @@ namespace quda
 
     // Send cycles
     for (int i = 0; i < n_replicates; i++) {
-      auto grid_idx = coordinate_from_index(i, comm_key);
-      auto block_idx = full_idx / block_dim;
+      auto partition_idx = coordinate_from_index(i, comm_key); // Which partition to send to?
+      auto processor_idx = comm_grid_idx / partition_dim;           // Which processor in that partition to send to?
 
-      auto dst_idx = grid_idx * grid_dim + block_idx;
+      auto dst_idx = partition_idx * processor_dim + processor_idx;
 
       int dst_rank = comm_rank_from_coords(dst_idx.data());
       int tag = rank * total_rank + dst_rank; // tag = src_rank * total_rank + dst_rank
@@ -60,12 +73,14 @@ namespace quda
     param_type param(*meta);
     Field *buffer_field = Field::Create(param);
 
-    CommKey thread_dim = {meta->full_dim(0), meta->full_dim(1), meta->full_dim(2), meta->full_dim(3)};
+    CommKey field_dim = {meta->full_dim(0), meta->full_dim(1), meta->full_dim(2), meta->full_dim(3)};
 
     // Receive cycles
     for (int i = 0; i < n_replicates; i++) {
-      auto thread_idx = coordinate_from_index(i, comm_key);
-      auto src_idx = (full_idx % grid_dim) * block_dim + thread_idx;
+      auto partition_idx
+        = coordinate_from_index(i, comm_key); // Here this means which partition of the field we are working on.
+      auto src_idx
+        = (comm_grid_idx % processor_dim) * partition_dim + partition_idx; // And where does this partition comes from?
 
       int src_rank = comm_rank_from_coords(src_idx.data());
       int tag = src_rank * total_rank + rank;
@@ -84,7 +99,7 @@ namespace quda
       comm_free(mh_recv);
       host_free(recv_buffer_h);
 
-      auto offset = thread_idx * thread_dim;
+      auto offset = partition_idx * field_dim;
 
       quda::copyFieldOffset(collect_field, *buffer_field, offset, pc_type);
     }
@@ -105,14 +120,15 @@ namespace quda
   void inline join_field(std::vector<Field *> &v_base_field, const Field &collect_field, const CommKey &comm_key,
                          QudaPCType pc_type = QUDA_4D_PC)
   {
-    CommKey full_dim = {comm_dim(0), comm_dim(1), comm_dim(2), comm_dim(3)};
-    CommKey full_idx = {comm_coord(0), comm_coord(1), comm_coord(2), comm_coord(3)};
+    CommKey comm_grid_dim = {comm_dim(0), comm_dim(1), comm_dim(2), comm_dim(3)};
+    CommKey comm_grid_idx = {comm_coord(0), comm_coord(1), comm_coord(2), comm_coord(3)};
 
     int rank = comm_rank();
-    int total_rank = product(full_dim);
+    int total_rank = product(comm_grid_dim);
 
-    auto grid_dim = full_dim / comm_key;  // Communicator grid.
-    auto block_dim = full_dim / grid_dim; // The full field needs to be partitioned according to the communicator grid.
+    auto processor_dim = comm_grid_dim / comm_key; // Communicator grid.
+    auto partition_dim
+      = comm_grid_dim / processor_dim; // The full field needs to be partitioned according to the communicator grid.
 
     int n_replicates = product(comm_key);
     std::vector<void *> v_send_buffer_h(n_replicates, nullptr);
@@ -128,20 +144,20 @@ namespace quda
     param_type param(meta);
     Field *buffer_field = Field::Create(param);
 
-    CommKey thread_dim = {meta.full_dim(0), meta.full_dim(1), meta.full_dim(2), meta.full_dim(3)};
+    CommKey field_dim = {meta.full_dim(0), meta.full_dim(1), meta.full_dim(2), meta.full_dim(3)};
 
     // Send cycles
     for (int i = 0; i < n_replicates; i++) {
 
-      auto thread_idx = coordinate_from_index(i, comm_key);
-      auto dst_idx = (full_idx % grid_dim) * block_dim + thread_idx;
+      auto partition_idx = coordinate_from_index(i, comm_key);
+      auto dst_idx = (comm_grid_idx % processor_dim) * partition_dim + partition_idx;
 
       int dst_rank = comm_rank_from_coords(dst_idx.data());
       int tag = rank * total_rank + dst_rank;
 
       size_t bytes = meta.TotalBytes();
 
-      auto offset = thread_idx * thread_dim;
+      auto offset = partition_idx * field_dim;
       quda::copyFieldOffset(*buffer_field, collect_field, offset, pc_type);
 
       v_send_buffer_h[i] = pinned_malloc(bytes);
@@ -155,10 +171,10 @@ namespace quda
     // Receive cycles
     for (int i = 0; i < n_replicates; i++) {
 
-      auto grid_idx = coordinate_from_index(i, comm_key);
-      auto block_idx = full_idx / block_dim;
+      auto partition_idx = coordinate_from_index(i, comm_key);
+      auto processor_idx = comm_grid_idx / partition_dim;
 
-      auto src_idx = grid_idx * grid_dim + block_idx;
+      auto src_idx = partition_idx * processor_dim + processor_idx;
 
       int src_rank = comm_rank_from_coords(src_idx.data());
       int tag = src_rank * total_rank + rank;
