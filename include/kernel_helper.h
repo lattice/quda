7acd76a4549fb42036f1a598322462e5a5551f14
Xiao-Yong Jin <xjin@anl.gov>
2020-12-16 12:09:15 -0600

    place alignas proper

--- include/kernel_helper.h
+++ include/kernel_helper.h
@@ -7,7 +7,7 @@ namespace quda {
   struct constant_param_t {
     static constexpr size_t max_size = device::max_constant_param_size();
     size_t bytes;
-    char host[max_size] alignas(16);
+    char host alignas(16) [max_size];
     void *device_ptr;
     char device_name[128];
   };
