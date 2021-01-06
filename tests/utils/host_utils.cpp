a60adc72ff318dd93333b27f818ecbfce9c419e4
James Osborn <osborn@alcf.anl.gov>
2021-01-06 15:47:20 -0600

    add a few explicit instantiations

--- tests/utils/host_utils.cpp
+++ tests/utils/host_utils.cpp
@@ -1015,6 +1015,9 @@ template <typename Float> void constructUnitGaugeField(Float **res, QudaGaugePar
   applyGaugeFieldScaling(res, Vh, param);
 }
 
+template void constructUnitGaugeField(float **res, QudaGaugeParam *param);
+template void constructUnitGaugeField(double **res, QudaGaugeParam *param);
+
 // normalize the vector a
 template <typename Float>
 static void normalize(complex<Float> *a, int len) {
@@ -1107,6 +1110,9 @@ template <typename Float> void constructRandomGaugeField(Float **res, QudaGaugeP
     }
   }
 }
+
+template void constructRandomGaugeField(float **res, QudaGaugeParam *param, QudaDslashType dslash_type);
+template void constructRandomGaugeField(double **res, QudaGaugeParam *param, QudaDslashType dslash_type);
 
 template <typename Float> void constructUnitaryGaugeField(Float **res)
 {
