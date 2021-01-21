d681dd87d5529e803d7e6090bbe5f8c65d1cce8c
cpviolator <dmhowarth26@gmail.com>
2021-01-20 22:10:14 -0800

    Remove quda_fp16 header from hip target, remove Double2 from reduce_helper

--- include/reduce_helper.h
+++ include/reduce_helper.h
@@ -17,8 +17,6 @@ using count_t = cuda::atomic<unsigned int, cuda::thread_scope_device>;
 using count_t = unsigned int;
 #endif
 
-#include <quda_define.h>
-
 namespace quda
 {
 
@@ -34,30 +32,10 @@ namespace quda
     qudaEvent_t &get_event();
   } // namespace reducer
 
-  template <typename T> struct plus;
-
-#if defined(QUDA_TARGET_HIP) 
-  // For some reason HIP finds ambiguity with the more general operator
-  // So to be sure I am being very specific.
-  template<>
-  struct plus<HIP_vector_type<double,2>> {
-    static constexpr bool do_sum = true; 
-    using Double2 = HIP_vector_type<double,2>;
-
-    __device__ __host__ inline Double2 operator()( Double2 a, Double2 b ) {
-	    Double2 ret_val{0,0};
-	    ret_val.x = a.x + b.x;
-	    ret_val.y = a.y + b.y;
-	    return ret_val;
-    }
-  };
-#endif
-
   template <typename T> struct plus {
     static constexpr bool do_sum = true;
     __device__ __host__ T operator()(T a, T b) const { return a + b; }
   };
-
 
   template <typename T> struct maximum {
     static constexpr bool do_sum = false;
