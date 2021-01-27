70776235fe9aebe2008048865bcace91d2b68ec4
Balint Joo <bjoo@jlab.org>
2021-01-27 17:43:08 -0600

    Removed quda_api_target.h

--- include/targets/cuda/quda_api_target.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cuda.h>
-#include <cuda_runtime.h>
-
-using qudaEvent_t = cudaEvent_t;
-using qudaIpcEventHandle_t = cudaIpcEventHandle_t;
-using qudaIpcMemHandle_t = cudaIpcMemHandle_t;
