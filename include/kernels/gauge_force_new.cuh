214416f7be323f66aab2ca1819bc5d9d605672f1
cpviolator <dmhowarth26@gmail.com>
2021-02-08 02:32:35 -0800

    And the relevant files

---
+++ include/kernels/gauge_force_new.cuh
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <gauge_field_order.h>
+#include <quda_matrix.h>
+#include <index_helper.cuh>
+#include <kernels/gauge_utils.cuh>
+#include <kernel.h>
+
+namespace quda {
+  
+  template <typename Float_, int nColor_, QudaReconstructType recon_in, int paths_ = 1>
+  struct GaugeForceNewArg {
+    // The number of paths corresponds to the gauge action type
+    // 1: Wilson
+    // 2: Symanzik
+    // 3: Luscher-Weisz
+    static constexpr int paths = paths_;
+    
+    using Float = Float_;
+    static constexpr int nColor = nColor_;
+    static_assert(nColor == 3, "Only nColor=3 enabled at this time");
+    typedef typename gauge_mapper<Float,recon_in>::type Gauge;
+    typedef typename gauge_mapper<Float,QUDA_RECONSTRUCT_10>::type Mom;
+    
+    Mom mom;
+    const Gauge in;
+    const double epsilon;
+    Float *path_coeff;
+    
+    dim3 threads;
+    int X[4]; // the regular volume parameters
+    int E[4]; // the extended volume parameters
+    int border[4]; // radius of border
+
+    GaugeForceNewArg(GaugeField &mom, const GaugeField &in, const double epsilon, Float *path_coeff) :
+      mom(mom),
+      in(in),
+      epsilon(epsilon),
+      path_coeff(path_coeff),
+      threads(mom.VolumeCB(), 2, 4)
+    {
+      for (int i=0; i<4; i++) {
+	X[i] = mom.X()[i];
+	E[i] = in.X()[i];
+	border[i] = (E[i] - X[i])/2;
+      }
+    }
+  };
+
+
+  template <typename Arg, int dir>
+  __device__ __host__ inline void GaugeForceKernelNew(Arg &arg, int idx, int parity)
+  {
+    using real = typename Arg::Float;
+    typedef Matrix<complex<real>,Arg::nColor> Link;
+
+    int x[4] = {0, 0, 0, 0};
+    getCoords(x, idx, arg.X, parity);
+    for (int dr=0; dr<4; ++dr) x[dr] += arg.border[dr]; // extended grid coordinates
+
+    Link staple, rectangle, parallelogram, Ux;
+    
+    if(Arg::paths == 1) {
+      computeForceStaple(arg, x, arg.X, parity, dir, staple);
+      staple = staple * 1.0;
+    }      
+    else if(Arg::paths == 2) {
+      computeForceStapleRectangle(arg, x, arg.X, parity, dir, staple, rectangle);
+      staple = staple * 1.0;
+      rectangle = rectangle * 2.0;
+      staple = staple + rectangle;
+    }
+    else if(Arg::paths == 3) {
+      computeForceStapleRectangle(arg, x, arg.X, parity, dir, staple, rectangle);
+      computeForceParallelogram(arg, x, arg.X, parity, dir, parallelogram);
+      staple = staple * 1.0;
+      rectangle = rectangle * 2.0;
+      parallelogram = parallelogram * 3.0;
+      
+      staple = staple + rectangle;
+      staple = staple + parallelogram;
+    }
+    
+    // multiply by U(x)
+    Ux = arg.in(dir, linkIndex(x,arg.E), parity);
+    Ux = Ux * staple;
+    
+    // update mom(x)
+    Link mom = arg.mom(dir, idx, parity);
+    mom = mom - arg.epsilon * Ux;
+    makeAntiHerm(mom);
+    arg.mom(dir, idx, parity) = mom;    
+  }
+  
+  template <typename Arg> struct GaugeForceNew
+  {
+    Arg &arg;
+    constexpr GaugeForceNew(Arg &arg) : arg(arg) {}
+    static constexpr const char *filename() { return KERNEL_FILE; }    
+    
+    __device__ __host__ void operator()(int x_cb, int parity, int dir)
+    {
+      switch(dir) {
+      case 0: GaugeForceKernelNew<Arg,0>(arg, x_cb, parity); break;
+      case 1: GaugeForceKernelNew<Arg,1>(arg, x_cb, parity); break;
+      case 2: GaugeForceKernelNew<Arg,2>(arg, x_cb, parity); break;
+      case 3: GaugeForceKernelNew<Arg,3>(arg, x_cb, parity); break;
+      }
+    }
+  };
+
+}
---
+++ lib/gauge_force_new.cu
@@ -0,0 +1,77 @@
+#include <tunable_nd.h>
+#include <instantiate.h>
+#include <kernels/gauge_force_new.cuh>
+
+namespace quda {
+
+  template <typename Float, int nColor, QudaReconstructType recon_in> class ForceGaugeNew : public TunableKernel3D
+  {
+    const GaugeField &in;
+    GaugeField &mom;
+    const QudaGaugeActionType action_type;
+    const double epsilon;
+    void *path_coeff;
+    
+    unsigned int minThreads() const { return mom.VolumeCB(); }
+    
+    // (2,3): 2 for parity in the y thread dim, 4 corresponds to the dimesions over
+    // which cache reuse will be used.
+  public:
+    ForceGaugeNew(const GaugeField &in, GaugeField &mom, const QudaGaugeActionType action_type, const double epsilon, void *path_coeff) :
+      TunableKernel3D(in, 2, 4),
+      in(in),
+      mom(mom),
+      action_type(action_type),
+      epsilon(epsilon),
+      path_coeff(path_coeff)
+    {
+      switch (action_type) {
+      case QUDA_GAUGE_ACTION_TYPE_WILSON: strcat(aux, ",wilson"); break;
+      case QUDA_GAUGE_ACTION_TYPE_SYMANZIK: strcat(aux, ",symanzik"); break;
+      case QUDA_GAUGE_ACTION_TYPE_LUSCHER_WEISZ: strcat(aux, ",luscher-weisz"); break;
+      default: errorQuda("Unexpected action type %d", action_type);
+      }
+      strcat(aux, comm_dim_partitioned_string());
+      apply(device::get_default_stream());
+    }
+    
+    void apply(const qudaStream_t &stream) {
+      TuneParam tp = tuneLaunch(*this, getTuning(), getVerbosity());
+      if(action_type == QUDA_GAUGE_ACTION_TYPE_WILSON) {
+	GaugeForceNewArg<Float, nColor, recon_in, 1> arg(mom, in, epsilon, (Float*)path_coeff);
+	launch<GaugeForceNew>(tp, stream, arg);
+      }
+      else if(action_type == QUDA_GAUGE_ACTION_TYPE_SYMANZIK) {
+	GaugeForceNewArg<Float, nColor, recon_in, 2> arg(mom, in, epsilon, (Float*)path_coeff);
+	launch<GaugeForceNew>(tp, stream, arg);
+      }
+      else if(action_type == QUDA_GAUGE_ACTION_TYPE_LUSCHER_WEISZ) {
+	GaugeForceNewArg<Float, nColor, recon_in, 3> arg(mom, in, epsilon, (Float*)path_coeff);
+	launch<GaugeForceNew>(tp, stream, arg);
+      }
+      else errorQuda("Unknown gauge action type %d", action_type);
+    }
+
+    void preTune() { mom.backup(); }
+    void postTune() { mom.restore(); }
+    
+    long long flops() const { return (288 - 48 + 1) * 198ll * 2 * mom.VolumeCB() * 4; }
+    long long bytes() const { return ((288 + 1ll) * in.Bytes() + 2ll*mom.Bytes()) * 2 * mom.VolumeCB() * 4; }
+  };
+  
+#ifdef GPU_GAUGE_FORCE
+  void gaugeForceNew(GaugeField& mom, const GaugeField& in, const QudaGaugeActionType action_type, const double epsilon, void *path_coeff)
+  {
+    checkPrecision(mom, in);
+    checkLocation(mom, in);
+    if (mom.Reconstruct() != QUDA_RECONSTRUCT_10) errorQuda("Reconstruction type %d not supported", mom.Reconstruct());    
+    instantiate<ForceGaugeNew>(in, mom, action_type, epsilon, path_coeff);
+  }
+#else
+  void gaugeForceNew(GaugeField&, const GaugeField&, const QudaGaugeActionType, const double, void*)
+  {
+    errorQuda("Gauge force has not been built");
+  }
+#endif
+
+} // namespace quda
