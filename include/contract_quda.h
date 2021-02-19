88a5d2f09006c10e937aee8f1e3e9ae72c9ca850
cpviolator <dmhowarth26@gmail.com>
2021-02-18 21:44:33 -0800

    adding slaph kernels

--- include/contract_quda.h
+++ include/contract_quda.h
@@ -3,7 +3,9 @@
 #include <quda_internal.h>
 #include <quda.h>
 
-namespace quda
-{
+namespace quda {
+
+  void evecProjectQuda(const ColorSpinorField &x, const ColorSpinorField &y, std::vector<Complex> &result);
+  
   void contractQuda(const ColorSpinorField &x, const ColorSpinorField &y, void *result, QudaContractType cType);
 } // namespace quda
--- include/kernels/contraction.cuh
+++ include/kernels/contraction.cuh
@@ -6,8 +6,99 @@
 #include <matrix_field.h>
 #include <kernel.h>
 
-namespace quda
-{
+namespace quda {
+
+  using spinor_array = vector_type<double2, 4>;
+
+  template <class T> __device__ int idx_from_t_xyz(int t, int xyz, T X[4])
+  {
+    int x[4];
+#pragma unroll
+    for (int d = 0; d < 4; d++) {
+      if (d != 3) {
+	x[d] = xyz % X[d];
+	xyz /= X[d];
+      }
+    }    
+    x[3] = t;    
+    return (((x[3] * X[2] + x[2]) * X[1] + x[1]) * X[0] + x[0]);
+  }
+
+  template <typename Float, int nColor_>
+  struct EvecProjectionArg : public ReduceArg<spinor_array>
+  {
+    using real = typename mapper<Float>::type;
+    static constexpr int nColor = nColor_;
+    static constexpr int nSpinX = 4;
+    static constexpr int nSpinY = 1;
+    static constexpr bool spin_project = true;
+    static constexpr bool spinor_direct_load = false; // false means texture load
+
+    typedef typename colorspinor_mapper<Float, nSpinX, nColor, spin_project, spinor_direct_load>::type F4;
+
+    typedef typename colorspinor_mapper<Float, nSpinY, nColor, spin_project, spinor_direct_load>::type F1;
+
+    F4 x;
+    F1 y;
+
+    dim3 threads;     // number of active threads required
+    int_fastdiv X[4]; // grid dimensions
+    
+    EvecProjectionArg(const ColorSpinorField &x, const ColorSpinorField &y) :
+      ReduceArg<spinor_array>(x.X()[3]),
+      x(x),
+      y(y),
+      // Launch xyz threads per t, t times.
+      threads(x.Volume()/x.X()[3], x.X()[3])
+    {
+      for(int i=0; i<4; i++) {
+	X[i] = x.X()[i];
+      }
+    }
+    __device__ __host__ spinor_array init() const { return spinor_array(); }
+  };
+  
+  
+  template <typename Arg> struct EvecProjection : plus<spinor_array> {
+    using reduce_t = spinor_array;
+    using plus<reduce_t>::operator();    
+    Arg &arg;
+    constexpr EvecProjection(Arg &arg) : arg(arg) {}
+    static constexpr const char *filename() { return KERNEL_FILE; }
+    
+    // Final param is unused in the MultiReduce functor in this use case.
+    __device__ __host__ inline reduce_t operator()(reduce_t &result, int xyz, int t, int)
+    {
+      constexpr int nSpinX = Arg::nSpinX;
+      constexpr int nSpinY = Arg::nSpinY;
+      constexpr int nColor = Arg::nColor;
+      using real = typename Arg::real;
+      using Vector4 = ColorSpinor<real, nColor, nSpinX>;
+      using Vector4 = ColorSpinor<real, nColor, nSpinY>;
+
+      reduce_t result_all_channels = spinor_array();
+
+      // Collect vector data
+      int parity = 0;
+      int idx = idx_from_t_xyz(t, xyz, arg.X);
+      // This helper will change the value of 'parity' to the correct one, 
+      // and return the checkerboard index.
+      int idx_cb = getParityCBFromFull(parity, arg.X, idx);
+      Vector4 x = arg.x(idx_cb, parity);
+      Vector1 y = arg.y(idx_cb, parity);
+
+      // Compute the inner product over colour
+      complex<real> prod;
+      for (int mu = 0; mu < nSpinX; mu++) {
+        prod = innerProduct(y, x, 0, mu);
+        result_all_channels[mu].x += prod.real();
+        result_all_channels[mu].y += prod.imag();
+      }
+      
+      return plus::operator()(result_all_channels, result);
+    }
+  };
+
 
   template <typename Float, int nColor_> struct ContractionArg {
     using real = typename mapper<Float>::type;
