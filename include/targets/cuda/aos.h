d3ea4d0be9c11b7aaf3221e74aa03f6b50aa1cf4
maddyscientist <mclark@nvidia.com>
2021-03-04 15:45:18 -0800

    remove __CUDA_ARCH__ from aos.h and replace with is_device SFINAE

--- include/targets/cuda/aos.h
+++ include/targets/cuda/aos.h
@@ -1,13 +1,18 @@
 #pragma once
 
-// trove requires CUDA and has issues with device debug
-#if !defined(DEVICE_DEBUG)
+#include <target_device.h>
 #include <trove/ptr.h>
-#else
-#define DISABLE_TROVE
-#endif
 
 namespace quda {
+
+  constexpr bool enable_trove()
+  {
+#ifndef DEVICE_DEBUG
+    return device::is_device();
+#else
+    return false; // trove has issues with device debug
+#endif
+  }
 
   /**
      @brief This is just a dummy structure we use for trove to define the
@@ -21,17 +26,38 @@ namespace quda {
     __host__ __device__ T &operator[](int i) { return v[i]; }
   };
 
+  template <typename T, bool use_trove> __device__ std::enable_if_t<use_trove, T> block_load(const T *in)
+  {
+    return *(trove::coalesced_ptr<const T>(in));
+  }
+
+  template <typename T, bool use_trove> __device__ __host__ std::enable_if_t<!use_trove, T> block_load(const T *in)
+  {
+    return *(in);
+  }
+
   template <typename T, int n> __host__ __device__ void block_load(T out[n], const T *in)
   {
     using struct_t = S<T, n>;
-#if defined( __CUDA_ARCH__) && !defined(DISABLE_TROVE)
-    struct_t v = *(trove::coalesced_ptr<const struct_t>(reinterpret_cast<const struct_t*>(in)));
-#else
-    struct_t v = *(reinterpret_cast<const struct_t*>(in));
-#endif
+    struct_t v = block_load<struct_t, enable_trove()>(reinterpret_cast<const struct_t*>(in));
 
 #pragma unroll
     for (int i = 0; i < n; i++) out[i] = v[i];
+  }
+
+  template <typename T> __host__ __device__ void block_load(T &out, const T *in)
+  {
+    out = block_load<T, enable_trove()>(in);
+  }
+
+  template <typename T, bool use_trove> __device__ std::enable_if_t<use_trove, void> block_store(T *out, const T &in)
+  {
+    *(trove::coalesced_ptr<T>(out)) = in;
+  }
+
+  template <typename T, bool use_trove> std::enable_if_t<!use_trove, void> block_store(T *out, const T &in)
+  {
+    *out = in;
   }
 
   template <typename T, int n> __host__ __device__ void block_store(T *out, const T in[n])
@@ -41,29 +67,12 @@ namespace quda {
 #pragma unroll
     for (int i = 0; i < n; i++) v[i] = in[i];
 
-#if defined( __CUDA_ARCH__) && !defined(DISABLE_TROVE)
-    *(trove::coalesced_ptr<struct_t>(reinterpret_cast<struct_t *>(out))) = v;
-#else
-    *(reinterpret_cast<struct_t*>(out)) = v;
-#endif
-  }
-
-  template <typename T> __host__ __device__ void block_load(T &out, const T *in)
-  {
-#if defined( __CUDA_ARCH__) && !defined(DISABLE_TROVE)
-    out = *(trove::coalesced_ptr<const T>(in));
-#else
-    out = *(in);
-#endif
+    block_store<struct_t, enable_trove()>(reinterpret_cast<struct_t*>(out), v);
   }
 
   template <typename T> __host__ __device__ void block_store(T *out, const T &in)
   {
-#if defined( __CUDA_ARCH__) && !defined(DISABLE_TROVE)
-    *(trove::coalesced_ptr<T>(out)) = in;
-#else
-    *out = in;
-#endif
+    block_store<T, enable_trove()>(out, in);
   }
 
 }
