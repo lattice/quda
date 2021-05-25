c6d4f17c322bd4fdc7a6b208cdc9da25937054c6
Xiao-Yong Jin <xjin@anl.gov>
2021-05-25 18:54:30 +0000

    omp: reduce constexpr max_block_size to 512

--- include/targets/omptarget/target_device.h
+++ include/targets/omptarget/target_device.h
@@ -102,7 +102,7 @@ namespace quda {
     template <int block_size_y = 1, int block_size_z = 1>
       constexpr unsigned int max_block_size()
       {
-        return std::max(warp_size(), 1024 / (block_size_y * block_size_z));
+        return std::max(warp_size(), 512 / (block_size_y * block_size_z));
       }
 
     /**
