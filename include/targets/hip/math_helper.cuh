5a3c6cefd3cc8d805538cbcf1e9739951b00c1a6
Balint Joo <bjoo@jlab.org>
2021-06-30 18:15:48 -0400

    Added device side dp sincos and rsqrt

--- include/targets/hip/math_helper.cuh
+++ include/targets/hip/math_helper.cuh
@@ -2,10 +2,11 @@
 
 #include <cmath>
 #include <target_device.h>
+#include <hip/math_functions.h>
 
 namespace quda {
-
-   /**
+   
+  /**
    * @brief Combined sin and cos colculation in QUDA NAMESPACE
    * @param a the angle
    * @param s pointer to the storage for the result of the sin
@@ -27,10 +28,19 @@ namespace quda {
 
   // Impl for float device
   template <> struct sincosf_impl<true> {
-    __device__ inline void operator()(const float& a, float * s, float *c) { __sincosf(a, s, c); }
+    __device__ inline void operator()(const float& a, float * s, float *c) { ::sincosf(a, s, c); }
   };
-  
-  
+ 
+  // impl for double type host
+  template <bool is_device> struct sincos_impl {
+    inline void operator()(const double& a, double *s, double *c) { *s=::sin(a); *c=::cos(a); }
+  };
+
+  // impl for double type on device
+  template <> struct sincos_impl<true> {
+    __device__ inline void operator()(const double& a, double *s, double *c) { ::sincos(a,s,c) ; }
+  };
+
   /**
    * @brief Combined sin and cos colculation in QUDA NAMESPACE
    * @param a the angle
@@ -42,7 +52,8 @@ namespace quda {
   template<>
   inline __host__ __device__ void sincos(const float& a, float * s, float *c) { target::dispatch<sincosf_impl>(a, s, c); }
 
-  
+  template<>
+  inline __host__ __device__ void sincos(const double& a, double * s, double *c) { target::dispatch<sincos_impl>(a, s, c); }  
   
   /**
    * @brief Reciprocal square root function (rsqrt)
@@ -63,15 +74,32 @@ namespace quda {
   	   return 1.0/::sqrtf(a);
   	}
   };
-  
+ 
+   // Impl for float type host
+  template <bool is_device> struct rsqrt_impl {
+        inline double operator()(const double& a) {
+           return 1.0/::sqrt(a);
+        }
+  };
+
   template<> struct rsqrtf_impl<true> { 
     __device__ inline float operator()(const float& a) { 
-    	return rsqrtf(a);
+	    return ::rsqrtf(a);
     }
    };
 
+   template<> struct rsqrt_impl<true> {
+    __device__ inline double operator()(const double& a) {
+	    return ::rsqrt(a);
+    }
+   };
+
+
+
   template<>
   inline __host__ __device__ float rsqrt(const float& a) { return target::dispatch<rsqrtf_impl>(a); }
+  template<>
+  inline __host__ __device__ double rsqrt(const double& a) { return target::dispatch<rsqrt_impl>(a); }
 
    /**
      Generic wrapper for Trig functions -- used in gauge field order 
@@ -88,10 +116,17 @@ namespace quda {
   };
   
   template <bool is_device> struct sinf_impl { inline float operator()(const float& a) { return ::sinf(a); } };
-  template <> struct sinf_impl<true> { __device__ inline float operator()(const float& a) { return __sinf(a); } };
+  template <> struct sinf_impl<true> { __device__ inline float operator()(const float& a) { return ::sinf(a); } };
 
   template <bool is_device> struct cosf_impl { inline float operator()(const float& a) { return ::cosf(a); } };
-  template <> struct cosf_impl<true> { __device__ inline float operator()(const float& a) { return __cosf(a); } };
+  template <> struct cosf_impl<true> { __device__ inline float operator()(const float& a) { return ::cosf(a); } };
+ 
+
+  template <bool is_device> struct sin_impl { inline double operator()(const double& a) { return ::sin(a); } };
+  template <> struct sin_impl<true> { __device__ inline double operator()(const double& a) { return ::sin(a); } };
+
+  template <bool is_device> struct cos_impl { inline double operator()(const double& a) { return ::cos(a); } };
+  template <> struct cos_impl<true> { __device__ inline double operator()(const double& a) { return ::cos(a); } };
   
   /**
      Specialization of Trig functions using floats
@@ -114,6 +149,30 @@ template <>
     	target::dispatch<sincosf_impl>(ampi,s,c); 
     }
   };
+
+  /**
+     Specialization of Trig functions using doubles
+   */
+  template <>
+    struct Trig<false,double> {
+    __device__ __host__ static double Atan2( const double &a, const double &b) { return ::atan2(a,b); }
+    __device__ __host__ static double Sin(const double &a) { return target::dispatch<sin_impl>(a); }
+    __device__ __host__ static double Cos(const double &a) { return target::dispatch<cos_impl>(a); }
+    __device__ __host__ static void SinCos(const double &a, double *s, double *c) { target::dispatch<sincos_impl>(a,s,c); }
+  };
+
+template <>
+    struct Trig<true,double> {
+    __device__ __host__ static double Atan2( const double &a, const double &b) { return ::atan2(a,b)/M_PI; }
+    __device__ __host__ static double Sin(const double &a) { return target::dispatch<sin_impl>(a*static_cast<double>(M_PI)); }
+    __device__ __host__ static double Cos(const double &a) { return target::dispatch<cos_impl>(a*static_cast<double>(M_PI)); }
+    __device__ __host__ static void SinCos(const double &a, double *s, double *c) {
+        auto ampi = a * static_cast<double>(M_PI);
+        target::dispatch<sincos_impl>(ampi,s,c);
+    }
+  };
+
+
 
   template <bool is_device> struct fpow_impl { template <typename real> inline real operator()(real a, int b) { return ::pow(a, b); } };
 
