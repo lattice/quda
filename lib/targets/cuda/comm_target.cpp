lattice/quda

commit fba4ff48dd9fc28afd8945cef9594bf3e84389c1

Moved cudaIpcGetMemHandle call to be protected by whether we do P2P. This issue arose in use with QDP-JIT. Turning P2P off caused the regular device_malloc to be called, which allocated via QDP-JIT's allocator. This was not compatible with the IPC call. When P2P is enabled a driver based malloc  is used and the situation is safe

lib/targets/cuda/comm_target.cpp
@@ -48,9 +48,9 @@ void comm_create_neighbor_memory(void *remote[QUDA_MAX_DIM][2], void *local)
                                                       dim, -disp, sizeof(remote_handle));
       }
       // now send
-      cudaIpcMemHandle_t local_handle;
-      CHECK_CUDA_ERROR(cudaIpcGetMemHandle(&local_handle, local));
       if (comm_peer2peer_enabled(dir,dim)) {
+        cudaIpcMemHandle_t local_handle;
+        CHECK_CUDA_ERROR(cudaIpcGetMemHandle(&local_handle, local));
         sendHandle = comm_declare_send_relative(&local_handle, dim, disp, sizeof(local_handle));
       }
       if (receiveHandle) comm_start(receiveHandle);

commit 2e720eff0fd417a3967178bdf23247cf88101437

Fix stack-use-after-scope bug introduced in fba4ff4

lib/targets/cuda/comm_target.cpp
@@ -48,8 +48,8 @@ void comm_create_neighbor_memory(void *remote[QUDA_MAX_DIM][2], void *local)
                                                       dim, -disp, sizeof(remote_handle));
       }
       // now send
+      cudaIpcMemHandle_t local_handle;
       if (comm_peer2peer_enabled(dir,dim)) {
-        cudaIpcMemHandle_t local_handle;
         CHECK_CUDA_ERROR(cudaIpcGetMemHandle(&local_handle, local));
         sendHandle = comm_declare_send_relative(&local_handle, dim, disp, sizeof(local_handle));
       }