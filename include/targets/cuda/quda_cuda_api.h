a7df1ae789b0eab0e997be3da9d217267e07349f
maddyscientist <mclark@nvidia.com>
2021-08-22 21:41:32 -0700

    Move device::get_cuda_stream to targets/cuda/quda_cuda_api.h

--- include/targets/cuda/quda_cuda_api.h
+++ include/targets/cuda/quda_cuda_api.h
@@ -1,12 +1,26 @@
+#pragma once
+
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+/**
+   @file quda_cuda_api.h
+   @brief Header file that declares some functions that will be called from within the CUDA target
+*/
+
 namespace quda {
 
-  /**
-     @file quda_cuda_api.h
-     @brief Header file that declares some functions that will be called from within the CUDA target
-  */
+  namespace device {
+
+    /**
+       @brief Return CUDA stream from QUDA stream.  This is a
+       temporary addition until all kernels have been made generic.
+       @param stream QUDA stream we which to convert to CUDA stream
+       @return CUDA stream
+    */
+    cudaStream_t get_cuda_stream(const qudaStream_t &stream);
+
+  }
 
   namespace cuda {
 
