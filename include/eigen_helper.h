be36d3ed55b15ba7640ecce8eeea47d4084760af
maddyscientist <mclark@nvidia.com>
2021-08-23 15:26:52 -0700

    Replace __PGI with __NVCOMPILER

--- include/eigen_helper.h
+++ include/eigen_helper.h
@@ -5,7 +5,7 @@
 #define EIGEN_USE_BLAS
 #endif
 
-#if defined(__PGI) // WAR for nvc++ until we update to latest Eigen
+#if defined(__NVCOMPILER) // WAR for nvc++ until we update to latest Eigen
 #define EIGEN_DONT_VECTORIZE
 #endif
 
