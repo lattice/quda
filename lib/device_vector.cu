1888c160e3bcbf3700efb75e112c522de5290130
Jiqun Tu <jtu@nvidia.com>
2021-10-14 10:11:10 -0400

    Apply clang-format to devcie_vector.cu

--- lib/device_vector.cu
+++ lib/device_vector.cu
@@ -5,75 +5,69 @@
 namespace quda
 {
 
-  template <class T>
-    struct AxpbyArg: kernel_param<> {
-      T *out;
+  template <class T> struct AxpbyArg : kernel_param<> {
+    T *out;
 
-      T a;
-      const T *x;
+    T a;
+    const T *x;
 
-      T b;
-      const T *y;
+    T b;
+    const T *y;
 
-      AxpbyArg(T *out, T a, const T *x, T b, const T *y, int size):
-        kernel_param(size), out(out), a(a), x(x), b(b), y(y) { }
-    };
+    AxpbyArg(T *out, T a, const T *x, T b, const T *y, int size) : kernel_param(size), out(out), a(a), x(x), b(b), y(y)
+    {
+    }
+  };
 
-  template <class Arg>
-    struct Axpby {
-      const Arg &arg;
-      constexpr Axpby(const Arg &arg) : arg(arg) {}
-      static constexpr const char *filename() { return KERNEL_FILE; }
+  template <class Arg> struct Axpby {
+    const Arg &arg;
+    constexpr Axpby(const Arg &arg) : arg(arg) { }
+    static constexpr const char *filename() { return KERNEL_FILE; }
 
-      __device__ __host__ inline void operator()(int thread_idx) {
-        arg.out[thread_idx] += arg.a * arg.x[thread_idx] + arg.b * arg.y[thread_idx];
-      }
-    };
+    __device__ __host__ inline void operator()(int thread_idx)
+    {
+      arg.out[thread_idx] += arg.a * arg.x[thread_idx] + arg.b * arg.y[thread_idx];
+    }
+  };
 
+  template <class T> struct axpby_wrapper : TunableKernel1D {
 
-  template <class T>
-    struct axpby_wrapper: TunableKernel1D {
+    AxpbyArg<T> arg;
 
-      AxpbyArg<T> arg;
+    device_vector<T> _backup_vector;
+    T *_backup_ptr = nullptr;
 
-      device_vector<T> _backup_vector;
-      T *_backup_ptr = nullptr;
+    axpby_wrapper(T *out, T a, const T *x, T b, const T *y, int size) :
+      TunableKernel1D(size, QUDA_CUDA_FIELD_LOCATION), arg(out, a, x, b, y, size)
+    {
+      strcat(aux, ",axpby");
+      apply(device::get_default_stream());
+    }
 
-      axpby_wrapper(T *out, T a, const T *x, T b, const T *y, int size): TunableKernel1D(size, QUDA_CUDA_FIELD_LOCATION), arg(out, a, x, b, y, size)
-      { 
-        strcat(aux, ",axpby");
-        apply(device::get_default_stream());
-      }
+    void apply(const qudaStream_t &stream)
+    {
+      TuneParam tp = tuneLaunch(*this, getTuning(), getVerbosity());
+      launch<Axpby>(tp, stream, arg);
+    }
 
-      void apply(const qudaStream_t &stream)
-      {
-        TuneParam tp = tuneLaunch(*this, getTuning(), getVerbosity());
-        launch<Axpby>(tp, stream, arg);
-      }
+    void preTune()
+    {
+      _backup_vector.resize(arg.threads.x);
+      _backup_vector.from_device(arg.out);
+      _backup_ptr = arg.out;
+      arg.out = _backup_vector.data();
+    }
 
-      void preTune()
-      {
-        _backup_vector.resize(arg.threads.x);
-        _backup_vector.from_device(arg.out);
-        _backup_ptr = arg.out;
-        arg.out = _backup_vector.data();
-      }
+    void postTune() { arg.out = _backup_ptr; }
 
-      void postTune()
-      {
-        arg.out = _backup_ptr;
-      }
+    unsigned int minThreads() const { return arg.threads.x; }
+    long long flops() const { return 4 * arg.threads.x; }
+    long long bytes() const { return 3 * sizeof(T) * arg.threads.x; }
+  };
 
-      unsigned int minThreads() const { return arg.threads.x; }
-      long long flops() const { return 4 * arg.threads.x; }
-      long long bytes() const { return 3 * sizeof(T) * arg.threads.x; }
-
-    };
-
-  void axpby(device_vector<float> &out, float a, const device_vector<float> &x, float b,
-      const device_vector<float> &y)
+  void axpby(device_vector<float> &out, float a, const device_vector<float> &x, float b, const device_vector<float> &y)
   {
     axpby_wrapper<float> w(out.data(), a, x.data(), b, y.data(), out.size());
   }
 
-}
+} // namespace quda
