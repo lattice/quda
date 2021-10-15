ddf6bb1d96a90720516804e3689041a136fef6d3
maddyscientist <mclark@nvidia.com>
2021-10-14 17:56:12 -0700

    Improved quda::BlockReduce: 1.) handle multi-dimensional thread blocks, 2.) add support for Max and Min reductions, 3.) add some static_asserts to ensure correct usage

--- include/cub_helper.cuh
+++ include/cub_helper.cuh
@@ -49,26 +49,32 @@ namespace quda {
      @brief This is a convenience wrapper that allows us to perform
      reductions at the block level
   */
-  template <typename T, int block_size_x, int batch_size = 1> struct BlockReduce
+  template <typename T, int block_size_x, int batch_size = 1, int block_size_y = 1, int block_size_z = 1> struct BlockReduce
   {
-    using block_reduce_t = cub::BlockReduce<T, block_size_x, cub::BLOCK_REDUCE_WARP_REDUCTIONS, 1, 1, __COMPUTE_CAPABILITY__>;
+    static_assert(!((block_size_y > 1 || block_size_z > 1) && batch_size > 1), "Cannot use batching and multi-dimensional BlockReduce");
+
+    using block_reduce_t =
+      cub::BlockReduce<T, block_size_x, cub::BLOCK_REDUCE_WARP_REDUCTIONS, block_size_y, block_size_z, __COMPUTE_CAPABILITY__>;
     const int batch;
 
     __device__ __host__ inline BlockReduce(int batch = 0) : batch(batch) {}
 
-    template <bool is_device, typename dummy = void> struct sum { T operator()(const T &value, bool, int, bool) { return value; } };
+    template <bool is_device, typename dummy = void> struct reduce {
+      template <typename reducer_t> T operator()(const T &value, bool, int, bool, bool, reducer_t) { return value; }
+    };
 
-    template <typename dummy> struct sum<true, dummy> {
-      __device__ inline T operator()(const T &value_, bool pipeline, int batch, bool all_sum)
+    template <typename dummy> struct reduce<true, dummy> {
+      template <typename reducer_t>
+      __device__ inline T operator()(const T &value_, bool pipeline, int batch, bool all, bool is_sum, const reducer_t &r)
       {
         static __shared__ typename block_reduce_t::TempStorage storage[batch_size];
         block_reduce_t block_reduce(storage[batch]);
         if (!pipeline) __syncthreads(); // only synchronize if we are not pipelining
-        T value = block_reduce.Sum(value_);
+        T value = is_sum ? block_reduce.Sum(value_) : block_reduce.Reduce(value_, r);
 
-        if (all_sum) {
+        if (all) {
           T &value_shared = reinterpret_cast<T&>(storage[batch]);
-          if (threadIdx.x == 0 && threadIdx.y == 0) value_shared = value;
+          if (threadIdx.x == 0 && threadIdx.y == 0 && threadIdx.z == 0) value_shared = value;
           __syncthreads();
           value = value_shared;
         }
@@ -78,13 +84,37 @@ namespace quda {
 
     template <bool pipeline = false> __device__ __host__ inline T Sum(const T &value)
     {
-      return target::dispatch<sum>(value, pipeline, batch, false);
+      return target::dispatch<reduce>(value, pipeline, batch, false, true, cub::Sum());
     }
 
     template <bool pipeline = false> __device__ __host__ inline T AllSum(const T &value)
     {
-      return target::dispatch<sum>(value, pipeline, batch, true);
+      static_assert(batch_size == 1, "Cannot do AllSum with batch_size > 1");
+      return target::dispatch<reduce>(value, pipeline, batch, true, true, cub::Sum());
     }
+
+    template <bool pipeline = false> __device__ __host__ inline T Max(const T &value)
+    {
+      return target::dispatch<reduce>(value, pipeline, batch, false, false, cub::Max());
+    }
+
+    template <bool pipeline = false> __device__ __host__ inline T AllMax(const T &value)
+    {
+      static_assert(batch_size == 1, "Cannot do AllMax with batch_size > 1");
+      return target::dispatch<reduce>(value, pipeline, batch, true, false, cub::Max());
+    }
+
+    template <bool pipeline = false> __device__ __host__ inline T Min(const T &value)
+    {
+      return target::dispatch<reduce>(value, pipeline, batch, false, false, cub::Min());
+    }
+
+    template <bool pipeline = false> __device__ __host__ inline T AllMin(const T &value)
+    {
+      static_assert(batch_size == 1, "Cannot do AllMin with batch_size > 1");
+      return target::dispatch<reduce>(value, pipeline, batch, true, false, cub::Min());
+    }
+
   };
 
 } // namespace quda
