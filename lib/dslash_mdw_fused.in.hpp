e63b6ad6a8df5a2dcaf6a942b7906d512b491b1b
Jiqun Tu <jtu@nvidia.com>
2021-12-14 12:19:39 -0500

    - Change the cmake syntax from semicolon separated list to comma separated list. - Add clang-format flags for cmake string. - Change the *.in* file names so they are recognized in github.

---
+++ lib/dslash_mdw_fused.in.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <quda_internal.h>
+#include <color_spinor_field.h>
+#include <gauge_field.h>
+#include <clover_field.h>
+#include <worker.h>
+#include <domain_wall_helper.h>
+#include <fast_intdiv.h>
+
+// Tensor core functions for Mobius DWF
+namespace quda
+{
+
+  namespace mobius_tensor_core
+  {
+
+    template <int Ls>
+    void apply_fused_dslash_impl(ColorSpinorField &out, const ColorSpinorField &in, const GaugeField &U,
+                                 ColorSpinorField &y, const ColorSpinorField &x, double m_f, double m_5,
+                                 const Complex *b_5, const Complex *c_5, bool dagger, int parity, int shift[4],
+                                 int halo_shift[4], MdwfFusedDslashType type);
+
+    template <int...> struct IntList {
+    };
+
+    void inline apply_fused_dslash_list(ColorSpinorField &out, const ColorSpinorField &in, const GaugeField &U,
+                                        ColorSpinorField &y, const ColorSpinorField &x, double m_f, double m_5,
+                                        const Complex *b_5, const Complex *c_5, bool dagger, int parity, int shift[4],
+                                        int halo_shift[4], MdwfFusedDslashType type, IntList<>)
+    {
+      errorQuda("Ls = %d has not been instantiated", in.X(4));
+    }
+
+    template <int Ls, int... N>
+    void apply_fused_dslash_list(ColorSpinorField &out, const ColorSpinorField &in, const GaugeField &U,
+                                 ColorSpinorField &y, const ColorSpinorField &x, double m_f, double m_5,
+                                 const Complex *b_5, const Complex *c_5, bool dagger, int parity, int shift[4],
+                                 int halo_shift[4], MdwfFusedDslashType type, IntList<Ls, N...>)
+    {
+      if (in.X(4) == Ls) {
+        apply_fused_dslash_impl<Ls>(out, in, U, y, x, m_f, m_5, b_5, c_5, dagger, parity, shift, halo_shift, type);
+      } else {
+        apply_fused_dslash_list(out, in, U, y, x, m_f, m_5, b_5, c_5, dagger, parity, shift, halo_shift, type,
+                                IntList<N...>());
+      }
+    }
+
+    void inline apply_fused_dslash(ColorSpinorField &out, const ColorSpinorField &in, const GaugeField &U,
+                                   ColorSpinorField &y, const ColorSpinorField &x, double m_f, double m_5,
+                                   const Complex *b_5, const Complex *c_5, bool dagger, int parity, int shift[4],
+                                   int halo_shift[4], MdwfFusedDslashType type)
+    {
+      // clang-format off
+      IntList<@QUDA_MDW_FUSED_LS_LIST_COMMA@> int_list;
+      // clang-format on
+      apply_fused_dslash_list(out, in, U, y, x, m_f, m_5, b_5, c_5, dagger, parity, shift, halo_shift, type, int_list);
+    }
+
+  } // namespace mobius_tensor_core
+} // namespace quda
