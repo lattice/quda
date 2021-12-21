8b75d45d4fd17c8c6e8e368fd783883afcdc4d67
Jiqun Tu <jtu@nvidia.com>
2021-12-21 11:06:49 -0500

    Move kernel and arg of device_vector.cu to a .cuh file.

--- lib/device_vector.cu
+++ lib/device_vector.cu
@@ -1,34 +1,8 @@
 #include <device_vector.h>
-#include <tunable_nd.h>
-#include <tunable_reduction.h>
+#include <kernels/device_vector_axpby.cuh>
 
 namespace quda
 {
-
-  template <class T> struct AxpbyArg : kernel_param<> {
-    T *out;
-
-    T a;
-    const T *x;
-
-    T b;
-    const T *y;
-
-    AxpbyArg(T *out, T a, const T *x, T b, const T *y, int size) : kernel_param(size), out(out), a(a), x(x), b(b), y(y)
-    {
-    }
-  };
-
-  template <class Arg> struct Axpby {
-    const Arg &arg;
-    constexpr Axpby(const Arg &arg) : arg(arg) { }
-    static constexpr const char *filename() { return KERNEL_FILE; }
-
-    __device__ __host__ inline void operator()(int thread_idx)
-    {
-      arg.out[thread_idx] += arg.a * arg.x[thread_idx] + arg.b * arg.y[thread_idx];
-    }
-  };
 
   template <class T> struct axpby_wrapper : TunableKernel1D {
 
