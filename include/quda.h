137d36ed0b51bcd8f600d35dfa86f4b77ce3e128
chrisculver <chris.cb.culver@gmail.com>
2022-01-18 22:59:06 +0000

    Revert "Trying to merge"
    This reverts commit 321f8081833a2a719b8ea8f6365ac45b071b578e, reversing
    changes made to 4fbe6997134379cbeb822e076ebee2238e117223.

--- include/quda.h
+++ include/quda.h
@@ -1680,19 +1680,6 @@ extern "C" {
 
   void setMPICommHandleQuda(void *mycomm);
 
-
-
-  ///-----------LAPH SCATTERING-----------------------///
-
-  void createMesonBlocksQuda(const void *mesonBlocks, const void *eigenVectors);
-
-
-  ///-----------END LAPH SCATTERING--------------------///
-
-
-
-
-
 #ifdef __cplusplus
 }
 #endif
--- lib/laph.cpp
+++
@@ -1,7 +0,0 @@
-#include "quda.h"
-
-
-void createMesonBlocksQuda(const void *mesonBlocks, const void *eigenVectors)
-{
-
-}
