3e1cde0f4cb3a8488045c471cc2e1b9565d7865c
maddyscientist <mclark@nvidia.com>
2022-02-08 09:54:08 -0800

    Define NSPIN4 when contraction kernels are enabled

--- include/quda_internal.h
+++ include/quda_internal.h
@@ -22,7 +22,7 @@
 // these are helper macros used to enable spin-1, spin-2 and spin-4 building blocks as needed
 #if defined(GPU_WILSON_DIRAC) || defined(GPU_DOMAIN_WALL_DIRAC) || defined(GPU_CLOVER_DIRAC)                           \
   || defined(GPU_TWISTED_MASS_DIRAC) || defined(GPU_TWISTED_CLOVER_DIRAC) || defined(GPU_NDEG_TWISTED_MASS_DIRAC)      \
-  || defined(GPU_CLOVER_HASENBUSCH_TWIST) || defined(GPU_COVDEV)
+  || defined(GPU_CLOVER_HASENBUSCH_TWIST) || defined(GPU_COVDEV) || defined(GPU_CONTRACT)
 #define NSPIN4
 #endif
 
