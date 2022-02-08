960573f1a94b93e87bc4282c0e56aef986eb18ce
James Osborn <osborn@alcf.anl.gov>
2022-02-08 13:16:16 -0600

    fix generic version

--- include/targets/generic/reduce_helper.h
+++ include/targets/generic/reduce_helper.h
@@ -144,7 +144,7 @@ namespace quda
     // finish the reduction if last block
     if (isLastBlockDone[target::thread_idx().z]) {
       auto i = target::thread_idx().y * block_size_x + target::thread_idx().x;
-      T sum = arg.init();
+      T sum = r.init();
       while (i < target::grid_dim().x) {
         sum = r(sum, const_cast<T &>(static_cast<volatile T *>(arg.partial)[idx * target::grid_dim().x + i]));
         i += block_size_x * block_size_y;
