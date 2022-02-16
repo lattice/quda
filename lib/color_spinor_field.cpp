lattice/quda

commit b3cc6f8f992f186ede78a3614a5ff203e4aa7f8e

Cleanup of LatticeField member static arrays: now use explicit array objects to enable us to use std::exchange to allow us to fully implement move construction and assignment with comms preservation (will be in next commit).  Updated various interfaces as a result of this change, specifically with respect to extended gauge-field exchange.  The various comms-related arrays now all use a consistent ordering [dim][dir], and replacement of fwd/back message handlers with a [dir] index

lib/color_spinor_field.cpp
@@ -21,7 +21,6 @@ namespace quda
     v(nullptr),
     norm_offset(0),
     ghost(),
-    ghostNorm(),
     ghostFace(),
     ghost_buf{ },
     dslash_constant(nullptr),
@@ -43,7 +42,6 @@ namespace quda
     v(nullptr),
     norm_offset(0),
     ghost(),
-    ghostNorm(),
     ghostFace(),
     ghost_buf{ },
     dslash_constant(nullptr),
@@ -81,7 +79,6 @@ namespace quda
     v(nullptr),
     norm_offset(0),
     ghost(),
-    ghostNorm(),
     ghostFace(),
     ghost_buf{ },
     dslash_constant(nullptr),
@@ -116,8 +113,8 @@ namespace quda
     v_h(std::exchange(field.v_h, nullptr)),
     norm_offset(std::exchange(field.norm_offset, 0)),
     ghost(),
-    ghostNorm(),
     ghostFace(),
+    ghostFaceCB(),
     ghost_buf{ },
     dslash_constant(std::exchange(field.dslash_constant, nullptr)),
     bytes(std::exchange(field.bytes, 0)),
@@ -648,7 +645,6 @@ namespace quda
 
   void ColorSpinorField::exchange(void **ghost, void **sendbuf, int nFace) const
   {
-
     // FIXME: use LatticeField MsgHandles
     MsgHandle *mh_send_fwd[4];
     MsgHandle *mh_from_back[4];
@@ -842,7 +838,7 @@ namespace quda
     return *(components[idx]);
   }
 
-  void *const *ColorSpinorField::Ghost() const { return ghost_buf; }
+  void *const *ColorSpinorField::Ghost() const { return ghost_buf.data; }
 
   const void *ColorSpinorField::Ghost2() const
   {
@@ -1086,9 +1082,6 @@ namespace quda
         if (commDimPartitioned(i)) {
           for (int b = 0; b < 2; b++) {
             ghost[b][i] = static_cast<char *>(ghost_recv_buffer_d[b]) + ghost_offset[i][0];
-            if (ghost_precision == QUDA_HALF_PRECISION || ghost_precision == QUDA_QUARTER_PRECISION)
-              ghostNorm[b][i] = static_cast<char *>(ghost[b][i])
-                + nFace * surface[i] * (nSpin / (spin_project ? 2 : 1)) * nColor * 2 * ghost_precision;
           }
         }
       }
@@ -1206,24 +1199,12 @@ namespace quda
     if (!commDimPartitioned(dim)) return;
     if (gdr && !comm_gdr_enabled()) errorQuda("Requesting GDR comms but GDR is not enabled");
 
-    if (dir == 0) { // receive from forwards
-      // receive from the processor in the +1 direction
-      if (comm_peer2peer_enabled(1, dim)) {
-        comm_start(mh_recv_p2p_fwd[bufferIndex][dim]);
-      } else if (gdr) {
-        comm_start(mh_recv_rdma_fwd[bufferIndex][dim]);
-      } else {
-        comm_start(mh_recv_fwd[bufferIndex][dim]);
-      }
-    } else { // receive from backwards
-      // receive from the processor in the -1 direction
-      if (comm_peer2peer_enabled(0, dim)) {
-        comm_start(mh_recv_p2p_back[bufferIndex][dim]);
-      } else if (gdr) {
-        comm_start(mh_recv_rdma_back[bufferIndex][dim]);
-      } else {
-        comm_start(mh_recv_back[bufferIndex][dim]);
-      }
+    if (comm_peer2peer_enabled(1 - dir, dim)) {
+      comm_start(mh_recv_p2p[bufferIndex][dim][1 - dir]);
+    } else if (gdr) {
+      comm_start(mh_recv_rdma[bufferIndex][dim][1 - dir]);
+    } else {
+      comm_start(mh_recv[bufferIndex][dim][1 - dir]);
     }
   }
 
@@ -1239,15 +1220,8 @@ namespace quda
     if (gdr && !comm_gdr_enabled()) errorQuda("Requesting GDR comms but GDR is not enabled");
 
     if (!comm_peer2peer_enabled(dir, dim)) {
-      if (dir == 0)
-        if (gdr)
-          comm_start(mh_send_rdma_back[bufferIndex][dim]);
-        else
-          comm_start(mh_send_back[bufferIndex][dim]);
-      else if (gdr)
-        comm_start(mh_send_rdma_fwd[bufferIndex][dim]);
-      else
-        comm_start(mh_send_fwd[bufferIndex][dim]);
+      if (gdr) comm_start(mh_send_rdma[bufferIndex][dim][dir]);
+      else     comm_start(mh_send[bufferIndex][dim][dir]);
     } else { // doing peer-to-peer
 
       // if not using copy engine then the packing kernel will remotely write the halos
@@ -1259,16 +1233,10 @@ namespace quda
         qudaMemcpyP2PAsync(ghost_dst, my_face_dim_dir_d[bufferIndex][dim][dir], ghost_face_bytes[dim], stream);
       } // remote_write
 
-      if (dir == 0) {
         // record the event
-        qudaEventRecord(ipcCopyEvent[bufferIndex][0][dim], stream);
-        // send to the processor in the -1 direction
-        comm_start(mh_send_p2p_back[bufferIndex][dim]);
-      } else {
-        qudaEventRecord(ipcCopyEvent[bufferIndex][1][dim], stream);
-        // send to the processor in the +1 direction
-        comm_start(mh_send_p2p_fwd[bufferIndex][dim]);
-      }
+      qudaEventRecord(ipcCopyEvent[bufferIndex][dim][dir], stream);
+      // send to the processor in the -1 direction
+      comm_start(mh_send_p2p[bufferIndex][dim][dir]);
     }
   }
 
@@ -1278,10 +1246,8 @@ namespace quda
     sendStart(dir, stream, gdr_send);
   }
 
-  static bool complete_recv_fwd[QUDA_MAX_DIM] = {};
-  static bool complete_recv_back[QUDA_MAX_DIM] = {};
-  static bool complete_send_fwd[QUDA_MAX_DIM] = {};
-  static bool complete_send_back[QUDA_MAX_DIM] = {};
+  static bool complete_recv[QUDA_MAX_DIM][2] = {};
+  static bool complete_send[QUDA_MAX_DIM][2] = {};
 
   int ColorSpinorField::commsQuery(int d, const qudaStream_t &, bool gdr_send, bool gdr_recv)
   {
@@ -1295,60 +1261,31 @@ namespace quda
     if (!commDimPartitioned(dim)) return 1;
     if ((gdr_send || gdr_recv) && !comm_gdr_enabled()) errorQuda("Requesting GDR comms but GDR is not enabled");
 
-    if (dir == 0) {
-
-      // first query send to backwards
-      if (comm_peer2peer_enabled(0, dim)) {
-        if (!complete_send_back[dim]) complete_send_back[dim] = comm_query(mh_send_p2p_back[bufferIndex][dim]);
-      } else if (gdr_send) {
-        if (!complete_send_back[dim]) complete_send_back[dim] = comm_query(mh_send_rdma_back[bufferIndex][dim]);
-      } else {
-        if (!complete_send_back[dim]) complete_send_back[dim] = comm_query(mh_send_back[bufferIndex][dim]);
-      }
-
-      // second query receive from forwards
-      if (comm_peer2peer_enabled(1, dim)) {
-        if (!complete_recv_fwd[dim]) complete_recv_fwd[dim] = comm_query(mh_recv_p2p_fwd[bufferIndex][dim]);
-      } else if (gdr_recv) {
-        if (!complete_recv_fwd[dim]) complete_recv_fwd[dim] = comm_query(mh_recv_rdma_fwd[bufferIndex][dim]);
-      } else {
-        if (!complete_recv_fwd[dim]) complete_recv_fwd[dim] = comm_query(mh_recv_fwd[bufferIndex][dim]);
-      }
-
-      if (complete_recv_fwd[dim] && complete_send_back[dim]) {
-        complete_send_back[dim] = false;
-        complete_recv_fwd[dim] = false;
-        return 1;
-      }
-
-    } else { // dir == 1
-
-      // first query send to forwards
-      if (comm_peer2peer_enabled(1, dim)) {
-        if (!complete_send_fwd[dim]) complete_send_fwd[dim] = comm_query(mh_send_p2p_fwd[bufferIndex][dim]);
-      } else if (gdr_send) {
-        if (!complete_send_fwd[dim]) complete_send_fwd[dim] = comm_query(mh_send_rdma_fwd[bufferIndex][dim]);
-      } else {
-        if (!complete_send_fwd[dim]) complete_send_fwd[dim] = comm_query(mh_send_fwd[bufferIndex][dim]);
-      }
-
-      // second query receive from backwards
-      if (comm_peer2peer_enabled(0, dim)) {
-        if (!complete_recv_back[dim]) complete_recv_back[dim] = comm_query(mh_recv_p2p_back[bufferIndex][dim]);
-      } else if (gdr_recv) {
-        if (!complete_recv_back[dim]) complete_recv_back[dim] = comm_query(mh_recv_rdma_back[bufferIndex][dim]);
-      } else {
-        if (!complete_recv_back[dim]) complete_recv_back[dim] = comm_query(mh_recv_back[bufferIndex][dim]);
-      }
-
-      if (complete_recv_back[dim] && complete_send_fwd[dim]) {
-        complete_send_fwd[dim] = false;
-        complete_recv_back[dim] = false;
-        return 1;
-      }
+    // first query send to backwards
+    if (comm_peer2peer_enabled(dir, dim)) {
+      if (!complete_send[dim][dir]) complete_send[dim][dir] = comm_query(mh_send_p2p[bufferIndex][dim][dir]);
+    } else if (gdr_send) {
+      if (!complete_send[dim][dir]) complete_send[dim][dir] = comm_query(mh_send_rdma[bufferIndex][dim][dir]);
+    } else {
+      if (!complete_send[dim][dir]) complete_send[dim][dir] = comm_query(mh_send[bufferIndex][dim][dir]);
     }
 
-    return 0;
+    // second query receive from forwards
+    if (comm_peer2peer_enabled(1 - dir, dim)) {
+      if (!complete_recv[dim][1 - dir]) complete_recv[dim][1 - dir] = comm_query(mh_recv_p2p[bufferIndex][dim][1 - dir]);
+    } else if (gdr_recv) {
+      if (!complete_recv[dim][1 - dir]) complete_recv[dim][1 - dir] = comm_query(mh_recv_rdma[bufferIndex][dim][1 - dir]);
+    } else {
+      if (!complete_recv[dim][1 - dir]) complete_recv[dim][1 - dir] = comm_query(mh_recv[bufferIndex][dim][1 - dir]);
+    }
+
+    if (complete_recv[dim][1 - dir] && complete_send[dim][dir]) {
+      complete_send[dim][dir] = false;
+      complete_recv[dim][1 - dir] = false;
+      return 1;
+    } else {
+      return 0;
+    }
   }
 
   void ColorSpinorField::commsWait(int d, const qudaStream_t &, bool gdr_send, bool gdr_recv)
@@ -1363,49 +1300,24 @@ namespace quda
     if (!commDimPartitioned(dim)) return;
     if ((gdr_send && gdr_recv) && !comm_gdr_enabled()) errorQuda("Requesting GDR comms but GDR is not enabled");
 
-    if (dir == 0) {
-
-      // first wait on send to backwards
-      if (comm_peer2peer_enabled(0, dim)) {
-        comm_wait(mh_send_p2p_back[bufferIndex][dim]);
-        qudaEventSynchronize(ipcCopyEvent[bufferIndex][0][dim]);
-      } else if (gdr_send) {
-        comm_wait(mh_send_rdma_back[bufferIndex][dim]);
-      } else {
-        comm_wait(mh_send_back[bufferIndex][dim]);
-      }
-
-      // second wait on receive from forwards
-      if (comm_peer2peer_enabled(1, dim)) {
-        comm_wait(mh_recv_p2p_fwd[bufferIndex][dim]);
-        qudaEventSynchronize(ipcRemoteCopyEvent[bufferIndex][1][dim]);
-      } else if (gdr_recv) {
-        comm_wait(mh_recv_rdma_fwd[bufferIndex][dim]);
-      } else {
-        comm_wait(mh_recv_fwd[bufferIndex][dim]);
-      }
-
+    // first wait on send to "dir"
+    if (comm_peer2peer_enabled(dir, dim)) {
+      comm_wait(mh_send_p2p[bufferIndex][dim][dir]);
+      qudaEventSynchronize(ipcCopyEvent[bufferIndex][dim][dir]);
+    } else if (gdr_send) {
+      comm_wait(mh_send_rdma[bufferIndex][dim][dir]);
     } else {
+      comm_wait(mh_send[bufferIndex][dim][dir]);
+    }
 
-      // first wait on send to forwards
-      if (comm_peer2peer_enabled(1, dim)) {
-        comm_wait(mh_send_p2p_fwd[bufferIndex][dim]);
-        qudaEventSynchronize(ipcCopyEvent[bufferIndex][1][dim]);
-      } else if (gdr_send) {
-        comm_wait(mh_send_rdma_fwd[bufferIndex][dim]);
-      } else {
-        comm_wait(mh_send_fwd[bufferIndex][dim]);
-      }
-
-      // second wait on receive from backwards
-      if (comm_peer2peer_enabled(0, dim)) {
-        comm_wait(mh_recv_p2p_back[bufferIndex][dim]);
-        qudaEventSynchronize(ipcRemoteCopyEvent[bufferIndex][0][dim]);
-      } else if (gdr_recv) {
-        comm_wait(mh_recv_rdma_back[bufferIndex][dim]);
-      } else {
-        comm_wait(mh_recv_back[bufferIndex][dim]);
-      }
+    // second wait on receive from "1 - dir"
+    if (comm_peer2peer_enabled(1 - dir, dim)) {
+      comm_wait(mh_recv_p2p[bufferIndex][dim][1 - dir]);
+      qudaEventSynchronize(ipcRemoteCopyEvent[bufferIndex][dim][1 - dir]);
+    } else if (gdr_recv) {
+      comm_wait(mh_recv_rdma[bufferIndex][dim][1 - dir]);
+    } else {
+      comm_wait(mh_recv[bufferIndex][dim][1 - dir]);
     }
   }
 
@@ -1449,7 +1361,7 @@ namespace quda
 
       packGhostHost(sendbuf, parity, nFace, dagger);
 
-      exchange(ghost_buf, sendbuf, nFace);
+      exchange(ghost_buf.data, sendbuf, nFace);
 
       host_free(sendbuf);
     } else {

include/color_spinor_field.h
@@ -305,7 +305,6 @@ namespace quda
 
   class ColorSpinorField : public LatticeField
   {
-
   private:
     void create(const ColorSpinorParam &param);
     void destroy();
@@ -339,14 +338,10 @@ namespace quda
     size_t norm_offset; /** offset to the norm (if applicable) */
 
     // multi-GPU parameters
-
-    void *ghost[2][QUDA_MAX_DIM];     // pointers to the ghost regions - NULL by default
-    void *ghostNorm[2][QUDA_MAX_DIM]; // pointers to ghost norms - NULL by default
-
-    mutable int ghostFace[QUDA_MAX_DIM];   // the size of each face
-    mutable int ghostFaceCB[QUDA_MAX_DIM]; // the size of each checkboarded face
-
-    mutable void *ghost_buf[2 * QUDA_MAX_DIM]; // wrapper that points to current ghost zone
+    array<array<void*, QUDA_MAX_DIM>, 2> ghost;     // pointers to the ghost regions - NULL by default
+    mutable lat_dim_t ghostFace;   // the size of each face
+    mutable lat_dim_t ghostFaceCB; // the size of each checkboarded face
+    mutable array<void*, 2 * QUDA_MAX_DIM> ghost_buf; // wrapper that points to current ghost zone
 
     mutable DslashConstant *dslash_constant; // constants used by dslash and packing kernels
 
@@ -694,8 +689,8 @@ namespace quda
     QudaFieldOrder FieldOrder() const { return fieldOrder; }
     QudaGammaBasis GammaBasis() const { return gammaBasis; }
 
-    const int *GhostFace() const { return ghostFace; }
-    const int *GhostFaceCB() const { return ghostFaceCB; }
+    const int *GhostFace() const { return ghostFace.data; }
+    const int *GhostFaceCB() const { return ghostFaceCB.data; }
 
     /**
        Return the offset in bytes to the start of the ghost zone in a
@@ -706,6 +701,7 @@ namespace quda
     size_t GhostOffset(const int dim, const int dir) const { return ghost_offset[dim][dir]; }
 
     const void *Ghost2() const;
+
     /**
        Return array of pointers to the ghost zones (ordering dim*2+dir)
      */

include/comm_quda.h
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <quda_constants.h>
 #include <quda_api.h>
+#include <array.h>
 
 using namespace quda;
 
@@ -246,7 +247,7 @@ extern "C" {
     @param[in] local The process-local memory pointer to be exchanged
     from this process
   */
-  void comm_create_neighbor_memory(void *remote[QUDA_MAX_DIM][2], void *local);
+  void comm_create_neighbor_memory(array<array<void *, 2>, QUDA_MAX_DIM> &remote, void * local);
 
   /**
      @brief Deallocate the remote addresses to logically neighboring
@@ -254,7 +255,7 @@ extern "C" {
      @param[in] remote Array of remote memory pointers to neighboring
      pointers
    */
-  void comm_destroy_neighbor_memory(void *remote[QUDA_MAX_DIM][2]);
+  void comm_destroy_neighbor_memory(array<array<void *, 2>, QUDA_MAX_DIM> &remote);
 
   /**
      @brief Create unique events shared between each logical pair of
@@ -266,14 +267,16 @@ extern "C" {
      @param[out] remote Array of remote events to neighboring processes
      @param[in] local Array of local event to neighboring processes
    */
-  void comm_create_neighbor_event(qudaEvent_t remote[2][QUDA_MAX_DIM], qudaEvent_t local[2][QUDA_MAX_DIM]);
+  void comm_create_neighbor_event(array<array<qudaEvent_t, 2>, QUDA_MAX_DIM> &remote,
+                                  array<array<qudaEvent_t, 2>, QUDA_MAX_DIM> &local);
 
   /**
      @brief Destroy the coupled events
      @param[out] remote Array of remote events to neighboring processes
      @param[in] local Array of local event to neighboring processes
    */
-  void comm_destroy_neighbor_event(qudaEvent_t remote[2][QUDA_MAX_DIM], qudaEvent_t local[2][QUDA_MAX_DIM]);
+  void comm_destroy_neighbor_event(array<array<qudaEvent_t, 2>, QUDA_MAX_DIM> &remote,
+                                   array<array<qudaEvent_t, 2>, QUDA_MAX_DIM> &local);
 
   /**
      @brief Returns true if any peer-to-peer capability is present on

include/lattice_field.h
@@ -240,47 +240,47 @@ namespace quda {
     /**
        Double buffered static GPU halo send buffer
     */
-    static void *ghost_send_buffer_d[2];
+    inline static array<void *, 2> ghost_send_buffer_d = { };
 
     /**
        Double buffered static GPU halo receive buffer
      */
-    static void *ghost_recv_buffer_d[2];
+    inline static array<void *, 2> ghost_recv_buffer_d = { };
 
     /**
        Double buffered static pinned send buffers
     */
-    static void *ghost_pinned_send_buffer_h[2];
+    inline static array<void *, 2> ghost_pinned_send_buffer_h = { };
 
     /**
        Double buffered static pinned recv buffers
     */
-    static void *ghost_pinned_recv_buffer_h[2];
+    inline static array<void *, 2> ghost_pinned_recv_buffer_h = { };
 
     /**
        Mapped version of pinned send buffers
     */
-    static void *ghost_pinned_send_buffer_hd[2];
+    inline static array<void *, 2> ghost_pinned_send_buffer_hd = { };
 
     /**
        Mapped version of pinned recv buffers
     */
-    static void *ghost_pinned_recv_buffer_hd[2];
+    inline static array<void *, 2> ghost_pinned_recv_buffer_hd = { };
 
     /**
        Remove ghost pointer for sending to
     */
-    static void *ghost_remote_send_buffer_d[2][QUDA_MAX_DIM][2];
+    inline static array<array<array<void *, 2>, QUDA_MAX_DIM>, 2> ghost_remote_send_buffer_d;
 
     /**
        The current size of the static ghost allocation
     */
-    static size_t ghostFaceBytes;
+    inline static size_t ghostFaceBytes = 0;
 
     /**
        Whether the ghost buffers have been initialized
     */
-    static bool initGhostFaceBuffer;
+    inline static bool initGhostFaceBuffer = false;
 
     /**
        Size in bytes of this ghost field
@@ -295,136 +295,161 @@ namespace quda {
     /**
        Size in bytes of the ghost in each dimension
     */
-    mutable size_t ghost_face_bytes[QUDA_MAX_DIM];
+    mutable array<size_t, QUDA_MAX_DIM> ghost_face_bytes;
 
     /**
        Actual allocated size in bytes of the ghost in each dimension
     */
-    mutable size_t ghost_face_bytes_aligned[QUDA_MAX_DIM];
+    mutable array<size_t, QUDA_MAX_DIM> ghost_face_bytes_aligned;
 
     /**
        Byte offsets to each ghost zone
     */
-    mutable size_t ghost_offset[QUDA_MAX_DIM][2];
+    mutable array<array<size_t, 2>, QUDA_MAX_DIM> ghost_offset;
 
     /**
        Pinned memory buffer used for sending messages
     */
-    void *my_face_h[2];
+    array<void *, 2> my_face_h;
 
     /**
        Mapped version of my_face_h
     */
-    void *my_face_hd[2];
+    array<void *, 2> my_face_hd;
 
     /**
        Device memory buffer for sending messages
      */
-    void *my_face_d[2];
+    array<void *, 2> my_face_d;
 
-    /** Local pointers to the pinned my_face buffer */
-    void *my_face_dim_dir_h[2][QUDA_MAX_DIM][2];
+    /**
+       Local pointers to the pinned my_face buffer
+    */
+    array<array<array<void *, 2>, QUDA_MAX_DIM>, 2> my_face_dim_dir_h;
 
-    /** Local pointers to the mapped my_face buffer */
-    void *my_face_dim_dir_hd[2][QUDA_MAX_DIM][2];
+    /**
+       Local pointers to the mapped my_face buffer
+    */
+    array<array<array<void *, 2>, QUDA_MAX_DIM>, 2> my_face_dim_dir_hd;
 
-    /** Local pointers to the device ghost_send buffer */
-    void *my_face_dim_dir_d[2][QUDA_MAX_DIM][2];
+    /**
+       Local pointers to the device ghost_send buffer
+    */
+    array<array<array<void *, 2>, QUDA_MAX_DIM>, 2> my_face_dim_dir_d;
 
     /**
        Memory buffer used for receiving all messages
     */
-    void *from_face_h[2];
+    array<void *, 2> from_face_h;
 
     /**
        Mapped version of from_face_h
     */
-    void *from_face_hd[2];
+    array<void *, 2> from_face_hd;
 
     /**
        Device memory buffer for receiving messages
      */
-    void *from_face_d[2];
+    array<void *, 2> from_face_d;
 
-    /** Local pointers to the pinned from_face buffer */
-    void *from_face_dim_dir_h[2][QUDA_MAX_DIM][2];
+    /**
+       Local pointers to the pinned from_face buffer
+    */
+    array<array<array<void *, 2>, QUDA_MAX_DIM>, 2> from_face_dim_dir_h;
 
-    /** Local pointers to the mapped from_face buffer */
-    void *from_face_dim_dir_hd[2][QUDA_MAX_DIM][2];
+    /**
+       Local pointers to the mapped from_face buffer
+    */
+    array<array<array<void *, 2>, QUDA_MAX_DIM>, 2> from_face_dim_dir_hd;
 
-    /** Local pointers to the device ghost_recv buffer */
-    void *from_face_dim_dir_d[2][QUDA_MAX_DIM][2];
+    /**
+       Local pointers to the device ghost_recv buffer
+    */
+    array<array<array<void *, 2>, QUDA_MAX_DIM>, 2> from_face_dim_dir_d;
 
-    /** Message handles for receiving from forwards */
-    MsgHandle *mh_recv_fwd[2][QUDA_MAX_DIM];
+    /**
+       Message handles for receiving
+    */
+    array<array<array<MsgHandle *, 2>, QUDA_MAX_DIM>, 2> mh_recv;
 
-    /** Message handles for receiving from backwards */
-    MsgHandle *mh_recv_back[2][QUDA_MAX_DIM];
+    /**
+       Message handles for sending
+    */
+    array<array<array<MsgHandle *, 2>, QUDA_MAX_DIM>, 2> mh_send;
 
-    /** Message handles for sending forwards */
-    MsgHandle *mh_send_fwd[2][QUDA_MAX_DIM];
+    /**
+       Message handles for receiving
+    */
+    array<array<array<MsgHandle *, 2>, QUDA_MAX_DIM>, 2> mh_recv_rdma;
 
-    /** Message handles for sending backwards */
-    MsgHandle *mh_send_back[2][QUDA_MAX_DIM];
+    /**
+       Message handles for sending
+    */
+    array<array<array<MsgHandle *, 2>, QUDA_MAX_DIM>, 2> mh_send_rdma;
 
-    /** Message handles for rdma receiving from forwards */
-    MsgHandle *mh_recv_rdma_fwd[2][QUDA_MAX_DIM];
+    /**
+       Message handles for receiving
+    */
+    inline static array<array<array<MsgHandle *, 2>, QUDA_MAX_DIM>, 2> mh_recv_p2p = { };
 
-    /** Message handles for rdma receiving from backwards */
-    MsgHandle *mh_recv_rdma_back[2][QUDA_MAX_DIM];
+    /**
+       Message handles for sending
+    */
+    inline static array<array<array<MsgHandle *, 2>, QUDA_MAX_DIM>, 2> mh_send_p2p = { };
 
-    /** Message handles for rdma sending to forwards */
-    MsgHandle *mh_send_rdma_fwd[2][QUDA_MAX_DIM];
+    /**
+       Buffer used by peer-to-peer message handler
+    */
+    inline static array<array<array<int, 2>, QUDA_MAX_DIM>, 2> buffer_send_p2p = { };
 
-    /** Message handles for rdma sending to backwards */
-    MsgHandle *mh_send_rdma_back[2][QUDA_MAX_DIM];
+    /**
+       Buffer used by peer-to-peer message handler
+    */
+    inline static array<array<array<int, 2>, QUDA_MAX_DIM>, 2> buffer_recv_p2p = { };
 
-    /** Peer-to-peer message handler for signaling event posting */
-    static MsgHandle *mh_send_p2p_fwd[2][QUDA_MAX_DIM];
+    /**
+       Local copy of event used for peer-to-peer synchronization
+    */
+    inline static array<array<array<qudaEvent_t, 2>, QUDA_MAX_DIM>, 2> ipcCopyEvent = { };
 
-    /** Peer-to-peer message handler for signaling event posting */
-    static MsgHandle *mh_send_p2p_back[2][QUDA_MAX_DIM];
+    /**
+       Remote copy of event used for peer-to-peer synchronization
+    */
+    inline static array<array<array<qudaEvent_t, 2>, QUDA_MAX_DIM>, 2> ipcRemoteCopyEvent = { };
 
-    /** Peer-to-peer message handler for signaling event posting */
-    static MsgHandle *mh_recv_p2p_fwd[2][QUDA_MAX_DIM];
-
-    /** Peer-to-peer message handler for signaling event posting */
-    static MsgHandle *mh_recv_p2p_back[2][QUDA_MAX_DIM];
-
-    /** Buffer used by peer-to-peer message handler */
-    static int buffer_send_p2p_fwd[2][QUDA_MAX_DIM];
-
-    /** Buffer used by peer-to-peer message handler */
-    static int buffer_recv_p2p_fwd[2][QUDA_MAX_DIM];
-
-    /** Buffer used by peer-to-peer message handler */
-    static int buffer_send_p2p_back[2][QUDA_MAX_DIM];
-
-    /** Buffer used by peer-to-peer message handler */
-    static int buffer_recv_p2p_back[2][QUDA_MAX_DIM];
-
-    /** Local copy of event used for peer-to-peer synchronization */
-    static qudaEvent_t ipcCopyEvent[2][2][QUDA_MAX_DIM];
-
-    /** Remote copy of event used for peer-to-peer synchronization */
-    static qudaEvent_t ipcRemoteCopyEvent[2][2][QUDA_MAX_DIM];
-
-    /** Whether we have initialized communication for this field */
+    /**
+       Whether we have initialized communication for this field
+    */
     bool initComms;
 
-    /** Whether we have initialized peer-to-peer communication */
-    static bool initIPCComms;
+    /**
+       Whether we have initialized peer-to-peer communication
+    */
+    inline static bool initIPCComms = false;
 
-    /** Used as a label in the autotuner */
+    /**
+       Bool which is triggered if the ghost field is reset
+    */
+    inline static bool ghost_field_reset = false;
+
+    /**
+       Used as a label in the autotuner
+    */
     std::string vol_string;
 
-    /** used as a label in the autotuner */
+    /**
+       Used as a label in the autotuner
+    */
     std::string aux_string;
 
-    /** Sets the vol_string for use in tuning */
+    /**
+       Sets the vol_string for use in tuning
+    */
     virtual void setTuningString();
 
-    /** The type of allocation we are going to do for this field */
+    /**
+       The type of allocation we are going to do for this field
+    */
     QudaMemoryType mem_type;
 
     void precisionCheck()
@@ -443,6 +468,11 @@ namespace quda {
     mutable bool backed_up;
 
   public:
+
+    /**
+       Static variable that is determined which ghost buffer we are using
+     */
+    inline static int bufferIndex = 0;
 
     /**
        @brief Default constructor
@@ -557,16 +587,6 @@ namespace quda {
        Handle to remote copy event used for peer-to-peer synchronization
     */
     const qudaEvent_t &getIPCRemoteCopyEvent(int dir, int dim) const;
-
-    /**
-       Static variable that is determined which ghost buffer we are using
-     */
-    static int bufferIndex;
-
-    /**
-       Bool which is triggered if the ghost field is reset
-    */
-    static bool ghost_field_reset;
 
     /**
        @return The dimension of the lattice 

commit b2dc9b2d5932bcbb3a15593297208ac916d2f4f9

Fix bug introduced in b3cc6f8f992f186ede78a3614a5ff203e4aa7f8e: dim<->dir swizzle not applied

lib/color_spinor_field.cpp
@@ -1483,7 +1483,7 @@ namespace quda
               if (comms_complete[2 * dim + dir]) {
                 comms_done++;
                 if (comm_peer2peer_enabled(1 - dir, dim))
-                  qudaStreamWaitEvent(device::get_default_stream(), ipcRemoteCopyEvent[bufferIndex][1 - dir][dim], 0);
+                  qudaStreamWaitEvent(device::get_default_stream(), ipcRemoteCopyEvent[bufferIndex][dim][1 - dir], 0);
               }
             }
           }
@@ -1520,7 +1520,7 @@ namespace quda
         if (!comm_dim_partitioned(dim)) continue;
         for (int dir = 0; dir < 2; dir++) {
           if (comm_peer2peer_enabled(dir, dim))
-            qudaStreamWaitEvent(device::get_default_stream(), ipcCopyEvent[bufferIndex][dir][dim], 0);
+            qudaStreamWaitEvent(device::get_default_stream(), ipcCopyEvent[bufferIndex][dim][dir], 0);
         }
       }
     }