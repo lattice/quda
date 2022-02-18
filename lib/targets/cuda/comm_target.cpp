364c5d03e9226cb74b105f34e8692e2722b9bc7d
Mathias Wagner <mathiasw@nvidia.com>
2022-02-18 10:09:45 +0100

    fix warning

--- lib/targets/cuda/comm_target.cpp
+++ lib/targets/cuda/comm_target.cpp
@@ -33,10 +33,10 @@ int comm_peer2peer_performance(int local_gpuid, int neighbor_gpuid)
 
 void comm_create_neighbor_memory(void *remote[QUDA_MAX_DIM][2], void *local)
 {
+#ifndef NVSHMEM_COMMS
   // handles for obtained ghost pointers
   cudaIpcMemHandle_t remote_handle[2][QUDA_MAX_DIM];
 
-#ifndef NVSHMEM_COMMS
   for (int dim = 0; dim < 4; ++dim) {
     if (comm_dim(dim) == 1) continue;
     for (int dir = 0; dir < 2; ++dir) {
