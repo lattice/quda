4a79b71b82f3c10d6d51558cba9a2a27984353c8
Balint Joo <joob@ornl.gov>
2022-03-09 13:30:44 -0500

    Removed commented out code

--- cmake/find_target_cuda_dependencies.cmake
+++ cmake/find_target_cuda_dependencies.cmake
@@ -1,13 +1,6 @@
+# CUDA Specific CMake
+
 enable_language(CUDA)
 
 find_dependency(CUDAToolkit REQUIRED)
 
-#if( QUDA_NVSHMEM )
-#  set(NVSHMEM_LIBS @NVSHMEM_LIBS@)
-#  set(NVSHMEM_INCLUDE @NVSHMEM_INCLUDE@)
-#  add_library(nvshmem_lib STATIC IMPORTED)
-#  set_target_properties(nvshmem_lib PROPERTIES IMPORTED_LOCATION ${NVSHMEM_LIBS})
-#  set_target_properties(nvshmem_lib PROPERTIES CUDA_SEPARABLE_COMPILATION ON)
-#  set_target_properties(nvshmem_lib PROPERTIES CUDA_RESOLVE_DEVICE_SYMBOLS OFF)
-#  set_target_properties(nvshmem_lib PROPERTIES IMPORTED_LINK_INTERFACE_LANGUAGES CUDA)
-#endif()
