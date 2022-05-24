ee450c043106b44e46101f76c716dbded6d38b36
maddyscientist <mclark@nvidia.com>
2022-05-24 11:58:24 -0700

    Gaussian RNG functions now use sincospi and not sincos

--- include/kernels/spinor_noise.cuh
+++ include/kernels/spinor_noise.cuh
@@ -24,11 +24,11 @@ namespace quda {
 
   template<typename real, typename Arg> // Gauss
   __device__ __host__ inline void genGauss(Arg &arg, RNGState& localState, int parity, int x_cb, int s, int c) {
-    real phi = 2.0*M_PI*uniform<real>::rand(localState);
+    real phi = 2.0 * uniform<real>::rand(localState);
     real radius = uniform<real>::rand(localState);
     radius = sqrt(-log(radius));
     real phi_sin, phi_cos;
-    quda::sincos(phi, &phi_sin, &phi_cos);
+    quda::sincospi(phi, &phi_sin, &phi_cos);
     arg.v(parity, x_cb, s, c) = radius * complex<real>(phi_cos, phi_sin);
   }
 
