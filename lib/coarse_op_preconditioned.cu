0f929a85c9cc956756d88054bcf4e06c007e1bb8
maddyscientist <mclark@nvidia.com>
2022-07-04 07:42:59 -0700

    TransformReduce can use an index mapper, a functor to transform the iterator index to a desired array index.  Use this abstraction to introduce a milc_mapper object to allow us to correctly pick up dimension subsets of MILC-ordered gauge fields.  This fixes the printing of norms during MMA-enabled multigrid construction, and fixes some uninitialized variable warnings in compute-sanitizer.  Finally, for consistency between mma and non-mma builds, only print the Yhat norms after ghost exchange

--- lib/coarse_op_preconditioned.cu
+++ lib/coarse_op_preconditioned.cu
@@ -260,14 +260,6 @@ namespace quda
           (Yhat, Y, Xinv);
       }
 
-      if (getVerbosity() >= QUDA_VERBOSE)
-      {
-        if (use_mma && X.Location() == QUDA_CUDA_FIELD_LOCATION)
-          warningQuda("There is a known issue with Yhat norms 0 through 3 for CUDA+MMA builds. These are harmless and will be addressed in the future.\n");
-        for (int d = 0; d < 8; d++)
-          printfQuda("Yhat[%d] = %e (%e %e = %e x %e)\n", d, Yhat.norm2(d), Yhat.abs_max(d),
-                     Y.abs_max(d) * Xinv.abs_max(0), Y.abs_max(d), Xinv.abs_max(0));
-      }
     }
 
     // fill back in the bulk of Yhat so that the backward link is updated on the previous node
@@ -279,6 +271,12 @@ namespace quda
     // need to put this in the ghost zone of the next node - but only send forwards the forwards
     // links and not overwrite the backwards ghost
     Yhat.exchangeGhost(QUDA_LINK_FORWARDS);
+
+    if (getVerbosity() >= QUDA_VERBOSE) {
+      for (int d = 0; d < 8; d++)
+        printfQuda("Yhat[%d] = %e (%e < %e x %e)\n", d, Yhat.norm2(d), Yhat.abs_max(d), Y.abs_max(d), Xinv.abs_max(0));
+    }
+
   }
 
   template <typename storeFloat, typename Float, int N>
