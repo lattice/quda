2d4bacb1512dbf321c7fe847c35b01c818b21e2b
Evan Weinberg <eweinberg@nvidia.com>
2022-07-08 14:26:46 -0700

    Working towards partitioned Polyakov loop calculations

--- include/kernels/gauge_polyakov_loop.cuh
+++ include/kernels/gauge_polyakov_loop.cuh
@@ -8,8 +8,40 @@
 
 namespace quda {
 
+  /**
+     @brief Calculates the Polyakov loop in a given direction, returning the product matrix
+
+     @return The product of the gauge path
+     @param[in] arg Kernel argumnt
+     @param[in] x Full index array
+     @param[in] parity Parity index
+  */
+  template <int dir, typename Arg>
+  __device__ __host__ inline typename Arg::Link
+  computePolyakovLoop(const Arg &arg, int x[4], int parity)
+  {
+    int dx[4] = {0, 0, 0, 0};
+
+    using Link = typename Arg::Link;
+
+    // polyloop: current matrix
+    // link: the loaded matrix in this round
+    Link polyloop, link;
+    setIdentity(&polyloop);
+
+    int nbr_oddbit = parity;
+
+    for (int dt = 0; dt < arg.X[dir]; dt++) {
+      dx[dir] = dt;
+      link = arg.U(dir, linkIndexShift(x, dx, arg.X), nbr_oddbit);
+      polyloop = polyloop * link;
+      nbr_oddbit = nbr_oddbit ^ 1;
+    } // dt
+    return polyloop;
+  }
+
   template <typename Float_, int nColor_, QudaReconstructType recon_>
-  struct GaugePolyakovLoopArg : public ReduceArg<array<double, 2>> {
+  struct GaugePolyakovLoopSplitArg : public kernel_param<> {
     using Float = Float_;
     static constexpr int nColor = nColor_;
     static_assert(nColor == 3, "Only nColor=3 enabled at this time");
@@ -21,8 +53,8 @@ namespace quda {
     Gauge P;
     Gauge U;
 
-    GaugePolyakovLoopArg(GaugeField &P_, const GaugeField &U_) :
-      ReduceArg<reduce_t>(dim3(P_.LocalVolumeCB(), 2, 1)),
+    GaugePolyakovLoopSplitArg(GaugeField &P_, const GaugeField &U_) :
+      kernel_param(dim3(P_.LocalVolumeCB(), 2, 1)),
       P(P_),
       U(U_)
     {
@@ -34,41 +66,74 @@ namespace quda {
 
   };
 
-  template <typename Arg> struct PolyakovLoop : plus<typename Arg::reduce_t> {
+  template <typename Arg> struct PolyakovLoopSplit {
+    const Arg &arg;
+    constexpr PolyakovLoopSplit(const Arg &arg) : arg(arg) {}
+    static constexpr const char *filename() { return KERNEL_FILE; }
+
+    __device__ __host__ inline void operator()(int x_cb, int parity)
+    {
+      int x[4];
+      getCoords(x, x_cb, arg.X, parity);
+
+      auto polyloop = computePolyakovLoop<3>(arg, x, parity);
+
+      // save loop
+      arg.P(0, x_cb, parity) = polyloop;
+
+    }
+
+  };
+
+  template <typename Float_, int nColor_, QudaReconstructType recon_, bool compute_loop_>
+  struct GaugePolyakovLoopTraceArg : public ReduceArg<array<double, 2>> {
+    using Float = Float_;
+    static constexpr int nColor = nColor_;
+    static_assert(nColor == 3, "Only nColor=3 enabled at this time");
+    static constexpr QudaReconstructType recon = recon_;
+    static constexpr bool compute_loop = compute_loop_;
+    using Gauge = typename gauge_mapper<Float,recon>::type;
+    using Link = Matrix<complex<Float>, 3>;
+
+    int X[4];
+    Gauge U;
+
+    GaugePolyakovLoopTraceArg(const GaugeField &U_) :
+      ReduceArg<reduce_t>(dim3(U_.LocalVolumeCB() / U_.X()[3], 2, 1)),
+      U(U_)
+    {
+      for (int dir=0; dir<4; ++dir){
+        X[dir] = U_.X()[dir];
+      }
+    }
+
+  };
+
+  template <typename Arg> struct PolyakovLoopTrace : plus<typename Arg::reduce_t> {
     using reduce_t = typename Arg::reduce_t;
     using plus<reduce_t>::operator();
     static constexpr int reduce_block_dim = 2; // x_cb in x, parity in y
     const Arg &arg;
-    constexpr PolyakovLoop(const Arg &arg) : arg(arg) {}
+    constexpr PolyakovLoopTrace(const Arg &arg) : arg(arg) {}
     static constexpr const char *filename() { return KERNEL_FILE; }
 
     // return the (temporal) Polyakov Loop at 3-d site (x_cb, parity)
     __device__ __host__ inline reduce_t operator()(reduce_t &value, int x_cb, int parity)
     {
-      reduce_t ploop{0, 0};
-
-      int x[4];
-      getCoords(x, x_cb, arg.X, parity);
-      int dx[4] = {0, 0, 0, 0};
-
       using Link = typename Arg::Link;
 
-      // polyloop: current matrix
-      // link: the loaded matrix in this round
-      Link polyloop, link;
-      setIdentity(&polyloop);
+      Link polyloop;
+      reduce_t ploop{0, 0};
 
-      int nbr_oddbit = parity;
-
-      for (int dt = 0; dt < arg.X[3]; dt++) {
-        dx[3] = dt;
-        link = arg.U(3, linkIndexShift(x, dx, arg.X), nbr_oddbit);
-        polyloop = polyloop * link;
-        nbr_oddbit = nbr_oddbit ^ 1;
-      } // t
-
-      // save loop
-      arg.P(0, x_cb, parity) = polyloop;
+      if constexpr (arg.compute_loop) {
+        // Dimension is not split, we need to compute the Polyakov loop
+        int x[4];
+        getCoords(x, x_cb, arg.X, parity);
+        polyloop = computePolyakovLoop<3>(arg, x, parity);
+      } else {
+        // Dimension is split, we're only computing the trace
+        polyloop = arg.U(0, x_cb, parity);
+      }
 
       // accumulate trace
       auto tr = getTrace( polyloop );
--- lib/gauge_polyakov_loop.cu
+++ lib/gauge_polyakov_loop.cu
@@ -1,49 +1,45 @@
 #include <gauge_field.h>
 #include <gauge_tools.h>
 #include <instantiate.h>
+#include <tunable_nd.h>
 #include <tunable_reduction.h>
 #include <kernels/gauge_polyakov_loop.cuh>
 
 namespace quda {
 
   template<typename Float, int nColor, QudaReconstructType recon>
-  class GaugePolyakovLoop : public TunableReduction2D {
+  class GaugePolyakovLoopSplit : public TunableKernel2D {
     GaugeField &product_field;
     const GaugeField &u;
-    using reduce_t = array<double, 2>;
-    reduce_t &ploop;
+    unsigned int minThreads() const { return product_field.LocalVolumeCB(); }
 
   public:
-    GaugePolyakovLoop(GaugeField &product_field, const GaugeField &u, array<double, 2> &ploop) :
-      TunableReduction2D(u),
+    GaugePolyakovLoopSplit(const GaugeField &u, GaugeField &product_field) :
+      TunableKernel2D(u, 2),
       product_field(product_field),
-      u(u),
-      ploop(ploop)
+      u(u)
     {
+      strcat(aux, ",4d_vol=");
+      strcat(aux, u.VolString());
       strcat(aux, ",3d_vol=");
       strcat(aux, product_field.VolString());
 
       apply(device::get_default_stream());
-
-      // We normalize by the 3-d volume
-      long vol3d = product_field.Volume() * comm_dim(0) * comm_dim(1) * comm_dim(2);
-      ploop[0] /= vol3d;
-      ploop[1] /= vol3d;
     }
 
     void apply(const qudaStream_t &stream)
     {
       TuneParam tp = tuneLaunch(*this, getTuning(), getVerbosity());
-      GaugePolyakovLoopArg<Float, nColor, recon> arg(product_field, u);
-      launch<PolyakovLoop>(ploop, tp, stream, arg);
+      GaugePolyakovLoopSplitArg<Float, nColor, recon> arg(product_field, u);
+      launch<PolyakovLoopSplit>(tp, stream, arg);
     }
 
     long long flops() const
     {
       auto Nc = u.Ncolor();
       auto mat_mul_flops = 8ll * Nc * Nc * Nc - 2 * Nc * Nc;
-      // multiplies for each loop plus traces
-      return mat_mul_flops * u.Volume() / 4 + 2 * Nc * product_field.Volume();
+      // multiplies for each loop
+      return mat_mul_flops * u.Volume() / 4;
     }
 
     long long bytes() const {
@@ -52,29 +48,95 @@ namespace quda {
     }
   };
 
+  template<typename Float, int nColor, QudaReconstructType recon>
+  class GaugePolyakovLoopTrace : public TunableReduction2D {
+    const GaugeField &u;
+    using reduce_t = array<double, 2>;
+    reduce_t &ploop;
+    bool compute_loop;
+
+  public:
+    GaugePolyakovLoopTrace(const GaugeField &u, array<double, 2> &ploop, bool compute_loop) :
+      TunableReduction2D(u),
+      u(u),
+      ploop(ploop),
+      compute_loop(compute_loop)
+    {
+      strcat(aux, ",4d_vol=");
+      strcat(aux, u.VolString());
+      if (compute_loop) strcat(aux, ",loop");
+
+      apply(device::get_default_stream());
+
+      // We normalize by the 3-d volume
+      long vol3d = u.Volume() * comm_dim(0) * comm_dim(1) * comm_dim(2) / u.X()[3];
+      ploop[0] /= vol3d;
+      ploop[1] /= vol3d;
+    }
+
+    void apply(const qudaStream_t &stream)
+    {
+      TuneParam tp = tuneLaunch(*this, getTuning(), getVerbosity());
+      if (compute_loop) {
+        GaugePolyakovLoopTraceArg<Float, nColor, recon, true> arg(u);
+        launch<PolyakovLoopTrace>(ploop, tp, stream, arg);
+      } else {
+        GaugePolyakovLoopTraceArg<Float, nColor, recon, false> arg(u);
+        launch<PolyakovLoopTrace>(ploop, tp, stream, arg);
+      }
+    }
+
+    long long flops() const
+    {
+      auto Nc = u.Ncolor();
+      auto mat_mul_flops = 8ll * Nc * Nc * Nc - 2 * Nc * Nc;
+      // multiplies for each loop plus traces
+      return ((compute_loop) ? (mat_mul_flops * u.Volume() / 4) : 0) + 2 * Nc * u.Volume() / u.X()[3];
+    }
+
+    long long bytes() const {
+      // links * one LatticeColorMatrix
+      return compute_loop ? (u.Bytes()) / 4 : u.Bytes();
+    }
+  };
+
   void gaugePolyakovLoop(double ploop[2], const GaugeField& u, int dir) {
 
     if (dir != 3) errorQuda("Unsupported direction %d", dir);
-    if (comm_dim(dir) != 1) errorQuda("Splitting in the %d direction is not supported yet", dir);
-
-    // Form a usable (or metadata) 3-d gauge "container"
-    GaugeFieldParam gParam(u);
-    lat_dim_t x;
-    for (int d = 0; d < 3; d++) x[d] = u.X()[d];
-    x[3] = 1;
-    gParam.x = x;
-    gParam.create = QUDA_NULL_FIELD_CREATE; // not needed in case where decomp isn't split in `t` direction
-    gParam.ghostExchange = QUDA_GHOST_EXCHANGE_NO;
-    gParam.location = QUDA_CUDA_FIELD_LOCATION;
-    gParam.geometry = QUDA_SCALAR_GEOMETRY;
-
-    std::unique_ptr<GaugeField> product_field_ = std::make_unique<cudaGaugeField>(gParam);
-    GaugeField& product_field = reinterpret_cast<GaugeField&>(*product_field_.get());
 
     // output array
     array<double, 2> loop;
 
-    instantiate<GaugePolyakovLoop, ReconstructNo12>(product_field, u, loop);
+    // If the dir dimension isn't partitioned, we can just do a quick compute + reduce
+    if (commDimPartitioned(dir)) {
+      // comms aren't actually supported yet
+      if (comm_dim(dir) > 1) errorQuda("Splitting in the %d direction is not supported yet", dir);
+
+      // Form a usable 3-d gauge slice
+      GaugeFieldParam gParam(u);
+      lat_dim_t x;
+      for (int d = 0; d < 3; d++) x[d] = u.X()[d];
+      x[3] = 1;
+      gParam.x = x;
+      gParam.create = QUDA_NULL_FIELD_CREATE;
+      gParam.ghostExchange = QUDA_GHOST_EXCHANGE_NO;
+      gParam.location = QUDA_CUDA_FIELD_LOCATION;
+      gParam.geometry = QUDA_SCALAR_GEOMETRY;
+
+      std::unique_ptr<GaugeField> product_field_ = std::make_unique<cudaGaugeField>(gParam);
+      GaugeField& product_field = reinterpret_cast<GaugeField&>(*product_field_.get());
+
+      instantiate<GaugePolyakovLoopSplit, ReconstructNo12>(u, product_field);
+
+      // Combine (tbd)
+
+      // Trace only
+      instantiate<GaugePolyakovLoopTrace, ReconstructNo12>(product_field, loop, false);
+
+    } else {
+      // construct loop and trace
+      instantiate<GaugePolyakovLoopTrace, ReconstructNo12>(u, loop, true);
+    }
 
     ploop[0] = loop[0];
     ploop[1] = loop[1];
