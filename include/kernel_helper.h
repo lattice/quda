524b3f279853b660c8bd3a3a09c152705b1135b1
Xiao-Yong Jin <xjin@anl.gov>
2022-07-27 16:40:01 +0000

    kernel_param: force use_kernel_arg when larger than 1
    Introduce int use_kernel_arg in BlasFunctor and caxpyxmazMR_,
    and force use_kernel_arg with caxpyxmazMR_::use_kernel_arg = 2.

--- include/kernel_helper.h
+++ include/kernel_helper.h
@@ -12,8 +12,8 @@ namespace quda
     kernel_t(const void *func, const char *name) : func(func), name(name) { }
   };
 
-  template <bool use_kernel_arg_ = true> struct kernel_param {
-    static constexpr bool use_kernel_arg = use_kernel_arg_;
+  template <int use_kernel_arg_ = true> struct kernel_param {
+    static constexpr int use_kernel_arg = use_kernel_arg_;
     dim3 threads;          /** number of active threads required */
     int comms_rank;        /** per process value of comm_rank() */
     int comms_rank_global; /** per process value comm_rank_global() */
