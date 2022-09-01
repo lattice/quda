429ba11805672653767a3e32adc289b60cb436b9
maddyscientist <mclark@nvidia.com>
2022-09-01 11:14:52 -0700

    Add missing header from last commit

---
+++ include/field_cache.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <map>
+#include <stack>
+
+namespace quda {
+
+  template <typename T>
+  struct FieldKey {
+    std::string volume;
+    std::string aux;
+
+    FieldKey(const T &a) : volume(a.VolString()), aux(a.AuxString()) { }
+
+    bool operator<(const FieldKey<T> &other) const
+    {
+      if (volume < other.volume) {
+        return true;
+      } else if (volume == other.volume) {
+        return aux < other.aux ? true : false;
+      }
+      return false;
+    }
+  };
+
+  template <typename T>
+  class FieldTmp {
+    static std::map<FieldKey<T>, std::stack<T>> cache;
+    T tmp;
+
+  public:
+    operator T&() { return tmp; }
+    FieldTmp(const T &a);
+    ~FieldTmp();
+
+    static void destroy();
+  };
+
+  template <typename T> auto getTmp(const T &a) { return FieldTmp<T>(a); }
+
+}
