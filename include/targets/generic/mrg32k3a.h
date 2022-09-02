39b06cf056a12b5361e1d273ada0536299f00429
James Osborn <osborn@alcf.anl.gov>
2022-09-02 12:54:43 -0500

    use static_cast

--- include/targets/generic/mrg32k3a.h
+++ include/targets/generic/mrg32k3a.h
@@ -57,14 +57,14 @@ namespace quda
         for (int i = 0; i < 3; ++i) {
           Arr3<uint64_t> t {0u, 0u, 0u};
           for (int k = 0; k < 3; ++k) {
-            uint64_t aik = (uint64_t)a[i][k];
-            t[0] += aik * (uint64_t)a[k][0] % m;
-            t[1] += aik * (uint64_t)a[k][1] % m;
-            t[2] += aik * (uint64_t)a[k][2] % m;
+            uint64_t aik = static_cast<uint64_t>(a[i][k]);
+            t[0] += aik * static_cast<uint64_t>(a[k][0]) % m;
+            t[1] += aik * static_cast<uint64_t>(a[k][1]) % m;
+            t[2] += aik * static_cast<uint64_t>(a[k][2]) % m;
           }
-          x[i][0] = (uint32_t)(t[0] % m);
-          x[i][1] = (uint32_t)(t[1] % m);
-          x[i][2] = (uint32_t)(t[2] % m);
+          x[i][0] = static_cast<uint32_t>(t[0] % m);
+          x[i][1] = static_cast<uint32_t>(t[1] % m);
+          x[i][2] = static_cast<uint32_t>(t[2] % m);
         }
         return x;
       }
@@ -79,15 +79,21 @@ namespace quda
 
       inline void matvecmod(Trans a, State &v, uint64_t m)
       {
-        uint64_t v0 = (uint64_t)v[0];
-        uint64_t v1 = (uint64_t)v[1];
-        uint64_t v2 = (uint64_t)v[2];
+        uint64_t v0 = static_cast<uint64_t>(v[0]);
+        uint64_t v1 = static_cast<uint64_t>(v[1]);
+        uint64_t v2 = static_cast<uint64_t>(v[2]);
         v[0]
-          = (uint32_t)((((uint64_t)a[0][0] * v0) % m + ((uint64_t)a[0][1] * v1) % m + ((uint64_t)a[0][2] * v2) % m) % m);
+          = static_cast<uint32_t>(((static_cast<uint64_t>(a[0][0]) * v0) % m + (static_cast<uint64_t>(a[0][1]) * v1) % m
+                                   + (static_cast<uint64_t>(a[0][2]) * v2) % m)
+                                  % m);
         v[1]
-          = (uint32_t)((((uint64_t)a[1][0] * v0) % m + ((uint64_t)a[1][1] * v1) % m + ((uint64_t)a[1][2] * v2) % m) % m);
+          = static_cast<uint32_t>(((static_cast<uint64_t>(a[1][0]) * v0) % m + (static_cast<uint64_t>(a[1][1]) * v1) % m
+                                   + (static_cast<uint64_t>(a[1][2]) * v2) % m)
+                                  % m);
         v[2]
-          = (uint32_t)((((uint64_t)a[2][0] * v0) % m + ((uint64_t)a[2][1] * v1) % m + ((uint64_t)a[2][2] * v2) % m) % m);
+          = static_cast<uint32_t>(((static_cast<uint64_t>(a[2][0]) * v0) % m + (static_cast<uint64_t>(a[2][1]) * v1) % m
+                                   + (static_cast<uint64_t>(a[2][2]) * v2) % m)
+                                  % m);
       }
 
       constexpr double norm = 2.328306549295728e-10;
@@ -100,12 +106,14 @@ namespace quda
       constexpr uint32_t defaultSEED = 12345U;
       constexpr int subsequenceBase = 76;
 
-      constexpr Trans a1 {State {0u, 1u, 0u}, State {0u, 0u, 1u}, State {(uint32_t)(m1 - a13n), (uint32_t)a12, 0u}};
-      constexpr Trans a2 {State {0u, 1u, 0u}, State {0u, 0u, 1u}, State {(uint32_t)(m2 - a23n), 0u, (uint32_t)a21}};
+      constexpr Trans a1 {State {0u, 1u, 0u}, State {0u, 0u, 1u},
+                          State {static_cast<uint32_t>(m1 - a13n), static_cast<uint32_t>(a12), 0u}};
+      constexpr Trans a2 {State {0u, 1u, 0u}, State {0u, 0u, 1u},
+                          State {static_cast<uint32_t>(m2 - a23n), 0u, static_cast<uint32_t>(a21)}};
 
       constexpr int maxpower2 = 190;
-      constexpr Arr<Trans, maxpower2> a1sq = squaremodarray<maxpower2>(a1, (uint64_t)m1);
-      constexpr Arr<Trans, maxpower2> a2sq = squaremodarray<maxpower2>(a2, (uint64_t)m2);
+      constexpr Arr<Trans, maxpower2> a1sq = squaremodarray<maxpower2>(a1, static_cast<uint64_t>(m1));
+      constexpr Arr<Trans, maxpower2> a2sq = squaremodarray<maxpower2>(a2, static_cast<uint64_t>(m2));
 
       static_assert(a1sq[76]
                       == Trans {State {82758667u, 1871391091u, 4127413238u}, State {3672831523u, 69195019u, 1871391091u},
@@ -123,8 +131,8 @@ namespace quda
         uint64_t s = offset;
         while (s > 0) {
           if (s & 1u) {
-            matvecmod(a1sq[base + i], prn.s1, (uint64_t)m1);
-            matvecmod(a2sq[base + i], prn.s2, (uint64_t)m2);
+            matvecmod(a1sq[base + i], prn.s1, static_cast<uint64_t>(m1));
+            matvecmod(a2sq[base + i], prn.s2, static_cast<uint64_t>(m2));
           }
           s >>= 1u;
           ++i;
@@ -134,14 +142,16 @@ namespace quda
       inline void seed(MRG32k3a &prn, uint64_t seed, uint64_t subsequence)
       {
         if (seed) {
-          const uint64_t d1 = (uint64_t)defaultSEED * (uint64_t)((uint32_t)seed ^ 0x55555555u);
-          const uint64_t d2 = (uint64_t)defaultSEED * (uint64_t)((uint32_t)(seed >> 32u) ^ 0xAAAAAAAAu);
-          prn.s1[0] = (uint32_t)(d1 % (uint64_t)m1);
-          prn.s1[1] = (uint32_t)(d2 % (uint64_t)m1);
-          prn.s1[2] = (uint32_t)(d1 % (uint64_t)m1);
-          prn.s2[0] = (uint32_t)(d2 % (uint64_t)m2);
-          prn.s2[1] = (uint32_t)(d1 % (uint64_t)m2);
-          prn.s2[2] = (uint32_t)(d2 % (uint64_t)m2);
+          const uint64_t d1
+            = static_cast<uint64_t>(defaultSEED) * static_cast<uint64_t>((static_cast<uint32_t>(seed) ^ 0x55555555u));
+          const uint64_t d2 = static_cast<uint64_t>(defaultSEED)
+            * static_cast<uint64_t>((static_cast<uint32_t>(seed >> 32u) ^ 0xAAAAAAAAu));
+          prn.s1[0] = static_cast<uint32_t>((d1 % static_cast<uint64_t>(m1));
+          prn.s1[1] = static_cast<uint32_t>((d2 % static_cast<uint64_t>(m1));
+          prn.s1[2] = static_cast<uint32_t>((d1 % static_cast<uint64_t>(m1));
+          prn.s2[0] = static_cast<uint32_t>((d2 % static_cast<uint64_t>(m2));
+          prn.s2[1] = static_cast<uint32_t>((d1 % static_cast<uint64_t>(m2));
+          prn.s2[2] = static_cast<uint32_t>((d2 % static_cast<uint64_t>(m2));
         } else {
           prn.s1[0] = defaultSEED;
           prn.s1[1] = defaultSEED;
@@ -161,14 +171,14 @@ namespace quda
         if (p1 < 0.0) p1 += m1;
         prn.s1[0] = prn.s1[1];
         prn.s1[1] = prn.s1[2];
-        prn.s1[2] = (uint32_t)p1;
+        prn.s1[2] = static_cast<uint32_t>(p1);
 
         p2 = a21 * (double)(prn.s2[2]) - a23n * (double)(prn.s2[0]);
         p2 = std::fmod(p2, m2);
         if (p2 < 0.0) p2 += m2;
         prn.s2[0] = prn.s2[1];
         prn.s2[1] = prn.s2[2];
-        prn.s2[2] = (uint32_t)p2;
+        prn.s2[2] = static_cast<uint32_t>(p2);
 
         if (p1 <= p2)
           return (p1 - p2 + m1) * norm;
