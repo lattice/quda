6be4e67ed9d5740dcad13a922259268d801cdd4f
Evan Weinberg <eweinberg@nvidia.com>
2022-09-14 15:10:50 -0700

    clang-format

--- include/quda_milc_interface.h
+++ include/quda_milc_interface.h
@@ -1027,9 +1027,9 @@ extern "C" {
    * @param[in] stopWtheta, 0 for MILC criterion and 1 to use the theta value
    * @param[in,out] milc_sitelink, MILC gauge field to be fixed
    */
-  void qudaGaugeFixingOVR(int precision, unsigned int gauge_dir, int Nsteps,
-                          int verbose_interval, double relax_boost, double tolerance,
-                          unsigned int reunit_interval, unsigned int stopWtheta, QudaMILCSiteArg_t *arg);
+  void qudaGaugeFixingOVR(int precision, unsigned int gauge_dir, int Nsteps, int verbose_interval, double relax_boost,
+                          double tolerance, unsigned int reunit_interval, unsigned int stopWtheta,
+                          QudaMILCSiteArg_t *arg);
 
   /**
    * @brief Gauge fixing with Steepest descent method with FFTs with support for single GPU only.
