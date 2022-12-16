c2b78e869fb892c8653e305e27e6225ab96dcd19
maddyscientist <mclark@nvidia.com>
2022-12-16 10:13:56 -0800

    Add max deviation test to dslash_coarse.cpp

--- tests/multigrid_benchmark_test.cpp
+++ tests/multigrid_benchmark_test.cpp
@@ -111,10 +111,10 @@ void initFields(QudaPrecision prec)
     quda::RNG rng(xD[0], 1234);
     for (auto &yi : yD) spinorNoise(yi, rng, QUDA_NOISE_GAUSS);
 
-    gaugeNoise(*Y_d, rng, QUDA_NOISE_GAUSS);
-    gaugeNoise(*Yhat_d, rng, QUDA_NOISE_GAUSS);
-    gaugeNoise(*X_d, rng, QUDA_NOISE_GAUSS);
-    gaugeNoise(*Xinv_d, rng, QUDA_NOISE_GAUSS);
+    gaugeNoise(*Y_d, rng, QUDA_NOISE_UNIFORM);
+    gaugeNoise(*Yhat_d, rng, QUDA_NOISE_UNIFORM);
+    gaugeNoise(*X_d, rng, QUDA_NOISE_UNIFORM);
+    gaugeNoise(*Xinv_d, rng, QUDA_NOISE_UNIFORM);
   }
 }
 
@@ -163,7 +163,13 @@ TEST(multi_rhs_test, verify)
     default: errorQuda("Undefined test %d", test_type);
     }
 
-    EXPECT_LE(blas::xmyNorm(xD[i], x_ref), 1e-6);
+    // require that each component differs by no more than 1e-4
+    auto max_dev = blas::max_deviation(xD[i], x_ref);
+    EXPECT_LE(max_dev, 1e-4);
+    // require that the relative L2 norm differs by no more than 1e-5
+    auto x2 = blas::norm2(x_ref);
+    auto l2_dev = blas::xmyNorm(xD[i], x_ref);
+    EXPECT_LE(sqrt(l2_dev / x2), 1e-6);
   }
 }
 
