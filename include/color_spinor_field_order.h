lattice/quda

commit 7ab8d257ac8375a631ea18e7bc9c03120bd710dc

Move is_boundary to index_helper.cuh and use with dslash_coarse to facilitate some template removal without causing local memory

include/kernels/dslash_coarse.cuh
@@ -129,8 +129,8 @@ namespace quda {
      @param[in] color_off Which color column offset are we acting on
      @param[in] arg Arguments
    */
-  template <int Mc, int thread_dim, typename V, typename Arg>
-  __device__ __host__ inline void applyDslash(V &out, int thread_dir, int x_cb, int src_idx, int parity, int s_row, int color_block, int color_offset, const Arg &arg)
+  template <int Mc, typename V, typename Arg>
+  __device__ __host__ inline void applyDslash(V &out, int thread_dim, int thread_dir, int x_cb, int src_idx, int parity, int s_row, int color_block, int color_offset, const Arg &arg)
   {
     const int their_spinor_parity = (arg.nParity == 2) ? 1-parity : 0;
 
@@ -141,11 +141,11 @@ namespace quda {
 
       //Forward gather - compute fwd offset for spinor fetch
 #pragma unroll
-      for(int d = thread_dim; d < Arg::nDim; d += Arg::dim_stride) // loop over dimension
-      {
-	const int fwd_idx = linkIndexP1(coord, arg.dim, d);
+      for(int d0 = 0; d0 < Arg::nDim; d0 += Arg::dim_stride) { // loop over dimension
+        int d = d0 + thread_dim;
+	const int fwd_idx = linkIndexHop(coord, arg.dim, d, arg.nFace);
 
-	if (arg.commDim[d] && (coord[d] + arg.nFace >= arg.dim[d]) ) {
+	if (arg.commDim[d] && is_boundary(coord, d, 1, arg) ) {
 	  if constexpr (doHalo<Arg::type>()) {
             int ghost_idx = ghostFaceIndex<1>(coord, arg.dim, d, arg.nFace);
 
@@ -196,10 +196,11 @@ namespace quda {
 
       //Backward gather - compute back offset for spinor and gauge fetch
 #pragma unroll
-      for(int d = thread_dim; d < Arg::nDim; d += Arg::dim_stride)
-	{
-	const int back_idx = linkIndexM1(coord, arg.dim, d);
-	if (arg.commDim[d] && (coord[d] - arg.nFace < 0) ) {
+      for(int d0 = thread_dim; d0 < Arg::nDim; d0 += Arg::dim_stride) {
+        const int d = d0 + thread_dim;
+	const int back_idx = linkIndexHop(coord, arg.dim, d, -arg.nFace);
+
+	if (arg.commDim[d] && is_boundary(coord, d, 0, arg)) {
 	  if constexpr (doHalo<Arg::type>()) {
             const int ghost_idx = ghostFaceIndex<0>(coord, arg.dim, d, arg.nFace);
 #pragma unroll
@@ -349,11 +350,7 @@ namespace quda {
       array<complex <typename Arg::real>, Mc> out{ };
 
       if (Arg::dslash) {
-        if (dim == 0)      applyDslash<Mc, 0>(out, dir, x_cb, src_idx, parity, s, color_block, color_offset, arg);
-        else if (dim == 1) applyDslash<Mc, 1>(out, dir, x_cb, src_idx, parity, s, color_block, color_offset, arg);
-        else if (dim == 2) applyDslash<Mc, 2>(out, dir, x_cb, src_idx, parity, s, color_block, color_offset, arg);
-        else if (dim == 3) applyDslash<Mc, 3>(out, dir, x_cb, src_idx, parity, s, color_block, color_offset, arg);
-
+        applyDslash<Mc>(out, dim, dir, x_cb, src_idx, parity, s, color_block, color_offset, arg);
         target::dispatch<dim_collapse>(out, dir, dim, arg);
       }
 

include/index_helper.cuh
@@ -109,6 +109,28 @@ namespace quda {
     return (((y[3] * X[2] + y[2]) * X[1] + y[1]) * X[0] + y[0]) >> 1;
   }
 
+  template <typename T, typename Arg>
+  constexpr bool is_boundary(T &coord, int dim, int dir, const Arg &arg)
+  {
+    bool is_boundary = false;
+    if (dir == 0) {
+      switch (dim) { // backwards boundary
+      case 0: is_boundary = coord[0] - arg.nFace < 0; break;
+      case 1: is_boundary = coord[1] - arg.nFace < 0; break;
+      case 2: is_boundary = coord[2] - arg.nFace < 0; break;
+      case 3: is_boundary = coord[3] - arg.nFace < 0; break;
+      }
+    } else if (dir == 1) {
+      switch (dim) { // forwards boundary
+      case 0: is_boundary = coord[0] + arg.nFace >= arg.dim[0]; break;
+      case 1: is_boundary = coord[1] + arg.nFace >= arg.dim[1]; break;
+      case 2: is_boundary = coord[2] + arg.nFace >= arg.dim[2]; break;
+      case 3: is_boundary = coord[3] + arg.nFace >= arg.dim[3]; break;
+      }
+    }
+    return is_boundary;
+}
+
   /**
      Compute the checkerboard 1-d index from the 4-d coordinate x[] -1 in the mu direction
 

include/kernels/coarse_op_preconditioned.cuh
@@ -42,19 +42,6 @@ namespace quda {
     }
   };
 
-  template <typename T, typename Arg>
-  constexpr bool is_boundary(T &coord, int d, const Arg &arg)
-  {
-    bool is_boundary = false;
-    switch (d) {
-    case 0: is_boundary = coord[0] - arg.nFace < 0; break;
-    case 1: is_boundary = coord[1] - arg.nFace < 0; break;
-    case 2: is_boundary = coord[2] - arg.nFace < 0; break;
-    case 3: is_boundary = coord[3] - arg.nFace < 0; break;
-    }
-    return is_boundary;
-  }
-
   template <typename Arg>
   inline __device__ __host__ auto computeYhat(const Arg &arg, int d, int x_cb, int parity, int i0, int j0)
   {
@@ -69,7 +56,7 @@ namespace quda {
     real yHatMax = 0.0;
 
     // first do the backwards links Y^{+\mu} * X^{-\dagger}
-    if (arg.comm_dim[d] && is_boundary(coord, d, arg)) {
+    if (arg.comm_dim[d] && is_boundary(coord, 0, d, arg)) {
 
       auto yHat = make_tile_C<complex,true>(arg.tile);
 

commit c070d6c12be6a244e652f3f9afe075f13a4f7134

Fix some bugs 1. if we reset the field accessor's scale, we also need to set its ghost parent's scale, 2. fix bug introduced in 7ab8d257ac8375a631ea18e7bc9c03120bd710dc

include/color_spinor_field_order.h
@@ -636,6 +636,10 @@ namespace quda
       GhostOrder(const GhostOrder &) = default;
       GhostOrder(const ColorSpinorField &, int, void * const *) { }
       GhostOrder &operator=(const GhostOrder &) = default;
+
+    public:
+      /** Does this field type support ghost zones? */
+      static constexpr bool supports_ghost_zone = false;
     };
 
     template <typename store_t, typename norm_t, bool ghost_fixed, bool block_float_ghost> struct ghost_t {
@@ -673,6 +677,9 @@ namespace quda
       ghost_accessor_t ghostAccessor;
 
     public:
+      /** Does this field type support ghost zones? */
+      static constexpr bool supports_ghost_zone = true;
+
       GhostOrder() = default;
       GhostOrder(const GhostOrder &) = default;
 
@@ -830,8 +837,6 @@ namespace quda
       using norm_t = float;
 
     public:
-      /** Does this field type support ghost zones? */
-      static constexpr bool supports_ghost_zone = true;
       static constexpr bool fixed = fixed_point<Float, storeFloat>();
       static constexpr int nSpin = nSpin_;
       static constexpr int nColor = nColor_;
@@ -879,6 +884,7 @@ namespace quda
           v.scale = static_cast<Float>(std::numeric_limits<storeFloat>::max() / max);
           v.scale_inv = static_cast<Float>(max / std::numeric_limits<storeFloat>::max());
         }
+        if constexpr (GhostOrder::supports_ghost_zone) GhostOrder::resetScale(max);
       }
 
       /**

include/kernels/dslash_coarse.cuh
@@ -196,7 +196,7 @@ namespace quda {
 
       //Backward gather - compute back offset for spinor and gauge fetch
 #pragma unroll
-      for(int d0 = thread_dim; d0 < Arg::nDim; d0 += Arg::dim_stride) {
+      for(int d0 = 0; d0 < Arg::nDim; d0 += Arg::dim_stride) {
         const int d = d0 + thread_dim;
 	const int back_idx = linkIndexHop(coord, arg.dim, d, -arg.nFace);
 