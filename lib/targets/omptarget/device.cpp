301bc9d7671d4f08530e45b5c259a2728dac0372
Xiao-Yong Jin <xjin@anl.gov>
2023-03-24 03:52:20 +0000

    omp: fix sizeof
    A declaration of a parameter as “array of type” shall
    be adjusted to “qualified pointer to type”.

--- lib/targets/omptarget/device.cpp
+++ lib/targets/omptarget/device.cpp
@@ -108,14 +108,15 @@ namespace quda
 
     void get_visible_devices_string(char device_list_string[128])
     {
+      constexpr int len = 128;
       char *device_order_env = getenv("ZE_AFFINITY_MASK");
       char *omptarget_devices = getenv("LIBOMPTARGET_DEVICES");
       if(omptarget_devices && device_order_env)
-        snprintf(device_list_string, sizeof(device_list_string), "%s%s", omptarget_devices, device_order_env);
+        snprintf(device_list_string, len, "%s%s", omptarget_devices, device_order_env);
       else if(omptarget_devices)
-        snprintf(device_list_string, sizeof(device_list_string), "%s", omptarget_devices);
+        snprintf(device_list_string, len, "%s", omptarget_devices);
       else if(device_order_env)
-        snprintf(device_list_string, sizeof(device_list_string), "%s", device_order_env);
+        snprintf(device_list_string, len, "%s", device_order_env);
     }
 
     void print_device_properties()
