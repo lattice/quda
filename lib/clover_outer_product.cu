1e1af7f561c266efbda76e1a604218b13b30dc22
Marcogarofalo <marcogarofalo31@gmail.com>
2023-05-10 11:31:51 +0200

    dirty fix for multiple mpi tasks

--- lib/clover_outer_product.cu
+++ lib/clover_outer_product.cu
@@ -142,10 +142,10 @@ namespace quda {
     int dag = 1;
 
     for (unsigned int i=0; i<x.size(); i++) {
-      x[i]->Even().allocateGhostBuffer(1);
-      x[i]->Odd().allocateGhostBuffer(1);
-      p[i]->Even().allocateGhostBuffer(1);
-      p[i]->Odd().allocateGhostBuffer(1);
+      // x[i]->Even().allocateGhostBuffer(1);
+      // x[i]->Odd().allocateGhostBuffer(1);
+      // p[i]->Even().allocateGhostBuffer(1);
+      // p[i]->Odd().allocateGhostBuffer(1);
 
       for (int parity=0; parity<2; parity++) {
 	ColorSpinorField& inA = (parity&1) ? p[i]->Odd() : p[i]->Even();
@@ -153,7 +153,10 @@ namespace quda {
 	ColorSpinorField& inC = (parity&1) ? x[i]->Odd() : x[i]->Even();
 	ColorSpinorField& inD = (parity&1) ? p[i]->Even(): p[i]->Odd();
 
+        static constexpr int nFace = 1;
+        inB.exchangeGhost((QudaParity)(1-parity), nFace, dag);
         exchangeGhost(inB, parity, dag);
+        inD.exchangeGhost((QudaParity)(1-parity), nFace, 1-dag);
         exchangeGhost(inD, parity, 1-dag);
 
         instantiate<CloverForce, ReconstructNo12>(U, force, inA, inB, inC, inD, parity, coeff[i]);
