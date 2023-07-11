b16b9b2bc7e86cf03fae82e6d4793d2b0692efc9
James Osborn <osborn@alcf.anl.gov>
2023-07-11 13:09:16 -0500

    add shared memory helper

--- include/targets/generic/thread_local_cache.h
+++ include/targets/generic/thread_local_cache.h
@@ -1,15 +1,144 @@
+#pragma once
+
+#include <target_device.h>
+#include <shared_memory_helper.h>
 #include <shared_memory_cache_helper.h>
 
 /**
-   @file thread_local_cache_helper.h
-   @brief Convenience overloads to allow ThreadLocalCache objects to
-   appear in simple expressions.  The actual implementation of
-   ThreadLocalCache is target specific, and located in e.g.,
-   include/targets/cuda/thread_local_cache.h, etc.
+   @file thread_local_cache.h
+
+   Thread local cache object which may use shared memory for optimization.
+   The storage can be a single object or an array of objects.
  */
 
 namespace quda
 {
+
+  template <typename T>
+  using atom_t = std::conditional_t<sizeof(T) % 16 == 0, int4, std::conditional_t<sizeof(T) % 8 == 0, int2, int>>;
+
+  template <int N> struct SizeStatic {
+    static constexpr unsigned int size() { return N; }
+  };
+
+  /**
+     @brief Class for threads to store a unique value, or array of values, which can use
+     shared memory for optimization purposes.
+   */
+  template <typename T, int N_ = 0, typename O = void> class ThreadLocalCache : SharedMemory<atom_t<T>, SizeStatic<N_>, O>
+  {
+  public:
+    using value_type = T;
+    using offset_type = O; // type of object that may also use shared memory at the same time and is located before this one
+    static constexpr int N = N_; // size of array, 0 means to behave like T instead of array<T, 1>
+    static constexpr int len = std::max(1,N); // actual number of elements to store
+    using Smem = SharedMemory<atom_t<T>, SizeStatic<N>, O>;
+
+  private:
+    using atom_t = atom_t<T>;
+    static_assert(sizeof(T) % 4 == 0, "Thread local cache does not support sub-word size types");
+
+    // The number of elements of type atom_t that we break T into for optimal shared-memory access
+    static constexpr int n_element = sizeof(T) / sizeof(atom_t);
+
+    const int stride;
+
+    constexpr Smem smem() const { return *dynamic_cast<const Smem*>(this); }
+
+    __device__ __host__ inline void save_detail(const T &a, const int k) const
+    {
+      atom_t tmp[n_element];
+      memcpy(tmp, (void *)&a, sizeof(T));
+      int j = target::thread_idx_linear<3>();
+#pragma unroll
+      for (int i = 0; i < n_element; i++) smem()[(k*n_element + i) * stride + j] = tmp[i];
+    }
+
+    __device__ __host__ inline T load_detail(const int k) const
+    {
+      atom_t tmp[n_element];
+      int j = target::thread_idx_linear<3>();
+#pragma unroll
+      for (int i = 0; i < n_element; i++) tmp[i] = smem()[(k*n_element + i) * stride + j];
+      T a;
+      memcpy((void *)&a, tmp, sizeof(T));
+      return a;
+    }
+
+    static constexpr unsigned int get_offset(dim3 block)
+    {
+      unsigned int o = 0;
+      if constexpr (!std::is_same_v<O, void>) { o = O::shared_mem_size(block); }
+      return o;
+    }
+
+  public:
+    static constexpr unsigned int shared_mem_size(dim3 block)
+    {
+      return get_offset(block) + len * block.x * block.y * block.z * sizeof(T);
+    }
+
+    /**
+       @brief Constructor for ThreadLocalCache.
+    */
+    constexpr ThreadLocalCache() : stride(target::block_size<3>()) {}
+
+    /**
+       @brief Save the value into the thread local cache.  Used when N==0 so cache acts like single object.
+       @param[in] a The value to store in the thread local cache
+     */
+    __device__ __host__ inline void save(const T &a) const {
+      static_assert(N == 0);
+      save_detail(a, 0);
+    }
+
+    /**
+       @brief Save the value into an element of the thread local cache.
+       @param[in] a The value to store in the thread local cache
+       @param[in] k The index to use
+     */
+    __device__ __host__ inline void save(const T &a, const int k) const { save_detail(a, k); }
+
+    /**
+       @brief Load a value from the thread local cache.  Used when N==0 so cache acts like single object.
+       @return The value stored in the thread local cache
+     */
+    __device__ __host__ inline T load() const {
+      static_assert(N == 0);
+      return load_detail(0);
+    }
+
+    /**
+       @brief Load a value from an element of the thread local cache
+       @param[in] k The index to use
+       @return The value stored in the thread local cache at that index
+     */
+    __device__ __host__ inline T load(const int k) const { return load_detail(k); }
+
+    /**
+       @brief Cast operator to allow cache objects to be used where T is expected (when N==0).
+     */
+    __device__ __host__ operator T() const {
+      static_assert(N == 0);
+      return load();
+    }
+
+    /**
+       @brief Assignment operator to allow cache objects to be used on
+       the lhs where T is otherwise expected (when N==0).
+     */
+    __device__ __host__ void operator=(const T &src) const {
+      static_assert(N == 0);
+      save(src);
+    }
+
+    /**
+       @brief Subscripting operator returning value at index for convenience.
+       @param[in] i The index to use
+       @return The value stored in the thread local cache at that index
+     */
+    __device__ __host__ T operator[](int i) { return load(i); }
+  };
 
   template <typename T, int N, typename O> __device__ __host__ inline T operator+(const ThreadLocalCache<T, N, O> &a, const T &b)
   {
