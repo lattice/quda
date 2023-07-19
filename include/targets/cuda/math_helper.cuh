ce5d3969a70b865f56298b73936ba225d9bb3086
maddyscientist <mclark@nvidia.com>
2023-07-19 14:43:17 -0700

    Make math_helper.cuh safe to include in non CUDA-aware compiler

--- include/targets/cuda/math_helper.cuh
+++ include/targets/cuda/math_helper.cuh
@@ -3,7 +3,11 @@
 #include <cmath>
 #include <target_device.h>
 
-#if (CUDA_VERSION >= 11070) && !defined(_NVHPC_CUDA)
+#if defined(__CUDACC__) || defined(_NVHPC_CUDA) || (defined(__clang__) && defined(__CUDA__))
+#define QUDA_CUDA_CC
+#endif
+
+#if (CUDA_VERSION >= 11070) && defined(QUDA_CUDA_CC) && !defined(_NVHPC_CUDA)
 #define BUILTIN_ASSUME(x) \
   bool p = x;             \
   __builtin_assume(p);
@@ -34,6 +38,7 @@ namespace quda {
     template <typename T> inline void operator()(const T& a, T *s, T *c) { ::sincos(a, s, c); }
   };
 
+#ifdef QUDA_CUDA_CC
   template <> struct sincos_impl<true> {
     template <typename T> __device__ inline void operator()(const T& a, T *s, T *c)
     {
@@ -41,6 +46,7 @@ namespace quda {
       sincos(a, s, c);
     }
   };
+#endif
 
   /**
    * @brief Combined sin and cos calculation in QUDA NAMESPACE
@@ -55,9 +61,11 @@ namespace quda {
     inline void operator()(const float& a, float *s, float *c) { ::sincosf(a, s, c); }
   };
 
+#ifdef QUDA_CUDA_CC
   template <> struct sincosf_impl<true> {
     __device__ inline void operator()(const float& a, float *s, float *c) { __sincosf(a, s, c); }
   };
+#endif
 
   /**
    * @brief Combined sin and cos calculation in QUDA NAMESPACE
@@ -75,10 +83,11 @@ namespace quda {
     template <typename T> inline void operator()(const T& a, T *s, T *c) { ::sincos(a * static_cast<T>(M_PI), s, c); }
   };
 
+#ifdef QUDA_CUDA_CC
   template <> struct sincospi_impl<true> {
     template <typename T> __device__ inline void operator()(const T& a, T *s, T *c) { sincospi(a, s, c); }
   };
-
+#endif
 
   /**
    * @brief Combined sinpi and cospi calculation in QUDA NAMESPACE
@@ -100,20 +109,25 @@ namespace quda {
   template<>
   inline __host__ __device__ void sincospi(const float& a, float *s, float *c) { quda::sincos(a * static_cast<float>(M_PI), s, c); }
 
+  template <bool is_device> struct sinpi_impl {
+    template <typename T> inline T operator()(T a) { return ::sin(a * static_cast<T>(M_PI)); }
+  };
+#ifdef QUDA_CUDA_CC
+  template <> struct sinpi_impl<true> { template <typename T> __device__ inline T operator()(T a) { return ::sinpi(a); } };
+#endif
 
   /**
    * @brief Sine pi calculation in QUDA NAMESPACE
    * @param a the angle
    * @return result of the sin(a * pi)
    */
-  template<typename T> inline __host__ __device__ T sinpi(T a) { return ::sinpi(a); }
+  template<typename T> inline __host__ __device__ T sinpi(T a) { return target::dispatch<sinpi_impl>(a); }
 
-#ifndef _NVHPC_CUDA
-  template <bool is_device> struct sinpif_impl { inline float operator()(float a) { return ::sinpif(a); } };
-#else
+
   template <bool is_device> struct sinpif_impl { inline float operator()(float a) { return ::sinf(a * static_cast<float>(M_PI)); } };
-#endif
+#ifdef QUDA_CUDA_CC
   template <> struct sinpif_impl<true> { __device__ inline float operator()(float a) { return __sinf(a * static_cast<float>(M_PI)); } };
+#endif
 
   /**
    * @brief Sine pi calculation in QUDA NAMESPACE.
@@ -124,20 +138,27 @@ namespace quda {
    */
   template<> inline __host__ __device__ float sinpi(float a) { return target::dispatch<sinpif_impl>(a); }
 
+  template <bool is_device> struct cospi_impl {
+    template <typename T> inline T operator()(T a) { return ::cos(a * static_cast<T>(M_PI)); }
+  };
+#ifdef QUDA_CUDA_CC
+  template <> struct cospi_impl<true> {
+    template <typename T> __device__ inline T operator()(T a) { return ::cospi(a); }
+  };
+#endif
 
   /**
    * @brief Cosine pi calculation in QUDA NAMESPACE
    * @param a the angle
    * @return result of the cos(a * pi)
    */
-  template<typename T> inline __host__ __device__ T cospi(T a) { return ::cospi(a); }
+  template<typename T> inline __host__ __device__ T cospi(T a) { return target::dispatch<cospi_impl>(a); }
 
-#ifndef _NVHPC_CUDA
-  template <bool is_device> struct cospif_impl { inline float operator()(float a) { return ::cospif(a); } };
-#else
+
   template <bool is_device> struct cospif_impl { inline float operator()(float a) { return ::cosf(a * static_cast<float>(M_PI)); } };
-#endif
+#ifdef QUDA_CUDA_CC
   template <> struct cospif_impl<true> { __device__ inline float operator()(float a) { return __cosf(a * static_cast<float>(M_PI)); } };
+#endif
 
   /**
    * @brief Cosine pi calculation in QUDA NAMESPACE.
@@ -153,9 +174,11 @@ namespace quda {
     template <typename T> inline T operator()(T a) { return static_cast<T>(1.0) / sqrt(a); }
   };
 
+#ifdef QUDA_CUDA_CC
   template <> struct rsqrt_impl<true> {
     template <typename T> __device__ inline T operator()(T a) { return ::rsqrt(a); }
   };
+#endif
 
   /**
    * @brief Reciprocal square root function (rsqrt)
@@ -169,6 +192,7 @@ namespace quda {
 
   template <bool is_device> struct fpow_impl { template <typename real> inline real operator()(real a, int b) { return std::pow(a, b); } };
 
+#ifdef QUDA_CUDA_CC
   template <> struct fpow_impl<true> {
     __device__ inline double operator()(double a, int b) { return ::pow(a, b); }
 
@@ -179,6 +203,7 @@ namespace quda {
       return b & 1 ? sign * power : power;
     }
   };
+#endif
 
   /*
     @brief Fast power function that works for negative "a" argument
@@ -189,7 +214,9 @@ namespace quda {
   template <typename real> __device__ __host__ inline real fpow(real a, int b) { return target::dispatch<fpow_impl>(a, b); }
 
   template <bool is_device> struct fdividef_impl { inline float operator()(float a, float b) { return a / b; } };
+#ifdef QUDA_CUDA_CC
   template <> struct fdividef_impl<true> { __device__ inline float operator()(float a, float b) { return __fdividef(a, b); } };
+#endif
 
   /**
      @brief Optimized division routine on the device
@@ -197,3 +224,5 @@ namespace quda {
   __device__ __host__ inline float fdividef(float a, float b) { return target::dispatch<fdividef_impl>(a, b); }
 
 }
+
+#undef QUDA_CUDA_CC
