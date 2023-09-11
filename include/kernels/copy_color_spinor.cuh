34d388d5096a4a796b5219b1ec5faccaa1934871
Roman Gruber <rgruber@student.ethz.ch>
2023-09-11 12:22:01 +0200

    global minus for openqcd gamma matrix transformation

--- include/kernels/copy_color_spinor.cuh
+++ include/kernels/copy_color_spinor.cuh
@@ -164,6 +164,8 @@ namespace quda {
     __device__ __host__ inline void operator()(complex<FloatOut> out[Ns*Nc], const complex<FloatIn> in[Ns*Nc]) const {
       int s1[4] = {0, 1, 0, 1};
       int s2[4] = {2, 3, 2, 3};
+
+      /* RG: I added a global minus here to ix the global minus imposed by the transformation */
       FloatOut K1[4] = {static_cast<FloatOut>(-kU),
                         static_cast<FloatOut>(-kU),
                         static_cast<FloatOut>(-kU),
