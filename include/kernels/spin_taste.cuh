2d9a4b86d3e838674bbf46b97b6b16a40ea31080
alexstrel <astrel@fnal.gov>
2023-12-11 11:12:10 -0600

    moved getSign sub-routine into operator()

--- include/kernels/spin_taste.cuh
+++ include/kernels/spin_taste.cuh
@@ -28,53 +28,15 @@ namespace quda {
       checkPrecision(out_, in_); // check all precisions match
       checkLocation(out_, in_);  // check all locations match
       if (!in_.isNative())
-        errorQuda("Unsupported field order colorspinor= %d\n", in_.FieldOrder());
+        errorQuda("Unsupported field order colorspinor= \n", in_.FieldOrder());
       if (!out_.isNative())
-        errorQuda("Unsupported field order colorspinor= %d\n", out_.FieldOrder());
+        errorQuda("Unsupported field order colorspinor= \n", out_.FieldOrder());
       #pragma unroll
       for (int i=0; i<4; i++) { X[i] = in_.X()[i]; }
     }
   };
 
   // FIXME only works with even local volumes
-  template <typename Arg> constexpr auto getSign(int x, int y, int z, int t, const Arg &arg) {
-    typename Arg::Float sign = 1.0;
-    if (Arg::gamma == QUDA_SPIN_TASTE_G1) {
-      sign = 1.0;
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_GX) {
-      sign = 1.0 - 2.0 * ((y + z + t) % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_GY) {
-      sign = 1.0 - 2.0 * ((x + z + t) % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_GZ) {
-      sign = 1.0 - 2.0 * ((x + y + t) % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_GT) {
-      sign = 1.0 - 2.0 * ((x + y + z) % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_G5) {
-      sign = 1.0 - 2.0 * ((x + y + z + t) % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_GYGZ) {
-      sign = 1.0 - 2.0 * ((y + z) % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_GZGX) {
-      sign = 1.0 - 2.0 * ((z + x) % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_GXGY) {
-      sign = 1.0 - 2.0 * ((x + y) % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_GXGT) {
-      sign = 1.0 - 2.0 * ((x + t) % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_GYGT) {
-      sign = 1.0 - 2.0 * ((y + t) % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_GZGT) {
-      sign = 1.0 - 2.0 * ((z + t) % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_G5GX) {
-      sign = 1.0 - 2.0 * (x % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_G5GY) {
-      sign = 1.0 - 2.0 * (y % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_G5GZ) {
-      sign = 1.0 - 2.0 * (z % 2);
-    } else if (Arg::gamma == QUDA_SPIN_TASTE_G5GT) {
-      sign = 1.0 - 2.0 * (t % 2);
-    }
-    return sign;
-  }
-
   template <typename Arg> struct SpinTastePhase
   {
     const Arg &arg;
@@ -83,14 +45,46 @@ namespace quda {
 
     __device__ __host__ void operator()(int x_cb, int parity)
     {
-      using real = typename mapper<typename Arg::Float>::type;
-      using Vector = ColorSpinor<real, Arg::nColor, 1>;
+      using real   = typename mapper<typename Arg::Float>::type;
+      using Vector = ColorSpinor<real, Arg::nColor, 1>; 
 
       int x[4];
 
       getCoords(x, x_cb, arg.X, parity);
 
-      real sign = getSign(x[0], x[1], x[2], x[3], arg);
+      real sign = 1.0;
+
+      if        (Arg::gamma == QUDA_SPIN_TASTE_GX) {
+        sign = 1.0 - 2.0 * ((x[1] + x[2] + x[3]) % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_GY) {
+        sign = 1.0 - 2.0 * ((x[0] + x[2] + x[3]) % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_GZ) {
+        sign = 1.0 - 2.0 * ((x[0] + x[1] + x[3]) % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_GT) {
+        sign = 1.0 - 2.0 * ((x[0] + x[1] + x[2]) % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_G5) {
+        sign = 1.0 - 2.0 * ((x[0] + x[1] + x[2] + x[3]) % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_GYGZ) {
+        sign = 1.0 - 2.0 * ((x[1] + x[2]) % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_GZGX) {
+        sign = 1.0 - 2.0 * ((x[2] + x[0]) % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_GXGY) {
+        sign = 1.0 - 2.0 * ((x[0] + x[1]) % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_GXGT) {
+        sign = 1.0 - 2.0 * ((x[0] + x[3]) % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_GYGT) {
+        sign = 1.0 - 2.0 * ((x[1] + x[3]) % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_GZGT) {
+        sign = 1.0 - 2.0 * ((x[2] + x[3]) % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_G5GX) {
+        sign = 1.0 - 2.0 * (x[0] % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_G5GY) {
+        sign = 1.0 - 2.0 * (x[1] % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_G5GZ) {
+        sign = 1.0 - 2.0 * (x[2] % 2);
+      } else if (Arg::gamma == QUDA_SPIN_TASTE_G5GT) {
+        sign = 1.0 - 2.0 * (x[3] % 2);
+      }
 
       Vector out = arg.in(x_cb, parity);
 
