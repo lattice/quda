49c58e044a3ffac58d217cc4dc6e67281589ab93
maddyscientist <mclark@nvidia.com>
2024-03-27 15:02:28 -0700

    invert_test will now test all precision when driven by ctest rather than separate double and single invocations

--- tests/invert_test_gtest.hpp
+++ tests/invert_test_gtest.hpp
@@ -5,8 +5,8 @@
 // tuple containing parameters for Schwarz solver
 using schwarz_t = ::testing::tuple<QudaSchwarzType, QudaInverterType, QudaPrecision>;
 
-using test_t
-  = ::testing::tuple<QudaInverterType, QudaSolutionType, QudaSolveType, QudaPrecision, int, int, schwarz_t, QudaResidualType>;
+using test_t = ::testing::tuple<QudaPrecision, QudaPrecision, QudaInverterType, QudaSolutionType, QudaSolveType, int,
+                                int, schwarz_t, QudaResidualType>;
 
 class InvertTest : public ::testing::TestWithParam<test_t>
 {
@@ -15,20 +15,44 @@ protected:
 
 public:
   InvertTest() : param(GetParam()) { }
+
+  virtual void SetUp()
+  {
+    // check if outer precision has changed and update if it has
+    if (::testing::get<0>(param) != last_prec) {
+      if (last_prec != QUDA_INVALID_PRECISION) {
+        freeGaugeQuda();
+        if (dslash_type == QUDA_CLOVER_WILSON_DSLASH || dslash_type == QUDA_TWISTED_CLOVER_DSLASH) freeCloverQuda();
+      }
+
+      // Load the gauge field to the device
+      gauge_param.cuda_prec = ::testing::get<0>(param);
+      loadGaugeQuda(gauge.data(), &gauge_param);
+
+      if (dslash_type == QUDA_CLOVER_WILSON_DSLASH || dslash_type == QUDA_TWISTED_CLOVER_DSLASH) {
+        // Load the clover terms to the device
+        inv_param.clover_cuda_prec = ::testing::get<0>(param);
+        loadCloverQuda(clover.data(), clover_inv.data(), &inv_param);
+      }
+      last_prec = ::testing::get<0>(param);
+    }
+  }
 };
 
 bool skip_test(test_t param)
 {
-  auto inverter_type = ::testing::get<0>(param);
-  auto solution_type = ::testing::get<1>(param);
-  auto solve_type = ::testing::get<2>(param);
-  auto prec_sloppy = ::testing::get<3>(param);
-  auto multishift = ::testing::get<4>(param);
-  auto solution_accumulator_pipeline = ::testing::get<5>(param);
-  auto schwarz_param = ::testing::get<6>(param);
+  auto prec = ::testing::get<0>(param);
+  auto prec_sloppy = ::testing::get<1>(param);
+  auto inverter_type = ::testing::get<2>(param);
+  auto solution_type = ::testing::get<3>(param);
+  auto solve_type = ::testing::get<4>(param);
+  auto multishift = ::testing::get<5>(param);
+  auto solution_accumulator_pipeline = ::testing::get<6>(param);
+  auto schwarz_param = ::testing::get<7>(param);
   auto prec_precondition = ::testing::get<2>(schwarz_param);
 
   if (prec < prec_sloppy) return true;              // outer precision >= sloppy precision
+  if (!(QUDA_PRECISION & prec)) return true;        // precision not enabled so skip it
   if (!(QUDA_PRECISION & prec_sloppy)) return true; // precision not enabled so skip it
   if (!(QUDA_PRECISION & prec_precondition) && prec_precondition != QUDA_INVALID_PRECISION)
     return true; // precision not enabled so skip it
@@ -68,16 +92,16 @@ TEST_P(InvertTest, verify)
 
   inv_param.tol = 0.0;
   inv_param.tol_hq = 0.0;
-  auto res_t = ::testing::get<7>(GetParam());
+  auto res_t = ::testing::get<8>(GetParam());
   if (res_t & QUDA_L2_RELATIVE_RESIDUAL) inv_param.tol = tol;
   if (res_t & QUDA_HEAVY_QUARK_RESIDUAL) inv_param.tol_hq = tol_hq;
 
   auto tol = inv_param.tol;
   if (is_chiral(inv_param.dslash_type)) { tol *= std::sqrt(static_cast<double>(inv_param.Ls)); }
   // FIXME eventually we should build in refinement to the *NR solvers to remove the need for this
-  if (is_normal_residual(::testing::get<0>(GetParam()))) tol *= 50;
+  if (is_normal_residual(::testing::get<2>(GetParam()))) tol *= 50;
   // Slight loss of precision possible when reconstructing full solution
-  if (is_full_solution(::testing::get<1>(GetParam())) && is_preconditioned_solve(::testing::get<2>(GetParam())))
+  if (is_full_solution(::testing::get<3>(GetParam())) && is_preconditioned_solve(::testing::get<4>(GetParam())))
     tol *= 10;
 
   for (auto rsd : solve(GetParam())) {
@@ -89,21 +113,22 @@ TEST_P(InvertTest, verify)
 std::string gettestname(::testing::TestParamInfo<test_t> param)
 {
   std::string name;
-  name += get_solver_str(::testing::get<0>(param.param)) + std::string("_");
-  name += get_solution_str(::testing::get<1>(param.param)) + std::string("_");
-  name += get_solve_str(::testing::get<2>(param.param)) + std::string("_");
-  name += get_prec_str(::testing::get<3>(param.param));
-  if (::testing::get<4>(param.param) > 1)
-    name += std::string("_shift") + std::to_string(::testing::get<4>(param.param));
+  name += get_prec_str(::testing::get<0>(param.param)) + std::string("_");
+  name += get_prec_str(::testing::get<1>(param.param)) + std::string("_");
+  name += get_solver_str(::testing::get<2>(param.param)) + std::string("_");
+  name += get_solution_str(::testing::get<3>(param.param)) + std::string("_");
+  name += get_solve_str(::testing::get<4>(param.param));
   if (::testing::get<5>(param.param) > 1)
-    name += std::string("_solution_accumulator_pipeline") + std::to_string(::testing::get<5>(param.param));
-  auto &schwarz_param = ::testing::get<6>(param.param);
+    name += std::string("_shift") + std::to_string(::testing::get<5>(param.param));
+  if (::testing::get<6>(param.param) > 1)
+    name += std::string("_solution_accumulator_pipeline") + std::to_string(::testing::get<6>(param.param));
+  auto &schwarz_param = ::testing::get<7>(param.param);
   if (::testing::get<0>(schwarz_param) != QUDA_INVALID_SCHWARZ) {
     name += std::string("_") + get_schwarz_str(::testing::get<0>(schwarz_param));
     name += std::string("_") + get_solver_str(::testing::get<1>(schwarz_param));
     name += std::string("_") + get_prec_str(::testing::get<2>(schwarz_param));
   }
-  auto res_t = ::testing::get<7>(param.param);
+  auto res_t = ::testing::get<8>(param.param);
   if (res_t & QUDA_L2_RELATIVE_RESIDUAL) name += std::string("_l2");
   if (res_t & QUDA_HEAVY_QUARK_RESIDUAL) name += std::string("_heavy_quark");
   return name;
@@ -118,6 +143,8 @@ auto direct_solvers = Values(QUDA_CGNE_INVERTER, QUDA_CGNR_INVERTER, QUDA_CA_CGN
                              QUDA_CG3NE_INVERTER, QUDA_CG3NR_INVERTER, QUDA_GCR_INVERTER, QUDA_CA_GCR_INVERTER,
                              QUDA_BICGSTAB_INVERTER, QUDA_BICGSTABL_INVERTER, QUDA_MR_INVERTER);
 
+auto precisions = Values(QUDA_DOUBLE_PRECISION, QUDA_SINGLE_PRECISION);
+
 auto sloppy_precisions
   = Values(QUDA_DOUBLE_PRECISION, QUDA_SINGLE_PRECISION, QUDA_HALF_PRECISION, QUDA_QUARTER_PRECISION);
 
@@ -129,41 +156,43 @@ auto no_heavy_quark = Values(QUDA_L2_RELATIVE_RESIDUAL);
 
 // preconditioned normal solves
 INSTANTIATE_TEST_SUITE_P(NormalEvenOdd, InvertTest,
-                         Combine(normal_solvers, Values(QUDA_MATPCDAG_MATPC_SOLUTION, QUDA_MAT_SOLUTION),
-                                 Values(QUDA_NORMOP_PC_SOLVE), sloppy_precisions, Values(1),
-                                 solution_accumulator_pipelines, no_schwarz, no_heavy_quark),
+                         Combine(precisions, sloppy_precisions, normal_solvers,
+                                 Values(QUDA_MATPCDAG_MATPC_SOLUTION, QUDA_MAT_SOLUTION), Values(QUDA_NORMOP_PC_SOLVE),
+                                 Values(1), solution_accumulator_pipelines, no_schwarz, no_heavy_quark),
                          gettestname);
 
 // full system normal solve
 INSTANTIATE_TEST_SUITE_P(NormalFull, InvertTest,
-                         Combine(normal_solvers, Values(QUDA_MATDAG_MAT_SOLUTION), Values(QUDA_NORMOP_SOLVE),
-                                 sloppy_precisions, Values(1), solution_accumulator_pipelines, no_schwarz, no_heavy_quark),
+                         Combine(precisions, sloppy_precisions, normal_solvers, Values(QUDA_MATDAG_MAT_SOLUTION),
+                                 Values(QUDA_NORMOP_SOLVE), Values(1), solution_accumulator_pipelines, no_schwarz,
+                                 no_heavy_quark),
                          gettestname);
 
 // preconditioned direct solves
 INSTANTIATE_TEST_SUITE_P(EvenOdd, InvertTest,
-                         Combine(direct_solvers, Values(QUDA_MATPC_SOLUTION, QUDA_MAT_SOLUTION),
-                                 Values(QUDA_DIRECT_PC_SOLVE), sloppy_precisions, Values(1),
-                                 solution_accumulator_pipelines, no_schwarz, no_heavy_quark),
+                         Combine(precisions, sloppy_precisions, direct_solvers,
+                                 Values(QUDA_MATPC_SOLUTION, QUDA_MAT_SOLUTION), Values(QUDA_DIRECT_PC_SOLVE),
+                                 Values(1), solution_accumulator_pipelines, no_schwarz, no_heavy_quark),
                          gettestname);
 
 // full system direct solve
 INSTANTIATE_TEST_SUITE_P(Full, InvertTest,
-                         Combine(direct_solvers, Values(QUDA_MAT_SOLUTION), Values(QUDA_DIRECT_SOLVE), sloppy_precisions,
-                                 Values(1), solution_accumulator_pipelines, no_schwarz, no_heavy_quark),
+                         Combine(precisions, sloppy_precisions, direct_solvers, Values(QUDA_MAT_SOLUTION),
+                                 Values(QUDA_DIRECT_SOLVE), Values(1), solution_accumulator_pipelines, no_schwarz,
+                                 no_heavy_quark),
                          gettestname);
 
 // preconditioned multi-shift solves
 INSTANTIATE_TEST_SUITE_P(MultiShiftEvenOdd, InvertTest,
-                         Combine(Values(QUDA_CG_INVERTER), Values(QUDA_MATPCDAG_MATPC_SOLUTION),
-                                 Values(QUDA_NORMOP_PC_SOLVE), sloppy_precisions, Values(10),
+                         Combine(precisions, sloppy_precisions, Values(QUDA_CG_INVERTER),
+                                 Values(QUDA_MATPCDAG_MATPC_SOLUTION), Values(QUDA_NORMOP_PC_SOLVE), Values(10),
                                  solution_accumulator_pipelines, no_schwarz, no_heavy_quark),
                          gettestname);
 
 // Schwarz-preconditioned normal solves
 INSTANTIATE_TEST_SUITE_P(SchwarzNormal, InvertTest,
-                         Combine(Values(QUDA_PCG_INVERTER), Values(QUDA_MATPCDAG_MATPC_SOLUTION),
-                                 Values(QUDA_NORMOP_PC_SOLVE), sloppy_precisions, Values(1),
+                         Combine(precisions, sloppy_precisions, Values(QUDA_PCG_INVERTER),
+                                 Values(QUDA_MATPCDAG_MATPC_SOLUTION), Values(QUDA_NORMOP_PC_SOLVE), Values(1),
                                  solution_accumulator_pipelines,
                                  Combine(Values(QUDA_ADDITIVE_SCHWARZ), Values(QUDA_CG_INVERTER, QUDA_CA_CG_INVERTER),
                                          Values(QUDA_HALF_PRECISION, QUDA_QUARTER_PRECISION)),
@@ -172,8 +201,8 @@ INSTANTIATE_TEST_SUITE_P(SchwarzNormal, InvertTest,
 
 // Schwarz-preconditioned direct solves
 INSTANTIATE_TEST_SUITE_P(SchwarzEvenOdd, InvertTest,
-                         Combine(Values(QUDA_GCR_INVERTER), Values(QUDA_MATPC_SOLUTION), Values(QUDA_DIRECT_PC_SOLVE),
-                                 sloppy_precisions, Values(1), solution_accumulator_pipelines,
+                         Combine(precisions, sloppy_precisions, Values(QUDA_GCR_INVERTER), Values(QUDA_MATPC_SOLUTION),
+                                 Values(QUDA_DIRECT_PC_SOLVE), Values(1), solution_accumulator_pipelines,
                                  Combine(Values(QUDA_ADDITIVE_SCHWARZ), Values(QUDA_MR_INVERTER, QUDA_CA_GCR_INVERTER),
                                          Values(QUDA_HALF_PRECISION, QUDA_QUARTER_PRECISION)),
                                  no_heavy_quark),
@@ -181,7 +210,7 @@ INSTANTIATE_TEST_SUITE_P(SchwarzEvenOdd, InvertTest,
 
 // Heavy-Quark preconditioned solves
 INSTANTIATE_TEST_SUITE_P(HeavyQuarkEvenOdd, InvertTest,
-                         Combine(Values(QUDA_CG_INVERTER), Values(QUDA_MATPC_SOLUTION), Values(QUDA_NORMOP_PC_SOLVE),
-                                 sloppy_precisions, Values(1), solution_accumulator_pipelines, no_schwarz,
+                         Combine(precisions, sloppy_precisions, Values(QUDA_CG_INVERTER), Values(QUDA_MATPC_SOLUTION),
+                                 Values(QUDA_NORMOP_PC_SOLVE), Values(1), solution_accumulator_pipelines, no_schwarz,
                                  Values(QUDA_L2_RELATIVE_RESIDUAL | QUDA_HEAVY_QUARK_RESIDUAL, QUDA_HEAVY_QUARK_RESIDUAL)),
                          gettestname);
