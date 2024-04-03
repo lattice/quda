5e35ab034b5d301f5bcb6b3e3c8169b75f9847de
Marcogarofalo <marcogarofalo31@gmail.com>
2024-04-03 17:16:51 +0200

    add support for doublet to computeCloverForce

--- include/kernels/clover_outer_product.cuh
+++ include/kernels/clover_outer_product.cuh
@@ -30,11 +30,16 @@ namespace quda {
     int displacement;
     bool partitioned[4];
     real coeff;
+    const bool doublet;         // whether we applying the operator to a doublet
+    const size_t VolumeCB;
+    const size_t GhostFaceCB;
 
     CloverForceArg(GaugeField &force, const GaugeField &U, const ColorSpinorField &inA, const ColorSpinorField &inB,
                    const ColorSpinorField &inC, const ColorSpinorField &inD, const unsigned int parity,
                    const double coeff) :
-      kernel_param(dim3(dim == -1 ? inA.VolumeCB() : inB.GhostFaceCB()[dim])), // inB since it has a ghost allocated
+      // kernel_param(dim3(dim == -1 ? inA.VolumeCB() : inB.GhostFaceCB()[dim])), // inB since it has a ghost allocated
+      kernel_param(dim3(dim == -1 ?  inA.TwistFlavor() == QUDA_TWIST_NONDEG_DOUBLET ? inA.VolumeCB()/2 : inA.VolumeCB()  : 
+      inA.TwistFlavor() == QUDA_TWIST_NONDEG_DOUBLET ? inB.GhostFaceCB()[dim]/2 : inB.GhostFaceCB()[dim])), // inB since it has a ghost allocated
       force(force),
       inA(inA),
       inB(inB),
@@ -43,7 +48,10 @@ namespace quda {
       U(U),
       parity(parity),
       displacement(1),
-      coeff(coeff)
+      coeff(coeff),
+      doublet(inA.TwistFlavor() == QUDA_TWIST_NONDEG_DOUBLET),
+      VolumeCB(inA.VolumeCB()),
+      GhostFaceCB(inB.GhostFaceCB()[dim])
     {
       for (int i=0; i<4; ++i) this->X[i] = U.X()[i];
       for (int i=0; i<4; ++i) this->partitioned[i] = commDimPartitioned(i) ? true : false;
@@ -76,31 +84,37 @@ namespace quda {
       using Spinor = ColorSpinor<typename Arg::real, Arg::nColor, Arg::nSpin>;
       using Link = Matrix<Complex, Arg::nColor>;
 
-      Spinor A = arg.inA(x_cb, 0);
-      Spinor C = arg.inC(x_cb, 0);
+      for (int flavor=0; flavor<=arg.doublet; ++flavor){
 
-#pragma unroll
-      for (int dim=0; dim<4; ++dim) {
-        int shift[4] = {0, 0, 0, 0};
-        shift[dim] = 1;
-        const int nbr_idx = neighborIndex(x_cb, shift, arg.partitioned, arg.parity, arg.X);
-
-        if (nbr_idx >= 0) {
-          Spinor B_shift = arg.inB(nbr_idx, 0);
-          Spinor D_shift = arg.inD(nbr_idx, 0);
-
-          B_shift = (B_shift.project(dim,1)).reconstruct(dim,1);
-          Link result = outerProdSpinTrace(B_shift,A);
-
-          D_shift = (D_shift.project(dim,-1)).reconstruct(dim,-1);
-          result += outerProdSpinTrace(D_shift,C);
-
-          Link temp = arg.force(dim, x_cb, arg.parity);
-          Link U = arg.U(dim, x_cb, arg.parity);
-          result = temp + U*result*arg.coeff;
-          arg.force(dim, x_cb, arg.parity) = result;
-        }
-      } // dim
+        const int flavor_offset_idx = flavor * (arg.VolumeCB/2);
+        Spinor A = arg.inA(x_cb + flavor_offset_idx, 0);
+        Spinor C = arg.inC(x_cb + flavor_offset_idx, 0);
+        
+  #pragma unroll
+        for (int dim=0; dim<4; ++dim) {
+          int shift[4] = {0, 0, 0, 0};
+          shift[dim] = 1;
+          // const int nbr_idx = getNeighborIndexCB(coord, d, +1, arg.dc);
+          const int nbr_idx = neighborIndex(x_cb, shift, arg.partitioned, arg.parity, arg.X);
+          if (nbr_idx >= 0) {
+            Spinor B_shift = arg.inB(nbr_idx + flavor_offset_idx, 0);
+            Spinor D_shift = arg.inD(nbr_idx + flavor_offset_idx, 0);
+            
+            B_shift = (B_shift.project(dim,1)).reconstruct(dim,1);
+            Link result = outerProdSpinTrace(B_shift,A);
+            
+            D_shift = (D_shift.project(dim,-1)).reconstruct(dim,-1);
+            result += outerProdSpinTrace(D_shift,C);
+            
+            Link temp = arg.force(dim, x_cb, arg.parity);
+            Link U = arg.U(dim, x_cb, arg.parity);
+            // printf("Interion:   x_cb = %d  dim = %d nbr_idx = %d  result=%g   U=%g   temp =%g\n", x_cb, dim, nbr_idx,result(0,0).real(),U(0,0).real(),temp(0,0).real() );
+            result = temp + U*result*arg.coeff;
+            arg.force(dim, x_cb, arg.parity) = result;
+            // printf("Interion:   x_cb = %d  dim = %d nbr_idx = %d  result=%g   U=%g   temp =%g parity=%d\n", x_cb, dim, nbr_idx,result(0,0).real(),U(0,0).real(),temp(0,0).real() ,arg.parity);
+          }
+        } // dim
+      } // flavor
     }
   };
 
@@ -117,23 +131,27 @@ namespace quda {
       using Link = Matrix<Complex, Arg::nColor>;
 
       int x[4];
-      coordsFromIndexExterior(x, x_cb, arg.X, Arg::dim, arg.displacement, arg.parity);
-      const unsigned int bulk_cb_idx = ((((x[3]*arg.X[2] + x[2])*arg.X[1] + x[1])*arg.X[0] + x[0]) >> 1);
-      Spinor A = arg.inA(bulk_cb_idx, 0);
-      Spinor C = arg.inC(bulk_cb_idx, 0);
+      for (int flavor=0; flavor<=arg.doublet; ++flavor){
+        const int flavor_offset_bulk_idx = flavor * (arg.VolumeCB/2);
+        const int flavor_offset_ghost_idx = flavor * (arg.GhostFaceCB/2);
+        coordsFromIndexExterior(x, x_cb, arg.X, Arg::dim, arg.displacement, arg.parity);
+        const unsigned int bulk_cb_idx = ((((x[3]*arg.X[2] + x[2])*arg.X[1] + x[1])*arg.X[0] + x[0]) >> 1);
+        Spinor A = arg.inA(bulk_cb_idx + flavor_offset_bulk_idx, 0);
+        Spinor C = arg.inC(bulk_cb_idx + flavor_offset_bulk_idx, 0);
 
-      HalfSpinor projected_tmp = arg.inB.Ghost(Arg::dim, 1, x_cb, 0);
-      Spinor B_shift = projected_tmp.reconstruct(Arg::dim, 1);
-      Link result = outerProdSpinTrace(B_shift,A);
+        HalfSpinor projected_tmp = arg.inB.Ghost(Arg::dim, 1, x_cb + flavor_offset_ghost_idx, 0);
+        Spinor B_shift = projected_tmp.reconstruct(Arg::dim, 1);
+        Link result = outerProdSpinTrace(B_shift,A);
 
-      projected_tmp = arg.inD.Ghost(Arg::dim, 1, x_cb, 0);
-      Spinor D_shift = projected_tmp.reconstruct(Arg::dim,-1);
-      result += outerProdSpinTrace(D_shift,C);
+        projected_tmp = arg.inD.Ghost(Arg::dim, 1, x_cb + flavor_offset_ghost_idx, 0);
+        Spinor D_shift = projected_tmp.reconstruct(Arg::dim,-1);
+        result += outerProdSpinTrace(D_shift,C);
 
-      Link temp = arg.force(Arg::dim, bulk_cb_idx, arg.parity);
-      Link U = arg.U(Arg::dim, bulk_cb_idx, arg.parity);
-      result = temp + U*result*arg.coeff;
-      arg.force(Arg::dim, bulk_cb_idx, arg.parity) = result;
+        Link temp = arg.force(Arg::dim, bulk_cb_idx, arg.parity);
+        Link U = arg.U(Arg::dim, bulk_cb_idx, arg.parity);
+        result = temp + U*result*arg.coeff;
+        arg.force(Arg::dim, bulk_cb_idx, arg.parity) = result;
+      }
     }
   };
 
