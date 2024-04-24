77c312753b2e9992d45564c1b3fdc4f9d3379dd5
alexstrel <astrel@fnal.gov>
2024-04-24 16:14:42 -0500

    corrected previous commit with more strict error message

--- lib/milc_interface.cpp
+++ lib/milc_interface.cpp
@@ -1311,7 +1311,7 @@ void qudaShift(int external_precision, int quda_precision, const void *const lin
   invertParam.dslash_type = saveDslash;
 
   if ((sym < 1) || (sym > 3)) {
-    printfQuda("Wrong shift. Select forward (1), backward (2) or symmetric (3).\n");
+    errorQuda("Wrong shift. Select forward (1), backward (2) or symmetric (3).\n");
   } else {
     shiftQuda(dst, src, dir, sym, &invertParam);
   }
