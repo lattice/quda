60553e234ccd07740b572103f2159e091d2c9aa7
maddyscientist <mclark@nvidia.com>
2024-05-14 21:51:30 -0700

    4-d domain wall kernel should be built if twisted clover is enabled

--- lib/dslash_domain_wall_4d.cu
+++ lib/dslash_domain_wall_4d.cu
@@ -60,7 +60,7 @@ namespace quda
                          cvector_ref<const ColorSpinorField> &x, int parity, bool dagger, const int *comm_override,
                          TimeProfile &profile)
   {
-    if constexpr (is_enabled<QUDA_DOMAIN_WALL_4D_DSLASH>()) {
+    if constexpr (is_enabled<QUDA_DOMAIN_WALL_4D_DSLASH>() || is_enabled<QUDA_TWISTED_CLOVER_DSLASH>()) {
       instantiate<DomainWall4DApply>(out, in, U, a, m_5, b_5, c_5, x, parity, dagger, comm_override, profile);
     } else {
       errorQuda("Domain-wall dslash has not been built");
