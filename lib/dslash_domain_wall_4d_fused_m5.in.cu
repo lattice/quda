8ffb905794030f008bfeec0af2d80e78208f74b5
Ferenc Pittler <pittler@hiskp.uni-bonn.de>
2024-11-07 14:17:07 +0100

    typo corrected

--- lib/dslash_domain_wall_4d_fused_m5.in.cu
+++ lib/dslash_domain_wall_4d_fused_m5.in.cu
@@ -18,7 +18,7 @@ namespace quda
   template DomainWall4DApplyFusedM5<Float, nColor, DDArg, ReconstructWilson::recon[reconI]>::DomainWall4DApplyFusedM5(
     cvector_ref<ColorSpinorField> &out, cvector_ref<const ColorSpinorField> &in, cvector_ref<const ColorSpinorField> &x,
     const GaugeField &U, cvector_ref<ColorSpinorField> &y, const Complex *b_5, const Complex *c_5, double a, double m_5,
-    int parity, bool dagger, const int *comm_override, double m_f, Dslash5TypeList<dslash5_type>,
-    DistanceType<distance_pc>, TimeProfile &profile);
+    int parity, bool dagger, const int *comm_override, double m_f, DistanceType<distance_pc>, Dslash5TypeList<dslash5_type>,
+    TimeProfile &profile);
 
 } // namespace quda
