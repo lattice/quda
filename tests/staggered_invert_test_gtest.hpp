da3275c5d933764de26119632712f3f549792a8f
Evan Weinberg <eweinberg@nvidia.com>
2024-12-18 19:07:22 -0800

    Slight tweak for asqtad single invert

--- tests/staggered_invert_test_gtest.hpp
+++ tests/staggered_invert_test_gtest.hpp
@@ -108,7 +108,7 @@ TEST_P(StaggeredInvertTest, verify)
   if (solve_type == QUDA_DIRECT_SOLVE && inverter_type == QUDA_CA_GCR_INVERTER) inv_param.ca_basis = QUDA_POWER_BASIS;
 
   // Single precision needs a tiny bump due to small host/device precision deviations
-  if (prec == QUDA_SINGLE_PRECISION) verify_tol *= 1.01;
+  if (prec == QUDA_SINGLE_PRECISION) verify_tol *= 1.03;
 
   for (auto rsd : solve(GetParam())) {
     if (res_t & QUDA_L2_RELATIVE_RESIDUAL) { EXPECT_LE(rsd[0], verify_tol); }
