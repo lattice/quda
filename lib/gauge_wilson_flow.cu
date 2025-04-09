25c89ec92d7a1de9526c0f09a7e93c0f7c2f0a37
leonhostetler <leonhostetler@gmail.com>
2025-04-09 17:25:37 -0500

    Updated bytes() for GaugeWFlowStep kernel

--- lib/gauge_wilson_flow.cu
+++ lib/gauge_wilson_flow.cu
@@ -152,11 +152,13 @@ namespace quda {
       long long threads = in.LocalVolume() * wflow_dim;
       long long mat_flops = nColor * nColor * (8 * nColor - 2);
       long long mat_muls = 1; // 1 comes from Z * conj(U) term
-      switch (wflow_type) {
+      switch (wflow_type) { // Add mat-muls coming from staple calculation
       case QUDA_GAUGE_SMEAR_WILSON_FLOW: mat_muls += 4 * (wflow_dim - 1); break;
       case QUDA_GAUGE_SMEAR_SYMANZIK_FLOW: mat_muls += 28 * (wflow_dim - 1); break;
       default : errorQuda("Unknown Wilson Flow type");
       }
+      // Leon: This seems to miss the two mat-muls occurring in exponentiate_iQ(Z)
+      // as well as the 1 mat-mul of exponentiate_iQ(Z) * U
       return mat_muls * mat_flops * threads;
     }
 
@@ -168,7 +170,12 @@ namespace quda {
       case QUDA_GAUGE_SMEAR_SYMANZIK_FLOW: links = 24; break;
       default : errorQuda("Unknown Wilson Flow type");
       }
-      auto temp_io = step_type == WFLOW_STEP_W2 ? 2 : step_type == WFLOW_STEP_VT ? 1 : 0;
+      // Leon: I am not certain that the byte counting is correct here!
+      // First and last steps have 1 store (retrieve) to (from) temp
+      auto temp_io = 1;
+      // Middle steps have an additional store or retrieve to or from temp
+      if( step_type==WFLOW_STEP_W2 || step_type==WFLOW_4THORDER_STEP_2 || step_type==WFLOW_4THORDER_STEP_3 
+      || step_type==WFLOW_4THORDER_STEP_4 || step_type==WFLOW_4THORDER_STEP_5 ); temp_io += 1;
       return ((1 + (wflow_dim - 1) * links) * in.Bytes() + out.Bytes() + temp_io * temp.Bytes());
     }
   }; // GaugeWFlowStep
