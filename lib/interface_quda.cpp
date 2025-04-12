aa949d11a61fb9efcbfcd6c1a7269c2222ade32b
leonhostetler <leonhostetler@gmail.com>
2025-04-12 10:19:06 -0500

    printf --> logQuda

--- lib/interface_quda.cpp
+++ lib/interface_quda.cpp
@@ -5208,7 +5208,7 @@ void performGaugeSmearQuda(QudaGaugeSmearParam *smear_param, QudaGaugeObservable
 void performWFlowQuda(QudaGaugeSmearParam *smear_param, QudaGaugeObservableParam *obs_param)
 {
   auto profile = pushProfile(profileWFlow);
-  pushOutputPrefix("performWFlowQuda: ");
+  pushOutputPrefix("");
   checkGaugeSmearParam(smear_param);
 
   if (smear_param->restart) {
@@ -5239,23 +5239,23 @@ void performWFlowQuda(QudaGaugeSmearParam *smear_param, QudaGaugeObservableParam
   QudaBoolean compute_charge = obs_param[measurement_n].compute_qcharge;
 
   // Print observables header
-  printf("performWFlowQuda: flow t, Energy_t, Energy_s");
-  if (compute_plaq) printf(", Plaq_t, Plaq_s");
-  if (compute_rect) printf(", Rect_t, Rect_s");
-  if (compute_ploop) printf(", Ploop_r, Ploop_i");
-  if (compute_charge) printf(", charge");
-  printf("\n");
+  logQuda(QUDA_SUMMARIZE, "performWFlowQuda: flow t, Energy_t, Energy_s");
+  if (compute_plaq) logQuda(QUDA_SUMMARIZE, ", Plaq_t, Plaq_s");
+  if (compute_rect) logQuda(QUDA_SUMMARIZE, ", Rect_t, Rect_s");
+  if (compute_ploop) logQuda(QUDA_SUMMARIZE, ", Ploop_r, Ploop_i");
+  if (compute_charge) logQuda(QUDA_SUMMARIZE, ", charge");
+  logQuda(QUDA_SUMMARIZE, "\n");
 
   // Print initial values
-  printf("performWFlowQuda: %le %.16e %+.16e", smear_param->t0, obs_param[measurement_n].energy[2],
+  logQuda(QUDA_SUMMARIZE, "performWFlowQuda: %le %.16e %+.16e", smear_param->t0, obs_param[measurement_n].energy[2],
          obs_param[measurement_n].energy[1]);
   if (compute_plaq)
-    printf(" %+.16e %+.16e", obs_param[measurement_n].plaquette[2], obs_param[measurement_n].plaquette[1]);
+    logQuda(QUDA_SUMMARIZE, " %+.16e %+.16e", obs_param[measurement_n].plaquette[2], obs_param[measurement_n].plaquette[1]);
   if (compute_rect)
-    printf(" %+.16e %+.16e", obs_param[measurement_n].rectangle[2], obs_param[measurement_n].rectangle[1]);
-  if (compute_ploop) printf(" %+.16e %+.16e", obs_param[measurement_n].ploop[0], obs_param[measurement_n].ploop[1]);
-  if (compute_charge) printf(" %+.16e", obs_param[measurement_n].qcharge);
-  printf("\n");
+    logQuda(QUDA_SUMMARIZE, " %+.16e %+.16e", obs_param[measurement_n].rectangle[2], obs_param[measurement_n].rectangle[1]);
+  if (compute_ploop) logQuda(QUDA_SUMMARIZE, " %+.16e %+.16e", obs_param[measurement_n].ploop[0], obs_param[measurement_n].ploop[1]);
+  if (compute_charge) logQuda(QUDA_SUMMARIZE, " %+.16e", obs_param[measurement_n].qcharge);
+  logQuda(QUDA_SUMMARIZE, "\n");
 
   for (unsigned int i = 0; i < smear_param->n_steps; i++) {
     // This uses 3-stage third order or 6-stage fourth order Runge-Kutta integration
@@ -5273,15 +5273,15 @@ void performWFlowQuda(QudaGaugeSmearParam *smear_param, QudaGaugeObservableParam
 
       gaugeObservables(out, obs_param[measurement_n]);
 
-      printf("performWFlowQuda: %le %.16e %+.16e", (smear_param->t0 + smear_param->epsilon * (i + 1)),
+      logQuda(QUDA_SUMMARIZE, "performWFlowQuda: %le %.16e %+.16e", (smear_param->t0 + smear_param->epsilon * (i + 1)),
              obs_param[measurement_n].energy[2], obs_param[measurement_n].energy[1]);
       if (compute_plaq)
-        printf(" %+.16e %+.16e", obs_param[measurement_n].plaquette[2], obs_param[measurement_n].plaquette[1]);
+        logQuda(QUDA_SUMMARIZE, " %+.16e %+.16e", obs_param[measurement_n].plaquette[2], obs_param[measurement_n].plaquette[1]);
       if (compute_rect)
-        printf(" %+.16e %+.16e", obs_param[measurement_n].rectangle[2], obs_param[measurement_n].rectangle[1]);
-      if (compute_ploop) printf(" %+.16e %+.16e", obs_param[measurement_n].ploop[0], obs_param[measurement_n].ploop[1]);
-      if (compute_charge) printf(" %+.16e", obs_param[measurement_n].qcharge);
-      printf("\n");
+        logQuda(QUDA_SUMMARIZE, " %+.16e %+.16e", obs_param[measurement_n].rectangle[2], obs_param[measurement_n].rectangle[1]);
+      if (compute_ploop) logQuda(QUDA_SUMMARIZE, " %+.16e %+.16e", obs_param[measurement_n].ploop[0], obs_param[measurement_n].ploop[1]);
+      if (compute_charge) logQuda(QUDA_SUMMARIZE, " %+.16e", obs_param[measurement_n].qcharge);
+      logQuda(QUDA_SUMMARIZE, "\n");
     }
   }
   // copy out to gaugeSmeared so that flowed gauge can be saved to host and WFlow can be restarted 
