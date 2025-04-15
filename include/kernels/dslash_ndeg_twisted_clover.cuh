e203f54efe02a9e99b9f692a982be1a8c6e0e1e4
maddyscientist <mclark@nvidia.com>
2025-04-15 10:50:30 -0700

    Fix compilation of ndeg clover with NVSHMEM enabled

--- include/kernels/dslash_ndeg_twisted_clover.cuh
+++ include/kernels/dslash_ndeg_twisted_clover.cuh
@@ -41,7 +41,7 @@ namespace quda
     using real = typename mapper<typename Arg::Float>::type;
     using Vec = ColorSpinor<real, Arg::nColor, 4>;
     using Cache = SharedMemoryCache<Vec>;
-    using Ops = std::conditional_t<kernel_type == INTERIOR_KERNEL, KernelOps<Cache>, NoKernelOps>;
+    using Ops = std::conditional_t<kernel_type == INTERIOR_KERNEL || kernel_type == UBER_KERNEL, KernelOps<Cache>, NoKernelOps>;
   };
 
   template <int nParity, bool dagger, bool xpay, KernelType kernel_type, typename Arg>
@@ -84,38 +84,38 @@ namespace quda
       // defined in dslash_wilson.cuh
       applyWilson<nParity, dagger, mykernel_type>(out, arg, coord, parity, idx, thread_dim, active, src_idx);
 
-      if constexpr(mykernel_type == INTERIOR_KERNEL) {
-	if(arg.dd_x.isZero(coord)) {
-        out = arg.a * out;
+      if constexpr (mykernel_type == INTERIOR_KERNEL) {
+	if (arg.dd_x.isZero(coord)) {
+          out = arg.a * out;
       	} else {
-        // apply the chiral and flavor twists
-        // use consistent load order across s to ensure better cache locality
-        Vector x = arg.x[src_idx](my_flavor_idx, my_spinor_parity);
-        SharedMemoryCache<Vector> cache {*this};
-        cache.save(x);
+          // apply the chiral and flavor twists
+          // use consistent load order across s to ensure better cache locality
+          Vector x = arg.x[src_idx](my_flavor_idx, my_spinor_parity);
+          SharedMemoryCache<Vector> cache {*this};
+          cache.save(x);
 
-        x.toRel(); // switch to chiral basis
+          x.toRel(); // switch to chiral basis
 
-        Vector tmp;
+          Vector tmp;
 #pragma unroll
-        for (int chirality = 0; chirality < 2; chirality++) {
-          constexpr int n = Arg::nColor * Arg::nSpin / 2;
-          HMatrix<real, n> A = arg.A(coord.x_cb, parity, chirality);
-          HalfVector x_chi = x.chiral_project(chirality);
-          HalfVector Ax_chi = A * x_chi;
-          // i * mu * gamma_5 * tau_3
-          const complex<real> b(0.0, (chirality^flavor) == 0 ? static_cast<real>(arg.b) : -static_cast<real>(arg.b));
-          Ax_chi += b * x_chi;
-          tmp += Ax_chi.chiral_reconstruct(chirality);
-        }
+          for (int chirality = 0; chirality < 2; chirality++) {
+            constexpr int n = Arg::nColor * Arg::nSpin / 2;
+            HMatrix<real, n> A = arg.A(coord.x_cb, parity, chirality);
+            HalfVector x_chi = x.chiral_project(chirality);
+            HalfVector Ax_chi = A * x_chi;
+            // i * mu * gamma_5 * tau_3
+            const complex<real> b(0.0, (chirality^flavor) == 0 ? static_cast<real>(arg.b) : -static_cast<real>(arg.b));
+            Ax_chi += b * x_chi;
+            tmp += Ax_chi.chiral_reconstruct(chirality);
+          }
 
-        tmp.toNonRel();
-        // tmp += (c * tau_1) * x
-        cache.sync();
-        tmp += arg.c * cache.load_y(target::thread_idx().y + 1 - 2 * flavor);
+          tmp.toNonRel();
+          // tmp += (c * tau_1) * x
+          cache.sync();
+          tmp += arg.c * cache.load_y(target::thread_idx().y + 1 - 2 * flavor);
 
-        // add the Wilson part with normalisation
-        out = tmp + arg.a * out;
+          // add the Wilson part with normalisation
+          out = tmp + arg.a * out;
 	}
       } else if (active) {
         Vector x = arg.out[src_idx](my_flavor_idx, my_spinor_parity);
