c50b0b0fe674456447567a8b5a811f83cfbbf271
Ferenc Pittler <f.pittler@cyi.ac.cy>
2025-04-27 15:24:13 +0200

    correcting typos

--- lib/dslash_wilson_clover_hasenbusch_twist.in.cu
+++ lib/dslash_wilson_clover_hasenbusch_twist.in.cu
@@ -8,8 +8,8 @@ namespace quda
   constexpr int reconI = @QUDA_DSLASH_RECONI@;
   constexpr bool distance_pc = @QUDA_DSLASH_DISTANCE@;
 
-  using DDArg @QUDA_DSLASH_DDARG@;
-  using Float precision_type_mapper<precision>::type;
+  using DDArg = @QUDA_DSLASH_DDARG@;
+  using Float = precision_type_mapper<precision>::type;
 
   template struct WilsonCloverHasenbuschTwistApply<Float, nColor, DDArg, ReconstructWilson::recon[reconI]>;
 
