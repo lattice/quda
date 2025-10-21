9b7525d2639c4648843dfabfc7f42f5e01c612d0
Roman Gruber <rgruber@student.ethz.ch>
2025-10-21 15:20:37 +0200

    use correct build flag

--- lib/copy_gauge_inc.cu
+++ lib/copy_gauge_inc.cu
@@ -27,7 +27,7 @@ namespace quda {
 #else
         errorQuda("QUDA_RECONSTRUCT=%d does not enable reconstruct-8", QUDA_RECONSTRUCT);
 #endif
-#if defined(GPU_STAGGERED_DIRAC) || defined(BUILD_OPENQCD_INTERFACE)
+#if defined(GPU_STAGGERED_DIRAC) || defined(BUILD_QCD_PLUS_QED)
       } else if (out.Reconstruct() == QUDA_RECONSTRUCT_13) {
 #if QUDA_RECONSTRUCT & 2
         typedef typename gauge_mapper<FloatOut, QUDA_RECONSTRUCT_13>::type G;
@@ -56,7 +56,7 @@ namespace quda {
 #else
         errorQuda("QUDA_RECONSTRUCT=%d does not enable reconstruct-9", QUDA_RECONSTRUCT);
 #endif
-#endif // defined(GPU_STAGGERED_DIRAC) || defined(BUILD_OPENQCD_INTERFACE)
+#endif // defined(GPU_STAGGERED_DIRAC) || defined(BUILD_QCD_PLUS_QED)
       } else {
         errorQuda("Reconstruction %d and order %d not supported", out.Reconstruct(), out.Order());
       }
@@ -169,7 +169,7 @@ namespace quda {
 #else
         errorQuda("QUDA_RECONSTRUCT=%d does not enable reconstruct-8", QUDA_RECONSTRUCT);
 #endif
-#if defined(GPU_STAGGERED_DIRAC) || defined(BUILD_OPENQCD_INTERFACE)
+#if defined(GPU_STAGGERED_DIRAC) || defined(BUILD_QCD_PLUS_QED)
       } else if (in.Reconstruct() == QUDA_RECONSTRUCT_13) {
 #if QUDA_RECONSTRUCT & 2
         typedef typename gauge_mapper<FloatIn,QUDA_RECONSTRUCT_13>::type G;
@@ -191,7 +191,7 @@ namespace quda {
 #else
         errorQuda("QUDA_RECONSTRUCT=%d does not enable reconstruct-9", QUDA_RECONSTRUCT);
 #endif
-#endif // defined(GPU_STAGGERED_DIRAC) || defined(BUILD_OPENQCD_INTERFACE)
+#endif // defined(GPU_STAGGERED_DIRAC) || defined(BUILD_QCD_PLUS_QED)
       } else {
 	errorQuda("Reconstruction %d and order %d not supported", in.Reconstruct(), in.Order());
       }
