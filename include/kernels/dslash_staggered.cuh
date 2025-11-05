lattice/quda

commit 0642f638bb761ed991dff43664d731c93a76fa41

Add L2 gauge-field prefetching support to both Wilson and staggered dslash kernels.  Disabled by default (set with with Arg::prefetch_distance parameter), and TMA prefetch will be added in next push

include/kernels/dslash_staggered.cuh
@@ -52,6 +52,7 @@ namespace quda
     const bool is_first_time_slice; /** are we on the first (global) time slice */
     const bool is_last_time_slice; /** are we on the last (global) time slice */
     static constexpr bool improved = improved_;
+    static constexpr int prefetch_distance = 0;
 
     const real dagger_scale;
 
@@ -74,6 +75,43 @@ namespace quda
       }
     }
   };
+
+  /**
+     @brief Prefetch the gauge field into cache.
+     @param[in] dim The dimension we are presently working on
+     @param[in] dir The direction we are presently working on (1 = forwards, 0 = backwards)
+     @param[in] hop The hopping term we are presently working on (0 = 1 - hop, 1 = 3 - hop)
+     @param[in] coord Coordinates that we are working on with hop-3 boundary conditions evaluated
+     @param[in] coord1 Copy of coordinates that we are working on with hop-1 boundary conditions evaluated
+     @param[in] parity Partiry that we are working on
+     @param[in] arg Paramter struct
+   */
+  template <class coord_t, class Arg>
+  __device__ __host__ void prefetch(int dim, int dir, int hop, const coord_t &coord, const coord_t &coord1, int parity,
+                                    const Arg &arg)
+  {
+    if constexpr (arg.prefetch_distance == 0) return;
+
+    if constexpr (arg.improved) {
+      int step = 4 * dim + 2 * dir + hop + arg.prefetch_distance;
+      if (step >= 16) return;
+
+      // for TMA use arg.block_size and coord.x_cb_0
+      // also should have warp uniform parity
+      int dim2 = step / 4;
+      switch (step % 4) {
+      case 0: arg.U.prefetch(coord.x_cb, dim2, parity); break;
+      case 1: arg.L.prefetch(coord.x_cb, dim2, parity); break;
+#ifdef QUDA_DSLASH_DOUBLE_STORE
+      case 2: arg.Uback.prefetch(coord.x_cb, dim2, parity); break;
+      case 3: arg.Lback.prefetch(coord.x_cb, dim2, parity); break;
+#else
+      case 2: arg.U.prefetch(getNeighborIndexCB<1>(coord1, dim2, -1, arg.dc), dim2, 1 - parity); break;
+      case 3: arg.L.prefetch(getNeighborIndexCB<3>(coord, dim2, -1, arg.dc), dim2, 1 - parity); break;
+#endif
+      }
+    }
+  }
 
   /**
      @brief Applies the off-diagonal part of the Staggered / Asqtad
@@ -107,7 +145,8 @@ namespace quda
 
       // standard - forward direction
       if (arg.dd_in.doHopping(coord, d, +1)) {
-        const bool ghost = (coord[d] + 1 >= arg.dc.X[d]) && isActive<kernel_type>(active, thread_dim, d, coord, arg);
+        const bool ghost = coord1.in_boundary[1][d] & isActive<kernel_type>(active, thread_dim, d, coord, arg);
+
         if (doHalo<kernel_type>(d) && ghost) {
           const int ghost_idx = ghostFaceIndexStaggered<1>(coord, arg.dc.X, d, 1);
           const Link U = arg.improved ? arg.U(d, coord.x_cb, parity) : arg.U(d, coord.x_cb, parity, StaggeredPhase(coord, d, +1, arg));
@@ -116,14 +155,20 @@ namespace quda
             Vector in = arg.halo.Ghost(d, 1, ghost_idx + (src_idx + s) * arg.dc.ghostFaceCB[d], their_spinor_parity);
             out[s] = mv_add(U, in, out[s]);
           }
-        } else if (doBulk<kernel_type>() && !ghost) {
-          const int fwd_idx = getNeighborIndexCB<1>(coord1, d, 1, arg.dc);
-          const Link U = arg.improved ? arg.U(d, coord.x_cb, parity) : arg.U(d, coord.x_cb, parity, StaggeredPhase(coord, d, +1, arg));
+        }
+
+        if constexpr (doBulk<kernel_type>()) {
+          if (!ghost) {
+            const int fwd_idx = getNeighborIndexCB<1>(coord1, d, 1, arg.dc);
+            const Link U = arg.improved ? arg.U(d, coord.x_cb, parity) :
+                                          arg.U(d, coord.x_cb, parity, StaggeredPhase(coord, d, +1, arg));
 #pragma unroll
-          for (auto s = 0; s < n_src_tile; s++) {
-            Vector in = arg.in[src_idx + s](fwd_idx, their_spinor_parity);
-            out[s] = mv_add(U, in, out[s]);
+            for (auto s = 0; s < n_src_tile; s++) {
+              Vector in = arg.in[src_idx + s](fwd_idx, their_spinor_parity);
+              out[s] = mv_add(U, in, out[s]);
+            }
           }
+          prefetch(d, 0, 0, coord, coord1, parity, arg);
         }
       }
 
@@ -139,20 +184,25 @@ namespace quda
               = arg.halo.Ghost(d, 1, ghost_idx + (src_idx + s) * arg.dc.ghostFaceCB[d], their_spinor_parity);
             out[s] = mv_add(L, in, out[s]);
           }
-        } else if (doBulk<kernel_type>() && !ghost) {
-          const int fwd3_idx = getNeighborIndexCB<3>(coord, d, 1, arg.dc);
-          const Link L = arg.L(d, coord.x_cb, parity);
+        }
+
+        if constexpr (doBulk<kernel_type>()) {
+          if (!ghost) {
+            const int fwd3_idx = getNeighborIndexCB<3>(coord, d, 1, arg.dc);
+            const Link L = arg.L(d, coord.x_cb, parity);
 #pragma unroll
-          for (auto s = 0; s < n_src_tile; s++) {
-            const Vector in = arg.in[src_idx + s](fwd3_idx, their_spinor_parity);
-            out[s] = mv_add(L, in, out[s]);
+            for (auto s = 0; s < n_src_tile; s++) {
+              const Vector in = arg.in[src_idx + s](fwd3_idx, their_spinor_parity);
+              out[s] = mv_add(L, in, out[s]);
+            }
           }
+          prefetch(d, 0, 1, coord, coord1, parity, arg);
         }
       }
 
       if (arg.dd_in.doHopping(coord, d, -1)) {
         // Backward gather - compute back offset for spinor and gauge fetch
-        const bool ghost = (coord[d] - 1 < 0) && isActive<kernel_type>(active, thread_dim, d, coord, arg);
+        const bool ghost = coord1.in_boundary[1][d] & isActive<kernel_type>(active, thread_dim, d, coord, arg);
 
         if (doHalo<kernel_type>(d) && ghost) {
           const int ghost_idx2 = ghostFaceIndexStaggered<0>(coord, arg.dc.X, d, 1);
@@ -169,21 +219,26 @@ namespace quda
             Vector in = arg.halo.Ghost(d, 0, ghost_idx + (src_idx + s) * arg.dc.ghostFaceCB[d], their_spinor_parity);
             out[s] = mv_sub(conj(U), in, out[s]);
           }
-        } else if (doBulk<kernel_type>() && !ghost) {
-          const int back_idx = getNeighborIndexCB<1>(coord1, d, -1, arg.dc);
+        }
+
+        if constexpr (doBulk<kernel_type>()) {
+          if (!ghost) {
+            const int back_idx = getNeighborIndexCB<1>(coord1, d, -1, arg.dc);
 #ifdef QUDA_DSLASH_DOUBLE_STORE
-          const Link U = arg.improved ? arg.Uback(d, coord.x_cb, parity) :
-                                        arg.Uback(d, coord.x_cb, parity, StaggeredPhase(coord, d, -1, arg));
+            const Link U = arg.improved ? arg.Uback(d, coord.x_cb, parity) :
+                                          arg.Uback(d, coord.x_cb, parity, StaggeredPhase(coord, d, -1, arg));
 #else
-          const int gauge_idx = back_idx;
-          const Link U = arg.improved ? arg.U(d, gauge_idx, 1 - parity) :
-            arg.U(d, gauge_idx, 1 - parity, StaggeredPhase(coord, d, -1, arg));
+            const int gauge_idx = back_idx;
+            const Link U = arg.improved ? arg.U(d, gauge_idx, 1 - parity) :
+                                          arg.U(d, gauge_idx, 1 - parity, StaggeredPhase(coord, d, -1, arg));
 #endif
 #pragma unroll
-          for (auto s = 0; s < n_src_tile; s++) {
-            Vector in = arg.in[src_idx + s](back_idx, their_spinor_parity);
-            out[s] = mv_sub(conj(U), in, out[s]);
+            for (auto s = 0; s < n_src_tile; s++) {
+              Vector in = arg.in[src_idx + s](back_idx, their_spinor_parity);
+              out[s] = mv_sub(conj(U), in, out[s]);
+            }
           }
+          prefetch(d, 1, 0, coord, coord1, parity, arg);
         }
       }
 
@@ -203,19 +258,24 @@ namespace quda
               = arg.halo.Ghost(d, 0, ghost_idx + (src_idx + s) * arg.dc.ghostFaceCB[d], their_spinor_parity);
             out[s] = mv_sub(conj(L), in, out[s]);
           }
-        } else if (doBulk<kernel_type>() && !ghost) {
-          const int back3_idx = getNeighborIndexCB<3>(coord, d, -1, arg.dc);
+        }
+
+        if constexpr (doBulk<kernel_type>()) {
+          if (!ghost) {
+            const int back3_idx = getNeighborIndexCB<3>(coord, d, -1, arg.dc);
 #ifdef QUDA_DSLASH_DOUBLE_STORE
-          const Link L = arg.Lback(d, coord.x_cb, parity);
+            const Link L = arg.Lback(d, coord.x_cb, parity);
 #else
-          const int gauge_idx = back3_idx;
-          const Link L = arg.L(d, gauge_idx, 1 - parity);
+            const int gauge_idx = back3_idx;
+            const Link L = arg.L(d, gauge_idx, 1 - parity);
 #endif
 #pragma unroll
-          for (auto s = 0; s < n_src_tile; s++) {
-            const Vector in = arg.in[src_idx + s](back3_idx, their_spinor_parity);
-            out[s] = mv_sub(conj(L), in, out[s]);
+            for (auto s = 0; s < n_src_tile; s++) {
+              const Vector in = arg.in[src_idx + s](back3_idx, their_spinor_parity);
+              out[s] = mv_sub(conj(L), in, out[s]);
+            }
           }
+          prefetch(d, 1, 1, coord, coord1, parity, arg);
         }
       }
     } // nDim

include/dslash.h
@@ -76,6 +76,11 @@ namespace quda
 #ifdef QUDA_DSLASH_DOUBLE_STORE
       strcat(aux_base, ",double_store");
 #endif
+      if constexpr (Arg::prefetch_distance > 0) {
+        strcat(aux_base, ",prefetch=");
+        i32toa(tile_str, Arg::prefetch_distance);
+        strcat(aux_base, tile_str);
+      }
     }
 
     /**

include/dslash_helper.cuh
@@ -109,6 +109,7 @@ namespace quda
 
     if (kernel_type == INTERIOR_KERNEL) {
       coord.x_cb = idx;
+      coord.x_cb_0 = (target::block_idx().x - arg.pack_blocks) * target::block_dim().x;
       if (nDim == 5)
         coord.X = getCoords5CB(coord, idx, arg.dc.X, arg.X0h, parity, pc_type);
       else
@@ -298,6 +299,7 @@ namespace quda
     static constexpr int n_src_tile = n_src_tile_; // how many RHS per thread
     static constexpr int max_regs = 0;             // by default we don't limit register count
     static constexpr bool spill_shared = false;    // whether a given kernel should use shared memory spilling
+    static constexpr int prefetch_distance = 0;    // whether we are using prefetching in the dslash
 
     const int parity;  // only use this for single parity fields
     const int nParity; // number of parities we're working on
@@ -340,6 +342,7 @@ namespace quda
     int pack_blocks = 0;   // total number of blocks used for packing in the dslash
     int exterior_dims = 0; // dimension to run in the exterior Dslash
     int exterior_blocks = 0;
+    int block_size = 0;
 
     DDArg dd_out;
     DDArg dd_in;
@@ -707,6 +710,7 @@ namespace quda
     static constexpr KernelType kernel_type = kernel_type_;
     static constexpr int max_regs = Arg::max_regs;
     static constexpr bool spill_shared = Arg::spill_shared;
+    static constexpr bool is_dslash = true;
     Arg arg;
 
     dslash_functor_arg(const Arg &arg, unsigned int threads_x) :

include/index_helper.cuh
@@ -234,6 +234,7 @@ namespace quda {
     array<int, nDim> gx = {};   // nDim global lattice coordinates
     array<int, nDim> gDim = {}; // global lattice dimensions
     int x_cb;    // checkerboard lattice site index
+    int x_cb_0;  // value of x_cb on first thread in block
     int s;       // fifth dimension coord
     int X;       // full lattice site index
     constexpr const int& operator[](int i) const { return x[i]; }

include/kernel_helper.h
@@ -19,7 +19,9 @@ namespace quda
     static constexpr bool check_bounds = check_bounds_;
     static constexpr int max_regs = 0;          // by default we don't limit register count
     static constexpr bool spill_shared = false; // whether a given kernel should use shared memory spilling
+    static constexpr bool is_dslash = false;    // whether the arg is for a dslash (with its nested arg struct)
     dim3 threads;          /** number of active threads required */
+    int block_size;        /** product of thread block dimensions */
     int comms_rank;        /** per process value of comm_rank() */
     int comms_rank_global; /** per process value comm_rank_global() */
     int comms_coord[4];    /** array storing {comm_coord(0), ..., comm_coord(3)} */

include/kernels/dslash_wilson.cuh
@@ -43,6 +43,7 @@ namespace quda
     /** parameters for distance preconditioning */
     const real alpha0;
     const int t0;
+    static constexpr int prefetch_distance = 0;
 
     WilsonArg(cvector_ref<ColorSpinorField> &out, cvector_ref<const ColorSpinorField> &in, const ColorSpinorField &halo,
               const GaugeField &U, const GaugeField &Uback, double a, cvector_ref<const ColorSpinorField> &x,
@@ -64,6 +65,41 @@ namespace quda
       }
     }
   };
+
+  /**
+     @tparam distance The distance away we are prefetching
+     @param[in] dim The dimension we are presently working on
+     @param[in] dir The direction we are presently working on (1 = forwards, 0 = backwards)
+     @param[in] coord Coordinates that we are working on
+     @param[in] parity Partiry that we are working on
+     @param[in] arg Paramter struct
+  */
+  template <class coord_t, class Arg>
+  __device__ __host__ void prefetch(int dim, int dir, const coord_t &coord, int parity, const Arg &arg)
+  {
+    if constexpr (arg.prefetch_distance == 0) return;
+
+    int step = 2 * dim + dir + arg.prefetch_distance;
+    if (step >= 8) return;
+
+    // for TMA use arg.block_size
+    int dim2 = step / 2;
+    // need warp uniform variants of these and parity
+    const int x_cb = (Arg::nDim == 5 ? coord.x_cb % arg.dc.volume_4d_cb : coord.x_cb);
+
+    switch (step % 2) {
+    case 0: arg.U.prefetch(x_cb, dim2, parity); break;
+#ifdef QUDA_DSLASH_DOUBLE_STORE
+    case 1: arg.Uback.prefetch(x_cb, dim2, parity); break;
+#else
+    case 1: {
+      const int back_idx = getNeighborIndexCB(coord, dim2, -1, arg.dc);
+      const int idx1 = (Arg::nDim == 5 ? back_idx % arg.dc.volume_4d_cb : back_idx);
+      arg.U.prefetch(idx1, dim2, 1 - parity);
+    } break;
+#endif
+    }
+  }
 
   /**
      @brief Applies the off-diagonal part of the Wilson operator
@@ -117,12 +153,16 @@ namespace quda
                                          their_spinor_parity);
 
           out += fwd_coeff * (U * in).reconstruct(d, proj_dir);
-        } else if (doBulk<kernel_type>() && !ghost) {
+        }
 
-          Link U = arg.U(d, gauge_idx, gauge_parity);
-          Vector in = arg.in[src_idx](fwd_idx + coord.s * arg.dc.volume_4d_cb, their_spinor_parity);
+        if constexpr (doBulk<kernel_type>()) {
+          if (!ghost) {
+            Link U = arg.U(d, gauge_idx, gauge_parity);
+            Vector in = arg.in[src_idx](fwd_idx + coord.s * arg.dc.volume_4d_cb, their_spinor_parity);
+            out += fwd_coeff * (U * in.project(d, proj_dir)).reconstruct(d, proj_dir);
+          }
 
-          out += fwd_coeff * (U * in.project(d, proj_dir)).reconstruct(d, proj_dir);
+          prefetch(d, 0, coord, parity, arg);
         }
       }
 
@@ -155,16 +195,20 @@ namespace quda
                                          their_spinor_parity);
 
           out += bwd_coeff * (conj(U) * in).reconstruct(d, proj_dir);
-        } else if (doBulk<kernel_type>() && !ghost) {
+        }
 
+        if (doBulk<kernel_type>()) {
+          if (!ghost) {
 #ifdef QUDA_DSLASH_DOUBLE_STORE
-          Link U = arg.Uback(d, gauge_idx, gauge_parity);
+            Link U = arg.Uback(d, gauge_idx, gauge_parity);
 #else
-          Link U = arg.U(d, gauge_idx, 1 - gauge_parity);
+            Link U = arg.U(d, gauge_idx, 1 - gauge_parity);
 #endif
-          Vector in = arg.in[src_idx](back_idx + coord.s * arg.dc.volume_4d_cb, their_spinor_parity);
+            Vector in = arg.in[src_idx](back_idx + coord.s * arg.dc.volume_4d_cb, their_spinor_parity);
+            out += bwd_coeff * (conj(U) * in.project(d, proj_dir)).reconstruct(d, proj_dir);
+          }
 
-          out += bwd_coeff * (conj(U) * in.project(d, proj_dir)).reconstruct(d, proj_dir);
+          prefetch(d, 1, coord, parity, arg);
         }
       }
     } // nDim

include/targets/cuda/tunable_kernel.h
@@ -57,6 +57,8 @@ namespace quda
     launch_device(const kernel_t &kernel, const TuneParam &tp, const qudaStream_t &stream, const Arg &arg)
     {
       checkSharedBytes<Functor>(tp, arg);
+      const_cast<Arg &>(arg).block_size = tp.block.x * tp.block.y * tp.block.z;
+      if constexpr (Arg::is_dslash) const_cast<Arg &>(arg).arg.block_size = arg.block_size;
 #ifdef JITIFY
       launch_error = launch_jitify<Functor, grid_stride, Arg>(kernel.name, tp, stream, arg);
 #else
@@ -66,7 +68,7 @@ namespace quda
       return launch_error;
     }
 
-    template <typename Arg, size_t arg_size = sizeof(Arg)> void check_arg_size(Arg&)
+    template <typename Arg, size_t arg_size = sizeof(Arg)> void check_arg_size(Arg &)
     {
       static_assert(sizeof(Arg) <= device::max_constant_size(), "Parameter struct is greater than max constant size");
     }
@@ -76,6 +78,8 @@ namespace quda
     launch_device(const kernel_t &kernel, const TuneParam &tp, const qudaStream_t &stream, const Arg &arg)
     {
       checkSharedBytes<Functor>(tp, arg);
+      const_cast<Arg &>(arg).block_size = tp.block.x * tp.block.y * tp.block.z;
+      if constexpr (Arg::is_dslash) const_cast<Arg &>(arg).arg.block_size = arg.block_size;
 #ifdef JITIFY
       // note we do the copy to constant memory after the kernel has been compiled in launch_jitify
       launch_error = launch_jitify<Functor, grid_stride, Arg>(kernel.name, tp, stream, arg);
@@ -99,6 +103,8 @@ namespace quda
     void launch_cuda(const TuneParam &tp, const qudaStream_t &stream, const Arg &arg) const
     {
       checkSharedBytes<Functor>(tp, arg);
+      const_cast<Arg &>(arg).block_size = tp.block.x * tp.block.y * tp.block.z;
+      if constexpr (Arg::is_dslash) const_cast<Arg &>(arg).arg.block_size = arg.block_size;
       constexpr bool grid_stride = false;
       const_cast<TunableKernel *>(this)->launch_device<Functor, grid_stride>(KERNEL(raw_kernel), tp, stream, arg);
     }

include/targets/hip/tunable_kernel.h
@@ -54,9 +54,16 @@ namespace quda
     launch_device(const kernel_t &kernel, const TuneParam &tp, const qudaStream_t &stream, const Arg &arg)
     {
       checkSharedBytes<Functor>(tp, arg);
+      const_cast<Arg &>(arg).block_size = tp.block.x * tp.block.y * tp.block.z;
+      if constexpr (Arg::is_dslash) const_cast<Arg &>(arg).arg.block_size = arg.block_size;
       setMaxActiveBlocks(kernel, tp);
       launch_error = qudaLaunchKernel(kernel, tp, stream, static_cast<const void *>(&arg));
       return launch_error;
+    }
+
+    template <typename Arg, size_t arg_size = sizeof(Arg)> void check_arg_size(Arg &)
+    {
+      static_assert(sizeof(Arg) <= device::max_constant_size(), "Parameter struct is greater than max constant size");
     }
 
     template <template <typename> class Functor, bool grid_stride, typename Arg>
@@ -64,7 +71,9 @@ namespace quda
     launch_device(const kernel_t &kernel, const TuneParam &tp, const qudaStream_t &stream, const Arg &arg)
     {
       checkSharedBytes<Functor>(tp, arg);
-      static_assert(sizeof(Arg) <= device::max_constant_size(), "Parameter struct is greater than max constant size");
+      const_cast<Arg &>(arg).block_size = tp.block.x * tp.block.y * tp.block.z;
+      if constexpr (Arg::is_dslash) const_cast<Arg &>(arg).arg.block_size = arg.block_size;
+      check_arg_size(arg);
       qudaMemcpyAsync(device::get_constant_buffer<Arg>(), &arg, sizeof(Arg), qudaMemcpyHostToDevice, stream);
       setMaxActiveBlocks(kernel, tp);
       launch_error = qudaLaunchKernel(kernel, tp, stream, static_cast<const void *>(&arg));
@@ -82,6 +91,8 @@ namespace quda
     void launch_cuda(const TuneParam &tp, const qudaStream_t &stream, const Arg &arg) const
     {
       checkSharedBytes<Functor>(tp, arg);
+      const_cast<Arg &>(arg).block_size = tp.block.x * tp.block.y * tp.block.z;
+      if constexpr (Arg::is_dslash) const_cast<Arg &>(arg).arg.block_size = arg.block_size;
       constexpr bool grid_stride = false;
       const_cast<TunableKernel *>(this)->launch_device<Functor, grid_stride>(KERNEL(raw_kernel), tp, stream, arg);
     }

commit a5abce8a0bbd32f30582fd7ff4641d2a39af7277

Fix backward hopping ghost boundary check in staggered dslash
The backward hopping term was incorrectly using coord1.in_boundary[1][d]
(forward boundary) instead of coord1.in_boundary[0][d] (backward boundary).
This caused incorrect ghost zone detection for backward hops, manifesting
as failures in non-trivial MPI partitions.

Bug introduced in commit 0642f638bb when adding L2 prefetching support.

include/kernels/dslash_staggered.cuh
@@ -208,7 +208,7 @@ namespace quda
 
       if (arg.dd_in.doHopping(coord, d, -1)) {
         // Backward gather - compute back offset for spinor and gauge fetch
-        const bool ghost = coord1.in_boundary[1][d] & isActive<kernel_type>(active, thread_dim, d, coord, arg);
+        const bool ghost = coord1.in_boundary[0][d] & isActive<kernel_type>(active, thread_dim, d, coord, arg);
 
         if (doHalo<kernel_type>(d) && ghost) {
           const int ghost_idx2 = ghostFaceIndexStaggered<0>(coord, arg.dc.X, d, 1);