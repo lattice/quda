44b90008ff0cc0be1d414cbecb79d5fd86fdcf73
maddyscientist <mclark@nvidia.com>
2026-01-21 16:07:12 -0800

    tensor descriptor cache should work as expected now

--- lib/targets/cuda/tma_helper.cpp
+++ lib/targets/cuda/tma_helper.cpp
@@ -1,6 +1,7 @@
+#include <map>
+#include <array>
 #include <cuda.h>
 #include <tma_helper.hpp>
-#include <map>
 
 #ifdef USE_TENSOR_MEMORY_ACCELERATOR
 
@@ -94,17 +95,61 @@ namespace quda
     return tensor;
   }
 
-  static std::map<int, gauge::tensor_desc_t> tensor_map;
+  struct tensor_key_t {
+    static constexpr std::size_t volume_n = 32;
+    static constexpr std::size_t aux_n = 256;
+
+    uint32_t block_size {};
+    std::array<char, volume_n> volume {}; // zero-filled
+    std::array<char, aux_n> aux {};       // zero-filled
+    void *ptr {};
+
+    bool operator<(const tensor_key_t &other) const noexcept
+    {
+      if (block_size != other.block_size) return block_size < other.block_size;
+      int vc = std::memcmp(volume.data(), other.volume.data(), tensor_key_t::volume_n);
+      if (vc != 0) return vc < 0;
+      int ac = std::memcmp(aux.data(), other.aux.data(), tensor_key_t::aux_n);
+      if (ac != 0) return ac < 0;
+      // Required for strict weak ordering on arbitrary pointers
+      return std::less<void *> {}(ptr, other.ptr);
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const tensor_key_t &key)
+    {
+      auto print_buf = [&](auto const &buf) {
+        auto end = std::find(buf.begin(), buf.end(), '\0');
+        os.write(buf.data(), std::distance(buf.begin(), end));
+      };
+
+      os << "block_size=" << key.block_size << ", volume=\"";
+      print_buf(key.volume);
+      os << "\", aux=\"";
+      print_buf(key.aux);
+      os << "\", ptr=" << key.ptr;
+      return os;
+    }
+  };
+
+  static std::map<tensor_key_t, gauge::tensor_desc_t> tensor_map;
 
   gauge::tensor_desc_t &get_tensor_descriptor(const GaugeField &u, uint32_t block_size)
   {
-    auto tensor = tensor_map.find(block_size);
-    if (tensor != tensor_map.end()) {
-      return tensor->second;
-    } else {
-      tensor_map[block_size] = create_descriptor(u, block_size);
-    }
-    return tensor_map[block_size];
+    tensor_key_t key {}; // zero-inits arrays + ptr
+    key.block_size = block_size;
+    key.ptr = u.data();
+
+    const std::size_t vlen = std::min(u.VolString().size(), tensor_key_t::volume_n);
+    const std::size_t alen = std::min(u.AuxString().size(), tensor_key_t::aux_n);
+
+    std::memcpy(key.volume.data(), u.VolString().data(), vlen);
+    std::memcpy(key.aux.data(), u.AuxString().data(), alen);
+
+    auto it = tensor_map.find(key);
+    if (it != tensor_map.end()) return it->second;
+
+    auto [ins_it, inserted] = tensor_map.emplace(key, create_descriptor(u, block_size));
+    return ins_it->second;
   }
 
 } // namespace quda
