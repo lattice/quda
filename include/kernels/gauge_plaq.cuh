24b4ec5b571e7df5ec30cf6d7d34f19c09833b0a
maddyscientist <mclark@nvidia.com>
2026-09-21 11:01:22 -0700

    Sum plaquettes in reduction_t and bin the site result in plus.
    The local spatial/temporal (and rectangle) accumulators were RFA
    arrays, so each loop addition was a premature conversion. Keep those
    sums in reduction_t until plus.

--- include/kernels/gauge_plaq.cuh
+++ include/kernels/gauge_plaq.cuh
@@ -63,7 +63,7 @@ namespace quda {
     // return the plaquette at site (x_cb, parity)
     __device__ __host__ inline reduce_t operator()(reduce_t &value, int x_cb, int parity)
     {
-      reduce_t plaq {};
+      array<reduction_t, 2> plaq {};
 
       int x[4];
       getCoords(x, x_cb, arg.X, parity);
--- include/kernels/gauge_plaqrect.cuh
+++ include/kernels/gauge_plaqrect.cuh
@@ -108,7 +108,7 @@ namespace quda
     // return the rectangle and plaquette at site (x_cb, parity)
     __device__ __host__ inline reduce_t operator()(reduce_t &value, int x_cb, int parity)
     {
-      reduce_t plaqRect {};
+      array<reduction_t, 4> plaqRect {};
       int x[4];
       getCoords(x, x_cb, arg.X, parity);
 #pragma unroll
